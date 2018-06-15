04308b717c3f2850685b6505fcec481e04d3f6ae
openvela-robot <openvela-robot@xiaomi.com>
2018-06-15 14:08:36 +0800

    [automerger] Fix the reconnect issue in hfp client am: 53d6b16885
    Change-Id: Ifbe3f6ae33a2c9f08349a3f0268a60d5956f5e9f

--- stack/btm/btm_ble_multi_adv.cc
+++ stack/btm/btm_ble_multi_adv.cc
@@ -204,8 +204,7 @@ class BleAdvertisingManagerImpl
     BTM_GetDeviceIDRoot(irk);
     tSMP_ENC output;
 
-    if (!SMP_Encrypt(irk, BT_OCTET16_LEN, rand, 3, &output))
-      LOG_ASSERT(false) << "SMP_Encrypt failed";
+    SMP_Encrypt(irk, rand, 3, &output);
 
     /* set hash to be LSB of rpAddress */
     bda.address[5] = output.param_buf[0];
@@ -931,7 +930,7 @@ class BleAdvertisingManagerImpl
     }
   }
 
-  void Suspend() {
+  void Suspend() override {
     std::vector<SetEnableData> sets;
 
     for (AdvertisingInstance& inst : adv_inst) {
