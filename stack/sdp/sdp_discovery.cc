822629e9a573b228a982ae3fee62bdb628d370ec
liujinye <liujinye@xiaomi.com>
2018-07-23 10:00:25 +0800

    Uprev libchrome to r576279 (1/multiple)
    This patch brings the latest and greatest features of libchrome to
    android. It contains ~2600 patches.
    Reason for uprev: libbluetooth want to use some of the most recent
    features avaliable.
    
    Test: libchrome_test
    Change-Id: I5f8d5931b404767d3b86cb3bd8df2a05dd67a768
    Merged-In: I5f8d5931b404767d3b86cb3bd8df2a05dd67a768
    (cherry picked from commit 6563f68968249876cd96b805d37c33d75836e8d8)

--- stack/sdp/sdp_discovery.cc
+++ stack/sdp/sdp_discovery.cc
@@ -280,11 +280,6 @@ static void process_service_search_rsp(tCONN_CB* p_ccb, uint8_t* p_reply,
   uint16_t total, cur_handles, orig;
   uint8_t cont_len;
 
-  if (p_reply + 8 > p_reply_end) {
-    android_errorWriteLog(0x534e4554, "74249842");
-    sdp_disconnect(p_ccb, SDP_GENERIC_ERROR);
-    return;
-  }
   /* Skip transaction, and param len */
   p_reply += 4;
   BE_STREAM_TO_UINT16(total, p_reply);
@@ -302,12 +297,6 @@ static void process_service_search_rsp(tCONN_CB* p_ccb, uint8_t* p_reply,
   if (total > sdp_cb.max_recs_per_search) total = sdp_cb.max_recs_per_search;
   if (p_ccb->num_handles > sdp_cb.max_recs_per_search)
     p_ccb->num_handles = sdp_cb.max_recs_per_search;
-
-  if (p_reply + ((p_ccb->num_handles - orig) * 4) + 1 > p_reply_end) {
-    android_errorWriteLog(0x534e4554, "74249842");
-    sdp_disconnect(p_ccb, SDP_GENERIC_ERROR);
-    return;
-  }
 
   for (xx = orig; xx < p_ccb->num_handles; xx++)
     BE_STREAM_TO_UINT32(p_ccb->handles[xx], p_reply);
@@ -895,7 +884,7 @@ static uint8_t* add_attr(uint8_t* p, tSDP_DISCOVERY_DB* p_db,
           break;
         }
       }
-    /* Case falls through */
+      FALLTHROUGH_INTENDED; /* FALLTHROUGH */
 
     case TWO_COMP_INT_DESC_TYPE:
       switch (attr_len) {
