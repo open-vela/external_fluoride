eddb43f2760286342fd950c1bb84ba607914a5ab
openvela-robot <openvela-robot@xiaomi.com>
2018-08-10 20:06:42 +0800

    Merge "DO NOT MERGE: Add missing AVRCP message length checks inside avrc_msg_cback" into nyc-dev

--- osi/Android.mk
+++ osi/Android.mk
@@ -67,9 +67,7 @@ btosiCommonTestSrc := \
     ./test/future_test.cpp \
     ./test/hash_map_test.cpp \
     ./test/hash_map_utils_test.cpp \
-    ./test/leaky_bonded_queue_test.cpp \
     ./test/list_test.cpp \
-    ./test/metrics_test.cpp \
     ./test/properties_test.cpp \
     ./test/rand_test.cpp \
     ./test/reactor_test.cpp \
@@ -80,7 +78,6 @@ btosiCommonTestSrc := \
 
 btosiCommonIncludes := \
     $(LOCAL_PATH)/.. \
-    $(LOCAL_PATH)/../include \
     $(LOCAL_PATH)/../utils/include \
     $(LOCAL_PATH)/../stack/include \
     $(bluetooth_C_INCLUDES)
@@ -169,7 +166,7 @@ LOCAL_SRC_FILES := $(btosiCommonTestSrc)
 LOCAL_MODULE := net_test_osi
 LOCAL_MODULE_TAGS := tests
 LOCAL_SHARED_LIBRARIES := libc liblog libprotobuf-cpp-full libchrome libcutils
-LOCAL_STATIC_LIBRARIES := libosi libbt-protos libgmock
+LOCAL_STATIC_LIBRARIES := libosi libbt-protos
 
 LOCAL_CFLAGS += $(bluetooth_CFLAGS)
 LOCAL_CONLYFLAGS += $(bluetooth_CONLYFLAGS)
@@ -187,7 +184,7 @@ LOCAL_LDLIBS := -lrt -lpthread
 LOCAL_MODULE := net_test_osi
 LOCAL_MODULE_TAGS := tests
 LOCAL_SHARED_LIBRARIES := liblog libprotobuf-cpp-full libchrome
-LOCAL_STATIC_LIBRARIES := libosi-host libbt-protos libgmock_host
+LOCAL_STATIC_LIBRARIES := libosi-host libbt-protos
 
 LOCAL_CFLAGS += $(bluetooth_CFLAGS) -DOS_GENERIC
 LOCAL_CONLYFLAGS += $(bluetooth_CONLYFLAGS)
