9de6e6b5a9f096e27e1ef922076f7e929192e217
liujinye <liujinye@xiaomi.com>
2018-08-10 19:43:29 +0800

    Merge changes from topic "am-a988fd47-73ee-44e9-929f-5374e8851656" into nyc-dr1-dev
    * changes:
      [automerger] DO NOT MERGE Fix OOB read before buffer length check am: e64b4a38b0
      DO NOT MERGE Fix OOB read before buffer length check

--- service/ipc/binder/bluetooth_binder_server.h
+++ service/ipc/binder/bluetooth_binder_server.h
@@ -1,5 +1,5 @@
 //
-//  Copyright 2015 Google, Inc.
+//  Copyright (C) 2015 Google, Inc.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -23,28 +23,14 @@
 #include <utils/String16.h>
 #include <utils/Vector.h>
 
-#include <android/bluetooth/BnBluetooth.h>
-#include <android/bluetooth/IBluetoothCallback.h>
-#include <android/bluetooth/IBluetoothGattClient.h>
-#include <android/bluetooth/IBluetoothGattServer.h>
-#include <android/bluetooth/IBluetoothLeAdvertiser.h>
-#include <android/bluetooth/IBluetoothLeScanner.h>
-#include <android/bluetooth/IBluetoothLowEnergy.h>
-#include <bluetooth/uuid.h>
-
 #include "service/adapter.h"
+#include "service/common/bluetooth/binder/IBluetooth.h"
+#include "service/common/bluetooth/binder/IBluetoothCallback.h"
+#include "service/common/bluetooth/binder/IBluetoothGattClient.h"
+#include "service/common/bluetooth/binder/IBluetoothGattServer.h"
+#include "service/common/bluetooth/binder/IBluetoothLowEnergy.h"
+#include "service/common/bluetooth/uuid.h"
 #include "service/ipc/binder/remote_callback_list.h"
-
-using android::String16;
-using android::binder::Status;
-
-using android::bluetooth::BnBluetooth;
-using android::bluetooth::IBluetoothCallback;
-using android::bluetooth::IBluetoothGattClient;
-using android::bluetooth::IBluetoothGattServer;
-using android::bluetooth::IBluetoothLowEnergy;
-using android::bluetooth::IBluetoothLeAdvertiser;
-using android::bluetooth::IBluetoothLeScanner;
 
 namespace ipc {
 namespace binder {
@@ -57,37 +43,28 @@ class BluetoothBinderServer : public BnBluetooth,
   ~BluetoothBinderServer() override;
 
   // IBluetooth overrides:
-  Status IsEnabled(bool* _aidl_return) override;
-  Status GetState(int32_t* _aidl_return) override;
-  Status Enable(bool start_restricted, bool* _aidl_return) override;
-  Status EnableNoAutoConnect(bool* _aidl_return) override;
-  Status Disable(bool* _aidl_return) override;
+  bool IsEnabled() override;
+  int GetState() override;
+  bool Enable(bool start_restricted) override;
+  bool EnableNoAutoConnect() override;
+  bool Disable() override;
 
-  Status GetAddress(::android::String16* _aidl_return) override;
-  Status GetUUIDs(
-      ::std::vector<::android::bluetooth::UUID>* _aidl_return) override;
-  Status SetName(const ::android::String16& name, bool* _aidl_return) override;
-  Status GetName(::android::String16* _aidl_return) override;
+  std::string GetAddress() override;
+  std::vector<bluetooth::UUID> GetUUIDs() override;
+  bool SetName(const std::string& name) override;
+  std::string GetName() override;
 
-  Status RegisterCallback(
-      const ::android::sp<IBluetoothCallback>& callback) override;
-  Status UnregisterCallback(
-      const ::android::sp<IBluetoothCallback>& callback) override;
-  Status IsMultiAdvertisementSupported(bool* _aidl_return) override;
-  Status GetLowEnergyInterface(
-      ::android::sp<IBluetoothLowEnergy>* _aidl_return) override;
-  Status GetLeAdvertiserInterface(
-      ::android::sp<IBluetoothLeAdvertiser>* _aidl_return) override;
-  Status GetLeScannerInterface(
-      ::android::sp<IBluetoothLeScanner>* _aidl_return) override;
-  Status GetGattClientInterface(
-      ::android::sp<IBluetoothGattClient>* _aidl_return) override;
-  Status GetGattServerInterface(
-      ::android::sp<IBluetoothGattServer>* _aidl_return) override;
+  void RegisterCallback(
+      const android::sp<IBluetoothCallback>& callback) override;
+  void UnregisterCallback(
+      const android::sp<IBluetoothCallback>& callback) override;
 
-  android::status_t dump(
-      int fd, const android::Vector<android::String16>& args) override;
+  bool IsMultiAdvertisementSupported() override;
+  android::sp<IBluetoothLowEnergy> GetLowEnergyInterface() override;
+  android::sp<IBluetoothGattClient> GetGattClientInterface() override;
+  android::sp<IBluetoothGattServer> GetGattServerInterface() override;
 
+  android::status_t dump(int fd, const android::Vector<android::String16>& args) override;
   // bluetooth::Adapter::Observer overrides:
   void OnAdapterStateChanged(bluetooth::Adapter* adapter,
                              bluetooth::AdapterState prev_state,
@@ -100,14 +77,6 @@ class BluetoothBinderServer : public BnBluetooth,
   // The IBluetoothLowEnergy interface handle. This is lazily initialized on the
   // first call to GetLowEnergyInterface().
   android::sp<IBluetoothLowEnergy> low_energy_interface_;
-
-  // The IBluetoothLeAdvertiser interface handle. This is lazily initialized on
-  // the first call to GetLeAdvertiserInterface().
-  android::sp<IBluetoothLeAdvertiser> le_advertiser_interface_;
-
-  // The IBluetoothLeScanner interface handle. This is lazily initialized on the
-  // first call to GetLeScannerInterface().
-  android::sp<IBluetoothLeScanner> le_scanner_interface_;
 
   // The IBluetoothGattClient interface handle. This is lazily initialized on
   // the first call to GetGattClientInterface().
