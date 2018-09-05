bdbf97ba0f8f87169d396682c5d5c6a2234c8a0d
liujinye <liujinye@xiaomi.com>
2018-09-05 18:21:31 +0800

    DO NOT MERGE - Check data length when parsing AVRCP vendor specific command responses
    Bug: 111450531
    Bug: 111896861
    Test: PoC test program
    Change-Id: I564bee8f05efabc29383659a75e695b4da76c6aa
    (cherry picked from commit 7439ea940354f65a147c4ecfce3bada49c688047)

--- vendor_libs/test_vendor_lib/include/command_packet.h
+++ vendor_libs/test_vendor_lib/include/command_packet.h
@@ -20,7 +20,7 @@
 #include <vector>
 
 #include "base/macros.h"
-#include "packet.h"
+#include "vendor_libs/test_vendor_lib/include/packet.h"
 
 namespace test_vendor_lib {
 
@@ -42,14 +42,8 @@ namespace test_vendor_lib {
 // in size.
 class CommandPacket : public Packet {
  public:
-  explicit CommandPacket(std::vector<uint8_t> header);
-  explicit CommandPacket(uint16_t opcode);
-  CommandPacket(std::vector<uint8_t> header, std::vector<uint8_t> payload);
+  CommandPacket();
 
-  CommandPacket(const CommandPacket&) = default;
-  CommandPacket& operator=(const CommandPacket&) = default;
-  CommandPacket(CommandPacket&&) = default;
-  CommandPacket& operator=(CommandPacket&&) = default;
   virtual ~CommandPacket() override = default;
 
   // Returns the command opcode as defined in stack/include/hcidefs.h.
@@ -74,8 +68,13 @@ class CommandPacket : public Packet {
   // within an opcode group field. The upper 6 bits will be zero filled.
   uint16_t GetOCF() const;
 
-  // Size of a command packet header, which consists of a 2 octet opcode
-  static const size_t kCommandHeaderSize = 2;
+  // Size in octets of a command packet header, which consists of a 2 octet
+  // opcode and a 1 octet payload size.
+  static const size_t kCommandHeaderSize = 3;
+
+ private:
+  // Disallow any copies of the singleton to be made.
+  DISALLOW_COPY_AND_ASSIGN(CommandPacket);
 };
 
 }  // namespace test_vendor_lib
