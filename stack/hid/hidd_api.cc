45b9bfc6d132f0cdde8dd85ebd564c3d499b81d2
openvela-robot <openvela-robot@xiaomi.com>
2018-09-07 16:15:19 +0800

    Merge changes from topic "bt-security-avrc_ctrl_pars_vendor_rsp-length-check-nyc-dev" into oc-dev
    * changes:
      [automerger] DO NOT MERGE - Check AVRCP data length when parsing inside avrc_ctrl_pars_vendor_rsp() am: b4cf8416bf am: c0e2026eb1 am: eb2615a683 am: 38a6087b6a skipped: df71e4a376
      [automerger] DO NOT MERGE - Check AVRCP data length when parsing inside avrc_ctrl_pars_vendor_rsp() am: b4cf8416bf am: c0e2026eb1 am: eb2615a683 am: 38a6087b6a
      [automerger] DO NOT MERGE - Check AVRCP data length when parsing inside avrc_ctrl_pars_vendor_rsp() am: b4cf8416bf am: c0e2026eb1 am: eb2615a683
      [automerger] DO NOT MERGE - Check AVRCP data length when parsing inside avrc_ctrl_pars_vendor_rsp() am: b4cf8416bf am: c0e2026eb1
      [automerger] DO NOT MERGE - Check AVRCP data length when parsing inside avrc_ctrl_pars_vendor_rsp() am: b4cf8416bf
      DO NOT MERGE - Check AVRCP data length when parsing inside avrc_ctrl_pars_vendor_rsp()

--- stack/hid/hidd_api.cc
+++ stack/hid/hidd_api.cc
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
- *  Copyright 2016 The Android Open Source Project
- *  Copyright 2002-2012 Broadcom Corporation
+ *  Copyright (C) 2016 The Android Open Source Project
+ *  Copyright (C) 2002-2012 Broadcom Corporation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -34,7 +34,9 @@
 #include "hidd_int.h"
 #include "hiddefs.h"
 
+#if HID_DYNAMIC_MEMORY == FALSE
 tHID_DEV_CTB hd_cb;
+#endif
 
 /*******************************************************************************
  *
@@ -422,9 +424,9 @@ tHID_STATUS HID_DevVirtualCableUnplug(void) {
  * Returns          tHID_STATUS
  *
  ******************************************************************************/
-tHID_STATUS HID_DevPlugDevice(const RawAddress& addr) {
+tHID_STATUS HID_DevPlugDevice(BD_ADDR addr) {
   hd_cb.device.in_use = TRUE;
-  hd_cb.device.addr = addr;
+  memcpy(hd_cb.device.addr, addr, sizeof(BD_ADDR));
 
   return HID_SUCCESS;
 }
@@ -438,8 +440,8 @@ tHID_STATUS HID_DevPlugDevice(const RawAddress& addr) {
  * Returns          tHID_STATUS
  *
  ******************************************************************************/
-tHID_STATUS HID_DevUnplugDevice(const RawAddress& addr) {
-  if (hd_cb.device.addr == addr) {
+tHID_STATUS HID_DevUnplugDevice(BD_ADDR addr) {
+  if (!memcmp(hd_cb.device.addr, addr, sizeof(BD_ADDR))) {
     hd_cb.device.in_use = FALSE;
     hd_cb.device.conn.conn_state = HID_CONN_STATE_UNUSED;
     hd_cb.device.conn.ctrl_cid = 0;
@@ -564,11 +566,11 @@ tHID_STATUS HID_DevReportError(uint8_t error) {
  * Returns          tHID_STATUS
  *
  ******************************************************************************/
-tHID_STATUS HID_DevGetDevice(RawAddress* addr) {
+tHID_STATUS HID_DevGetDevice(BD_ADDR* addr) {
   HIDD_TRACE_API("%s", __func__);
 
   if (hd_cb.device.in_use) {
-    *addr = hd_cb.device.addr;
+    memcpy(addr, hd_cb.device.addr, sizeof(BD_ADDR));
   } else {
     return HID_ERR_NOT_REGISTERED;
   }
