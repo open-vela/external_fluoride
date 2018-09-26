a8ab8143594183ba1a9aa6e0702efed0d9441352
openvela-robot <openvela-robot@xiaomi.com>
2018-09-26 16:35:58 +0800

    Merge "Remove obsoleted Coverity-related comment" am: 21cd29ca65 am: 826f10ca20
    Change-Id: Id112020d622e87180feead6c3c6872314c537237

--- bta/hearing_aid/hearing_aid.cc
+++ bta/hearing_aid/hearing_aid.cc
@@ -186,7 +186,7 @@ struct HearingDevice {
         gap_handle(0),
         psm(0) {}
 
-  HearingDevice() { HearingDevice(RawAddress::kEmpty, false); }
+  HearingDevice() : HearingDevice(RawAddress::kEmpty, false) {}
 
   /* return true if this device represents left Hearing Aid. Returned value is
    * valid only after capabilities are discovered */
@@ -500,11 +500,10 @@ class HearingAidImpl : public HearingAid {
       return;
     }
 
-    const std::vector<tBTA_GATTC_SERVICE>* services =
-        BTA_GATTC_GetServices(conn_id);
+    const std::vector<gatt::Service>* services = BTA_GATTC_GetServices(conn_id);
 
-    const tBTA_GATTC_SERVICE* service = nullptr;
-    for (const tBTA_GATTC_SERVICE& tmp : *services) {
+    const gatt::Service* service = nullptr;
+    for (const gatt::Service& tmp : *services) {
       if (tmp.uuid != HEARING_AID_UUID) continue;
       LOG(INFO) << "Found Hearing Aid service, handle=" << loghex(tmp.handle);
       service = &tmp;
@@ -519,7 +518,7 @@ class HearingAidImpl : public HearingAid {
     }
 
     uint16_t psm_handle = 0x0000;
-    for (const tBTA_GATTC_CHARACTERISTIC& charac : service->characteristics) {
+    for (const gatt::Characteristic& charac : service->characteristics) {
       if (charac.uuid == READ_ONLY_PROPERTIES_UUID) {
         DVLOG(2) << "Reading read only properties "
                  << loghex(charac.value_handle);
@@ -1310,7 +1309,8 @@ void HearingAid::CleanUp() {
 };
 
 void HearingAid::DebugDump(int fd) {
-  dprintf(fd, "\nHearing Aid Manager:\n");
+  dprintf(fd, "Hearing Aid Manager:\n");
   if (instance) instance->Dump(fd);
   HearingAidAudioSource::DebugDump(fd);
+  dprintf(fd, "\n");
 }
