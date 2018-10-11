6354e6477eb02463d7c1e04342af2fbf87e4cdf6
liujinye <liujinye@xiaomi.com>
2018-10-11 09:47:46 +0800

    DO NOT MERGE HFP: Check AT command buffer boundary during parsing
    * add p_end parameter to tBTA_AG_AT_CMD_CBACK, bta_ag_at_hsp_cback
      and bta_ag_at_hfp_cback to indicate effective data range of p_arg
    * add checks for buffer copy overflow in bta_ag_at_hsp_cback and
      bta_ag_at_hfp_cback
    * add packet legnth checks with p_end in bta_ag_parse_cmer
    * add packet length checks with p_end in bta_ag_parse_bac
    
    Bug: 112860487
    Test: manual
    Change-Id: I6bbbc2ba29ad025c7d3ba023d8191af6a11c4aa9
    Merged-In: I6bbbc2ba29ad025c7d3ba023d8191af6a11c4aa9

--- stack/btu/btu_hcif.cc
+++ stack/btu/btu_hcif.cc
@@ -27,11 +27,9 @@
 
 #define LOG_TAG "bt_btu_hcif"
 
-#include <base/bind.h>
 #include <base/callback.h>
 #include <base/location.h>
 #include <base/logging.h>
-#include <base/threading/thread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -50,6 +48,9 @@
 #include "osi/include/osi.h"
 
 using tracked_objects::Location;
+
+// TODO(zachoverflow): remove this horrible hack
+extern fixed_queue_t* btu_hci_msg_queue;
 
 extern void btm_process_cancel_complete(uint8_t status, uint8_t mode);
 extern void btm_ble_test_command_complete(uint8_t* p);
@@ -125,18 +126,6 @@ static void btu_ble_rc_param_req_evt(uint8_t* p);
 #if (BLE_PRIVACY_SPT == TRUE)
 static void btu_ble_proc_enhanced_conn_cmpl(uint8_t* p, uint16_t evt_len);
 #endif
-
-static void do_in_hci_thread(const tracked_objects::Location& from_here,
-                             const base::Closure& task) {
-  base::MessageLoop* hci_message_loop = get_message_loop();
-  if (!hci_message_loop || !hci_message_loop->task_runner().get()) {
-    LOG_ERROR(LOG_TAG, "%s: HCI message loop not running, accessed from %s",
-              __func__, from_here.ToString().c_str());
-    return;
-  }
-
-  hci_message_loop->task_runner()->PostTask(from_here, task);
-}
 
 /*******************************************************************************
  *
@@ -343,7 +332,7 @@ void btu_hcif_process_event(UNUSED_ATTR uint8_t controller_id, BT_HDR* p_msg) {
           btu_ble_data_length_change_evt(p, hci_evt_len);
           break;
 
-        case HCI_BLE_PHY_UPDATE_COMPLETE_EVT:
+        case HCI_LE_PHY_UPDATE_COMPLETE_EVT:
           btm_ble_process_phy_update_pkt(ble_evt_len, p);
           break;
 
@@ -413,48 +402,58 @@ void cmd_with_cb_data_cleanup(cmd_with_cb_data* cb_wrapper) {
   cb_wrapper->posted_from.~Location();
 }
 
-static void btu_hcif_command_complete_evt_with_cb_on_task(BT_HDR* event,
-                                                          void* context) {
+static void btu_hcif_command_complete_evt_with_cb_on_task(BT_HDR* event) {
+  command_complete_hack_t* hack = (command_complete_hack_t*)&event->data[0];
+
   command_opcode_t opcode;
   uint8_t* stream =
-      event->data + event->offset +
+      hack->response->data + hack->response->offset +
       3;  // 2 to skip the event headers, 1 to skip the command credits
   STREAM_TO_UINT16(opcode, stream);
 
-  cmd_with_cb_data* cb_wrapper = (cmd_with_cb_data*)context;
+  cmd_with_cb_data* cb_wrapper = (cmd_with_cb_data*)hack->context;
   HCI_TRACE_DEBUG("command complete for: %s",
                   cb_wrapper->posted_from.ToString().c_str());
-  cb_wrapper->cb.Run(stream, event->len - 5);
+  cb_wrapper->cb.Run(stream, hack->response->len - 5);
   cmd_with_cb_data_cleanup(cb_wrapper);
   osi_free(cb_wrapper);
 
+  osi_free(hack->response);
   osi_free(event);
 }
 
 static void btu_hcif_command_complete_evt_with_cb(BT_HDR* response,
                                                   void* context) {
-  do_in_hci_thread(FROM_HERE,
-                   base::Bind(btu_hcif_command_complete_evt_with_cb_on_task,
-                              response, context));
+  BT_HDR* event = static_cast<BT_HDR*>(
+      osi_calloc(sizeof(BT_HDR) + sizeof(command_complete_hack_t)));
+  command_complete_hack_t* hack = (command_complete_hack_t*)&event->data[0];
+
+  hack->callback = btu_hcif_command_complete_evt_with_cb_on_task;
+  hack->response = response;
+  hack->context = context;
+  event->event = BTU_POST_TO_TASK_NO_GOOD_HORRIBLE_HACK;
+
+  fixed_queue_enqueue(btu_hci_msg_queue, event);
 }
 
-static void btu_hcif_command_status_evt_with_cb_on_task(uint8_t status,
-                                                        BT_HDR* event,
-                                                        void* context) {
+static void btu_hcif_command_status_evt_with_cb_on_task(BT_HDR* event) {
+  command_status_hack_t* hack = (command_status_hack_t*)&event->data[0];
+
   command_opcode_t opcode;
-  uint8_t* stream = event->data + event->offset;
+  uint8_t* stream = hack->command->data + hack->command->offset;
   STREAM_TO_UINT16(opcode, stream);
 
-  CHECK(status != 0);
+  CHECK(hack->status != 0);
 
   // report command status error
-  cmd_with_cb_data* cb_wrapper = (cmd_with_cb_data*)context;
+  cmd_with_cb_data* cb_wrapper = (cmd_with_cb_data*)hack->context;
   HCI_TRACE_DEBUG("command status for: %s",
                   cb_wrapper->posted_from.ToString().c_str());
-  cb_wrapper->cb.Run(&status, sizeof(uint16_t));
+  cb_wrapper->cb.Run(&hack->status, sizeof(uint16_t));
   cmd_with_cb_data_cleanup(cb_wrapper);
   osi_free(cb_wrapper);
 
+  osi_free(hack->command);
   osi_free(event);
 }
 
@@ -466,9 +465,18 @@ static void btu_hcif_command_status_evt_with_cb(uint8_t status, BT_HDR* command,
     return;
   }
 
-  do_in_hci_thread(
-      FROM_HERE, base::Bind(btu_hcif_command_status_evt_with_cb_on_task, status,
-                            command, context));
+  BT_HDR* event = static_cast<BT_HDR*>(
+      osi_calloc(sizeof(BT_HDR) + sizeof(command_status_hack_t)));
+  command_status_hack_t* hack = (command_status_hack_t*)&event->data[0];
+
+  hack->callback = btu_hcif_command_status_evt_with_cb_on_task;
+  hack->status = status;
+  hack->command = command;
+  hack->context = context;
+
+  event->event = BTU_POST_TO_TASK_NO_GOOD_HORRIBLE_HACK;
+
+  fixed_queue_enqueue(btu_hci_msg_queue, event);
 }
 
 /* This function is called to send commands to the Host Controller. |cb| is
@@ -573,7 +581,7 @@ static void btu_hcif_extended_inquiry_result_evt(uint8_t* p) {
 static void btu_hcif_connection_comp_evt(uint8_t* p) {
   uint8_t status;
   uint16_t handle;
-  RawAddress bda;
+  BD_ADDR bda;
   uint8_t link_type;
   uint8_t enc_mode;
 #if (BTM_SCO_INCLUDED == TRUE)
@@ -597,8 +605,8 @@ static void btu_hcif_connection_comp_evt(uint8_t* p) {
   else {
     memset(&esco_data, 0, sizeof(tBTM_ESCO_DATA));
     /* esco_data.link_type = HCI_LINK_TYPE_SCO; already zero */
-    esco_data.bd_addr = bda;
-    btm_sco_connected(status, &bda, handle, &esco_data);
+    memcpy(esco_data.bd_addr, bda, BD_ADDR_LEN);
+    btm_sco_connected(status, bda, handle, &esco_data);
   }
 #endif /* BTM_SCO_INCLUDED */
 }
@@ -613,7 +621,7 @@ static void btu_hcif_connection_comp_evt(uint8_t* p) {
  *
  ******************************************************************************/
 static void btu_hcif_connection_request_evt(uint8_t* p) {
-  RawAddress bda;
+  BD_ADDR bda;
   DEV_CLASS dc;
   uint8_t link_type;
 
@@ -693,16 +701,16 @@ static void btu_hcif_authentication_comp_evt(uint8_t* p) {
  ******************************************************************************/
 static void btu_hcif_rmt_name_request_comp_evt(uint8_t* p, uint16_t evt_len) {
   uint8_t status;
-  RawAddress bd_addr;
+  BD_ADDR bd_addr;
 
   STREAM_TO_UINT8(status, p);
   STREAM_TO_BDADDR(bd_addr, p);
 
   evt_len -= (1 + BD_ADDR_LEN);
 
-  btm_process_remote_name(&bd_addr, p, evt_len, status);
+  btm_process_remote_name(bd_addr, p, evt_len, status);
 
-  btm_sec_rmt_name_request_complete(&bd_addr, p, status);
+  btm_sec_rmt_name_request_complete(bd_addr, p, status);
 }
 
 /*******************************************************************************
@@ -816,7 +824,7 @@ static void btu_hcif_esco_connection_comp_evt(uint8_t* p) {
 #if (BTM_SCO_INCLUDED == TRUE)
   tBTM_ESCO_DATA data;
   uint16_t handle;
-  RawAddress bda;
+  BD_ADDR bda;
   uint8_t status;
 
   STREAM_TO_UINT8(status, p);
@@ -830,8 +838,8 @@ static void btu_hcif_esco_connection_comp_evt(uint8_t* p) {
   STREAM_TO_UINT16(data.tx_pkt_len, p);
   STREAM_TO_UINT8(data.air_mode, p);
 
-  data.bd_addr = bda;
-  btm_sco_connected(status, &bda, handle, &data);
+  memcpy(data.bd_addr, bda, BD_ADDR_LEN);
+  btm_sco_connected(status, bda, handle, &data);
 #endif
 }
 
@@ -901,14 +909,6 @@ static void btu_hcif_hdl_command_complete(uint16_t opcode, uint8_t* p,
 
     case HCI_READ_RSSI:
       btm_read_rssi_complete(p);
-      break;
-
-    case HCI_READ_FAILED_CONTACT_COUNTER:
-      btm_read_failed_contact_counter_complete(p);
-      break;
-
-    case HCI_READ_AUTOMATIC_FLUSH_TIMEOUT:
-      btm_read_automatic_flush_timeout_complete(p);
       break;
 
     case HCI_READ_TRANSMIT_POWER_LEVEL:
@@ -1001,26 +1001,37 @@ static void btu_hcif_hdl_command_complete(uint16_t opcode, uint8_t* p,
  * Returns          void
  *
  ******************************************************************************/
-static void btu_hcif_command_complete_evt_on_task(BT_HDR* event,
-                                                  void* context) {
+static void btu_hcif_command_complete_evt_on_task(BT_HDR* event) {
+  command_complete_hack_t* hack = (command_complete_hack_t*)&event->data[0];
+
   command_opcode_t opcode;
   uint8_t* stream =
-      event->data + event->offset +
+      hack->response->data + hack->response->offset +
       3;  // 2 to skip the event headers, 1 to skip the command credits
   STREAM_TO_UINT16(opcode, stream);
 
   btu_hcif_hdl_command_complete(
       opcode, stream,
-      event->len -
+      hack->response->len -
           5,  // 3 for the command complete headers, 2 for the event headers
-      context);
+      hack->context);
 
+  osi_free(hack->response);
   osi_free(event);
 }
 
 static void btu_hcif_command_complete_evt(BT_HDR* response, void* context) {
-  do_in_hci_thread(FROM_HERE, base::Bind(btu_hcif_command_complete_evt_on_task,
-                                         response, context));
+  BT_HDR* event = static_cast<BT_HDR*>(
+      osi_calloc(sizeof(BT_HDR) + sizeof(command_complete_hack_t)));
+  command_complete_hack_t* hack = (command_complete_hack_t*)&event->data[0];
+
+  hack->callback = btu_hcif_command_complete_evt_on_task;
+  hack->response = response;
+  hack->context = context;
+
+  event->event = BTU_POST_TO_TASK_NO_GOOD_HORRIBLE_HACK;
+
+  fixed_queue_enqueue(btu_hci_msg_queue, event);
 }
 
 /*******************************************************************************
@@ -1035,7 +1046,7 @@ static void btu_hcif_command_complete_evt(BT_HDR* response, void* context) {
 static void btu_hcif_hdl_command_status(uint16_t opcode, uint8_t status,
                                         uint8_t* p_cmd,
                                         void* p_vsc_status_cback) {
-  RawAddress bd_addr;
+  BD_ADDR bd_addr;
   uint16_t handle;
 #if (BTM_SCO_INCLUDED == TRUE)
   tBTM_ESCO_DATA esco_data;
@@ -1089,10 +1100,10 @@ static void btu_hcif_hdl_command_status(uint16_t opcode, uint8_t status,
             if (p_cmd != NULL) {
               p_cmd++;
               STREAM_TO_BDADDR(bd_addr, p_cmd);
-              btm_acl_role_changed(status, &bd_addr, BTM_ROLE_UNDEFINED);
+              btm_acl_role_changed(status, bd_addr, BTM_ROLE_UNDEFINED);
             } else
               btm_acl_role_changed(status, NULL, BTM_ROLE_UNDEFINED);
-            l2c_link_role_changed(nullptr, BTM_ROLE_UNDEFINED,
+            l2c_link_role_changed(NULL, BTM_ROLE_UNDEFINED,
                                   HCI_ERR_COMMAND_DISALLOWED);
             break;
 
@@ -1181,20 +1192,33 @@ static void btu_hcif_hdl_command_status(uint16_t opcode, uint8_t status,
  * Returns          void
  *
  ******************************************************************************/
-static void btu_hcif_command_status_evt_on_task(uint8_t status, BT_HDR* event,
-                                                void* context) {
+static void btu_hcif_command_status_evt_on_task(BT_HDR* event) {
+  command_status_hack_t* hack = (command_status_hack_t*)&event->data[0];
+
   command_opcode_t opcode;
-  uint8_t* stream = event->data + event->offset;
+  uint8_t* stream = hack->command->data + hack->command->offset;
   STREAM_TO_UINT16(opcode, stream);
 
-  btu_hcif_hdl_command_status(opcode, status, stream, context);
+  btu_hcif_hdl_command_status(opcode, hack->status, stream, hack->context);
+
+  osi_free(hack->command);
   osi_free(event);
 }
 
 static void btu_hcif_command_status_evt(uint8_t status, BT_HDR* command,
                                         void* context) {
-  do_in_hci_thread(FROM_HERE, base::Bind(btu_hcif_command_status_evt_on_task,
-                                         status, command, context));
+  BT_HDR* event = static_cast<BT_HDR*>(
+      osi_calloc(sizeof(BT_HDR) + sizeof(command_status_hack_t)));
+  command_status_hack_t* hack = (command_status_hack_t*)&event->data[0];
+
+  hack->callback = btu_hcif_command_status_evt_on_task;
+  hack->status = status;
+  hack->command = command;
+  hack->context = context;
+
+  event->event = BTU_POST_TO_TASK_NO_GOOD_HORRIBLE_HACK;
+
+  fixed_queue_enqueue(btu_hci_msg_queue, event);
 }
 
 /*******************************************************************************
@@ -1238,16 +1262,15 @@ static void btu_hcif_flush_occured_evt(void) {}
  ******************************************************************************/
 static void btu_hcif_role_change_evt(uint8_t* p) {
   uint8_t status;
-  RawAddress bda;
+  BD_ADDR bda;
   uint8_t role;
 
   STREAM_TO_UINT8(status, p);
   STREAM_TO_BDADDR(bda, p);
   STREAM_TO_UINT8(role, p);
 
-  btm_blacklist_role_change_device(bda, status);
-  l2c_link_role_changed(&bda, role, status);
-  btm_acl_role_changed(status, &bda, role);
+  l2c_link_role_changed(bda, role, status);
+  btm_acl_role_changed(status, bda, role);
 }
 
 /*******************************************************************************
@@ -1322,7 +1345,7 @@ static void btu_hcif_ssr_evt(uint8_t* p, uint16_t evt_len) {
  *
  ******************************************************************************/
 static void btu_hcif_pin_code_request_evt(uint8_t* p) {
-  RawAddress bda;
+  BD_ADDR bda;
 
   STREAM_TO_BDADDR(bda, p);
 
@@ -1343,7 +1366,7 @@ static void btu_hcif_pin_code_request_evt(uint8_t* p) {
  *
  ******************************************************************************/
 static void btu_hcif_link_key_request_evt(uint8_t* p) {
-  RawAddress bda;
+  BD_ADDR bda;
 
   STREAM_TO_BDADDR(bda, p);
   btm_sec_link_key_request(bda);
@@ -1359,7 +1382,7 @@ static void btu_hcif_link_key_request_evt(uint8_t* p) {
  *
  ******************************************************************************/
 static void btu_hcif_link_key_notification_evt(uint8_t* p) {
-  RawAddress bda;
+  BD_ADDR bda;
   LINK_KEY key;
   uint8_t key_type;
 
@@ -1510,9 +1533,7 @@ static void btu_hcif_host_support_evt(uint8_t* p) {
  *
  ******************************************************************************/
 static void btu_hcif_io_cap_request_evt(uint8_t* p) {
-  RawAddress bda;
-  STREAM_TO_BDADDR(bda, p);
-  btm_io_capabilities_req(bda);
+  btm_io_capabilities_req(p);
 }
 
 /*******************************************************************************
