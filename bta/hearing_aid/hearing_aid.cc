49a2cde85b7dff8efaa966d31fe2236c61a57054
liujinye <liujinye@xiaomi.com>
2018-10-12 01:36:48 +0800

    [automerger skipped] [automerger] Fix possible OOB read in process_service_search_rsp am: b6fa6e4fff am: 8c06d18eea am: dd2fbd1c21 am: ea12ddddf5 skipped: 3d2f54aa0f am: 572fa878ff  -s ours am: 2a054eb8de  -s ours am: 2667debc95  -s ours am: 71a55dd26e  -s ours
    Change-Id: Ib692d9c2ae83857bbe2159474d8ce09a32efefe4

--- bta/hearing_aid/hearing_aid.cc
+++ bta/hearing_aid/hearing_aid.cc
@@ -189,7 +189,7 @@ struct HearingDevice {
         gap_handle(0),
         psm(0) {}
 
-  HearingDevice() { HearingDevice(RawAddress::kEmpty, false); }
+  HearingDevice() : HearingDevice(RawAddress::kEmpty, false) {}
 
   /* return true if this device represents left Hearing Aid. Returned value is
    * valid only after capabilities are discovered */
@@ -326,18 +326,6 @@ class HearingAidImpl : public HearingAid {
     DVLOG(2) << __func__ << " " << address;
     hearingDevices.Add(HearingDevice(address, true));
     BTA_GATTC_Open(gatt_if, address, true, GATT_TRANSPORT_LE, false);
-  }
-
-  void AddToWhiteList(const RawAddress& address) override {
-    VLOG(2) << __func__ << " address: " << address;
-    hearingDevices.Add(HearingDevice(address, true));
-    BTA_GATTC_Open(gatt_if, address, false, GATT_TRANSPORT_LE, false);
-    BTA_DmBleStartAutoConn();
-  }
-
-  void RemoveFromWhiteList(const RawAddress& address) override {
-    VLOG(2) << __func__ << " address: " << address;
-    BTA_GATTC_CancelOpen(gatt_if, address, false);
   }
 
   void AddFromStorage(const RawAddress& address, uint16_t psm,
@@ -515,11 +503,10 @@ class HearingAidImpl : public HearingAid {
       return;
     }
 
-    const std::vector<tBTA_GATTC_SERVICE>* services =
-        BTA_GATTC_GetServices(conn_id);
+    const std::vector<gatt::Service>* services = BTA_GATTC_GetServices(conn_id);
 
-    const tBTA_GATTC_SERVICE* service = nullptr;
-    for (const tBTA_GATTC_SERVICE& tmp : *services) {
+    const gatt::Service* service = nullptr;
+    for (const gatt::Service& tmp : *services) {
       if (tmp.uuid != HEARING_AID_UUID) continue;
       LOG(INFO) << "Found Hearing Aid service, handle=" << loghex(tmp.handle);
       service = &tmp;
@@ -534,7 +521,7 @@ class HearingAidImpl : public HearingAid {
     }
 
     uint16_t psm_handle = 0x0000;
-    for (const tBTA_GATTC_CHARACTERISTIC& charac : service->characteristics) {
+    for (const gatt::Characteristic& charac : service->characteristics) {
       if (charac.uuid == READ_ONLY_PROPERTIES_UUID) {
         DVLOG(2) << "Reading read only properties "
                  << loghex(charac.value_handle);
@@ -1134,7 +1121,8 @@ class HearingAidImpl : public HearingAid {
                           tBTA_GATT_REASON reason) {
     HearingDevice* hearingDevice = hearingDevices.FindByConnId(conn_id);
     if (!hearingDevice) {
-      VLOG(2) << "Skipping unknown device disconnect, conn_id=" << conn_id;
+      VLOG(2) << "Skipping unknown device disconnect, conn_id="
+              << loghex(conn_id);
       return;
     }
 
@@ -1331,7 +1319,8 @@ void HearingAid::CleanUp() {
 };
 
 void HearingAid::DebugDump(int fd) {
-  dprintf(fd, "\nHearing Aid Manager:\n");
+  dprintf(fd, "Hearing Aid Manager:\n");
   if (instance) instance->Dump(fd);
   HearingAidAudioSource::DebugDump(fd);
+  dprintf(fd, "\n");
 }
