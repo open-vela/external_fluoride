44f1e714cf424a5849030820493940140f1a89df
liujinye <liujinye@xiaomi.com>
2018-11-02 22:43:43 +0800

    Merge changes from topic "am-a8794701-2d32-4392-bf6f-9d00a3751e39" into nyc-dr1-dev
    * changes:
      [automerger] DO NOT MERGE: MCAP: Check response length in mca_ccb_hdl_rsp am: 840f70ca1e
      DO NOT MERGE: MCAP: Check response length in mca_ccb_hdl_rsp

--- btif/Android.mk
+++ btif/Android.mk
@@ -36,7 +36,6 @@ btifCommonSrc += \
   src/btif_debug.c \
   src/btif_debug_btsnoop.c \
   src/btif_debug_conn.c \
-  src/btif_debug_l2c.c \
   src/btif_dm.c \
   src/btif_gatt.c \
   src/btif_gatt_client.c \
