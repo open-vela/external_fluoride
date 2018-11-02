aecfc9f60115caf74766b63ccedb8ad7d6b2d59b
openvela-robot <openvela-robot@xiaomi.com>
2018-11-02 22:47:59 +0800

    Merge changes from topic "am-3290ac2a-4a57-4151-aaf8-9695d2ed6348" into nyc-mr1-dev
    * changes:
      [automerger] DO NOT MERGE: SDP: Check p_end in save_attr_seq and add_attr am: 2091fe7501 am: 4494b9be43
      [automerger] DO NOT MERGE: SDP: Check p_end in save_attr_seq and add_attr am: 2091fe7501
      DO NOT MERGE: SDP: Check p_end in save_attr_seq and add_attr

--- service/common/bluetooth/advertise_settings.h
+++ service/common/bluetooth/advertise_settings.h
@@ -61,15 +61,17 @@ class AdvertiseSettings {
     TX_POWER_LEVEL_HIGH = 0x03,
   };
 
-  AdvertiseSettings(Mode mode, base::TimeDelta timeout,
-                    TxPowerLevel tx_power_level, bool connectable);
+  AdvertiseSettings(Mode mode,
+                    base::TimeDelta timeout,
+                    TxPowerLevel tx_power_level,
+                    bool connectable);
 
   // The default constructor sets all fields to defaults:
   //   mode: MODE_LOW_POWER
   //   TX power level: TX_POWER_LEVEL_MEDIUM
   //   connectable: true
   AdvertiseSettings();
-  virtual ~AdvertiseSettings() = default;
+  ~AdvertiseSettings() = default;
 
   // Returns the advertise mode.
   Mode mode() const { return mode_; }
@@ -86,7 +88,7 @@ class AdvertiseSettings {
   // Comparison operator.
   bool operator==(const AdvertiseSettings& rhs) const;
 
- protected:
+ private:
   Mode mode_;
   base::TimeDelta timeout_;
   TxPowerLevel tx_power_level_;
