da1302ccfa7dcb457cede631f7e9eafa38451de6
openvela-robot <openvela-robot@xiaomi.com>
2018-11-02 21:31:37 +0800

    Merge changes from topic "Check-AT-command-buffer-boundary-during-parsing" into oc-dev am: 97ed603b62
    Change-Id: I68020bb39483cf639ef295b657385a16c6680b90

--- hci/src/hci_layer_android.cc
+++ hci/src/hci_layer_android.cc
@@ -1,6 +1,6 @@
 /******************************************************************************
  *
- *  Copyright 2017 Google, Inc.
+ *  Copyright (C) 2017 Google, Inc.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -20,14 +20,12 @@
 
 #include "hci_layer.h"
 
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-
 #include <base/location.h>
 #include <base/logging.h>
 #include "buffer_allocator.h"
 #include "osi/include/log.h"
+#include "sys/stat.h"
+#include "sys/types.h"
 
 #include <android/hardware/bluetooth/1.0/IBluetoothHci.h>
 #include <android/hardware/bluetooth/1.0/IBluetoothHciCallbacks.h>
@@ -47,7 +45,8 @@ using ::android::hardware::Void;
 using ::android::hardware::hidl_vec;
 
 extern void initialization_complete();
-extern void hci_event_received(const base::Location& from_here, BT_HDR* packet);
+extern void hci_event_received(const tracked_objects::Location& from_here,
+                               BT_HDR* packet);
 extern void acl_event_received(BT_HDR* packet);
 extern void sco_data_received(BT_HDR* packet);
 
