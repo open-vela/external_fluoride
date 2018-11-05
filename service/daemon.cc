6937a557633b544cb6cad0c981fa0de45a0429bc
liujinye <liujinye@xiaomi.com>
2018-11-05 17:07:59 +0800

    Merge "DO NOT MERGE: SDP: Check p_end in save_attr_seq and add_attr" into oc-mr1-dev

--- service/daemon.cc
+++ service/daemon.cc
@@ -1,5 +1,5 @@
 //
-//  Copyright 2015 Google, Inc.
+//  Copyright (C) 2015 Google, Inc.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -19,11 +19,8 @@
 #include <memory>
 
 #include <base/logging.h>
-#include <base/run_loop.h>
 
 #include "service/adapter.h"
-#include "service/hal/bluetooth_av_interface.h"
-#include "service/hal/bluetooth_avrcp_interface.h"
 #include "service/hal/bluetooth_gatt_interface.h"
 #include "service/hal/bluetooth_interface.h"
 #include "service/ipc/ipc_manager.h"
@@ -47,7 +44,7 @@ class DaemonImpl : public Daemon, public ipc::IPCManager::Delegate {
     CleanUpBluetoothStack();
   }
 
-  void StartMainLoop() override { base::RunLoop().Run(); }
+  void StartMainLoop() override { message_loop_->Run(); }
 
   Settings* GetSettings() const override { return settings_.get(); }
 
@@ -71,10 +68,6 @@ class DaemonImpl : public Daemon, public ipc::IPCManager::Delegate {
 
     if (!hal::BluetoothGattInterface::Initialize()) goto failed;
 
-    if (!hal::BluetoothAvInterface::Initialize()) goto failed;
-
-    if (!hal::BluetoothAvrcpInterface::Initialize()) goto failed;
-
     return true;
 
   failed:
@@ -87,10 +80,6 @@ class DaemonImpl : public Daemon, public ipc::IPCManager::Delegate {
       hal::BluetoothGattInterface::CleanUp();
     if (hal::BluetoothInterface::IsInitialized())
       hal::BluetoothInterface::CleanUp();
-    if (hal::BluetoothAvInterface::IsInitialized())
-      hal::BluetoothAvInterface::CleanUp();
-    if (hal::BluetoothAvrcpInterface::IsInitialized())
-      hal::BluetoothAvrcpInterface::CleanUp();
   }
 
   void CleanUpBluetoothStack() {
