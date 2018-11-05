0d17a35948f88b9dc22b91e9b66eafb2d4820628
liujinye <liujinye@xiaomi.com>
2018-11-05 09:59:04 +0800

    [automerger skipped] Merge "DO NOT MERGE HFP: Check AT command buffer boundary during parsing" into oc-mr1-dev am: cfaf7a9792  -s ours
    Change-Id: Ie772fff1acf9c3b9d0d8eeaecdfe0f4cb9586461

--- test/run_unit_tests.sh
+++ test/run_unit_tests.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 known_tests=(
-  bluetooth_test_common
   bluetoothtbd_test
   net_test_audio_a2dp_hw
   net_test_avrcp
@@ -10,6 +9,7 @@ known_tests=(
   net_test_bta
   net_test_btif
   net_test_btif_profile_queue
+  net_test_btif_state_machine
   net_test_device
   net_test_hci
   net_test_stack
