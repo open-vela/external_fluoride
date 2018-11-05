dd228588e3b75765a7bd701f826a78bd2064fae4
openvela-robot <openvela-robot@xiaomi.com>
2018-11-05 17:50:52 +0800

    Merge "DO NOT MERGE HFP: Check AT command buffer boundary during parsing" into oc-mr1-dev

--- bta/hd/bta_hd_api.cc
+++ bta/hd/bta_hd_api.cc
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
- *  Copyright 2016 The Android Open Source Project
- *  Copyright 2005-2012 Broadcom Corporation
+ *  Copyright (C) 2016 The Android Open Source Project
+ *  Copyright (C) 2005-2012 Broadcom Corporation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -27,7 +27,6 @@
 
 #if defined(BTA_HD_INCLUDED) && (BTA_HD_INCLUDED == TRUE)
 
-#include <log/log.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -105,30 +104,29 @@ extern void BTA_HdRegisterApp(tBTA_HD_APP_INFO* p_app_info,
   p_buf->hdr.event = BTA_HD_API_REGISTER_APP_EVT;
 
   if (p_app_info->p_name) {
-    strlcpy(p_buf->name, p_app_info->p_name, BTA_HD_APP_NAME_LEN);
+    strncpy(p_buf->name, p_app_info->p_name, BTA_HD_APP_NAME_LEN);
+    p_buf->name[BTA_HD_APP_NAME_LEN] = '\0';
   } else {
     p_buf->name[0] = '\0';
   }
 
   if (p_app_info->p_description) {
-    strlcpy(p_buf->description, p_app_info->p_description,
+    strncpy(p_buf->description, p_app_info->p_description,
             BTA_HD_APP_DESCRIPTION_LEN);
+    p_buf->description[BTA_HD_APP_DESCRIPTION_LEN] = '\0';
   } else {
     p_buf->description[0] = '\0';
   }
 
   if (p_app_info->p_provider) {
-    strlcpy(p_buf->provider, p_app_info->p_provider, BTA_HD_APP_PROVIDER_LEN);
+    strncpy(p_buf->provider, p_app_info->p_provider, BTA_HD_APP_PROVIDER_LEN);
+    p_buf->provider[BTA_HD_APP_PROVIDER_LEN] = '\0';
   } else {
     p_buf->provider[0] = '\0';
   }
 
   p_buf->subclass = p_app_info->subclass;
 
-  if (p_app_info->descriptor.dl_len > BTA_HD_APP_DESCRIPTOR_LEN) {
-    p_app_info->descriptor.dl_len = BTA_HD_APP_DESCRIPTOR_LEN;
-    android_errorWriteLog(0x534e4554, "113111784");
-  }
   p_buf->d_len = p_app_info->descriptor.dl_len;
   memcpy(p_buf->d_data, p_app_info->descriptor.dsc_list,
          p_app_info->descriptor.dl_len);
