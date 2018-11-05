63886bfddfb0a6f3e0697bb665166611a30d90ba
liujinye <liujinye@xiaomi.com>
2018-11-05 10:04:19 +0800

    [automerger skipped] Merge "DO NOT MERGE HFP: Check AT command buffer boundary during parsing" into oc-mr1-dev am: cfaf7a9792  -s ours am: 76e509cbd4
    Change-Id: Idf4b42277f636cba77e5012b19a4b7657af30e7b

--- test/suite/adapter/bluetooth_test.h
+++ test/suite/adapter/bluetooth_test.h
@@ -1,6 +1,6 @@
 /******************************************************************************
  *
- *  Copyright (C) 2015 Google, Inc.
+ *  Copyright 2015 Google, Inc.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -18,23 +18,17 @@
 
 #pragma once
 
-#include "service/hal/bluetooth_interface.h"
-
 #include <gtest/gtest.h>
-#include <map>
-#include <string>
-
-extern "C" {
 #include <hardware/bluetooth.h>
 #include <hardware/bt_gatt.h>
 #include <hardware/bt_pan.h>
 #include <hardware/bt_sock.h>
-#include <hardware/hardware.h>
 #include <signal.h>
 #include <time.h>
-
+#include <map>
+#include <string>
 #include "osi/include/semaphore.h"
-}
+#include "service/hal/bluetooth_interface.h"
 
 namespace bttest {
 
@@ -59,6 +53,10 @@ class BluetoothTest : public ::testing::Test,
   // Get the value of a specific property
   bt_property_t* GetProperty(bt_property_type_t type);
 
+  // Get the value of a specific remote device property
+  bt_property_t* GetRemoteDeviceProperty(const RawAddress* addr,
+                                         bt_property_type_t type);
+
   // Get the current discovery state
   bt_discovery_state_t GetDiscoveryState();
 
@@ -79,20 +77,25 @@ class BluetoothTest : public ::testing::Test,
   void TearDown() override;
 
   // A callback that is called when a property changes
-  void AdapterPropertiesCallback(
-      bt_status_t status,
-      int num_properties,
-      bt_property_t *properties);
+  void AdapterPropertiesCallback(bt_status_t status, int num_properties,
+                                 bt_property_t* properties) override;
+
+  // A callback that is called when the remote device's property changes
+  void RemoteDevicePropertiesCallback(bt_status_t status,
+                                      RawAddress* remote_bd_addr,
+                                      int num_properties,
+                                      bt_property_t* properties) override;
 
   // A callback that is called when the adapter state changes
-  void AdapterStateChangedCallback(bt_state_t state);
+  void AdapterStateChangedCallback(bt_state_t state) override;
 
   // A callback that is called when the Discovery state changes
-  void DiscoveryStateChangedCallback(bt_discovery_state_t state);
+  void DiscoveryStateChangedCallback(bt_discovery_state_t state) override;
 
   // Semaphores used to wait for specific callback execution. Each callback
   // has its own semaphore associated with it.
   semaphore_t* adapter_properties_callback_sem_;
+  semaphore_t* remote_device_properties_callback_sem_;
   semaphore_t* adapter_state_changed_callback_sem_;
   semaphore_t* discovery_state_changed_callback_sem_;
 
@@ -102,7 +105,10 @@ class BluetoothTest : public ::testing::Test,
 
   bt_state_t state_;
   int properties_changed_count_;
-  bt_property_t *last_changed_properties_;
+  bt_property_t* last_changed_properties_;
+  RawAddress curr_remote_device_;
+  int remote_device_properties_changed_count_;
+  bt_property_t* remote_device_last_changed_properties_;
   bt_discovery_state_t discovery_state_;
   bt_acl_state_t acl_state_;
   bt_bond_state_t bond_state_;
