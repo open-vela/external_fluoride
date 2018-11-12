35b875d63f8bda9194bad39a8c3b007b8a6fe138
liujinye <liujinye@xiaomi.com>
2018-11-12 16:35:58 +0800

    Revert "Fix OOB in avrc_pars_browse_rsp" am: 6d1c4974bf
    Change-Id: I164343e6a08e851173a730fb06173fd6dca60f03

--- tools/mcap_tool/mcap_test_mcl.cc
+++ tools/mcap_tool/mcap_test_mcl.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 The Android Open Source Project
+ * Copyright (C) 2017 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@
 
 #include <base/logging.h>
 
+#include "bdaddr.h"
 #include "mca_api.h"
 #include "mca_defs.h"
 #include "mcap_test_mcl.h"
@@ -24,7 +25,7 @@
 namespace SYSTEM_BT_TOOLS_MCAP_TOOL {
 
 McapMcl::McapMcl(btmcap_test_interface_t* mcap_test_interface,
-                 tMCA_HANDLE mcap_handle, const RawAddress& peer_bd_addr)
+                 tMCA_HANDLE mcap_handle, const bt_bdaddr_t& peer_bd_addr)
     : _mdl_list() {
   _mcap_handle = mcap_handle;
   _mcap_test_interface = mcap_test_interface;
@@ -34,7 +35,7 @@ McapMcl::McapMcl(btmcap_test_interface_t* mcap_test_interface,
 
 bool McapMcl::Connect(uint16_t ctrl_psm, uint16_t sec_mask) {
   tMCA_RESULT ret = _mcap_test_interface->connect_mcl(
-      _mcap_handle, _peer_bd_addr, ctrl_psm, sec_mask);
+      _mcap_handle, _peer_bd_addr.address, ctrl_psm, sec_mask);
   LOG_IF(INFO, ret != MCA_SUCCESS) << "ret=" << (int)ret;
   return ret == MCA_SUCCESS;
 }
@@ -133,7 +134,7 @@ bool McapMcl::DeleteMdl(uint16_t mdl_id) {
   return ret == MCA_SUCCESS;
 }
 
-RawAddress& McapMcl::GetPeerAddress() { return _peer_bd_addr; }
+bt_bdaddr_t& McapMcl::GetPeerAddress() { return _peer_bd_addr; }
 
 void McapMcl::SetHandle(tMCA_CL handle) { _mcl_handle = handle; }
 
