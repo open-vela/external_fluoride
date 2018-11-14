47568df68540c5febd9894234555c230a86d1ff7
openvela-robot <openvela-robot@xiaomi.com>
2018-11-14 23:32:31 +0800

    Use map instead of list for keeping apps interested in connection am: c436612cdc am: 3641f67e0c am: fbe4074c23
    Change-Id: I342158e57d2808fb53d142e95946ae509398f796

--- btif/src/bluetooth.cc
+++ btif/src/bluetooth.cc
@@ -69,7 +69,6 @@
 #include "osi/include/log.h"
 #include "osi/include/osi.h"
 #include "osi/include/wakelock.h"
-#include "stack/gatt/connection_manager.h"
 #include "stack_manager.h"
 
 /* Test interface includes */
@@ -324,7 +323,6 @@ static void dump(int fd, const char** arguments) {
   osi_allocator_debug_dump(fd);
   alarm_debug_dump(fd);
   HearingAid::DebugDump(fd);
-  gatt::connection_manager::dump(fd);
 #if (BTSNOOP_MEM == TRUE)
   btif_debug_btsnoop_dump(fd);
 #endif
