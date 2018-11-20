8daa6a247733bec939054cef2b54110dec8d10f9
openvela-robot <openvela-robot@xiaomi.com>
2018-11-20 22:31:31 +0800

    Fix potential usage of freed memory in btif_hl_proc_sdp_query_cfm
    Bug: 116222069
    Test: compilation
    Change-Id: Iebe2c500dfc2806ca321fdcd170e20c680619d4d
    Merged-In: Iebe2c500dfc2806ca321fdcd170e20c680619d4d

--- stack/include/btu.h
+++ stack/include/btu.h
@@ -1,6 +1,6 @@
 /******************************************************************************
  *
- *  Copyright 1999-2012 Broadcom Corporation
+ *  Copyright (C) 1999-2012 Broadcom Corporation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -27,42 +27,64 @@
 #ifndef BTU_H
 #define BTU_H
 
-#include "bt_common.h"
 #include "bt_target.h"
-#include "common/message_loop_thread.h"
+#include "bt_common.h"
 #include "osi/include/alarm.h"
 
-#include <base/callback.h>
-#include <base/location.h>
-#include <base/threading/thread.h>
+// HACK(zachoverflow): temporary dark magic
+#define BTU_POST_TO_TASK_NO_GOOD_HORRIBLE_HACK 0x1700 // didn't look used in bt_types...here goes nothing
+typedef struct {
+  void (*callback)(BT_HDR *);
+} post_to_task_hack_t;
+
+typedef struct {
+  void (*callback)(BT_HDR *);
+  BT_HDR *response;
+  void *context;
+} command_complete_hack_t;
+
+typedef struct {
+  void (*callback)(BT_HDR *);
+  uint8_t status;
+  BT_HDR *command;
+  void *context;
+} command_status_hack_t;
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 /* Global BTU data */
 extern uint8_t btu_trace_level;
 
-/* Functions provided by btu_hcif.cc
- ***********************************
-*/
-void btu_hcif_process_event(uint8_t controller_id, BT_HDR* p_buf);
-void btu_hcif_send_cmd(uint8_t controller_id, BT_HDR* p_msg);
-void btu_hcif_send_cmd_with_cb(const base::Location& posted_from,
-                               uint16_t opcode, uint8_t* params,
-                               uint8_t params_len,
-                               base::OnceCallback<void(uint8_t*, uint16_t)> cb);
+extern const BD_ADDR        BT_BD_ANY;
 
-/* Functions provided by btu_init.cc
- ***********************************
+/* Functions provided by btu_task.c
+************************************
 */
-void btu_init_core(void);
-void btu_free_core(void);
 
-/* Functions provided by btu_task.cc
- ***********************************
+#if (defined(HCILP_INCLUDED) && HCILP_INCLUDED == TRUE)
+extern void btu_check_bt_sleep (void);
+#endif
+
+/* Functions provided by btu_hcif.c
+************************************
 */
-base::MessageLoop* get_main_message_loop();
-bt_status_t do_in_main_thread(const base::Location& from_here,
-                              base::OnceClosure task);
+extern void  btu_hcif_process_event (UINT8 controller_id, BT_HDR *p_buf);
+extern void  btu_hcif_send_cmd (UINT8 controller_id, BT_HDR *p_msg);
+
+/* Functions provided by btu_core.c
+************************************
+*/
+extern void  btu_init_core(void);
+extern void  btu_free_core(void);
 
 void BTU_StartUp(void);
 void BTU_ShutDown(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
