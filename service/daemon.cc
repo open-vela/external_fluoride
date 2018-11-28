a964b43bb3cb165e391e89e5fe124e38a48f2ad0
openvela-robot <openvela-robot@xiaomi.com>
2018-11-28 14:30:47 +0800

    [automerger skipped] Fix buffer overflow in btif_dm_data_copy am: ea90417d99  -s ours
    Change-Id: Idf41fde0fc9fd67066c05f22f70493035b63fef0

--- service/daemon.cc
+++ service/daemon.cc
@@ -1,5 +1,5 @@
 //
-//  Copyright (C) 2015 Google, Inc.
+//  Copyright 2015 Google, Inc.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -19,8 +19,11 @@
 #include <memory>
 
 #include <base/logging.h>
+#include <base/run_loop.h>
 
 #include "service/adapter.h"
+#include "service/hal/bluetooth_av_interface.h"
+#include "service/hal/bluetooth_avrcp_interface.h"
 #include "service/hal/bluetooth_gatt_interface.h"
 #include "service/hal/bluetooth_interface.h"
 #include "service/ipc/ipc_manager.h"
@@ -44,7 +47,7 @@ class DaemonImpl : public Daemon, public ipc::IPCManager::Delegate {
     CleanUpBluetoothStack();
   }
 
-  void StartMainLoop() override { message_loop_->Run(); }
+  void StartMainLoop() override { base::RunLoop().Run(); }
 
   Settings* GetSettings() const override { return settings_.get(); }
 
@@ -68,6 +71,10 @@ class DaemonImpl : public Daemon, public ipc::IPCManager::Delegate {
 
     if (!hal::BluetoothGattInterface::Initialize()) goto failed;
 
+    if (!hal::BluetoothAvInterface::Initialize()) goto failed;
+
+    if (!hal::BluetoothAvrcpInterface::Initialize()) goto failed;
+
     return true;
 
   failed:
@@ -80,6 +87,10 @@ class DaemonImpl : public Daemon, public ipc::IPCManager::Delegate {
       hal::BluetoothGattInterface::CleanUp();
     if (hal::BluetoothInterface::IsInitialized())
       hal::BluetoothInterface::CleanUp();
+    if (hal::BluetoothAvInterface::IsInitialized())
+      hal::BluetoothAvInterface::CleanUp();
+    if (hal::BluetoothAvrcpInterface::IsInitialized())
+      hal::BluetoothAvrcpInterface::CleanUp();
   }
 
   void CleanUpBluetoothStack() {
