59385fda629d1dd3c481cd46138e8aa0cb7ac6e4
openvela-robot <openvela-robot@xiaomi.com>
2018-12-11 14:45:13 +0800

    DO NOT MERGE A security fix to check buffer length in l2c_lcc_proc_pdu
    Add check to make sure that data buffer is big enough to read the 2
    bytes for length.
    
    Also, fix a regression from the previous CL that checks the buffer length
    before doing a memcpy. The previous check is too strict causing valid
    sized buffers to be rejected. The length check is incorrect and off by the header size.
    
    Bug: 120665616
    Test: Run the SL4A Test for LE CoC, BleCoCTest
    Merged-In: I30b7a8af11d3a5f974cb39e06b0e3463bebc8e9a
    Change-Id: I30b7a8af11d3a5f974cb39e06b0e3463bebc8e9a
    (cherry picked from commit fcb1994de1f6ee34b8dc6804a2b32e20bf138073)
    (cherry picked from commit 1f1d8b97d80d25023c4c7b04d2aa18d367f4158d)
    (cherry picked from commit c117a1c951c65033987ed51d53407b359204c187)

--- service/example/heart_rate/server_main.cc
+++ service/example/heart_rate/server_main.cc
@@ -41,7 +41,10 @@ namespace {
 std::string kServiceName = "bluetooth-service";
 
 void QuitMessageLoop() {
+  // I don't know why both of these calls are necessary but the message loop
+  // doesn't stop unless I call both. Bug in base::MessageLoop?
   base::RunLoop().Quit();
+  base::MessageLoop::current()->QuitNow();
 }
 
 // Handles the case where the Bluetooth process dies.
@@ -131,7 +134,7 @@ int main(int argc, char* argv[]) {
     }
 
     LOG(ERROR) << "Starting Heart Rate server failed asynchronously";
-    base::RunLoop().QuitWhenIdle();
+    main_loop.QuitWhenIdle();
   };
 
   bool advertise =
