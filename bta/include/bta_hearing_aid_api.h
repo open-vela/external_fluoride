16702ed0c58356904356074538791cdc5e78f3cc
liujinye <liujinye@xiaomi.com>
2019-01-07 14:52:26 +0800

    [automerger skipped] DO NOT MERGE A security fix to check buffer length in l2c_lcc_proc_pdu am: c117a1c951 am: 0696c33d40 -s ours am skip reason: subject contains skip directive
    Change-Id: I2c1c45e8559cb4e5ef69a4431bb31b3dae57135f

--- bta/include/bta_hearing_aid_api.h
+++ bta/include/bta_hearing_aid_api.h
@@ -30,8 +30,8 @@ class HearingAidAudioReceiver {
  public:
   virtual ~HearingAidAudioReceiver() = default;
   virtual void OnAudioDataReady(const std::vector<uint8_t>& data) = 0;
-  virtual void OnAudioSuspend(std::promise<void> do_suspend_promise);
-  virtual void OnAudioResume(std::promise<void> do_resume_promise);
+  virtual void OnAudioSuspend(std::promise<void> do_suspend_promise) = 0;
+  virtual void OnAudioResume(std::promise<void> do_resume_promise) = 0;
 };
 
 class HearingAid {
@@ -41,7 +41,7 @@ class HearingAid {
   static void Initialize(bluetooth::hearing_aid::HearingAidCallbacks* callbacks,
                          base::Closure initCb);
   static void CleanUp();
-  static bool IsInitialized();
+  static bool IsHearingAidRunning();
   static HearingAid* Get();
   static void DebugDump(int fd);
 
@@ -56,8 +56,6 @@ class HearingAid {
 
   virtual void Connect(const RawAddress& address) = 0;
   virtual void Disconnect(const RawAddress& address) = 0;
-  virtual void AddToWhiteList(const RawAddress& address) = 0;
-  virtual void RemoveFromWhiteList(const RawAddress& address) = 0;
   virtual void SetVolume(int8_t volume) = 0;
 };
 
