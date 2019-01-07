e9d947d4462aaba0d6d3b88ccd7e0b5668b0a125
liujinye <liujinye@xiaomi.com>
2019-01-07 14:44:41 +0800

    [automerger] DO NOT MERGE A security fix to check buffer length in l2c_lcc_proc_pdu am: c1fcbd5508 am: 85b4574a31 am: 097ecf3d88 am: 2ebe3d52b0 skipped: dff13d810c am: 47dcb6a458 am: a2e761ac2b
    Change-Id: Ia655d085c410e45e44efc1fa1bb2737a7cccd310

--- bta/pan/bta_pan_ci.cc
+++ bta/pan/bta_pan_ci.cc
@@ -1,6 +1,6 @@
 /******************************************************************************
  *
- *  Copyright 2004-2012 Broadcom Corporation
+ *  Copyright (C) 2004-2012 Broadcom Corporation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -183,10 +183,10 @@ void bta_pan_ci_rx_writebuf(uint16_t handle, const RawAddress& dst,
  ******************************************************************************/
 BT_HDR* bta_pan_ci_readbuf(uint16_t handle, RawAddress& src, RawAddress& dst,
                            uint16_t* p_protocol, bool* p_ext, bool* p_forward) {
-  tBTA_PAN_SCB* p_scb = bta_pan_scb_by_handle(handle);
+  tBTA_PAN_SCB* p_scb;
   BT_HDR* p_buf;
 
-  if (p_scb == NULL) return NULL;
+  p_scb = bta_pan_scb_by_handle(handle);
 
   p_buf = (BT_HDR*)fixed_queue_try_dequeue(p_scb->data_queue);
   if (p_buf != NULL) {
