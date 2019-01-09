ecfb4a5b94e6b2461cb4e472c1c3dd3c8cd49e51
liujinye <liujinye@xiaomi.com>
2019-01-09 20:51:00 +0800

    Remove code related to Health Device Profile and MCAP Protocol am: 238f8359a9
    Change-Id: Ib10cda753d45163b4d799ca4c1275fb43284c336

--- btif/src/bluetooth.cc
+++ btif/src/bluetooth.cc
@@ -40,17 +40,16 @@
 #include <hardware/bt_hearing_aid.h>
 #include <hardware/bt_hf_client.h>
 #include <hardware/bt_hh.h>
-#include <hardware/bt_hl.h>
 #include <hardware/bt_mce.h>
 #include <hardware/bt_pan.h>
 #include <hardware/bt_rc.h>
 #include <hardware/bt_sdp.h>
 #include <hardware/bt_sock.h>
 
-#include "avrcp_service.h"
 #include "bt_utils.h"
 #include "bta/include/bta_hearing_aid_api.h"
 #include "bta/include/bta_hf_client_api.h"
+#include "btif/avrcp/avrcp_service.h"
 #include "btif_a2dp.h"
 #include "btif_api.h"
 #include "btif_av.h"
@@ -62,17 +61,16 @@
 #include "btif_storage.h"
 #include "btsnoop.h"
 #include "btsnoop_mem.h"
+#include "common/address_obfuscator.h"
+#include "common/metrics.h"
 #include "device/include/interop.h"
 #include "osi/include/alarm.h"
 #include "osi/include/allocation_tracker.h"
 #include "osi/include/log.h"
-#include "osi/include/metrics.h"
 #include "osi/include/osi.h"
 #include "osi/include/wakelock.h"
+#include "stack/gatt/connection_manager.h"
 #include "stack_manager.h"
-
-/* Test interface includes */
-#include "mca_api.h"
 
 using bluetooth::hearing_aid::HearingAidInterface;
 
@@ -100,8 +98,6 @@ extern const btsock_interface_t* btif_sock_get_interface();
 extern const bthh_interface_t* btif_hh_get_interface();
 /* hid device profile */
 extern const bthd_interface_t* btif_hd_get_interface();
-/* health device profile */
-extern const bthl_interface_t* btif_hl_get_interface();
 /*pan*/
 extern const btpan_interface_t* btif_pan_get_interface();
 /*map client*/
@@ -116,9 +112,6 @@ extern const btrc_ctrl_interface_t* btif_rc_ctrl_get_interface();
 extern const btsdp_interface_t* btif_sdp_get_interface();
 /*Hearing Aid client*/
 extern HearingAidInterface* btif_hearing_aid_get_interface();
-
-/* List all test interface here */
-extern const btmcap_test_interface_t* stack_mcap_get_interface();
 
 /*******************************************************************************
  *  Functions
@@ -323,13 +316,14 @@ static void dump(int fd, const char** arguments) {
   osi_allocator_debug_dump(fd);
   alarm_debug_dump(fd);
   HearingAid::DebugDump(fd);
+  connection_manager::dump(fd);
 #if (BTSNOOP_MEM == TRUE)
   btif_debug_btsnoop_dump(fd);
 #endif
 }
 
 static void dumpMetrics(std::string* output) {
-  system_bt_osi::BluetoothMetricsLogger::GetInstance()->WriteString(output);
+  bluetooth::common::BluetoothMetricsLogger::GetInstance()->WriteString(output);
 }
 
 static const void* get_profile_interface(const char* profile_id) {
@@ -363,9 +357,6 @@ static const void* get_profile_interface(const char* profile_id) {
   if (is_profile(profile_id, BT_PROFILE_HIDDEV_ID))
     return btif_hd_get_interface();
 
-  if (is_profile(profile_id, BT_PROFILE_HEALTH_ID))
-    return btif_hl_get_interface();
-
   if (is_profile(profile_id, BT_PROFILE_SDP_CLIENT_ID))
     return btif_sdp_get_interface();
 
@@ -377,9 +368,6 @@ static const void* get_profile_interface(const char* profile_id) {
 
   if (is_profile(profile_id, BT_PROFILE_AV_RC_CTRL_ID))
     return btif_rc_ctrl_get_interface();
-
-  if (is_profile(profile_id, BT_TEST_INTERFACE_MCAP_ID))
-    return stack_mcap_get_interface();
 
   if (is_profile(profile_id, BT_PROFILE_HEARING_AID_ID))
     return btif_hearing_aid_get_interface();
@@ -451,6 +439,11 @@ static bluetooth::avrcp::ServiceInterface* get_avrcp_service(void) {
   return bluetooth::avrcp::AvrcpService::GetServiceInterface();
 }
 
+static std::string obfuscate_address(const RawAddress& address) {
+  return bluetooth::common::AddressObfuscator::GetInstance()->Obfuscate(
+      address);
+}
+
 EXPORT_SYMBOL bt_interface_t bluetoothInterface = {
     sizeof(bluetoothInterface),
     init,
@@ -486,4 +479,5 @@ EXPORT_SYMBOL bt_interface_t bluetoothInterface = {
     interop_database_clear,
     interop_database_add,
     get_avrcp_service,
+    obfuscate_address,
 };
--- stack/btm/btm_main.cc
+++ stack/btm/btm_main.cc
@@ -66,29 +66,9 @@ void btm_init(void) {
     btm_sec_init(BTM_SEC_MODE_SC);
   else
     btm_sec_init(BTM_SEC_MODE_SP);
-#if (BTM_SCO_INCLUDED == TRUE)
   btm_sco_init(); /* SCO Database and Structures (If included) */
-#endif
 
   btm_cb.sec_dev_rec = list_new(osi_free);
 
   btm_dev_init(); /* Device Manager Structures & HCI_Reset */
-}
-
-/** This function is called to free dynamic memory and system resource allocated by btm_init */
-void btm_free(void) {
-  fixed_queue_free(btm_cb.page_queue, NULL);
-  btm_cb.page_queue = NULL;
-
-  fixed_queue_free(btm_cb.sec_pending_q, NULL);
-  btm_cb.sec_pending_q = NULL;
-
-  list_free(btm_cb.sec_dev_rec);
-  btm_cb.sec_dev_rec = NULL;
-
-  alarm_free(btm_cb.sec_collision_timer);
-  btm_cb.sec_collision_timer = NULL;
-
-  alarm_free(btm_cb.pairing_timer);
-  btm_cb.pairing_timer = NULL;
 }
