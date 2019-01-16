bc3389b34237d11d8f13f6fd4ea53dc2b7bfe77d
openvela-robot <openvela-robot@xiaomi.com>
2019-01-16 12:33:26 +0800

    btm_ble_multi_adv: Check data length in HCI interface
    For BleAdvertiserVscHciInterfaceImpl and
    BleAdvertiserLegacyHciInterfaceImpl, the maximum size of scan response
    and advertising packet data length should be BTM_BLE_AD_DATA_LEN (31).
    
    Bug: 121145627
    Test: POC
    Change-Id: I7653a6c186b7313ef2b1547bca120b9d41c90140
    (cherry picked from commit a99fe8a175a6d209e741871544ae3f857c8a7cbb)

--- profile/avrcp/tests/avrcp_device_test.cc
+++ profile/avrcp/tests/avrcp_device_test.cc
@@ -41,8 +41,8 @@ using TestAvrcpPacket = TestPacketType<Packet>;
 using TestBrowsePacket = TestPacketType<BrowsePacket>;
 
 using ::testing::_;
-using ::testing::Mock;
 using ::testing::MockFunction;
+using ::testing::Mock;
 using ::testing::NiceMock;
 using ::testing::Return;
 using ::testing::SaveArg;
@@ -813,24 +813,16 @@ TEST_F(AvrcpDeviceTest, getFolderItemsMtuTest) {
       base::Bind([](MockFunction<void(uint8_t, bool, const AvrcpResponse&)>* a,
                     uint8_t b, bool c, AvrcpResponse d) { a->Call(b, c, d); },
                  &response_cb);
-
-  Device device(RawAddress::kAny, true, cb, 0xFFFF,
-                truncated_packet->size() + FolderItem::kHeaderSize() + 5);
+  Device device(RawAddress::kAny, true, cb, 0xFFFF, truncated_packet->size());
   device.RegisterInterfaces(&interface, &a2dp_interface, nullptr);
 
   FolderInfo info0 = {"test_id0", true, "Test Folder0"};
   FolderInfo info1 = {"test_id1", true, "Test Folder1"};
-  FolderInfo info2 = {"test_id2", true, "Truncated folder"};
-  // Used to ensure that adding an item that would fit in the MTU fails if
-  // adding a large item failed.
-  FolderInfo small_info = {"test_id2", true, "Small"};
-
+  FolderInfo info2 = {"test_id1", true, "Truncated folder"};
   ListItem item0 = {ListItem::FOLDER, info0, SongInfo()};
   ListItem item1 = {ListItem::FOLDER, info1, SongInfo()};
-  ListItem item2 = {ListItem::FOLDER, info2, SongInfo()};
-  ListItem item3 = {ListItem::FOLDER, small_info, SongInfo()};
-
-  std::vector<ListItem> list0 = {item0, item1, item2, item3};
+  ListItem item2 = {ListItem::FOLDER, info1, SongInfo()};
+  std::vector<ListItem> list0 = {item0, item1, item2};
   EXPECT_CALL(interface, GetFolderItems(_, "", _))
       .WillRepeatedly(InvokeCb<2>(list0));
 
