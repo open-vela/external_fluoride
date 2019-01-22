67fe953cf210d999e04da4534fd4e7d633ad96d3
liujinye <liujinye@xiaomi.com>
2019-01-22 21:47:35 +0800

    [automerger] DO NOT MERGE btm_proc_smp_cback: Don't access p_dev_rec if freed am: 74c6d501ce am: 059e3c77e2 am: a244a4072c
    Change-Id: Ic43337c91c1cdcb9eaea22311cd7205dc05dcfa2

--- btif/include/stack_manager.h
+++ btif/include/stack_manager.h
@@ -31,8 +31,7 @@ typedef struct {
   bool (*get_stack_is_running)(void);
 } stack_manager_t;
 
-const stack_manager_t* stack_manager_get_interface();
+const stack_manager_t *stack_manager_get_interface();
 
-// TODO(zachoverflow): remove this terrible hack once the startup sequence is
-// more sane
-future_t* stack_manager_get_hack_future();
+// TODO(zachoverflow): remove this terrible hack once the startup sequence is more sane
+future_t *stack_manager_get_hack_future();
