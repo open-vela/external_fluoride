dffefa973363108875434193c09b0c02cf9ef97a
liujinye <liujinye@xiaomi.com>
2019-02-02 07:52:13 +0800

    Merge changes from topic "am-cdd47550-8877-443a-826f-db2b25d750ce" into oc-dev
    * changes:
      [automerger] DO NOT MERGE process_l2cap_cmd: Fix OOB am: 38f07a3c93 am: 14f6578d9e am: 53e323b2af am: d0584f3dcf skipped: 55b702e6c4
      [automerger] DO NOT MERGE process_l2cap_cmd: Fix OOB am: 38f07a3c93 am: 14f6578d9e am: 53e323b2af am: d0584f3dcf
      [automerger] DO NOT MERGE process_l2cap_cmd: Fix OOB am: 38f07a3c93 am: 14f6578d9e am: 53e323b2af
      [automerger] DO NOT MERGE process_l2cap_cmd: Fix OOB am: 38f07a3c93 am: 14f6578d9e
      [automerger] DO NOT MERGE process_l2cap_cmd: Fix OOB am: 38f07a3c93
      DO NOT MERGE process_l2cap_cmd: Fix OOB

--- test/rootcanal/bluetooth_hci.h
+++ test/rootcanal/bluetooth_hci.h
@@ -72,8 +72,6 @@ class BluetoothHci : public IBluetoothHci {
   test_vendor_lib::TestChannelTransport test_channel_transport_;
 };
 
-extern "C" IBluetoothHci* HIDL_FETCH_IBluetoothHci(const char* name);
-
 }  // namespace sim
 }  // namespace V1_0
 }  // namespace bluetooth
