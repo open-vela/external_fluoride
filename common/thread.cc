19642ca27a564a8f8234ab3818874b54d87685c1
openvela-robot <openvela-robot@xiaomi.com>
2019-02-07 08:54:01 +0800

    [DO NOT MERGE] btif: Lighter weight file existence check
    We switch from the heavier weight fopen() to using access() to
    check for file existence and readability.  This avoids some of
    the file opening overhead we don't need here.
    
    Bug: b/117993149
    Test: TreeHugger
    Change-Id: Ib47b24e9d96803b7822535f9f566aa7818b38953
    Merged-In: Ib47b24e9d96803b7822535f9f566aa7818b38953

--- common/thread.cc
+++ common/thread.cc
@@ -42,7 +42,7 @@ void Thread::run(Priority priority) {
     int rc;
     RUN_NO_INTR(rc = sched_setscheduler(linux_tid, SCHED_FIFO, &rt_params));
     if (rc != 0) {
-      PLOG(ERROR) << __func__ << ": unable to set SCHED_FIFO priority";
+      LOG(ERROR) << __func__ << ": unable to set SCHED_FIFO priority: " << strerror(errno);
     }
   }
   reactor_.Run();
--- hci/src/hci_layer_linux.cc
+++ hci/src/hci_layer_linux.cc
@@ -203,12 +203,12 @@ void hci_initialize() {
   addr.hci_dev = hci_interface;
   addr.hci_channel = HCI_CHANNEL_USER;
   if (bind(fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
-    PLOG(FATAL) << "socket bind error";
+    LOG(FATAL) << "socket bind error " << strerror(errno);
   }
 
   int sv[2];
   if (socketpair(AF_UNIX, SOCK_STREAM, 0, sv) < 0) {
-    PLOG(FATAL) << "socketpair failed";
+    LOG(FATAL) << "socketpair failed: " << strerror(errno);
   }
 
   reader_thread_ctrl_fd = sv[0];
@@ -274,7 +274,7 @@ void hci_transmit(BT_HDR* packet) {
 
   if (ret != packet->len + 1) LOG(ERROR) << "Should have send whole packet";
 
-  if (ret == -1) PLOG(FATAL) << "write failed";
+  if (ret == -1) LOG(FATAL) << strerror(errno);
 }
 
 static int wait_hcidev(void) {
@@ -288,7 +288,7 @@ static int wait_hcidev(void) {
 
   fd = socket(PF_BLUETOOTH, SOCK_RAW, BTPROTO_HCI);
   if (fd < 0) {
-    PLOG(ERROR) << "Bluetooth socket error";
+    LOG(ERROR) << "Bluetooth socket error: %s" << strerror(errno);
     return -1;
   }
 
@@ -298,7 +298,7 @@ static int wait_hcidev(void) {
   addr.hci_channel = HCI_CHANNEL_CONTROL;
 
   if (bind(fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
-    PLOG(ERROR) << "HCI Channel Control";
+    LOG(ERROR) << "HCI Channel Control: " << strerror(errno);
     close(fd);
     return -1;
   }
@@ -314,7 +314,7 @@ static int wait_hcidev(void) {
   ssize_t wrote;
   OSI_NO_INTR(wrote = write(fd, &ev, 6));
   if (wrote != 6) {
-    PLOG(ERROR) << "Unable to write mgmt command";
+    LOG(ERROR) << "Unable to write mgmt command: " << strerror(errno);
     ret = -1;
     goto end;
   }
@@ -323,7 +323,7 @@ static int wait_hcidev(void) {
     int n;
     OSI_NO_INTR(n = poll(fds, 1, MGMT_EV_POLL_TIMEOUT));
     if (n == -1) {
-      PLOG(ERROR) << "Poll error";
+      LOG(ERROR) << "Poll error: " << strerror(errno);
       ret = -1;
       break;
     } else if (n == 0) {
@@ -335,7 +335,7 @@ static int wait_hcidev(void) {
     if (fds[0].revents & POLLIN) {
       OSI_NO_INTR(n = read(fd, &ev, sizeof(struct mgmt_pkt)));
       if (n < 0) {
-        PLOG(ERROR) << "Error reading control channel";
+        LOG(ERROR) << "Error reading control channel: " << strerror(errno);
         ret = -1;
         break;
       }
