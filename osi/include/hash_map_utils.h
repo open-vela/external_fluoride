f45f96015db1c5dbb4f669832099d56e55e0d7a9
liujinye <liujinye@xiaomi.com>
2019-02-08 07:54:10 +0800

    Merge changes from topic "am-43952131-a4db-4e42-bfef-2d44a29b3fac" into nyc-dr1-dev
    * changes:
      [automerger] DO NOT MERGE Separate SDP procedure from bonding state (1/2) am: edd7e731ed
      DO NOT MERGE Separate SDP procedure from bonding state (1/2)

--- osi/include/hash_map_utils.h
+++ osi/include/hash_map_utils.h
@@ -18,8 +18,7 @@
 
 #pragma once
 
-#include <string>
-#include <unordered_map>
+#include "osi/include/hash_map.h"
 
 // Creates a hash map based on the |params| string containing key and value
 // pairs.  Pairs are expected in the form "key=value" separated by the ';'
@@ -30,11 +29,9 @@
 //   "key0=;key1=value1;"        -> map: [key0]="" [key1]="value1"
 //   "=value0;key1=value1;"      -> map: [key1]="value1"
 // A new hash map or NULL is returned and is owned by the caller.
-std::unordered_map<std::string, std::string>
-hash_map_utils_new_from_string_params(const char* params);
+hash_map_t *hash_map_utils_new_from_string_params(const char *params);
 
 // Dumps the contents of the hash_map to the log for debugging purposes.
 // If |map| is not NULL, all entries of |map| will be dumped, otherwise nothing
 // will be dumped. Note that this function does not take the ownership of |map|.
-void hash_map_utils_dump_string_keys_string_values(
-    std::unordered_map<std::string, std::string>& map);
+void hash_map_utils_dump_string_keys_string_values(hash_map_t *map);
