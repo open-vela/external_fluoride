56d23f24fac28abf377da084fca03f8059121f64
liujinye <liujinye@xiaomi.com>
2019-02-12 11:23:39 +0800

    [DO NOT MERGE] Handle edge cases where input or hash/data could be null.
    Bug: b/117993149
    Test: Manual; atest net_test_btif net_test_bluetooth
    Change-Id: Ia91ea822ee2147b2a2d14bea250a708e8c10bae4
    Merged-In: Ia91ea822ee2147b2a2d14bea250a708e8c10bae4

--- btif/src/btif_config.cc
+++ btif/src/btif_config.cc
@@ -24,14 +24,14 @@
 #include <ctype.h>
 #include <openssl/rand.h>
 #include <openssl/sha.h>
-#include <private/android_filesystem_config.h>
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
-#include <mutex>
 #include <sstream>
 #include <string>
+
+#include <mutex>
 
 #include "bt_types.h"
 #include "btcore/include/module.h"
@@ -55,17 +55,14 @@
 #define FILE_TIMESTAMP "TimeCreated"
 #define FILE_SOURCE "FileSource"
 #define TIME_STRING_LENGTH sizeof("YYYY-MM-DD HH:MM:SS")
-#define DISABLED "disabled"
 static const char* TIME_STRING_FORMAT = "%Y-%m-%d %H:%M:%S";
-
-constexpr int kBufferSize = 400 * 10;  // initial file is ~400B
-
-static bool use_key_attestation() { return getuid() == AID_BLUETOOTH; }
 
 #define BT_CONFIG_METRICS_SECTION "Metrics"
 #define BT_CONFIG_METRICS_SALT_256BIT "Salt256Bit"
-using bluetooth::BtifKeystore;
 using bluetooth::common::AddressObfuscator;
+constexpr int kBufferSize = 400 * 10;  // initial file is ~400B
+
+using bluetooth::BtifKeystore;
 
 // TODO(armansito): Find a better way than searching by a hardcoded path.
 #if defined(OS_GENERIC)
@@ -451,12 +448,6 @@ bool btif_config_set_bin(const std::string& section, const std::string& key,
 
   if (length > 0) CHECK(value != NULL);
 
-  size_t max_value = ((size_t)-1);
-  if (((max_value - 1) / 2) < length) {
-    LOG(ERROR) << __func__ << ": length too long";
-    return false;
-  }
-
   char* str = (char*)osi_calloc(length * 2 + 1);
 
   for (size_t i = 0; i < length; ++i) {
@@ -639,10 +630,6 @@ static void delete_config_files(void) {
 }
 
 static std::string hash_file(const char* filename) {
-  if (!use_key_attestation()) {
-    LOG(INFO) << __func__ << ": Disabled for multi-user";
-    return DISABLED;
-  }
   FILE* fp = fopen(filename, "rb");
   if (!fp) {
     LOG(ERROR) << __func__ << ": unable to open config file: '" << filename
@@ -652,7 +639,7 @@ static std::string hash_file(const char* filename) {
   uint8_t hash[SHA256_DIGEST_LENGTH];
   SHA256_CTX sha256;
   SHA256_Init(&sha256);
-  std::array<std::byte, kBufferSize> buffer;
+  std::array<unsigned char, kBufferSize> buffer;
   int bytes_read = 0;
   while ((bytes_read = fread(buffer.data(), 1, buffer.size(), fp))) {
     SHA256_Update(&sha256, buffer.data(), bytes_read);
@@ -667,31 +654,15 @@ static std::string hash_file(const char* filename) {
 }
 
 static std::string read_checksum_file(const char* checksum_filename) {
-  if (!use_key_attestation()) {
-    LOG(INFO) << __func__ << ": Disabled for multi-user";
-    return DISABLED;
-  }
-  std::string encrypted_hash = checksum_read(checksum_filename);
-  if (encrypted_hash.empty()) {
-    LOG(INFO) << __func__ << ": read empty hash.";
+  // Ensure file exists
+  if (access(checksum_filename, R_OK) != 0) {
     return "";
   }
-  return btif_keystore.Decrypt(encrypted_hash);
+  return btif_keystore.Decrypt(checksum_filename);
 }
 
 static void write_checksum_file(const char* checksum_filename,
                                 const std::string& hash) {
-  if (!use_key_attestation()) {
-    LOG(INFO) << __func__
-              << ": Disabled for multi-user, since config changed removing "
-                 "checksums.";
-    remove(CONFIG_FILE_CHECKSUM_PATH);
-    remove(CONFIG_BACKUP_CHECKSUM_PATH);
-    return;
-  }
-  std::string encrypted_checksum = btif_keystore.Encrypt(hash, 0);
-  CHECK(!encrypted_checksum.empty())
-      << __func__ << ": Failed encrypting checksum";
-  CHECK(checksum_save(encrypted_checksum, checksum_filename))
-      << __func__ << ": Failed to save checksum!";
+  bool result = btif_keystore.Encrypt(hash, checksum_filename, 0);
+  CHECK(result) << "Failed writing checksum";
 }
