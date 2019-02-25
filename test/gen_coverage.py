a58e222ac691b86781618002efcaf331268fb41f
liujinye <liujinye@xiaomi.com>
2019-02-25 19:58:00 +0800

    [automerger skipped] Merge "DO NOT MERGE - Merge pi-dev@5234907 into stage-aosp-master" into stage-aosp-master am: b97284078a -s ours am skip reason: subject contains skip directive
    Change-Id: I4bd6025a21607da5b2ccd79c8948497f48186649

--- test/gen_coverage.py
+++ test/gen_coverage.py
@@ -79,11 +79,6 @@ COVERAGE_TESTS = [
         "covered_files": [
             "system/bt/vendor_libs/test_vendor_lib/packets",
         ],
-    }, {
-        "test_name": "bluetooth_test_common",
-        "covered_files": [
-            "system/bt/common",
-        ],
     },
 ]
 
