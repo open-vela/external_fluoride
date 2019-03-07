125da9d7adf34fd9372a021a553965d2dbb02b47
liujinye <liujinye@xiaomi.com>
2019-03-07 10:50:04 +0800

    DO NOT MERGE Fix length for L2CAP config type EXT FLOW
    Bug: 119870451
    Test: POC
    Change-Id: I11041dd03caad5569e930ff36b50fc9c2719c57f

--- vendor_libs/test_vendor_lib/src/packet_stream.cc
+++ vendor_libs/test_vendor_lib/src/packet_stream.cc
@@ -16,43 +16,45 @@
 
 #define LOG_TAG "packet_stream"
 
-#include "packet_stream.h"
+#include "vendor_libs/test_vendor_lib/include/packet_stream.h"
 
-#include <base/logging.h>
+#include "base/logging.h"
 
+extern "C" {
 #include <errno.h>
 #include <unistd.h>
 
 #include "osi/include/log.h"
-
-using std::vector;
+}  // extern "C"
 
 namespace test_vendor_lib {
 
 std::unique_ptr<CommandPacket> PacketStream::ReceiveCommand(int fd) const {
-  vector<uint8_t> header;
-  vector<uint8_t> params_size;
-  vector<uint8_t> payload;
+  std::vector<uint8_t> header;
+  std::vector<uint8_t> payload;
 
   if (!ReceiveAll(header, CommandPacket::kCommandHeaderSize, fd)) {
     LOG_ERROR(LOG_TAG, "Error: receiving command header.");
     return std::unique_ptr<CommandPacket>(nullptr);
   }
 
-  if (!ReceiveAll(params_size, 1, fd)) {
-    LOG_ERROR(LOG_TAG, "Error: receiving params size.");
-    return std::unique_ptr<CommandPacket>(nullptr);
-  }
-
-  if (!ReceiveAll(payload, params_size[0], fd)) {
+  if (!ReceiveAll(payload, header.back(), fd)) {
     LOG_ERROR(LOG_TAG, "Error: receiving command payload.");
     return std::unique_ptr<CommandPacket>(nullptr);
   }
-  return std::unique_ptr<CommandPacket>(new CommandPacket(header, payload));
+
+  std::unique_ptr<CommandPacket> command(new CommandPacket());
+  if (!command->Encode(header, payload)) {
+    LOG_ERROR(LOG_TAG, "Error: encoding command packet.");
+    command.reset(nullptr);
+  }
+  return command;
 }
 
 serial_data_type_t PacketStream::ReceivePacketType(int fd) const {
-  vector<uint8_t> raw_type_octet;
+  LOG_INFO(LOG_TAG, "Receiving packet type.");
+
+  std::vector<uint8_t> raw_type_octet;
 
   if (!ReceiveAll(raw_type_octet, 1, fd)) {
     // TODO(dennischeng): Proper error handling.
@@ -70,23 +72,23 @@ serial_data_type_t PacketStream::ReceivePacketType(int fd) const {
   return type;
 }
 
-bool PacketStream::SendEvent(std::unique_ptr<EventPacket> event, int fd) const {
-  if (event->GetPayload()[0] != event->GetPayloadSize() - 1)
-    LOG_WARN(LOG_TAG, "Malformed event: 0x%04X, payload size %zu, reported %u",
-             event->GetEventCode(), event->GetPacketSize(),
-             event->GetPayload()[0]);
+bool PacketStream::SendEvent(const EventPacket& event, int fd) const {
+  LOG_INFO(LOG_TAG, "Sending event with event code: 0x%04X",
+           event.GetEventCode());
+  LOG_INFO(LOG_TAG, "Sending event with size: %zu octets",
+           event.GetPacketSize());
 
-  if (!SendAll({static_cast<uint8_t>(event->GetType())}, 1, fd)) {
+  if (!SendAll({static_cast<uint8_t>(event.GetType())}, 1, fd)) {
     LOG_ERROR(LOG_TAG, "Error: Could not send event type.");
     return false;
   }
 
-  if (!SendAll(event->GetHeader(), event->GetHeaderSize(), fd)) {
+  if (!SendAll(event.GetHeader(), event.GetHeaderSize(), fd)) {
     LOG_ERROR(LOG_TAG, "Error: Could not send event header.");
     return false;
   }
 
-  if (!SendAll(event->GetPayload(), event->GetPayloadSize(), fd)) {
+  if (!SendAll(event.GetPayload(), event.GetPayloadSize(), fd)) {
     LOG_ERROR(LOG_TAG, "Error: Could not send event payload.");
     return false;
   }
@@ -94,12 +96,13 @@ bool PacketStream::SendEvent(std::unique_ptr<EventPacket> event, int fd) const {
 }
 
 bool PacketStream::ValidateTypeOctet(serial_data_type_t type) const {
+  LOG_INFO(LOG_TAG, "Signal octet is 0x%02X.", type);
   // The only types of packets that should be received from the HCI are command
   // packets and data packets.
   return (type >= DATA_TYPE_COMMAND) && (type <= DATA_TYPE_SCO);
 }
 
-bool PacketStream::ReceiveAll(vector<uint8_t>& destination,
+bool PacketStream::ReceiveAll(std::vector<uint8_t>& destination,
                               size_t num_octets_to_receive, int fd) const {
   destination.resize(num_octets_to_receive);
   size_t octets_remaining = num_octets_to_receive;
@@ -107,20 +110,22 @@ bool PacketStream::ReceiveAll(vector<uint8_t>& destination,
     const int num_octets_received =
         read(fd, &destination[num_octets_to_receive - octets_remaining],
              octets_remaining);
-    if (num_octets_received < 0) return false;
+    if (num_octets_received < 0)
+      return false;
     octets_remaining -= num_octets_received;
   }
   return true;
 }
 
-bool PacketStream::SendAll(const vector<uint8_t>& source,
+bool PacketStream::SendAll(const std::vector<uint8_t>& source,
                            size_t num_octets_to_send, int fd) const {
   CHECK(source.size() >= num_octets_to_send);
   size_t octets_remaining = num_octets_to_send;
   while (octets_remaining > 0) {
     const int num_octets_sent = write(
         fd, &source[num_octets_to_send - octets_remaining], octets_remaining);
-    if (num_octets_sent < 0) return false;
+    if (num_octets_sent < 0)
+      return false;
     octets_remaining -= num_octets_sent;
   }
   return true;
