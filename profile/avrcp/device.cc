14587b5a307bfda832ae11d6ecf7eb04d2598415
openvela-robot <openvela-robot@xiaomi.com>
2019-03-08 15:57:00 +0800

    DO NOT MERGE - Merge PPRL.190305.001 into stage-aosp-master
    Bug: 127812889
    Change-Id: I433e1bf1f64c8be7187bda2c22a2554a936e4d43

--- profile/avrcp/device.cc
+++ profile/avrcp/device.cc
@@ -13,11 +13,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include "device.h"
 
 #include <base/message_loop/message_loop.h>
 
 #include "connection_handler.h"
-#include "device.h"
+#include "packet/avrcp/avrcp_reject_packet.h"
+#include "packet/avrcp/general_reject_packet.h"
+#include "packet/avrcp/get_play_status_packet.h"
+#include "packet/avrcp/pass_through_packet.h"
+#include "packet/avrcp/set_absolute_volume.h"
+#include "packet/avrcp/set_addressed_player.h"
 #include "stack_config.h"
 
 namespace bluetooth {
@@ -52,10 +58,17 @@ void Device::RegisterInterfaces(MediaInterface* media_interface,
   volume_interface_ = volume_interface;
 }
 
-base::WeakPtr<Device> Device::Get() { return weak_ptr_factory_.GetWeakPtr(); }
+void Device::SetBrowseMtu(uint16_t browse_mtu) {
+  DEVICE_LOG(INFO) << __PRETTY_FUNCTION__ << ": browse_mtu = " << browse_mtu;
+  browse_mtu_ = browse_mtu;
+}
 
 bool Device::IsActive() const {
   return address_ == a2dp_interface_->active_peer();
+}
+
+bool Device::IsInSilenceMode() const {
+  return a2dp_interface_->is_peer_in_silence_mode(address_);
 }
 
 void Device::VendorPacketHandler(uint8_t label,
@@ -457,7 +470,7 @@ void Device::PlaybackPosNotificationResponse(uint8_t label, bool interim,
   // We still try to send updates while music is playing to the non active
   // device even though the device thinks the music is paused. This makes
   // the status bar on the remote device move.
-  if (status.state == PlayState::PLAYING) {
+  if (status.state == PlayState::PLAYING && !IsInSilenceMode()) {
     DEVICE_VLOG(0) << __func__ << ": Queue next play position update";
     play_pos_update_cb_.Reset(base::Bind(&Device::HandlePlayPosUpdate,
                                          weak_ptr_factory_.GetWeakPtr()));
@@ -1041,7 +1054,7 @@ void Device::GetVFSListResponse(uint8_t label,
       // right now we always use folders of mixed type
       FolderItem folder_item(vfs_ids_.get_uid(folder.media_id), 0x00,
                              folder.is_playable, folder.name);
-      builder->AddFolder(folder_item);
+      if (!builder->AddFolder(folder_item)) break;
     } else if (items[i].type == ListItem::SONG) {
       auto song = items[i].song;
       auto title =
@@ -1058,7 +1071,9 @@ void Device::GetVFSListResponse(uint8_t label,
             filter_attributes_requested(song, pkt->GetAttributesRequested());
       }
 
-      builder->AddSong(song_item);
+      // If we fail to add a song, don't accidentally add one later that might
+      // fit.
+      if (!builder->AddSong(song_item)) break;
     }
   }
 
@@ -1091,7 +1106,10 @@ void Device::GetNowPlayingListResponse(
       item.attributes_ =
           filter_attributes_requested(song, pkt->GetAttributesRequested());
     }
-    builder->AddSong(item);
+
+    // If we fail to add a song, don't accidentally add one later that might
+    // fit.
+    if (!builder->AddSong(item)) break;
   }
 
   send_message(label, true, std::move(builder));
@@ -1131,9 +1149,12 @@ void Device::SetBrowsedPlayerResponse(
 }
 
 void Device::SendMediaUpdate(bool metadata, bool play_status, bool queue) {
+  bool is_silence = IsInSilenceMode();
+
   CHECK(media_interface_);
   DEVICE_VLOG(4) << __func__ << ": Metadata=" << metadata
-                 << " : play_status= " << play_status << " : queue=" << queue;
+                 << " : play_status= " << play_status << " : queue=" << queue
+                 << " ; is_silence=" << is_silence;
 
   if (queue) {
     HandleNowPlayingUpdate();
@@ -1141,7 +1162,9 @@ void Device::SendMediaUpdate(bool metadata, bool play_status, bool queue) {
 
   if (play_status) {
     HandlePlayStatusUpdate();
-    HandlePlayPosUpdate();
+    if (!is_silence) {
+      HandlePlayPosUpdate();
+    }
   }
 
   if (metadata) HandleTrackUpdate();
@@ -1284,40 +1307,29 @@ static std::string volumeToStr(int8_t volume) {
 }
 
 std::ostream& operator<<(std::ostream& out, const Device& d) {
-  out << "  " << d.address_.ToString();
+  out << d.address_.ToString();
   if (d.IsActive()) out << " <Active>";
   out << std::endl;
-  out << "    Current Volume: " << volumeToStr(d.volume_) << std::endl;
-  out << "    Current Browsed Player ID: " << d.curr_browsed_player_id_
-      << std::endl;
-  out << "    Registered Notifications: " << std::endl;
-  if (d.track_changed_.first) {
-    out << "      Track Changed" << std::endl;
-  }
-  if (d.play_status_changed_.first) {
-    out << "      Play Status" << std::endl;
-  }
-  if (d.play_pos_changed_.first) {
-    out << "      Play Position" << std::endl;
-  }
-  if (d.now_playing_changed_.first) {
-    out << "      Now Playing" << std::endl;
-  }
-  if (d.addr_player_changed_.first) {
-    out << "      Addressed Player" << std::endl;
-  }
-  if (d.avail_players_changed_.first) {
-    out << "      Available Players" << std::endl;
-  }
-  if (d.uids_changed_.first) {
-    out << "      UIDs Changed" << std::endl;
-  }
 
-  out << "    Last Play State: " << d.last_play_status_.state << std::endl;
-  out << "    Last Song Sent ID: \"" << d.last_song_info_.media_id << "\""
+  ScopedIndent indent(out);
+  out << "Current Volume: " << volumeToStr(d.volume_) << std::endl;
+  out << "Current Browsed Player ID: " << d.curr_browsed_player_id_
       << std::endl;
-  out << "    Current Folder: \"" << d.CurrentFolder() << "\"" << std::endl;
-  out << "    MTU Sizes: CTRL=" << d.ctrl_mtu_ << " BROWSE=" << d.browse_mtu_
+  out << "Registered Notifications:\n";
+  {
+    ScopedIndent indent(out);
+    if (d.track_changed_.first) out << "Track Changed\n";
+    if (d.play_status_changed_.first) out << "Play Status\n";
+    if (d.play_pos_changed_.first) out << "Play Position\n";
+    if (d.now_playing_changed_.first) out << "Now Playing\n";
+    if (d.addr_player_changed_.first) out << "Addressed Player\n";
+    if (d.avail_players_changed_.first) out << "Available Players\n";
+    if (d.uids_changed_.first) out << "UIDs Changed\n";
+  }
+  out << "Last Play State: " << d.last_play_status_.state << std::endl;
+  out << "Last Song Sent ID: \"" << d.last_song_info_.media_id << "\"\n";
+  out << "Current Folder: \"" << d.CurrentFolder() << "\"\n";
+  out << "MTU Sizes: CTRL=" << d.ctrl_mtu_ << " BROWSE=" << d.browse_mtu_
       << std::endl;
   // TODO (apanicke): Add supported features as well as media keys
   return out;
