d778f1fb62c823f002dfcba5a07712bb353328c1
liujinye <liujinye@xiaomi.com>
2019-03-29 12:45:50 +0800

    Fix assert macro logical inversion
    Rename FATAL_WHEN to ASSERT and LOG_FATAL_WHEN to ASSERT_LOG
    
    Test: atest
    Change-Id: I0e6e15ce7cfbb89e76910181f164e8ff32abc033

--- service/ipc/ipc_handler_linux.h
+++ service/ipc/ipc_handler_linux.h
@@ -1,5 +1,5 @@
 //
-//  Copyright (C) 2015 Google, Inc.
+//  Copyright 2015 Google, Inc.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -17,7 +17,6 @@
 #pragma once
 
 #include <atomic>
-
 #include <base/files/file_path.h>
 #include <base/files/scoped_file.h>
 #include <base/macros.h>
