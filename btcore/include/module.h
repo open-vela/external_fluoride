965be661f686cac875bff15a349ae0f0c45b17cc
liujinye <liujinye@xiaomi.com>
2019-04-04 14:57:41 +0800

    DO NOT MERGE: osi: Offload mutex pointer to local scope
    Create a shared_ptr for the callback_mutex in the alarm struct.
    When performing the callback, make a local shared_ptr reference.
    lock_guard on the local shared_ptr reference.
    
    Bug: 117997080
    Test: atest net_test_bluetooth
    Change-Id: Iab800f720f4ccc4735e4d494e0d458eb97b40a4a

--- btcore/include/module.h
+++ btcore/include/module.h
@@ -1,6 +1,6 @@
 /******************************************************************************
  *
- *  Copyright 2014 Google, Inc.
+ *  Copyright (C) 2014 Google, Inc.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -20,7 +20,6 @@
 
 #include <stdbool.h>
 
-#include "common/message_loop_thread.h"
 #include "osi/include/future.h"
 #include "osi/include/thread.h"
 
@@ -64,6 +63,6 @@ void module_clean_up(const module_t* module);
 // has finished, |callback| is called within the context of |callback_thread|
 // with |FUTURE_SUCCESS| or |FUTURE_FAIL| depending on whether startup succeeded
 // or not.
-void module_start_up_callbacked_wrapper(
-    const module_t* module,
-    bluetooth::common::MessageLoopThread* callback_thread, thread_fn callback);
+void module_start_up_callbacked_wrapper(const module_t* module,
+                                        thread_t* callback_thread,
+                                        thread_fn callback);
