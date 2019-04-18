1c6ecf6f187de743fb10bcc7331fd83239a3ba07
liujinye <liujinye@xiaomi.com>
2019-04-18 21:15:08 +0800

    Merge "DO NOT MERGE: osi: Offload mutex pointer to local scope" into qt-dev

--- test/rootcanal/bluetooth_hci.cc
+++ test/rootcanal/bluetooth_hci.cc
@@ -51,9 +51,7 @@ class BluetoothDeathRecipient : public hidl_death_recipient {
  public:
   BluetoothDeathRecipient(const sp<IBluetoothHci> hci) : mHci(hci) {}
 
-  void serviceDied(
-      uint64_t /* cookie */,
-      const wp<::android::hidl::base::V1_0::IBase>& /* who */) override {
+  virtual void serviceDied(uint64_t /* cookie */, const wp<::android::hidl::base::V1_0::IBase>& /* who */) {
     ALOGE("BluetoothDeathRecipient::serviceDied - Bluetooth service died");
     has_died_ = true;
     mHci->close();
