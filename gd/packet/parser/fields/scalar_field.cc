1215af2fcf9a6618bfaeb59dace3fa61c74020ae
openvela-robot <openvela-robot@xiaomi.com>
2019-04-23 15:00:09 +0800

    Root-canal: Add missing payload size field to loopback evt am: 5af185de3e
    Change-Id: I921f8b004ad8cd05d9b1161587a02c0348d9b7d3

--- gd/packet/parser/fields/scalar_field.cc
+++ gd/packet/parser/fields/scalar_field.cc
@@ -33,7 +33,7 @@ std::string ScalarField::GetType() const {
 
 void ScalarField::GenGetter(std::ostream& s, Size start_offset, Size end_offset) const {
   s << GetType();
-  s << " Get" << util::UnderscoreToCamelCase(GetName()) << "() const {";
+  s << " Get" << GetName() << "() const {";
   s << "ASSERT(was_validated_);";
 
   // Write the Getter Function Body
@@ -102,7 +102,7 @@ bool ScalarField::GenBuilderParameter(std::ostream& s) const {
     ERROR(this) << "Not implemented";
   }
   std::string param_type = util::GetTypeForSize(size_);
-  s << param_type << " " << GetName();
+  s << param_type << " " << util::CamelCaseToUnderScore(GetName());
   return true;
 }
 
@@ -116,17 +116,17 @@ void ScalarField::GenParameterValidator(std::ostream& s) const {
   if (util::RoundSizeUp(bits) == bits) {
     return;
   }
-  s << "ASSERT(" << GetName() << " < "
+  s << "ASSERT(" << util::CamelCaseToUnderScore(GetName()) << " < "
     << "(static_cast<uint64_t>(1) << " << bits << "));";
 }
 
 void ScalarField::GenInserter(std::ostream& s) const {
   if (GetSize().bits() == 8) {
-    s << "i.insert_byte(" << GetName() << "_);";
+    s << "i.insert_byte(" << util::CamelCaseToUnderScore(GetName()) << "_);";
   } else if (GetSize().bits() % 8 == 0) {
-    s << "insert(" << GetName() << "_, i);";
+    s << "insert(" << util::CamelCaseToUnderScore(GetName()) << "_, i);";
   } else {
-    s << "insert(" << GetName() << "_, i," << GetSize().bits() << ");";
+    s << "insert(" << util::CamelCaseToUnderScore(GetName()) << "_, i," << GetSize().bits() << ");";
   }
 }
 
