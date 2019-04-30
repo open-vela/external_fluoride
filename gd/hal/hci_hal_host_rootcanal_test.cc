cbd5e3eb57b686f623214e1e1182d46192c18c4a
openvela-robot <openvela-robot@xiaomi.com>
2019-04-30 14:33:37 +0800

    Merge "Create handlers for every module upon module start." am: 37de441d0d am: 0264bf2009 am: e3be06a9c4
    Change-Id: I69b3038c1d4ab31c89f2039a456cb8e8df6d1bf1

--- gd/hal/hci_hal_host_rootcanal_test.cc
+++ gd/hal/hci_hal_host_rootcanal_test.cc
@@ -139,7 +139,8 @@ class HciHalRootcanalTest : public ::testing::Test {
 
     HciHalHostRootcanalConfig::Get()->SetPort(kTestPort);
     fake_server_ = new FakeRootcanalDesktopHciServer;
-    hal_ = fake_registry_.Start<HciHal>(thread_);
+    fake_registry_.Start<HciHal>(thread_);
+    hal_ = fake_registry_.GetInstance<HciHal>();
     hal_->registerIncomingPacketCallback(&callbacks_);
     fake_server_socket_ = fake_server_->Accept();  // accept() after client is connected to avoid blocking
     std::queue<std::pair<uint8_t, HciPacket>> empty;
