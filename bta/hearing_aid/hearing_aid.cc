69a36ca94c29fd7116bd0a57034b1e1092b6462d
openvela-robot <openvela-robot@xiaomi.com>
2019-05-02 21:21:12 +0800

    Merge "Module: Add TestModule for dependency injection"

--- bta/hearing_aid/hearing_aid.cc
+++ bta/hearing_aid/hearing_aid.cc
@@ -1411,6 +1411,8 @@ class HearingAidImpl : public HearingAid {
     LOG(INFO) << "GAP_EVT_CONN_CLOSED: " << hearingDevice->address
               << ", playback_started=" << hearingDevice->playback_started;
 
+    hearingDevice->playback_started = false;
+
     if (hearingDevice->connecting_actively) {
       // cancel pending direct connect
       BTA_GATTC_CancelOpen(gatt_if, address, true);
