58b123f391a31895a9aa9e0ca026d849f34c0571
liujinye <liujinye@xiaomi.com>
2019-05-07 17:24:12 +0800

    Merge "Add benchmark for queue" am: 7ec4793b50 am: d6088efbdc am: c9b2d2c5df
    Change-Id: Idc59a98c9d69e8426c63787444bd0fc0b770430a

--- bta/hearing_aid/hearing_aid.cc
+++ bta/hearing_aid/hearing_aid.cc
@@ -609,7 +609,7 @@ class HearingAidImpl : public HearingAid {
       return;
     }
 
-    const std::vector<gatt::Service>* services = BTA_GATTC_GetServices(conn_id);
+    const std::list<gatt::Service>* services = BTA_GATTC_GetServices(conn_id);
 
     const gatt::Service* service = nullptr;
     for (const gatt::Service& tmp : *services) {
@@ -1422,8 +1422,6 @@ class HearingAidImpl : public HearingAid {
 
     LOG(INFO) << "GAP_EVT_CONN_CLOSED: " << hearingDevice->address
               << ", playback_started=" << hearingDevice->playback_started;
-
-    hearingDevice->playback_started = false;
 
     if (hearingDevice->connecting_actively) {
       // cancel pending direct connect
