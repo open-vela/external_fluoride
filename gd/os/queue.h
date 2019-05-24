9e3b0e349710fadb06591e652671875a704613fc
openvela-robot <openvela-robot@xiaomi.com>
2019-05-24 17:33:26 +0800

    Merge "Turn off the new pass manager for coverage builds"

--- gd/os/queue.h
+++ gd/os/queue.h
@@ -78,11 +78,15 @@ class Queue : public IQueueEnqueue<T>, public IQueueDequeue<T> {
   std::unique_ptr<T> TryDequeue() override;
 
  private:
-  void EnqueueCallbackInternal(EnqueueCallback callback);
+  void EnqueueCallbackInternal();
   // An internal queue that holds at most |capacity| pieces of data
   std::queue<std::unique_ptr<T>> queue_;
   // A mutex that guards data in this queue
   std::mutex mutex_;
+  // Current dequeue callback
+  EnqueueCallback enqueue_callback_;
+  // Current enqueue callback
+  DequeueCallback dequeue_callback_;
 
   class QueueEndpoint {
    public:
