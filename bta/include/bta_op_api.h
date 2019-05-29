4ed55285657c92842564f58b2f00fe59d7a92782
liujinye <liujinye@xiaomi.com>
2019-05-29 12:16:59 +0800

    DO NOT MERGE: btif: require pairing dialog for JustWorks SSP
    Bug: 110433804
    Test: Manual; atest net_test_bluetooth
    Change-Id: I84d50604dee67a01855228c72bb7e8d484de951c

--- bta/include/bta_op_api.h
+++ bta/include/bta_op_api.h
@@ -1,6 +1,6 @@
 /******************************************************************************
  *
- *  Copyright 2003-2012 Broadcom Corporation
+ *  Copyright (C) 2003-2012 Broadcom Corporation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -29,37 +29,39 @@
 #include "bta_api.h"
 
 /*****************************************************************************
- *  Constants and data types
- ****************************************************************************/
+**  Constants and data types
+*****************************************************************************/
 /* Extra Debug Code */
 #ifndef BTA_OPS_DEBUG
-#define BTA_OPS_DEBUG false
+#define BTA_OPS_DEBUG           FALSE
 #endif
 
 #ifndef BTA_OPC_DEBUG
-#define BTA_OPC_DEBUG false
+#define BTA_OPC_DEBUG           FALSE
 #endif
 
-/* Object format */
-#define BTA_OP_VCARD21_FMT 1  /* vCard 2.1 */
-#define BTA_OP_VCARD30_FMT 2  /* vCard 3.0 */
-#define BTA_OP_VCAL_FMT 3     /* vCal 1.0 */
-#define BTA_OP_ICAL_FMT 4     /* iCal 2.0 */
-#define BTA_OP_VNOTE_FMT 5    /* vNote */
-#define BTA_OP_VMSG_FMT 6     /* vMessage */
-#define BTA_OP_OTHER_FMT 0xFF /* other format */
 
-typedef uint8_t tBTA_OP_FMT;
+/* Object format */
+#define BTA_OP_VCARD21_FMT          1       /* vCard 2.1 */
+#define BTA_OP_VCARD30_FMT          2       /* vCard 3.0 */
+#define BTA_OP_VCAL_FMT             3       /* vCal 1.0 */
+#define BTA_OP_ICAL_FMT             4       /* iCal 2.0 */
+#define BTA_OP_VNOTE_FMT            5       /* vNote */
+#define BTA_OP_VMSG_FMT             6       /* vMessage */
+#define BTA_OP_OTHER_FMT            0xFF    /* other format */
+
+typedef UINT8 tBTA_OP_FMT;
 
 /* Object format mask */
-#define BTA_OP_VCARD21_MASK 0x01 /* vCard 2.1 */
-#define BTA_OP_VCARD30_MASK 0x02 /* vCard 3.0 */
-#define BTA_OP_VCAL_MASK 0x04    /* vCal 1.0 */
-#define BTA_OP_ICAL_MASK 0x08    /* iCal 2.0 */
-#define BTA_OP_VNOTE_MASK 0x10   /* vNote */
-#define BTA_OP_VMSG_MASK 0x20    /* vMessage */
-#define BTA_OP_ANY_MASK 0x40     /* Any type of object. */
+#define BTA_OP_VCARD21_MASK         0x01    /* vCard 2.1 */
+#define BTA_OP_VCARD30_MASK         0x02    /* vCard 3.0 */
+#define BTA_OP_VCAL_MASK            0x04    /* vCal 1.0 */
+#define BTA_OP_ICAL_MASK            0x08    /* iCal 2.0 */
+#define BTA_OP_VNOTE_MASK           0x10    /* vNote */
+#define BTA_OP_VMSG_MASK            0x20    /* vMessage */
+#define BTA_OP_ANY_MASK             0x40    /* Any type of object. */
 
-typedef uint8_t tBTA_OP_FMT_MASK;
+typedef UINT8 tBTA_OP_FMT_MASK;
 
 #endif /* BTA_OP_API_H */
+
