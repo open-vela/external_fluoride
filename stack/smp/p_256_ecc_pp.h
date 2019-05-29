ba23f8e41bfd3abf049a1cf59644e765c0b2b56b
liujinye <liujinye@xiaomi.com>
2019-05-29 12:16:59 +0800

    DO NOT MERGE: btif: require pairing dialog for JustWorks SSP
    Bug: 110433804
    Test: Manual; atest net_test_bluetooth
    Change-Id: I84d50604dee67a01855228c72bb7e8d484de951c

--- stack/smp/p_256_ecc_pp.h
+++ stack/smp/p_256_ecc_pp.h
@@ -1,6 +1,6 @@
 /******************************************************************************
  *
- *  Copyright 2006-2015 Broadcom Corporation
+ *  Copyright (C) 2006-2015 Broadcom Corporation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -16,50 +16,53 @@
  *
  ******************************************************************************/
 
-/******************************************************************************
- *
- *  This file contains simple pairing algorithms using Elliptic Curve
- *Cryptography for private public key
- *
- ******************************************************************************/
+ /******************************************************************************
+  *
+  *  This file contains simple pairing algorithms using Elliptic Curve Cryptography for private public key
+  *
+  ******************************************************************************/
 
 #pragma once
 
-#include <cstdbool>
+#include <stdbool.h>
 #include "p_256_multprecision.h"
 
+typedef unsigned long  DWORD;
+
 typedef struct {
-  uint32_t x[KEY_LENGTH_DWORDS_P256];
-  uint32_t y[KEY_LENGTH_DWORDS_P256];
-  uint32_t z[KEY_LENGTH_DWORDS_P256];
+    DWORD x[KEY_LENGTH_DWORDS_P256];
+    DWORD y[KEY_LENGTH_DWORDS_P256];
+    DWORD z[KEY_LENGTH_DWORDS_P256];
 } Point;
 
 typedef struct {
-  // curve's coefficients
-  uint32_t a[KEY_LENGTH_DWORDS_P256];
-  uint32_t b[KEY_LENGTH_DWORDS_P256];
+    // curve's coefficients
+    DWORD a[KEY_LENGTH_DWORDS_P256];
+    DWORD b[KEY_LENGTH_DWORDS_P256];
 
-  // whether a is -3
-  int a_minus3;
+    //whether a is -3
+    int a_minus3;
 
-  // prime modulus
-  uint32_t p[KEY_LENGTH_DWORDS_P256];
+    // prime modulus
+    DWORD p[KEY_LENGTH_DWORDS_P256];
 
-  // Omega, p = 2^m -omega
-  uint32_t omega[KEY_LENGTH_DWORDS_P256];
+    // Omega, p = 2^m -omega
+    DWORD omega[KEY_LENGTH_DWORDS_P256];
 
-  // base point, a point on E of order r
-  Point G;
+    // base point, a point on E of order r
+    Point G;
 
 } elliptic_curve_t;
 
 extern elliptic_curve_t curve;
 extern elliptic_curve_t curve_p256;
 
-bool ECC_ValidatePoint(const Point& p);
+bool ECC_ValidatePoint(const Point* p);
 
-void ECC_PointMult_Bin_NAF(Point* q, Point* p, uint32_t* n);
+void ECC_PointMult_Bin_NAF(Point *q, Point *p, DWORD *n, uint32_t keyLength);
 
-#define ECC_PointMult(q, p, n) ECC_PointMult_Bin_NAF(q, p, n)
+#define ECC_PointMult(q, p, n, keyLength)  ECC_PointMult_Bin_NAF(q, p, n, keyLength)
 
-void p_256_init_curve();
+void p_256_init_curve(UINT32 keyLength);
+
+
--- stack/smp/p_256_multprecision.h
+++ stack/smp/p_256_multprecision.h
@@ -1,6 +1,6 @@
 /******************************************************************************
  *
- *  Copyright 2006-2015 Broadcom Corporation
+ *  Copyright (C) 2006-2015 Broadcom Corporation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -12,46 +12,51 @@
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
- *  limitations under the License. *
+ *  limitations under the License.
+ *
  ******************************************************************************/
 
-/*******************************************************************************
- *
- *  This file contains simple pairing algorithms
- *
- ******************************************************************************/
+ /******************************************************************************
+  *
+  *  This file contains simple pairing algorithms
+  *
+  ******************************************************************************/
 #pragma once
 
 #include "bt_types.h"
 
-#define DWORD_BITS 32
-#define DWORD_BYTES 4
+/* Type definitions */
+typedef unsigned long  DWORD;
+
+#define DWORD_BITS      32
+#define DWORD_BYTES     4
 #define DWORD_BITS_SHIFT 5
 
+#define KEY_LENGTH_DWORDS_P192 6
 #define KEY_LENGTH_DWORDS_P256 8
 /* Arithmetic Operations */
 
-int multiprecision_compare(uint32_t* a, uint32_t* b);
-int multiprecision_iszero(uint32_t* a);
-void multiprecision_init(uint32_t* c);
-void multiprecision_copy(uint32_t* c, uint32_t* a);
-uint32_t multiprecision_dword_bits(uint32_t a);
-uint32_t multiprecision_most_signdwords(uint32_t* a);
-uint32_t multiprecision_most_signbits(uint32_t* a);
-void multiprecision_inv_mod(uint32_t* aminus, uint32_t* a);
-uint32_t multiprecision_add(uint32_t* c, uint32_t* a, uint32_t* b);  // c=a+b
-void multiprecision_add_mod(uint32_t* c, uint32_t* a, uint32_t* b);
-uint32_t multiprecision_sub(uint32_t* c, uint32_t* a, uint32_t* b);  // c=a-b
-void multiprecision_sub_mod(uint32_t* c, uint32_t* a, uint32_t* b);
-void multiprecision_rshift(uint32_t* c, uint32_t* a);  // c=a>>1, return carrier
-void multiprecision_lshift_mod(uint32_t* c,
-                               uint32_t* a);  // c=a<<b, return carrier
-uint32_t multiprecision_lshift(uint32_t* c,
-                               uint32_t* a);  // c=a<<b, return carrier
-void multiprecision_mult(uint32_t* c, uint32_t* a, uint32_t* b);  // c=a*b
-void multiprecision_mersenns_mult_mod(uint32_t* c, uint32_t* a, uint32_t* b);
-void multiprecision_mersenns_squa_mod(uint32_t* c, uint32_t* a);
-uint32_t multiprecision_lshift(uint32_t* c, uint32_t* a);
-void multiprecision_mult(uint32_t* c, uint32_t* a, uint32_t* b);
-void multiprecision_fast_mod(uint32_t* c, uint32_t* a);
-void multiprecision_fast_mod_P256(uint32_t* c, uint32_t* a);
+int multiprecision_compare(DWORD *a, DWORD *b, uint32_t keyLength);
+int multiprecision_iszero(DWORD *a, uint32_t keyLength);
+void multiprecision_init(DWORD *c, uint32_t keyLength);
+void multiprecision_copy(DWORD *c, DWORD *a, uint32_t keyLength);
+UINT32 multiprecision_dword_bits (DWORD a);
+UINT32 multiprecision_most_signdwords(DWORD *a, uint32_t keyLength);
+UINT32 multiprecision_most_signbits(DWORD *a, uint32_t keyLength);
+void multiprecision_inv_mod(DWORD *aminus, DWORD *a, uint32_t keyLength);
+DWORD multiprecision_add(DWORD *c, DWORD *a, DWORD *b, uint32_t keyLength); // c=a+b
+void multiprecision_add_mod(DWORD *c, DWORD *a, DWORD *b, uint32_t keyLength);
+DWORD multiprecision_sub(DWORD *c, DWORD *a, DWORD *b, uint32_t keyLength); // c=a-b
+void multiprecision_sub_mod(DWORD *c, DWORD *a, DWORD *b, uint32_t keyLength);
+void multiprecision_rshift(DWORD * c, DWORD * a, uint32_t keyLength);  // c=a>>1, return carrier
+void multiprecision_lshift_mod(DWORD * c, DWORD * a, uint32_t keyLength); // c=a<<b, return carrier
+DWORD multiprecision_lshift(DWORD * c, DWORD * a, uint32_t keyLength);  // c=a<<b, return carrier
+void multiprecision_mult(DWORD *c, DWORD *a, DWORD *b, uint32_t keyLength); // c=a*b
+void multiprecision_mersenns_mult_mod(DWORD *c, DWORD *a, DWORD *b, uint32_t keyLength);
+void multiprecision_mersenns_squa_mod(DWORD *c, DWORD *a, uint32_t keyLength);
+DWORD multiprecision_lshift(DWORD * c, DWORD * a, uint32_t keyLength);
+void multiprecision_mult(DWORD *c, DWORD *a, DWORD *b, uint32_t keyLength);
+void multiprecision_fast_mod(DWORD *c, DWORD *a);
+void multiprecision_fast_mod_P256(DWORD *c, DWORD *a);
+
+
