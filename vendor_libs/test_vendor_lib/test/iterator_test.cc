65d5af48df3bd06dcc2f3ace06a743b5ef66f104
liujinye <liujinye@xiaomi.com>
2019-06-01 23:03:03 +0800

    Snap for 5626839 from 085b571ed42d160edf214f76aa6a251e8163dfef to qt-qpr1-release
    Change-Id: I91ffd7e06185fd46ab2e07b0cf6401b94bb5645c

--- vendor_libs/test_vendor_lib/test/iterator_test.cc
+++ vendor_libs/test_vendor_lib/test/iterator_test.cc
@@ -46,13 +46,15 @@ class TestPacket : public HciPacket {
 class IteratorTest : public ::testing::Test {
  public:
   IteratorTest() = default;
-  ~IteratorTest() override = default;
+  ~IteratorTest() = default;
 
-  void SetUp() override {
+  void SetUp() {
     packet = TestPacket::make_new_packet(complete_l2cap_packet);
   }
 
-  void TearDown() override { packet.reset(); }
+  void TearDown() {
+    packet.reset();
+  }
 
   std::shared_ptr<TestPacket> packet;
 };
