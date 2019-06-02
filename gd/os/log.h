65d15b9f79e87499688d253d55550c3374c7be30
liujinye <liujinye@xiaomi.com>
2019-06-02 22:06:22 +0800

    [automerger skipped] [automerger] DO NOT MERGE: btif: require pairing dialog for JustWorks SSP am: 042fe6394c am: c80a6ed6e6 am: 6097686fc7 am: 0433d94f23 skipped: 4e06b9023f am: f9ec56dffb am: 364dd10aed am: 89f3f95225 -s ours am: bbaa32c57d -s ours am: c3fed5d94d
    Change-Id: I74ff2670c472b66151ed246f3302b1541d9174d9

--- gd/os/log.h
+++ gd/os/log.h
@@ -18,8 +18,6 @@
 
 #pragma once
 
-#include <stdlib.h>
-
 #ifndef LOG_TAG
 #define LOG_TAG "bt"
 #endif
@@ -39,7 +37,8 @@
 /* syslog didn't work well here since we would be redefining LOG_DEBUG. */
 #include <stdio.h>
 
-#define LOGWRAPPER(fmt, args...) fprintf(stderr, "%s - %s: " fmt "\n", LOG_TAG, __PRETTY_FUNCTION__, ##args)
+#define LOGWRAPPER(fmt, args...) \
+  fprintf(stderr, "%s - %s: " fmt "\n", LOG_TAG, __PRETTY_FUNCTION__, ##args)
 
 #define LOG_VERBOSE(...) LOGWRAPPER(__VA_ARGS__)
 #define LOG_DEBUG(...) LOGWRAPPER(__VA_ARGS__)
@@ -49,18 +48,15 @@
 
 #endif /* defined(OS_ANDROID) */
 
-#define ASSERT(condition)                                                       \
-  do {                                                                          \
-    if (!(condition)) {                                                         \
-      LOG_ERROR("%s:%d assertion '" #condition "' failed", __FILE__, __LINE__); \
-      abort();                                                                  \
-    }                                                                           \
-  } while (false)
+#define ASSERT(condition) \
+  if (!(condition)) { \
+    LOG_ERROR("%s:%d assertion '" #condition "' failed", __FILE__, __LINE__); \
+    abort(); \
+  }
 
-#define ASSERT_LOG(condition, fmt, args...)                                                    \
-  do {                                                                                         \
-    if (!(condition)) {                                                                        \
-      LOG_ERROR("%s:%d assertion '" #condition "' failed - " fmt, __FILE__, __LINE__, ##args); \
-      abort();                                                                                 \
-    }                                                                                          \
-  } while (false)
+#define ASSERT_LOG(condition, fmt, args...) \
+  if (!(condition)) { \
+    LOG_ERROR("%s:%d assertion '" #condition "' failed - " fmt, __FILE__, __LINE__, ##args); \
+    abort(); \
+  }
+
