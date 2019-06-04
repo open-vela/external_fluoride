4bfa0277f59d774b6ac067e4ab9dffd7b908bf12
liujinye <liujinye@xiaomi.com>
2019-06-04 20:05:29 +0800

    [automerger skipped] [automerger] DO NOT MERGE Separate SDP procedure from bonding state (1/2) am: 553eb90719 am: fffbd1d3b0 am: db47d7e348 am: 6976e5f214 skipped: 575b8c810b am: 5b7685e7a2 am: f4da01925c am: 0a1f845e2a -s ours am: 840a9ba63e -s ours am: 36c6a3069a
    Change-Id: I55f77ba77a635ca91d384885641d0180562372dd

--- bta/hearing_aid/hearing_aid.cc
+++ bta/hearing_aid/hearing_aid.cc
@@ -232,7 +232,7 @@ class HearingAidImpl : public HearingAid {
   uint16_t overwrite_min_ce_len;
 
  public:
-  virtual ~HearingAidImpl() = default;
+  ~HearingAidImpl() override = default;
 
   HearingAidImpl(bluetooth::hearing_aid::HearingAidCallbacks* callbacks,
                  Closure initCb)
@@ -1141,6 +1141,7 @@ class HearingAidImpl : public HearingAid {
 
     if (left == nullptr && right == nullptr) {
       HearingAidAudioSource::Stop();
+      audio_running = false;
       encoder_state_release();
       current_volume = VOLUME_UNKNOWN;
       return;
