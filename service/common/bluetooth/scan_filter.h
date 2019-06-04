64b5bc319b936380465d4eddb9cde3037710f76b
liujinye <liujinye@xiaomi.com>
2019-06-04 16:39:19 +0800

    Merge "DO NOT MERGE Separate SDP procedure from bonding state (1/2)" into nyc-dev

--- service/common/bluetooth/scan_filter.h
+++ service/common/bluetooth/scan_filter.h
@@ -27,7 +27,7 @@ namespace bluetooth {
 class ScanFilter {
  public:
   ScanFilter() = default;
-  virtual ~ScanFilter() = default;
+  ~ScanFilter() = default;
 
   // Copy constructor and assignment operator.
   ScanFilter(const ScanFilter& other);
@@ -64,7 +64,7 @@ class ScanFilter {
   // Comparison operator.
   bool operator==(const ScanFilter& rhs) const;
 
- protected:
+ private:
   std::string device_name_;
   std::string device_address_;
 
