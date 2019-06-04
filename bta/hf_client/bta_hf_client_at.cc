db1a65bde135075d88cfa6776eecde8b7265ad24
liujinye <liujinye@xiaomi.com>
2019-06-04 20:44:47 +0800

    DO NOT MERGE Separate SDP procedure from bonding state (1/2) am: 5240e71881
    Change-Id: I12aa00ba24326d58ce73f15b6ddc82f22201945e

--- bta/hf_client/bta_hf_client_at.cc
+++ bta/hf_client/bta_hf_client_at.cc
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  *  Copyright (c) 2014 The Android Open Source Project
- *  Copyright 2003-2012 Broadcom Corporation
+ *  Copyright (C) 2003-2012 Broadcom Corporation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -40,6 +40,13 @@
 
 /* timeout (in milliseconds) for AT hold timer */
 #define BTA_HF_CLIENT_AT_HOLD_TIMEOUT 41
+
+/******************************************************************************
+ *
+ *          DATA TYPES AND CONTAINERS
+ *
+ ******************************************************************************/
+extern fixed_queue_t* btu_bta_alarm_queue;
 
 /******************************************************************************
  *       SUPPORTED EVENT MESSAGES
@@ -157,8 +164,9 @@ static void bta_hf_client_at_resp_timer_cback(void* data) {
 }
 
 static void bta_hf_client_start_at_resp_timer(tBTA_HF_CLIENT_CB* client_cb) {
-  alarm_set_on_mloop(client_cb->at_cb.resp_timer, BTA_HF_CLIENT_AT_TIMEOUT,
-                     bta_hf_client_at_resp_timer_cback, (void*)client_cb);
+  alarm_set_on_queue(client_cb->at_cb.resp_timer, BTA_HF_CLIENT_AT_TIMEOUT,
+                     bta_hf_client_at_resp_timer_cback, (void*)client_cb,
+                     btu_bta_alarm_queue);
 }
 
 static void bta_hf_client_stop_at_resp_timer(tBTA_HF_CLIENT_CB* client_cb) {
@@ -170,7 +178,7 @@ static void bta_hf_client_send_at(tBTA_HF_CLIENT_CB* client_cb,
                                   uint16_t buf_len) {
   APPL_TRACE_DEBUG("%s", __func__);
   if ((client_cb->at_cb.current_cmd == BTA_HF_CLIENT_AT_NONE ||
-       !client_cb->svc_conn) &&
+       client_cb->svc_conn == false) &&
       !alarm_is_scheduled(client_cb->at_cb.hold_timer)) {
     uint16_t len;
 
@@ -226,8 +234,9 @@ static void bta_hf_client_stop_at_hold_timer(tBTA_HF_CLIENT_CB* client_cb) {
 
 static void bta_hf_client_start_at_hold_timer(tBTA_HF_CLIENT_CB* client_cb) {
   APPL_TRACE_DEBUG("%s", __func__);
-  alarm_set_on_mloop(client_cb->at_cb.hold_timer, BTA_HF_CLIENT_AT_HOLD_TIMEOUT,
-                     bta_hf_client_at_hold_timer_cback, (void*)client_cb);
+  alarm_set_on_queue(client_cb->at_cb.hold_timer, BTA_HF_CLIENT_AT_HOLD_TIMEOUT,
+                     bta_hf_client_at_hold_timer_cback, (void*)client_cb,
+                     btu_bta_alarm_queue);
 }
 
 /******************************************************************************
@@ -257,7 +266,7 @@ static void bta_hf_client_handle_ok(tBTA_HF_CLIENT_CB* client_cb) {
       client_cb->at_cb.current_cmd = BTA_HF_CLIENT_AT_NONE;
       return;
     case BTA_HF_CLIENT_AT_CLIP:  // last cmd is post slc seq
-      if (!client_cb->send_at_reply) {
+      if (client_cb->send_at_reply == false) {
         client_cb->send_at_reply = true;
       }
       break;
@@ -296,7 +305,7 @@ static void bta_hf_client_handle_error(tBTA_HF_CLIENT_CB* client_cb,
       bta_hf_client_cback_sco(client_cb, BTA_HF_CLIENT_AUDIO_CLOSE_EVT);
       break;
     case BTA_HF_CLIENT_AT_CLIP:  // last cmd is post slc seq
-      if (!client_cb->send_at_reply) {
+      if (client_cb->send_at_reply == false) {
         client_cb->send_at_reply = true;
       }
       break;
@@ -564,7 +573,7 @@ void bta_hf_client_ind(tBTA_HF_CLIENT_CB* client_cb,
   evt.ind.type = type;
   evt.ind.value = value;
 
-  evt.ind.bd_addr = client_cb->peer_addr;
+  bdcpy(evt.ind.bd_addr, client_cb->peer_addr);
   bta_hf_client_app_callback(BTA_HF_CLIENT_IND_EVT, &evt);
 }
 
@@ -585,7 +594,7 @@ void bta_hf_client_evt_val(tBTA_HF_CLIENT_CB* client_cb,
 
   memset(&evt, 0, sizeof(evt));
 
-  evt.val.bd_addr = client_cb->peer_addr;
+  bdcpy(evt.val.bd_addr, client_cb->peer_addr);
   evt.val.value = value;
 
   bta_hf_client_app_callback(type, &evt);
@@ -609,7 +618,7 @@ void bta_hf_client_operator_name(tBTA_HF_CLIENT_CB* client_cb, char* name) {
   strlcpy(evt.operator_name.name, name, BTA_HF_CLIENT_OPERATOR_NAME_LEN + 1);
   evt.operator_name.name[BTA_HF_CLIENT_OPERATOR_NAME_LEN] = '\0';
 
-  evt.operator_name.bd_addr = client_cb->peer_addr;
+  bdcpy(evt.operator_name.bd_addr, client_cb->peer_addr);
   bta_hf_client_app_callback(BTA_HF_CLIENT_OPERATOR_NAME_EVT, &evt);
 }
 
@@ -631,7 +640,7 @@ void bta_hf_client_clip(tBTA_HF_CLIENT_CB* client_cb, char* number) {
   strlcpy(evt.number.number, number, BTA_HF_CLIENT_NUMBER_LEN + 1);
   evt.number.number[BTA_HF_CLIENT_NUMBER_LEN] = '\0';
 
-  evt.number.bd_addr = client_cb->peer_addr;
+  bdcpy(evt.number.bd_addr, client_cb->peer_addr);
   bta_hf_client_app_callback(BTA_HF_CLIENT_CLIP_EVT, &evt);
 }
 
@@ -653,7 +662,7 @@ void bta_hf_client_ccwa(tBTA_HF_CLIENT_CB* client_cb, char* number) {
   strlcpy(evt.number.number, number, BTA_HF_CLIENT_NUMBER_LEN + 1);
   evt.number.number[BTA_HF_CLIENT_NUMBER_LEN] = '\0';
 
-  evt.number.bd_addr = client_cb->peer_addr;
+  bdcpy(evt.number.bd_addr, client_cb->peer_addr);
   bta_hf_client_app_callback(BTA_HF_CLIENT_CCWA_EVT, &evt);
 }
 
@@ -676,7 +685,7 @@ void bta_hf_client_at_result(tBTA_HF_CLIENT_CB* client_cb,
   evt.result.type = type;
   evt.result.cme = cme;
 
-  evt.result.bd_addr = client_cb->peer_addr;
+  bdcpy(evt.result.bd_addr, client_cb->peer_addr);
   bta_hf_client_app_callback(BTA_HF_CLIENT_AT_RESULT_EVT, &evt);
 }
 
@@ -708,7 +717,7 @@ void bta_hf_client_clcc(tBTA_HF_CLIENT_CB* client_cb, uint32_t idx,
     evt.clcc.number[BTA_HF_CLIENT_NUMBER_LEN] = '\0';
   }
 
-  evt.clcc.bd_addr = client_cb->peer_addr;
+  bdcpy(evt.clcc.bd_addr, client_cb->peer_addr);
   bta_hf_client_app_callback(BTA_HF_CLIENT_CLCC_EVT, &evt);
 }
 
@@ -732,7 +741,7 @@ void bta_hf_client_cnum(tBTA_HF_CLIENT_CB* client_cb, char* number,
   strlcpy(evt.cnum.number, number, BTA_HF_CLIENT_NUMBER_LEN + 1);
   evt.cnum.number[BTA_HF_CLIENT_NUMBER_LEN] = '\0';
 
-  evt.cnum.bd_addr = client_cb->peer_addr;
+  bdcpy(evt.cnum.bd_addr, client_cb->peer_addr);
   bta_hf_client_app_callback(BTA_HF_CLIENT_CNUM_EVT, &evt);
 }
 
@@ -754,7 +763,7 @@ void bta_hf_client_binp(tBTA_HF_CLIENT_CB* client_cb, char* number) {
   strlcpy(evt.number.number, number, BTA_HF_CLIENT_NUMBER_LEN + 1);
   evt.number.number[BTA_HF_CLIENT_NUMBER_LEN] = '\0';
 
-  evt.number.bd_addr = client_cb->peer_addr;
+  bdcpy(evt.number.bd_addr, client_cb->peer_addr);
   bta_hf_client_app_callback(BTA_HF_CLIENT_BINP_EVT, &evt);
 }
 
@@ -1587,7 +1596,7 @@ void bta_hf_client_at_parse(tBTA_HF_CLIENT_CB* client_cb, char* buf,
     client_cb->at_cb.offset += space_left;
 
     /* find end of last complete command before proceeding */
-    while (!bta_hf_client_check_at_complete(client_cb)) {
+    while (bta_hf_client_check_at_complete(client_cb) == false) {
       if (client_cb->at_cb.offset == 0) {
         APPL_TRACE_ERROR("HFPClient: AT parser buffer overrun, disconnecting");
 
@@ -1620,7 +1629,7 @@ void bta_hf_client_at_parse(tBTA_HF_CLIENT_CB* client_cb, char* buf,
   client_cb->at_cb.offset += len;
 
   /* If last event is complete, parsing can be started */
-  if (bta_hf_client_check_at_complete(client_cb)) {
+  if (bta_hf_client_check_at_complete(client_cb) == true) {
     bta_hf_client_at_parse_start(client_cb);
     bta_hf_client_at_clear_buf(client_cb);
   }
@@ -1886,7 +1895,7 @@ void bta_hf_client_send_at_btrh(tBTA_HF_CLIENT_CB* client_cb, bool query,
 
   APPL_TRACE_DEBUG("%s", __func__);
 
-  if (query) {
+  if (query == true) {
     at_len = snprintf(buf, sizeof(buf), "AT+BTRH?\r");
   } else {
     at_len = snprintf(buf, sizeof(buf), "AT+BTRH=%u\r", val);
@@ -1924,6 +1933,11 @@ void bta_hf_client_send_at_bcc(tBTA_HF_CLIENT_CB* client_cb) {
   buf = "AT+BCC\r";
 
   bta_hf_client_send_at(client_cb, BTA_HF_CLIENT_AT_BCC, buf, strlen(buf));
+
+  // At this point we should also open up an incoming SCO connection
+  tBTA_HF_CLIENT_DATA p_data;
+  p_data.hdr.layer_specific = client_cb->handle;
+  bta_hf_client_sco_listen(&p_data);
 }
 
 void bta_hf_client_send_at_cnum(tBTA_HF_CLIENT_CB* client_cb) {
--- btif/test/btif_profile_queue_test.cc
+++ btif/test/btif_profile_queue_test.cc
@@ -1,6 +1,6 @@
 /******************************************************************************
  *
- *  Copyright 2017 Google, Inc.
+ *  Copyright (C) 2017 Google, Inc.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -15,32 +15,28 @@
  *  limitations under the License.
  *
  ******************************************************************************/
-#include "btif/include/btif_profile_queue.h"
-
 #include <gtest/gtest.h>
 
-#include <base/bind.h>
-#include <base/callback.h>
-#include <base/location.h>
-
+#include "btif/include/btif_profile_queue.h"
 #include "stack_manager.h"
-#include "types/raw_address.h"
+
+static bool sStackRunning;
+
+bool get_stack_is_running(void) { return sStackRunning; }
+
+static stack_manager_t sStackManager = {nullptr, nullptr, nullptr, nullptr,
+                                        get_stack_is_running};
+
+const stack_manager_t* stack_manager_get_interface() { return &sStackManager; }
 
 typedef void(tBTIF_CBACK)(uint16_t event, char* p_param);
 typedef void(tBTIF_COPY_CBACK)(uint16_t event, char* p_dest, char* p_src);
-
-// NOTE: Local re-implementation of functions to avoid thread context switching
-static bool sStackRunning;
-bool get_stack_is_running(void) { return sStackRunning; }
-static stack_manager_t sStackManager = {nullptr, nullptr, nullptr, nullptr,
-                                        get_stack_is_running};
-const stack_manager_t* stack_manager_get_interface() { return &sStackManager; }
-bt_status_t do_in_jni_thread(const base::Location& from_here,
-                             base::OnceClosure task) {
-  std::move(task).Run();
+bt_status_t btif_transfer_context(tBTIF_CBACK* p_cback, uint16_t event,
+                                  char* p_params, int param_len,
+                                  tBTIF_COPY_CBACK* p_copy_cback) {
+  p_cback(event, p_params);
   return BT_STATUS_SUCCESS;
 }
-bool is_on_jni_thread() { return true; }
 
 enum ResultType {
   NOT_SET = 0,
@@ -57,8 +53,8 @@ class BtifProfileQueueTest : public ::testing::Test {
  public:
   static const uint16_t kTestUuid1 = 0x9527;
   static const uint16_t kTestUuid2 = 0x819F;
-  static const RawAddress kTestAddr1;
-  static const RawAddress kTestAddr2;
+  static const bt_bdaddr_t kTestAddr1;
+  static const bt_bdaddr_t kTestAddr2;
 
  protected:
   void SetUp() override {
@@ -68,20 +64,21 @@ class BtifProfileQueueTest : public ::testing::Test {
   void TearDown() override { btif_queue_release(); };
 };
 
-const RawAddress BtifProfileQueueTest::kTestAddr1{
+const bt_bdaddr_t BtifProfileQueueTest::kTestAddr1{
     {0x11, 0x22, 0x33, 0x44, 0x55, 0x66}};
-const RawAddress BtifProfileQueueTest::kTestAddr2{
+const bt_bdaddr_t BtifProfileQueueTest::kTestAddr2{
     {0xAB, 0xCD, 0xEF, 0x12, 0x34, 0x56}};
 
-static bt_status_t test_connect_cb(RawAddress* bda, uint16_t uuid) {
+static bt_status_t test_connect_cb(bt_bdaddr_t* bda, uint16_t uuid) {
   sResult = UNKNOWN;
-  if (*bda == BtifProfileQueueTest::kTestAddr1) {
+  if (!memcmp(bda, &BtifProfileQueueTest::kTestAddr1, sizeof(bt_bdaddr_t))) {
     if (uuid == BtifProfileQueueTest::kTestUuid1) {
       sResult = UUID1_ADDR1;
     } else if (uuid == BtifProfileQueueTest::kTestUuid2) {
       sResult = UUID2_ADDR1;
     }
-  } else if (*bda == BtifProfileQueueTest::kTestAddr2) {
+  } else if (!memcmp(bda, &BtifProfileQueueTest::kTestAddr2,
+                     sizeof(bt_bdaddr_t))) {
     if (uuid == BtifProfileQueueTest::kTestUuid1) {
       sResult = UUID1_ADDR2;
     } else if (uuid == BtifProfileQueueTest::kTestUuid2) {
@@ -133,30 +130,14 @@ TEST_F(BtifProfileQueueTest, test_multiple_connects_without_advance) {
   sResult = NOT_SET;
   btif_queue_connect(kTestUuid2, &kTestAddr1, test_connect_cb);
   EXPECT_EQ(sResult, NOT_SET);
-  // Third item for same UUID1, but different address ADDR2
   sResult = NOT_SET;
-  btif_queue_connect(kTestUuid1, &kTestAddr2, test_connect_cb);
-  EXPECT_EQ(sResult, NOT_SET);
-  // Fourth item for same UUID2, but different address ADDR2
-  sResult = NOT_SET;
-  btif_queue_connect(kTestUuid2, &kTestAddr2, test_connect_cb);
-  EXPECT_EQ(sResult, NOT_SET);
   // Connect next doesn't work
-  sResult = NOT_SET;
   btif_queue_connect_next();
   EXPECT_EQ(sResult, NOT_SET);
-  // Advance moves queue to execute second item
+  // Advance moves queue to execute next item
   sResult = NOT_SET;
   btif_queue_advance();
   EXPECT_EQ(sResult, UUID2_ADDR1);
-  // Advance moves queue to execute third item
-  sResult = NOT_SET;
-  btif_queue_advance();
-  EXPECT_EQ(sResult, UUID1_ADDR2);
-  // Advance moves queue to execute fourth item
-  sResult = NOT_SET;
-  btif_queue_advance();
-  EXPECT_EQ(sResult, UUID2_ADDR2);
 }
 
 TEST_F(BtifProfileQueueTest, test_cleanup_first_allow_second) {
