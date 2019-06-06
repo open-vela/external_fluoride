f6fb015bbf87906fc5caec31d7b8a5008f0df824
liujinye <liujinye@xiaomi.com>
2019-06-07 03:28:13 +0800

    [automerger skipped] DO NOT MERGE Fix for Bluetooth connection being dropped after HCI Read Encryption Key Size am: 461f0b4739 -s ours am: 35f2cb15f1 -s ours am: 75384547fb -s ours am: dd59bd4400 -s ours am: 82398ca371 -s ours am skip reason: change_id Iab0fd9f357d18a6b048b971d0393fbb47fd4d793 with SHA1 c887dd6ed1 is in history
    Change-Id: I52a7a78da40b378f0b20112f759830f75c834813

--- gd/os/linux_generic/repeating_alarm.cc
+++ gd/os/linux_generic/repeating_alarm.cc
@@ -32,14 +32,16 @@
 namespace bluetooth {
 namespace os {
 
-RepeatingAlarm::RepeatingAlarm(Handler* handler) : handler_(handler), fd_(timerfd_create(ALARM_CLOCK, 0)) {
+RepeatingAlarm::RepeatingAlarm(Thread* thread)
+  : thread_(thread),
+    fd_(timerfd_create(ALARM_CLOCK, 0)) {
   ASSERT(fd_ != -1);
 
-  token_ = handler_->thread_->GetReactor()->Register(fd_, [this] { on_fire(); }, nullptr);
+  token_ = thread_->GetReactor()->Register(fd_, [this] { on_fire(); }, nullptr);
 }
 
 RepeatingAlarm::~RepeatingAlarm() {
-  handler_->thread_->GetReactor()->Unregister(token_);
+  thread_->GetReactor()->Unregister(token_);
 
   int close_status;
   RUN_NO_INTR(close_status = close(fd_));
--- hci/src/hci_layer_android.cc
+++ hci/src/hci_layer_android.cc
@@ -83,25 +83,25 @@ class BluetoothHciCallbacks : public IBluetoothHciCallbacks {
     return packet;
   }
 
-  Return<void> initializationComplete(Status status) override {
+  Return<void> initializationComplete(Status status) {
     CHECK(status == Status::SUCCESS);
     initialization_complete();
     return Void();
   }
 
-  Return<void> hciEventReceived(const hidl_vec<uint8_t>& event) override {
+  Return<void> hciEventReceived(const hidl_vec<uint8_t>& event) {
     BT_HDR* packet = WrapPacketAndCopy(MSG_HC_TO_STACK_HCI_EVT, event);
     hci_event_received(FROM_HERE, packet);
     return Void();
   }
 
-  Return<void> aclDataReceived(const hidl_vec<uint8_t>& data) override {
+  Return<void> aclDataReceived(const hidl_vec<uint8_t>& data) {
     BT_HDR* packet = WrapPacketAndCopy(MSG_HC_TO_STACK_HCI_ACL, data);
     acl_event_received(packet);
     return Void();
   }
 
-  Return<void> scoDataReceived(const hidl_vec<uint8_t>& data) override {
+  Return<void> scoDataReceived(const hidl_vec<uint8_t>& data) {
     BT_HDR* packet = WrapPacketAndCopy(MSG_HC_TO_STACK_HCI_SCO, data);
     sco_data_received(packet);
     return Void();
