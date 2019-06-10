3de4e436316ce11fdcec38ffab79875643e8cd6e
liujinye <liujinye@xiaomi.com>
2019-06-10 18:43:21 +0800

    OS: Don't hold the lock while the callback runs am: 5831f4165c am: 5f46294fc0 am: 686d8189a8 am: aa19aa6d94
    Change-Id: I51221b5f65aefd808cd805aa0111d568220de540

--- gd/os/linux_generic/handler_unittest.cc
+++ gd/os/linux_generic/handler_unittest.cc
@@ -17,12 +17,9 @@
 #include "os/handler.h"
 
 #include <sys/eventfd.h>
-
-#include <future>
 #include <thread>
 
 #include "gtest/gtest.h"
-#include "os/log.h"
 
 namespace bluetooth {
 namespace os {
@@ -47,31 +44,27 @@ TEST_F(HandlerTest, empty) {}
 
 TEST_F(HandlerTest, post_task_invoked) {
   int val = 0;
-  std::promise<void> closure_ran;
-  Closure closure = [&val, &closure_ran]() {
-    val++;
-    closure_ran.set_value();
-  };
+  Closure closure = [&val]() { val++; };
   handler_->Post(closure);
-  closure_ran.get_future().wait();
-  ASSERT_EQ(val, 1);
+  std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  EXPECT_EQ(val, 1);
 }
 
 TEST_F(HandlerTest, post_task_cleared) {
   int val = 0;
-  std::promise<void> closure_started;
-  std::promise<void> closure_can_continue;
-  auto can_continue_future = closure_can_continue.get_future();
-  handler_->Post([&val, &can_continue_future, &closure_started]() {
-    closure_started.set_value();
+  Closure closure = [&val]() {
     val++;
-    can_continue_future.wait();
-  });
-  handler_->Post([]() { ASSERT_TRUE(false); });
-  closure_started.get_future().wait();
+    std::this_thread::sleep_for(std::chrono::milliseconds(5));
+  };
+  handler_->Post(std::move(closure));
+  closure = []() {
+    ASSERT_TRUE(false);
+  };
+  std::this_thread::sleep_for(std::chrono::milliseconds(5));
+  handler_->Post(std::move(closure));
   handler_->Clear();
-  closure_can_continue.set_value();
-  ASSERT_EQ(val, 1);
+  std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  EXPECT_EQ(val, 1);
 }
 
 }  // namespace
