7a788f32cf4b44703ad43b89d98d95b95b626cb5
liujinye <liujinye@xiaomi.com>
2019-06-22 03:15:04 +0800

    Snap for 5680585 from 3de0adecc11d24b3d8cc6cd706cd64a4f26544f9 to rvc-release
    Change-Id: Ic66d2a6c463278ddcfb7c6b4250783865762478c

--- vendor_libs/test_vendor_lib/model/controller/link_layer_controller.cc
+++ vendor_libs/test_vendor_lib/model/controller/link_layer_controller.cc
@@ -951,7 +951,7 @@ hci::Status LinkLayerController::RejectConnectionRequest(const Address& addr, ui
 }
 
 void LinkLayerController::RejectSlaveConnection(const Address& addr, uint8_t reason) {
-  CHECK(reason >= 0x0d && reason <= 0x0f);
+  CHECK(reason > 0x0f || reason < 0x0d);
   send_event_(EventPacketBuilder::CreateConnectionCompleteEvent(static_cast<hci::Status>(reason), 0xeff, addr,
                                                                 hci::LinkType::ACL, false)
                   ->ToVector());
