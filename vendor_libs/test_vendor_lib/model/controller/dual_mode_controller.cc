fde067cb82050f3e92cbcee9d9da4b465828bdc3
liujinye <liujinye@xiaomi.com>
2019-07-04 08:00:23 +0800

    Merge "Hearing Aid: set BLE phy to 2M if local and peer indicate PHY 2M support"

--- vendor_libs/test_vendor_lib/model/controller/dual_mode_controller.cc
+++ vendor_libs/test_vendor_lib/model/controller/dual_mode_controller.cc
@@ -60,7 +60,11 @@ void DualModeController::Initialize(const std::vector<std::string>& args) {
   if (args.size() < 2) return;
 
   Address addr;
-  if (Address::FromString(args[1], addr)) properties_.SetAddress(addr);
+  if (Address::FromString(args[1], addr)) {
+    properties_.SetAddress(addr);
+  } else {
+    LOG_FATAL(LOG_TAG, "Invalid address: %s", args[1].c_str());
+  }
 };
 
 std::string DualModeController::GetTypeString() const {
@@ -305,6 +309,9 @@ void DualModeController::RegisterScoChannel(
 void DualModeController::HciReset(packets::PacketView<true> args) {
   CHECK(args.size() == 0) << __func__ << " size=" << args.size();
   link_layer_controller_.Reset();
+  if (loopback_mode_ == hci::LoopbackMode::LOCAL) {
+    loopback_mode_ = hci::LoopbackMode::NO;
+  }
 
   SendCommandCompleteSuccess(OpCode::RESET);
 }
