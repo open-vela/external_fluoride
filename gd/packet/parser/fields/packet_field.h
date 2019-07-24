0c95fea3af187c8883334897e27aac7eb36082ce
liujinye <liujinye@xiaomi.com>
2019-07-24 19:03:16 +0800

    Store BLE keys using the address from the ble_auth_cmpl_evt am: 311ebcedb3
    Change-Id: I6a6c61496b1f98618c22585681b147b3ce0d9740

--- gd/packet/parser/fields/packet_field.h
+++ gd/packet/parser/fields/packet_field.h
@@ -59,7 +59,6 @@ class PacketField : public Loggable {
   // Generate the parameter for Create(), return true if a parameter was added.
   virtual bool GenBuilderParameter(std::ostream& s) const = 0;
 
-  // Generate the actual storage for the parameter, return true if it was added.
   virtual bool GenBuilderMember(std::ostream& s) const;
 
   // Returns whether or not the field must be validated.
