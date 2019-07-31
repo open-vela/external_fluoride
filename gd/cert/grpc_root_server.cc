cb79eaf740a22d1897d513c093ac7aef57080faf
liujinye <liujinye@xiaomi.com>
2019-08-01 03:09:16 +0800

    Snap for 5771744 from 29ae5840631a66b6bfe8d234095bf531149ce8b2 to rvc-release
    Change-Id: I6b0b227279e196fb4626201bbbc40893eb3bbc7f

--- gd/cert/grpc_root_server.cc
+++ gd/cert/grpc_root_server.cc
@@ -22,7 +22,6 @@
 #include "grpc/grpc_module.h"
 #include "hal/cert/cert.h"
 #include "hci/cert/cert.h"
-#include "l2cap/cert/cert.h"
 #include "os/log.h"
 #include "os/thread.h"
 #include "stack_manager.h"
@@ -54,9 +53,6 @@ class RootCertService : public ::bluetooth::cert::RootCert::Service {
         break;
       case BluetoothModule::HCI:
         modules.add<::bluetooth::hci::cert::AclManagerCertModule>();
-        break;
-      case BluetoothModule::L2CAP:
-        modules.add<::bluetooth::l2cap::cert::L2capModuleCertModule>();
         break;
       default:
         return ::grpc::Status(::grpc::StatusCode::INVALID_ARGUMENT, "invalid module under test");
