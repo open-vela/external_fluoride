4d599d4625af6dc6eec8e0c122fc4a99528f3a97
liujinye <liujinye@xiaomi.com>
2019-08-08 17:30:07 +0800

    Remove direct connection after notifying the connection up
    After notifying the connection up, remove direct connection related with
    this device to make sure no client is directly connecting the device in
    connection_manager.
    
    Bug: 138819537
    Test: 1. Repair BLE device 2. Reconnect BLE deivce
    3. reboot DUT, make sure Hearing Aid can be connected.
    
    Change-Id: I30ff7c47445ec2a3dbd9702ca79873063210e6df
    Merged-In: I30ff7c47445ec2a3dbd9702ca79873063210e6df

--- vendor_libs/test_vendor_lib/model/controller/dual_mode_controller.cc
+++ vendor_libs/test_vendor_lib/model/controller/dual_mode_controller.cc
@@ -60,11 +60,7 @@ void DualModeController::Initialize(const std::vector<std::string>& args) {
   if (args.size() < 2) return;
 
   Address addr;
-  if (Address::FromString(args[1], addr)) {
-    properties_.SetAddress(addr);
-  } else {
-    LOG_FATAL(LOG_TAG, "Invalid address: %s", args[1].c_str());
-  }
+  if (Address::FromString(args[1], addr)) properties_.SetAddress(addr);
 };
 
 std::string DualModeController::GetTypeString() const {
@@ -150,8 +146,6 @@ DualModeController::DualModeController(const std::string& properties_filename, u
   SET_HANDLER(OpCode::READ_LOCAL_VERSION_INFORMATION, HciReadLocalVersionInformation);
   SET_HANDLER(OpCode::READ_BD_ADDR, HciReadBdAddr);
   SET_HANDLER(OpCode::READ_LOCAL_SUPPORTED_COMMANDS, HciReadLocalSupportedCommands);
-  SET_HANDLER(OpCode::READ_LOCAL_SUPPORTED_FEATURES,
-              HciReadLocalSupportedFeatures);
   SET_HANDLER(OpCode::READ_LOCAL_SUPPORTED_CODECS, HciReadLocalSupportedCodecs);
   SET_HANDLER(OpCode::READ_LOCAL_EXTENDED_FEATURES, HciReadLocalExtendedFeatures);
   SET_HANDLER(OpCode::READ_REMOTE_EXTENDED_FEATURES, HciReadRemoteExtendedFeatures);
@@ -180,7 +174,6 @@ DualModeController::DualModeController(const std::string& properties_filename, u
   SET_HANDLER(OpCode::WRITE_LOCAL_NAME, HciWriteLocalName);
   SET_HANDLER(OpCode::READ_LOCAL_NAME, HciReadLocalName);
   SET_HANDLER(OpCode::WRITE_EXTENDED_INQUIRY_RESPONSE, HciWriteExtendedInquiryResponse);
-  SET_HANDLER(OpCode::REFRESH_ENCRYPTION_KEY, HciRefreshEncryptionKey);
   SET_HANDLER(OpCode::WRITE_VOICE_SETTING, HciWriteVoiceSetting);
   SET_HANDLER(OpCode::WRITE_CURRENT_IAC_LAP, HciWriteCurrentIacLap);
   SET_HANDLER(OpCode::WRITE_INQUIRY_SCAN_ACTIVITY, HciWriteInquiryScanActivity);
@@ -312,9 +305,6 @@ void DualModeController::RegisterScoChannel(
 void DualModeController::HciReset(packets::PacketView<true> args) {
   CHECK(args.size() == 0) << __func__ << " size=" << args.size();
   link_layer_controller_.Reset();
-  if (loopback_mode_ == hci::LoopbackMode::LOCAL) {
-    loopback_mode_ = hci::LoopbackMode::NO;
-  }
 
   SendCommandCompleteSuccess(OpCode::RESET);
 }
@@ -366,15 +356,6 @@ void DualModeController::HciReadLocalSupportedCommands(packets::PacketView<true>
   std::shared_ptr<packets::EventPacketBuilder> command_complete =
       packets::EventPacketBuilder::CreateCommandCompleteReadLocalSupportedCommands(hci::Status::SUCCESS,
                                                                                    properties_.GetSupportedCommands());
-  send_event_(command_complete->ToVector());
-}
-
-void DualModeController::HciReadLocalSupportedFeatures(
-    packets::PacketView<true> args) {
-  CHECK(args.size() == 0) << __func__ << " size=" << args.size();
-  std::shared_ptr<packets::EventPacketBuilder> command_complete = packets::
-      EventPacketBuilder::CreateCommandCompleteReadLocalSupportedFeatures(
-          hci::Status::SUCCESS, properties_.GetSupportedFeatures());
   send_event_(command_complete->ToVector());
 }
 
@@ -663,20 +644,6 @@ void DualModeController::HciWriteExtendedInquiryResponse(packets::PacketView<tru
   LOG_WARN(LOG_TAG, "Write EIR Inquiry - Size = %d (%d)", static_cast<int>(properties_.GetExtendedInquiryData().size()),
            static_cast<int>(clipped.size()));
   SendCommandCompleteSuccess(OpCode::WRITE_EXTENDED_INQUIRY_RESPONSE);
-}
-
-void DualModeController::HciRefreshEncryptionKey(
-    packets::PacketView<true> args) {
-  CHECK(args.size() == 2) << __func__ << " size=" << args.size();
-  auto args_itr = args.begin();
-  uint16_t handle = args_itr.extract<uint16_t>();
-  SendCommandStatusSuccess(OpCode::REFRESH_ENCRYPTION_KEY);
-  // TODO: Support this in the link layer
-  hci::Status status = hci::Status::SUCCESS;
-  send_event_(
-      packets::EventPacketBuilder::CreateEncryptionKeyRefreshCompleteEvent(
-          status, handle)
-          ->ToVector());
 }
 
 void DualModeController::HciWriteVoiceSetting(packets::PacketView<true> args) {
