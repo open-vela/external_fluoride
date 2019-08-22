bab81668300915fdc1d03451ac8aeb342c90e190
openvela-robot <openvela-robot@xiaomi.com>
2019-08-22 15:15:59 +0800

    GD: Move Address and ClassOfDevice to hci am: 94db40c6c9 am: cf95615e90 am: 5736c685a5 am: 47a5a33aa2 am: 36657718a2
    Change-Id: I663e0bc840ea41974744f4a8766f2cc0ed776dd7

--- gd/hci/facade.cc
+++ gd/hci/facade.cc
@@ -457,17 +457,12 @@ class ClassicSecurityManagerFacadeService : public ClassicSecurityManagerFacade:
                                     ::google::protobuf::Empty* response) {
     std::unique_lock<std::mutex> lock(mutex_);
     uint8_t num_keys_to_write = request->num_keys_to_write();
-    std::vector<KeyAndAddress> keys;
-    for (size_t i = 0; i < num_keys_to_write; i++) {
-      KeyAndAddress key;
-      common::LinkKey link_key;
-      ASSERT(Address::FromString(request->remote().address(), key.address_));
-      ASSERT(common::LinkKey::FromString(request->link_keys(), link_key));
-      std::copy(std::begin(link_key.link_key), std::end(link_key.link_key), std::begin(key.link_key_));
-      keys.push_back(key);
-    }
+    Address peer;
+    common::LinkKey link_key;
+    ASSERT(Address::FromString(request->remote().address(), peer));
+    ASSERT(common::LinkKey::FromString(request->link_keys(), link_key));
 
-    classic_security_manager_->WriteStoredLinkKey(keys);
+    classic_security_manager_->WriteStoredLinkKey(num_keys_to_write, peer, link_key);
     return ::grpc::Status::OK;
   };
 
