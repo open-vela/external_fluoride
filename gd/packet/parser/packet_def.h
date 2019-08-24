4bc2a35fcdee0a7b09c78c5a12596f9445b018fd
liujinye <liujinye@xiaomi.com>
2019-08-24 09:18:14 +0800

    Device: Initial bluetooth device API am: 58a1557b96 am: 332c0c2d4d am: 9a06e8fedb am: 9c82356443 am: 6d21af7dd0
    Change-Id: I1ad03ceca4d81b232d3f61790ce773248a938239

--- gd/packet/parser/packet_def.h
+++ gd/packet/parser/packet_def.h
@@ -41,8 +41,6 @@ class PacketDef : public ParentDef {
 
   void GenBuilderDefinition(std::ostream& s) const;
 
-  void GenTestDefine(std::ostream& s) const;
-
   FieldList GetParametersToValidate() const;
 
   void GenBuilderCreate(std::ostream& s) const;
