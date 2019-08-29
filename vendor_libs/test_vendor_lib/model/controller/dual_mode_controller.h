a6d40dea22cccea6930709b1893011d70a13ad9e
liujinye <liujinye@xiaomi.com>
2019-08-30 03:08:26 +0800

    Merge "Remove direct connection after notifying the connection up" into qt-qpr1-dev

--- vendor_libs/test_vendor_lib/model/controller/dual_mode_controller.h
+++ vendor_libs/test_vendor_lib/model/controller/dual_mode_controller.h
@@ -241,9 +241,6 @@ class DualModeController : public Device {
   // 7.3.56
   void HciWriteExtendedInquiryResponse(packets::PacketView<true> args);
 
-  // 7.3.57
-  void HciRefreshEncryptionKey(packets::PacketView<true> args);
-
   // 7.3.59
   void HciWriteSimplePairingMode(packets::PacketView<true> args);
 
@@ -264,9 +261,6 @@ class DualModeController : public Device {
 
   // 7.4.2
   void HciReadLocalSupportedCommands(packets::PacketView<true> args);
-
-  // 7.4.3
-  void HciReadLocalSupportedFeatures(packets::PacketView<true> args);
 
   // 7.4.4
   void HciReadLocalExtendedFeatures(packets::PacketView<true> args);
