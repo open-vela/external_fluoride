d25ee6080ccf5cf1a01875560020811dde477355
liujinye <liujinye@xiaomi.com>
2019-09-06 09:42:36 +0800

    RootCanal: Remove unregistered devices from Phys am: 2917d9e394 am: bc4992be18 am: 97600c046e am: f6901f3ff3
    Change-Id: If841eb738e1cc1764d9a227f016b0aa08e64755d

--- gd/packet/parser/struct_def.cc
+++ gd/packet/parser/struct_def.cc
@@ -39,14 +39,19 @@ TypeDef::Type StructDef::GetDefinitionType() const {
 }
 
 void StructDef::GenParse(std::ostream& s) const {
-  std::string iterator = "Iterator<kLittleEndian>";
-  if (!is_little_endian_) {
-    iterator = "Iterator<!kLittleEndian>";
+  if (is_little_endian_) {
+    s << "static Iterator<kLittleEndian> Parse(std::vector<" << name_ << ">& vec, Iterator<kLittleEndian> struct_it) {";
+  } else {
+    s << "static Iterator<!kLittleEndian> Parse(std::vector<" << name_
+      << ">& vec, Iterator<!kLittleEndian> struct_it) {";
   }
-  s << "static " << iterator << " Parse(" << name_ << "* to_return, " << iterator << " struct_it) {";
   s << "auto begin_it = struct_it;";
   s << "size_t end_index = struct_it.NumBytesRemaining();";
-  s << "if (end_index < " << GetSize().bytes() << ") { return struct_it.Subrange(0,0);}";
+  s << "if (end_index < " << GetSize().bytes() << ") { return struct_it + struct_it.NumBytesRemaining();}";
+  s << name_ << " one;";
+  if (parent_ != nullptr) {
+    s << "begin_it += one." << parent_->name_ << "::BitsOfHeader() / 8;";
+  }
   Size field_offset = Size(0);
   for (const auto& field : fields_) {
     Size next_field_offset = field->GetSize() + field_offset.bits();
@@ -56,11 +61,12 @@ void StructDef::GenParse(std::ostream& s) const {
         field->GetFieldType() != CountField::kFieldType) {
       s << "{";
       field->GenExtractor(s, field_offset, next_field_offset);
-      s << "to_return->" << field->GetName() << "_ = value;";
+      s << "one." << field->GetName() << "_ = value;";
       s << "}";
     }
     field_offset = next_field_offset;
   }
+  s << "vec.push_back(one);";
   s << "return struct_it + " << field_offset.bytes() << ";";
   s << "}";
 }
