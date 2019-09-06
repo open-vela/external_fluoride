5031ba0c6648afe57d9bf104a9c9301ccd2cecf2
liujinye <liujinye@xiaomi.com>
2019-09-06 15:15:20 +0800

    Merge "RootCanal: Add LE connection packets" am: 6564ffe147 am: af6a6c5ba5 am: fc2d61d1f2
    Change-Id: Ie530ee7ccda2121af92f240ccd0ecf772cfed1d6

--- gd/hci/hci_layer.cc
+++ gd/hci/hci_layer.cc
@@ -441,16 +441,16 @@ void HciLayer::UnregisterLeEventHandler(SubeventCode subevent_code) {
   impl_->UnregisterLeEventHandler(subevent_code);
 }
 
-SecurityInterface* HciLayer::GetSecurityInterface(common::Callback<void(EventPacketView)> event_handler,
-                                                  os::Handler* handler) {
+const SecurityInterface* HciLayer::GetSecurityInterface(common::Callback<void(EventPacketView)> event_handler,
+                                                        os::Handler* handler) {
   for (const auto event : SecurityInterface::SecurityEvents) {
     RegisterEventHandler(event, event_handler, handler);
   }
   return &impl_->security_interface;
 }
 
-LeSecurityInterface* HciLayer::GetLeSecurityInterface(common::Callback<void(LeMetaEventView)> event_handler,
-                                                      os::Handler* handler) {
+const LeSecurityInterface* HciLayer::GetLeSecurityInterface(common::Callback<void(LeMetaEventView)> event_handler,
+                                                            os::Handler* handler) {
   for (const auto subevent : LeSecurityInterface::LeSecurityEvents) {
     RegisterLeEventHandler(subevent, event_handler, handler);
   }
