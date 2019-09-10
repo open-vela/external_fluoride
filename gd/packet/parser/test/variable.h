e3cd72bc765c8ccb57736ae47539cc66296be217
openvela-robot <openvela-robot@xiaomi.com>
2019-09-10 20:15:37 +0800

    Add legacy/gd shim controller module am: 229de74f4c am: e6df26700c
    Change-Id: I4887df6ee8559f199cb1765ef30625ad098e39d4

---
+++ gd/packet/parser/test/variable.h
@@ -0,0 +1,52 @@
+/******************************************************************************
+ *
+ *  Copyright 2019 The Android Open Source Project
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at:
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+#pragma once
+
+#include <stdint.h>
+#include <optional>
+#include <sstream>
+#include <string>
+
+#include "packet/bit_inserter.h"
+#include "packet/iterator.h"
+
+namespace bluetooth {
+namespace packet {
+namespace parser {
+namespace test {
+
+class Variable final {
+ public:
+  std::string data;
+
+  Variable() = default;
+  Variable(const Variable&) = default;
+  Variable(const std::string& str);
+
+  void Serialize(BitInserter& bi) const;
+
+  size_t size() const;
+
+  static Iterator<true> Parse(Variable* instance, Iterator<true> it);
+};
+
+}  // namespace test
+}  // namespace parser
+}  // namespace packet
+}  // namespace bluetooth
