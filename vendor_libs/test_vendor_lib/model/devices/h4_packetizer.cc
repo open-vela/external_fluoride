5bb1dd583c9aa3cf78c0623ead706dfa3e1ed9fe
openvela-robot <openvela-robot@xiaomi.com>
2019-09-10 20:29:53 +0800

    Merge "AVRCP: Keep the device in the map on disconnect" into qt-qpr1-dev am: af894a94ad
    Change-Id: Icc0af6206936480a71bdcf7dcb921c4c6fe00b64

--- vendor_libs/test_vendor_lib/model/devices/h4_packetizer.cc
+++ vendor_libs/test_vendor_lib/model/devices/h4_packetizer.cc
@@ -52,9 +52,16 @@ size_t H4Packetizer::HciGetPacketLengthForType(hci::PacketType type, const uint8
   return (((preamble[offset + 1]) << 8) | preamble[offset]);
 }
 
-H4Packetizer::H4Packetizer(int fd, PacketReadCallback command_cb, PacketReadCallback event_cb,
-                           PacketReadCallback acl_cb, PacketReadCallback sco_cb)
-    : uart_fd_(fd), command_cb_(command_cb), event_cb_(event_cb), acl_cb_(acl_cb), sco_cb_(sco_cb) {}
+H4Packetizer::H4Packetizer(int fd, PacketReadCallback command_cb,
+                           PacketReadCallback event_cb,
+                           PacketReadCallback acl_cb, PacketReadCallback sco_cb,
+                           ClientDisconnectCallback disconnect_cb)
+    : uart_fd_(fd),
+      command_cb_(command_cb),
+      event_cb_(event_cb),
+      acl_cb_(acl_cb),
+      sco_cb_(sco_cb),
+      disconnect_cb_(disconnect_cb) {}
 
 size_t H4Packetizer::Send(uint8_t type, const uint8_t* data, size_t length) {
   struct iovec iov[] = {{&type, sizeof(type)}, {const_cast<uint8_t*>(data), length}};
@@ -73,7 +80,6 @@ size_t H4Packetizer::Send(uint8_t type, const uint8_t* data, size_t length) {
 }
 
 void H4Packetizer::OnPacketReady() {
-  ALOGE("%s: before switch", __func__);
   switch (hci_packet_type_) {
     case hci::PacketType::COMMAND:
       command_cb_(packet_);
@@ -98,20 +104,22 @@ void H4Packetizer::OnDataReady(int fd) {
   if (hci_packet_type_ == hci::PacketType::UNKNOWN) {
     uint8_t buffer[1] = {0};
     ssize_t bytes_read = TEMP_FAILURE_RETRY(read(fd, buffer, 1));
-    if (bytes_read != 1) {
-      if (bytes_read == 0) {
-        ALOGI("%s: Nothing ready, will retry!", __func__);
+    if (bytes_read == 0) {
+      ALOGI("%s: remote disconnected!", __func__);
+      disconnect_cb_();
+      return;
+    } else if (bytes_read < 0) {
+      if (errno == EAGAIN) {
+        // No data, try again later.
+        ALOGV("%s: Nothing ready, will retry!", __func__);
         return;
-      } else if (bytes_read < 0) {
-        if (errno == EAGAIN) {
-          // No data, try again later.
-          return;
-        } else {
-          LOG_ALWAYS_FATAL("%s: Read packet type error: %s", __func__, strerror(errno));
-        }
       } else {
-        LOG_ALWAYS_FATAL("%s: More bytes read than expected (%u)!", __func__, static_cast<unsigned int>(bytes_read));
+        LOG_ALWAYS_FATAL("%s: Read packet type error: %s", __func__,
+                         strerror(errno));
       }
+    } else if (bytes_read > 1) {
+      LOG_ALWAYS_FATAL("%s: More bytes read than expected (%u)!", __func__,
+                       static_cast<unsigned int>(bytes_read));
     }
     hci_packet_type_ = static_cast<hci::PacketType>(buffer[0]);
     if (hci_packet_type_ != hci::PacketType::ACL && hci_packet_type_ != hci::PacketType::SCO &&
@@ -147,8 +155,6 @@ void H4Packetizer::OnDataReady(int fd) {
           size_t packet_length = HciGetPacketLengthForType(hci_packet_type_, preamble_);
           packet_.resize(preamble_bytes + packet_length);
           memcpy(packet_.data(), preamble_, preamble_bytes);
-          ALOGI("%s: Read a preamble of size %d length %d %0x %0x %0x", __func__, static_cast<int>(bytes_read_),
-                static_cast<int>(packet_length), preamble_[0], preamble_[1], preamble_[2]);
           bytes_remaining_ = packet_length;
           if (bytes_remaining_ == 0) {
             OnPacketReady();
