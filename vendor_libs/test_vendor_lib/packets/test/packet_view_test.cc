88b90be6fb3fb350f2fa2a23cea3a5c24b5a0c68
openvela-robot <openvela-robot@xiaomi.com>
2019-09-11 21:20:40 +0800

    Merge "BLE: Updating the WL if addr_type is not matched for same BDaddr" into qt-qpr1-dev

--- vendor_libs/test_vendor_lib/packets/test/packet_view_test.cc
+++ vendor_libs/test_vendor_lib/packets/test/packet_view_test.cc
@@ -52,13 +52,15 @@ template <typename T>
 class IteratorTest : public ::testing::Test {
  public:
   IteratorTest() = default;
-  ~IteratorTest() override = default;
+  ~IteratorTest() = default;
 
-  void SetUp() override {
+  void SetUp() {
     packet = std::shared_ptr<T>(new T({View(std::make_shared<const vector<uint8_t>>(count_all), 0, count_all.size())}));
   }
 
-  void TearDown() override { packet.reset(); }
+  void TearDown() {
+    packet.reset();
+  }
 
   std::shared_ptr<T> packet;
 };
@@ -69,14 +71,14 @@ TYPED_TEST_CASE(IteratorTest, PacketViewTypes);
 class IteratorExtractTest : public ::testing::Test {
  public:
   IteratorExtractTest() = default;
-  ~IteratorExtractTest() override = default;
+  ~IteratorExtractTest() = default;
 };
 
 template <typename T>
 class PacketViewTest : public IteratorTest<T> {
  public:
   PacketViewTest() = default;
-  ~PacketViewTest() override = default;
+  ~PacketViewTest() = default;
 };
 
 using PacketViewTypes = ::testing::Types<PacketView<true>, PacketView<false>>;
@@ -85,13 +87,13 @@ TYPED_TEST_CASE(PacketViewTest, PacketViewTypes);
 class PacketViewMultiViewTest : public ::testing::Test {
  public:
   PacketViewMultiViewTest() = default;
-  ~PacketViewMultiViewTest() override = default;
+  ~PacketViewMultiViewTest() = default;
 };
 
 class ViewTest : public ::testing::Test {
  public:
   ViewTest() = default;
-  ~ViewTest() override = default;
+  ~ViewTest() = default;
 };
 
 TEST(IteratorExtractTest, extractLeTest) {
