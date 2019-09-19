698e60622430414ac5eb8b5076f02d89bb6af904
openvela-robot <openvela-robot@xiaomi.com>
2019-09-19 13:33:47 +0800

    Add logging indication of legacy/gd stack am: a2a9faa7fe am: 970027742e
    Change-Id: I5d0b42f3a379bfb724200d41b356302ffde0b7e1

--- main/shim/entry.cc
+++ main/shim/entry.cc
@@ -33,7 +33,3 @@ future_t* bluetooth::shim::StopGabeldorscheStack() {
 bluetooth::shim::IController* bluetooth::shim::GetController() {
   return GetGabeldorscheStack()->GetController();
 }
-
-bluetooth::shim::IHciLayer* bluetooth::shim::GetHciLayer() {
-  return GetGabeldorscheStack()->GetHciLayer();
-}
