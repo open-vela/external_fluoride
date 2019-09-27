fddd3d2ceedc823a6ccaf3ff1fcea491126d4779
openvela-robot <openvela-robot@xiaomi.com>
2019-09-27 17:57:54 +0800

    [automerger skipped] Merge "Use memcpy instead of casting to convert device_class to int" into pi-dev am: 2e2864e169 -s ours am skip reason: change_id Iee71ce35576e438317841d41a81fda6a87e1984a with SHA1 50bf4cf1bf is in history
    Change-Id: Icbe38484058fc4d1f2af1445cb4c2c1ff6634627

--- main/bte_main.cc
+++ main/bte_main.cc
@@ -1,6 +1,6 @@
 /******************************************************************************
  *
- *  Copyright (C) 2009-2012 Broadcom Corporation
+ *  Copyright 2009-2012 Broadcom Corporation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -27,6 +27,7 @@
 #define LOG_TAG "bt_main"
 
 #include <base/logging.h>
+#include <base/threading/thread.h>
 #include <fcntl.h>
 #include <pthread.h>
 #include <signal.h>
@@ -52,7 +53,6 @@
 #include "osi/include/future.h"
 #include "osi/include/log.h"
 #include "osi/include/osi.h"
-#include "osi/include/thread.h"
 #include "stack_config.h"
 
 /*******************************************************************************
@@ -79,13 +79,30 @@
 static const hci_t* hci;
 
 /*******************************************************************************
+ *  Externs
+ ******************************************************************************/
+extern void btu_hci_msg_process(BT_HDR* p_msg);
+
+/*******************************************************************************
  *  Static functions
  ******************************************************************************/
 
-/*******************************************************************************
- *  Externs
- ******************************************************************************/
-fixed_queue_t* btu_hci_msg_queue;
+/******************************************************************************
+ *
+ * Function         post_to_hci_message_loop
+ *
+ * Description      Post an HCI event to the main thread
+ *
+ * Returns          None
+ *
+ *****************************************************************************/
+void post_to_main_message_loop(const base::Location& from_here, BT_HDR* p_msg) {
+  if (do_in_main_thread(from_here, base::Bind(&btu_hci_msg_process, p_msg)) !=
+      BT_STATUS_SUCCESS) {
+    LOG(ERROR) << __func__ << ": do_in_main_thread failed from "
+               << from_here.ToString();
+  }
+}
 
 /******************************************************************************
  *
@@ -105,14 +122,7 @@ void bte_main_boot_entry(void) {
     return;
   }
 
-  btu_hci_msg_queue = fixed_queue_new(SIZE_MAX);
-  if (btu_hci_msg_queue == NULL) {
-    LOG_ERROR(LOG_TAG, "%s unable to allocate hci message queue.", __func__);
-    return;
-  }
-
-  data_dispatcher_register_default(hci->event_dispatcher, btu_hci_msg_queue);
-  hci->set_data_queue(btu_hci_msg_queue);
+  hci->set_data_cb(base::Bind(&post_to_main_message_loop));
 
   module_init(get_module(STACK_CONFIG_MODULE));
 }
@@ -127,13 +137,6 @@ void bte_main_boot_entry(void) {
  *
  *****************************************************************************/
 void bte_main_cleanup() {
-  data_dispatcher_register_default(hci_layer_get_interface()->event_dispatcher,
-                                   NULL);
-  hci->set_data_queue(NULL);
-  fixed_queue_free(btu_hci_msg_queue, NULL);
-
-  btu_hci_msg_queue = NULL;
-
   module_clean_up(get_module(STACK_CONFIG_MODULE));
 
   module_clean_up(get_module(INTEROP_MODULE));
