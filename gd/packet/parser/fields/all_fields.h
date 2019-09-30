ebb56da830362790dd82ecc9602b8dbba74469e2
openvela-robot <openvela-robot@xiaomi.com>
2019-09-30 16:27:27 +0800

    Revert "L2CAP Classic dynamic channel and allocator impl" am: 64508b507e am: 72afcfcf36
    Change-Id: Iede5ea343853e2d1bc15439d8892884892211bc4

--- gd/packet/parser/fields/all_fields.h
+++ gd/packet/parser/fields/all_fields.h
@@ -32,5 +32,4 @@
 #include "fields/scalar_field.h"
 #include "fields/size_field.h"
 #include "fields/struct_field.h"
-#include "fields/variable_length_struct_field.h"
 #include "fields/vector_field.h"
