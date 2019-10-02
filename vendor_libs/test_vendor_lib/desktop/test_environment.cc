8a016bd697413b1e8c12d6fef2c8042b5214042d
openvela-robot <openvela-robot@xiaomi.com>
2019-10-03 01:03:13 +0800

    Snap for 5915132 from a296fe0ef13ebd675f0b5672441cde9cda6c58c8 to qt-qpr1-release
    Change-Id: I5fda8bbc06147644f5673217a0dfa0f3eb148b99

--- vendor_libs/test_vendor_lib/desktop/test_environment.cc
+++ vendor_libs/test_vendor_lib/desktop/test_environment.cc
@@ -24,6 +24,8 @@
 #include <string.h>
 #include <utils/Log.h>
 
+#include "hci_internals.h"
+
 namespace android {
 namespace bluetooth {
 namespace root_canal {
@@ -141,10 +143,6 @@ int TestEnvironment::ConnectToRemoteServer(const std::string& server, int port) 
 
 void TestEnvironment::SetUpTestChannel() {
   int socket_fd = test_channel_transport_.SetUp(test_port_);
-  test_channel_.AddPhy({"BR_EDR"});
-  test_channel_.AddPhy({"LOW_ENERGY"});
-  test_channel_.SetTimerPeriod({"10"});
-  test_channel_.StartTimer({});
 
   test_channel_.RegisterSendResponse(
       [](const std::string& response) { ALOGI("No test channel: %s", response.c_str()); });
