b4fb1ffb84bf63a409fb29c922caf0733e9f07cb
liujinye <liujinye@xiaomi.com>
2019-10-03 15:58:42 +0800

    Merge "JustWorks: Auto-accept only incoming temporary pairing." into pi-dev

--- vendor_libs/test_vendor_lib/test/iterator_test.cc
+++ vendor_libs/test_vendor_lib/test/iterator_test.cc
@@ -29,30 +29,22 @@ class TestPacket : public HciPacket {
   static std::shared_ptr<TestPacket> make_new_packet(vector<uint8_t> v) {
     return std::shared_ptr<TestPacket>(new TestPacket(v));
   }
-  size_t get_length() {
-    return test_vector_.size();
-  }
-  uint8_t& get_at_index(size_t index) {
-    return test_vector_[index];
-  }
+  size_t get_length() { return test_vector_.size(); }
+  uint8_t& get_at_index(size_t index) { return test_vector_[index]; }
 
  private:
-  TestPacket(vector<uint8_t> v) {
-    test_vector_ = v;
-  }
+  TestPacket(vector<uint8_t> v) { test_vector_ = v; }
   vector<uint8_t> test_vector_;
 };
 
 class IteratorTest : public ::testing::Test {
  public:
   IteratorTest() = default;
-  ~IteratorTest() override = default;
+  ~IteratorTest() = default;
 
-  void SetUp() override {
-    packet = TestPacket::make_new_packet(complete_l2cap_packet);
-  }
+  void SetUp() { packet = TestPacket::make_new_packet(complete_l2cap_packet); }
 
-  void TearDown() override { packet.reset(); }
+  void TearDown() { packet.reset(); }
 
   std::shared_ptr<TestPacket> packet;
 };
@@ -85,7 +77,8 @@ TEST_F(IteratorTest, plusEqTest) {
   Iterator plus_eq = packet->get_begin();
   for (size_t i = 0; i < complete_l2cap_packet.size(); i += 2) {
     ASSERT_EQ(complete_l2cap_packet[i], *plus_eq)
-        << "+= test: Dereferenced iterator does not equal expected at index " << i;
+        << "+= test: Dereferenced iterator does not equal expected at index "
+        << i;
     plus_eq += 2;
   }
 }
@@ -112,7 +105,8 @@ TEST_F(IteratorTest, additionTest) {
   Iterator plus = packet->get_begin();
   for (size_t i = 0; i < complete_l2cap_packet.size(); i++) {
     ASSERT_EQ(complete_l2cap_packet[i], *plus)
-        << "+ test: Dereferenced iterator does not equal expected at index " << i;
+        << "+ test: Dereferenced iterator does not equal expected at index "
+        << i;
     plus = plus + static_cast<size_t>(1);
   }
 }
@@ -122,7 +116,8 @@ TEST_F(IteratorTest, minusEqTest) {
   minus_eq -= 1;
   for (size_t i = complete_l2cap_packet.size() - 1; i > 0; i -= 2) {
     ASSERT_EQ(complete_l2cap_packet[i], *minus_eq)
-        << "-= test: Dereferenced iterator does not equal expected at index " << i;
+        << "-= test: Dereferenced iterator does not equal expected at index "
+        << i;
     minus_eq -= 2;
   }
 }
@@ -151,7 +146,8 @@ TEST_F(IteratorTest, subtractionTest) {
   minus = minus - static_cast<size_t>(1);
   for (size_t i = complete_l2cap_packet.size() - 1; i > 0; i--) {
     ASSERT_EQ(complete_l2cap_packet[i], *minus)
-        << "- test: Dereferenced iterator does not equal expected at index " << i;
+        << "- test: Dereferenced iterator does not equal expected at index "
+        << i;
     minus = minus - static_cast<size_t>(1);
   }
 }
@@ -161,7 +157,8 @@ TEST_F(IteratorTest, plusEqBoundsTest) {
   plus_eq--;
   for (size_t i = 0; i < 100; i++) {
     plus_eq += i;
-    ASSERT_EQ(packet->get_end(), plus_eq) << "+= test: Iterator exceeded the upper bound set by get_length()";
+    ASSERT_EQ(packet->get_end(), plus_eq)
+        << "+= test: Iterator exceeded the upper bound set by get_length()";
   }
 }
 
@@ -169,8 +166,9 @@ TEST_F(IteratorTest, preIncrementBoundsTest) {
   Iterator plus_plus = packet->get_end();
   plus_plus--;
   for (size_t i = 0; i < 100; i++) {
-    ASSERT_EQ(packet->get_end(), ++plus_plus) << "Pre-increment test: Iterator exceeded the upper bound set "
-                                                 "by get_length()";
+    ASSERT_EQ(packet->get_end(), ++plus_plus)
+        << "Pre-increment test: Iterator exceeded the upper bound set "
+           "by get_length()";
   }
 }
 
@@ -178,8 +176,9 @@ TEST_F(IteratorTest, postIncrementBoundsTest) {
   Iterator plus_plus = packet->get_end();
   plus_plus--;
   for (size_t i = 0; i < 100; i++) {
-    ASSERT_EQ(packet->get_end(), plus_plus++) << "Post-increment test: Iterator exceeded the upper bound set "
-                                                 "by get_length()";
+    ASSERT_EQ(packet->get_end(), plus_plus++)
+        << "Post-increment test: Iterator exceeded the upper bound set "
+           "by get_length()";
   }
 }
 
@@ -188,7 +187,8 @@ TEST_F(IteratorTest, additionBoundsTest) {
   plus--;
   for (size_t i = 0; i < 100; i++) {
     plus = plus + static_cast<size_t>(i);
-    ASSERT_EQ(packet->get_end(), plus) << "+ test: Iterator exceeded the upper bound set by get_length()";
+    ASSERT_EQ(packet->get_end(), plus)
+        << "+ test: Iterator exceeded the upper bound set by get_length()";
   }
 }
 
@@ -196,8 +196,9 @@ TEST_F(IteratorTest, minusEqBoundsTest) {
   Iterator minus_eq = packet->get_begin();
   for (size_t i = 0; i < 100; i++) {
     minus_eq -= i;
-    ASSERT_EQ(complete_l2cap_packet[0], *minus_eq) << "-= test: Iterator is less than the lower bound set by "
-                                                      "packet->get_begin()";
+    ASSERT_EQ(complete_l2cap_packet[0], *minus_eq)
+        << "-= test: Iterator is less than the lower bound set by "
+           "packet->get_begin()";
   }
 }
 
@@ -223,8 +224,9 @@ TEST_F(IteratorTest, subtractionBoundsTest) {
   Iterator minus = packet->get_begin();
   for (size_t i = 0; i < 100; i++) {
     minus = minus - static_cast<size_t>(i);
-    ASSERT_EQ(complete_l2cap_packet[0], *minus) << "- test: Iterator is less than the lower bound set "
-                                                   "by packet->get_begin()";
+    ASSERT_EQ(complete_l2cap_packet[0], *minus)
+        << "- test: Iterator is less than the lower bound set "
+           "by packet->get_begin()";
   }
 }
 };  // namespace test_vendor_lib
