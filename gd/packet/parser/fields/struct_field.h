3f4e5f4ad1a601ead79ac68d7f398e065fdb2f1a
liujinye <liujinye@xiaomi.com>
2019-10-03 13:23:37 +0800

    Merge "PDL: Add for_struct to GenExtractor" am: 705f0b9ced am: d217cb5124 am: 8d1dbb56b3
    Change-Id: I695e97b01a62c4c835c2fcf45493ecac4a435fde

--- gd/packet/parser/fields/struct_field.h
+++ gd/packet/parser/fields/struct_field.h
@@ -21,15 +21,13 @@
 
 class StructField : public PacketField {
  public:
-  StructField(std::string name, std::string type_name, Size size, ParseLocation loc);
+  StructField(std::string name, std::string type_name, int size, ParseLocation loc);
 
   static const std::string kFieldType;
 
   virtual const std::string& GetFieldType() const override;
 
   virtual Size GetSize() const override;
-
-  virtual Size GetBuilderSize() const override;
 
   virtual std::string GetDataType() const override;
 
