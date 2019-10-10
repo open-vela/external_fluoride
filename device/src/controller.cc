324ec7c60d06fa7fe9c052efdedc85501e2a905b
liujinye <liujinye@xiaomi.com>
2019-10-10 14:04:47 +0800

    [system][bt] fix -Wdangling-gsl
    BtAddrString() returns a std::string. It's not safe to chain a call to
    c_str() as otherwise the returned std::string is a temporary, and the
    expression evaluates to an immediately dangling pointer.
    
    Bug: 139945549
    Bug: 142558228
    Test: mm
    Change-Id: I30972458abcc563b24ee0d80b289c3efd6c3e04d
    Signed-off-by: Nick Desaulniers <ndesaulniers@google.com>

--- device/src/controller.cc
+++ device/src/controller.cc
@@ -1,6 +1,6 @@
 /******************************************************************************
  *
- *  Copyright 2014 Google, Inc.
+ *  Copyright (C) 2014 Google, Inc.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -27,20 +27,11 @@
 #include "btcore/include/module.h"
 #include "btcore/include/version.h"
 #include "hcimsgs.h"
-#include "main/shim/controller.h"
-#include "main/shim/shim.h"
 #include "osi/include/future.h"
 #include "stack/include/btm_ble_api.h"
 
-const bt_event_mask_t BLE_EVENT_MASK = {{0x00, 0x00, 0x00, 0x00, 0x00, 0x02,
-#if (BLE_PRIVACY_SPT == TRUE)
-                                         0x1E,
-#else
-                                         /* Disable "LE Enhanced Connection
-                                            Complete" when privacy is off */
-                                         0x1C,
-#endif
-                                         0x7f}};
+const bt_event_mask_t BLE_EVENT_MASK = {
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x1E, 0x7f}};
 
 const bt_event_mask_t CLASSIC_EVENT_MASK = {HCI_DUMO_EVENT_MASK_EXT};
 
@@ -57,7 +48,7 @@ static const hci_t* hci;
 static const hci_packet_factory_t* packet_factory;
 static const hci_packet_parser_t* packet_parser;
 
-static RawAddress address;
+static bt_bdaddr_t address;
 static bt_version_t bt_version;
 
 static uint8_t supported_commands[HCI_SUPPORTED_COMMANDS_ARRAY_SIZE];
@@ -74,11 +65,6 @@ static uint8_t ble_resolving_list_max_size;
 static uint8_t ble_supported_states[BLE_SUPPORTED_STATES_SIZE];
 static bt_device_features_t features_ble;
 static uint16_t ble_suggested_default_data_length;
-static uint16_t ble_supported_max_tx_octets;
-static uint16_t ble_supported_max_tx_time;
-static uint16_t ble_supported_max_rx_octets;
-static uint16_t ble_supported_max_rx_time;
-
 static uint16_t ble_maxium_advertising_data_length;
 static uint8_t ble_number_of_supported_advertising_sets;
 static uint8_t local_supported_codecs[MAX_LOCAL_SUPPORTED_CODECS_SIZE];
@@ -226,12 +212,6 @@ static future_t* start_up(void) {
     }
 
     if (HCI_LE_DATA_LEN_EXT_SUPPORTED(features_ble.as_array)) {
-      response =
-          AWAIT_COMMAND(packet_factory->make_ble_read_maximum_data_length());
-      packet_parser->parse_ble_read_maximum_data_length_response(
-          response, &ble_supported_max_tx_octets, &ble_supported_max_tx_time,
-          &ble_supported_max_rx_octets, &ble_supported_max_rx_time);
-
       response = AWAIT_COMMAND(
           packet_factory->make_ble_read_suggested_default_data_length());
       packet_parser->parse_ble_read_suggested_default_data_length_response(
@@ -298,7 +278,7 @@ EXPORT_SYMBOL extern const module_t controller_module = {
 
 static bool get_is_ready(void) { return readable; }
 
-static const RawAddress* get_address(void) {
+static const bt_bdaddr_t* get_address(void) {
   CHECK(readable);
   return &address;
 }
@@ -402,13 +382,6 @@ static bool supports_ble_privacy(void) {
   return HCI_LE_ENHANCED_PRIVACY_SUPPORTED(features_ble.as_array);
 }
 
-static bool supports_ble_set_privacy_mode() {
-  CHECK(readable);
-  CHECK(ble_supported);
-  return HCI_LE_ENHANCED_PRIVACY_SUPPORTED(features_ble.as_array) &&
-         HCI_LE_SET_PRIVACY_MODE_SUPPORTED(supported_commands);
-}
-
 static bool supports_ble_packet_extension(void) {
   CHECK(readable);
   CHECK(ble_supported);
@@ -470,12 +443,6 @@ static uint16_t get_ble_suggested_default_data_length(void) {
   CHECK(readable);
   CHECK(ble_supported);
   return ble_suggested_default_data_length;
-}
-
-static uint16_t get_ble_maximum_tx_data_length(void) {
-  CHECK(readable);
-  CHECK(ble_supported);
-  return ble_supported_max_tx_octets;
 }
 
 static uint16_t get_ble_maxium_advertising_data_length(void) {
@@ -558,7 +525,6 @@ static const controller_t interface = {
     supports_ble_packet_extension,
     supports_ble_connection_parameters_request,
     supports_ble_privacy,
-    supports_ble_set_privacy_mode,
     supports_ble_2m_phy,
     supports_ble_coded_phy,
     supports_ble_extended_advertising,
@@ -570,7 +536,6 @@ static const controller_t interface = {
     get_acl_packet_size_classic,
     get_acl_packet_size_ble,
     get_ble_suggested_default_data_length,
-    get_ble_maximum_tx_data_length,
     get_ble_maxium_advertising_data_length,
     get_ble_number_of_supported_advertising_sets,
 
@@ -584,7 +549,7 @@ static const controller_t interface = {
     get_local_supported_codecs,
     get_le_all_initiating_phys};
 
-const controller_t* bluetooth::legacy::controller_get_interface() {
+const controller_t* controller_get_interface() {
   static bool loaded = false;
   if (!loaded) {
     loaded = true;
@@ -595,14 +560,6 @@ const controller_t* bluetooth::legacy::controller_get_interface() {
   }
 
   return &interface;
-}
-
-const controller_t* controller_get_interface() {
-  if (bluetooth::shim::is_gd_shim_enabled()) {
-    return bluetooth::shim::controller_get_interface();
-  } else {
-    return bluetooth::legacy::controller_get_interface();
-  }
 }
 
 const controller_t* controller_get_test_interface(
