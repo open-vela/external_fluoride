fbb00d8c2d4fe0c74752a124f8b4da8a02530cf2
openvela-robot <openvela-robot@xiaomi.com>
2019-10-17 03:05:05 +0800

    Snap for 5945950 from 99de5266fe6ecd4d7f5d370ffebb4aaa889a9bb1 to rvc-release
    Change-Id: Icf6049c44d118b4a963cbfaf8bc7bb5b7cf22a2f

--- gd/os/log.h
+++ gd/os/log.h
@@ -18,6 +18,8 @@
 
 #pragma once
 
+#include <stdlib.h>
+
 #ifndef LOG_TAG
 #define LOG_TAG "bt"
 #endif
@@ -26,11 +28,11 @@
 
 #include <log/log.h>
 
-#define LOG_VERBOSE(fmt, args...) ALOGV("%s: " fmt, __PRETTY_FUNCTION__, ##args)
-#define LOG_DEBUG(fmt, args...) ALOGD("%s: " fmt, __PRETTY_FUNCTION__, ##args)
-#define LOG_INFO(fmt, args...) ALOGI("%s: " fmt, __PRETTY_FUNCTION__, ##args)
-#define LOG_WARN(fmt, args...) ALOGW("%s: " fmt, __PRETTY_FUNCTION__, ##args)
-#define LOG_ERROR(fmt, args...) ALOGE("%s: " fmt, __PRETTY_FUNCTION__, ##args)
+#define LOG_VERBOSE(fmt, args...) ALOGV("%s:%d %s: " fmt, __FILE__, __LINE__, __func__, ##args)
+#define LOG_DEBUG(fmt, args...) ALOGD("%s:%d %s: " fmt, __FILE__, __LINE__, __func__, ##args)
+#define LOG_INFO(fmt, args...) ALOGI("%s:%d %s: " fmt, __FILE__, __LINE__, __func__, ##args)
+#define LOG_WARN(fmt, args...) ALOGW("%s:%d %s: " fmt, __FILE__, __LINE__, __func__, ##args)
+#define LOG_ERROR(fmt, args...) ALOGE("%s:%d %s: " fmt, __FILE__, __LINE__, __func__, ##args)
 
 #else
 
@@ -38,25 +40,31 @@
 #include <stdio.h>
 
 #define LOGWRAPPER(fmt, args...) \
-  fprintf(stderr, "%s - %s: " fmt "\n", LOG_TAG, __PRETTY_FUNCTION__, ##args)
+  fprintf(stderr, "%s - %s:%d - %s: " fmt "\n", LOG_TAG, __FILE__, __LINE__, __func__, ##args)
 
 #define LOG_VERBOSE(...) LOGWRAPPER(__VA_ARGS__)
 #define LOG_DEBUG(...) LOGWRAPPER(__VA_ARGS__)
 #define LOG_INFO(...) LOGWRAPPER(__VA_ARGS__)
 #define LOG_WARN(...) LOGWRAPPER(__VA_ARGS__)
 #define LOG_ERROR(...) LOGWRAPPER(__VA_ARGS__)
+#define LOG_ALWAYS_FATAL(...) \
+  do {                        \
+    LOGWRAPPER(__VA_ARGS__);  \
+    abort();                  \
+  } while (false)
 
 #endif /* defined(OS_ANDROID) */
 
-#define ASSERT(condition) \
-  if (!(condition)) { \
-    LOG_ERROR("%s:%d assertion '" #condition "' failed", __FILE__, __LINE__); \
-    abort(); \
-  }
+#define ASSERT(condition)                                    \
+  do {                                                       \
+    if (!(condition)) {                                      \
+      LOG_ALWAYS_FATAL("assertion '" #condition "' failed"); \
+    }                                                        \
+  } while (false)
 
-#define ASSERT_LOG(condition, fmt, args...) \
-  if (!(condition)) { \
-    LOG_ERROR("%s:%d assertion '" #condition "' failed - " fmt, __FILE__, __LINE__, ##args); \
-    abort(); \
-  }
-
+#define ASSERT_LOG(condition, fmt, args...)                                 \
+  do {                                                                      \
+    if (!(condition)) {                                                     \
+      LOG_ALWAYS_FATAL("assertion '" #condition "' failed - " fmt, ##args); \
+    }                                                                       \
+  } while (false)
