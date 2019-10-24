bbd520fe337f2cfbde27b45b8e42b31994bddb57
openvela-robot <openvela-robot@xiaomi.com>
2019-10-25 05:07:57 +0800

    Merge "Prevent bluetooth native crash during failed security authentication" into qt-qpr1-dev am: ea46ec2639
    Change-Id: Ie39b1231cadac6daac7608b8b8c06182c3065c6b

--- vendor_libs/test_vendor_lib/packets/hci/event_packet_builder.h
+++ vendor_libs/test_vendor_lib/packets/hci/event_packet_builder.h
@@ -16,7 +16,6 @@
 
 #pragma once
 
-#include <base/logging.h>
 #include <cstdint>
 #include <memory>
 #include <string>
@@ -106,6 +105,10 @@ class EventPacketBuilder : public HciPacketBuilder {
   // Bluetooth Core Specification Version 4.2, Volume 2, Part E, Section 7.4.2
   static std::unique_ptr<EventPacketBuilder> CreateCommandCompleteReadLocalSupportedCommands(
       hci::Status status, const std::vector<uint8_t>& supported_commands);
+
+  // Bluetooth Core Specification Version 4.2, Volume 2, Part E, Section 7.4.3
+  static std::unique_ptr<EventPacketBuilder> CreateCommandCompleteReadLocalSupportedFeatures(
+      hci::Status status, uint64_t supported_features);
 
   // Bluetooth Core Specification Version 4.2, Volume 2, Part E, Section 7.4.4
   static std::unique_ptr<EventPacketBuilder> CreateCommandCompleteReadLocalExtendedFeatures(
@@ -202,6 +205,10 @@ class EventPacketBuilder : public HciPacketBuilder {
   static std::unique_ptr<EventPacketBuilder> CreateExtendedInquiryResultEvent(
       const Address& bt_address, uint8_t page_scan_repetition_mode, ClassOfDevice class_of_device,
       uint16_t clock_offset, uint8_t rssi, const std::vector<uint8_t>& extended_inquiry_response);
+
+  // Bluetooth Core Specification Version 4.2, Volume 2, Part E, Section 7.7.39
+  static std::unique_ptr<EventPacketBuilder> CreateEncryptionKeyRefreshCompleteEvent(hci::Status status,
+                                                                                     uint16_t handle);
 
   // Bluetooth Core Specification Version 4.2, Volume 2, Part E, Section 7.7.40
   static std::unique_ptr<EventPacketBuilder> CreateIoCapabilityRequestEvent(const Address& peer);
