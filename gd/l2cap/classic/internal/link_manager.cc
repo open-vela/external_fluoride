95c13a3ece6bc32905085f90f24ad2c2b712aad5
openvela-robot <openvela-robot@xiaomi.com>
2019-10-25 13:30:54 +0800

    Merge "Add required header_libs" am: b50cfe70d2 am: 14517ed8b2 am: 30b47b1b83 am: 72659001d7
    Change-Id: I2e2f8d00bd184760053bdd93a42ac34213402689

--- gd/l2cap/classic/internal/link_manager.cc
+++ gd/l2cap/classic/internal/link_manager.cc
@@ -85,13 +85,11 @@ void LinkManager::ConnectFixedChannelServices(hci::Address device,
 void LinkManager::ConnectDynamicChannelServices(hci::Address device,
                                                 PendingDynamicChannelConnection pending_dynamic_channel_connection,
                                                 Psm psm) {
+  // TODO: if there is no link, establish link. Otherwise send command.
   auto* link = GetLink(device);
-  if (link == nullptr) {
-    acl_manager_->CreateConnection(device);
-    // TODO: Resume the channel connection after link is established
+  if (link != nullptr) {
     return;
   }
-  link->SendConnectionRequest(psm, link->ReserveDynamicChannel());
 }
 
 Link* LinkManager::GetLink(const hci::Address device) {
