5d0d7878077f4b6f68f3cc3e7c4687fb25748a9a
liujinye <liujinye@xiaomi.com>
2019-10-26 23:13:19 +0800

    Snap for 5968679 from fbadb0d1421825fcd7b7a8442ff1410603c705a1 to qt-qpr2-release
    Change-Id: I03a4e1138516a3d0e4cb98963f773c23a4f1f2da

--- vendor_libs/test_vendor_lib/model/devices/device_properties.cc
+++ vendor_libs/test_vendor_lib/model/devices/device_properties.cc
@@ -14,16 +14,19 @@
  * limitations under the License.
  */
 
+#define LOG_TAG "device_properties"
+
 #include "device_properties.h"
 
 #include <memory>
 
+#include <base/logging.h>
 #include "base/files/file_util.h"
 #include "base/json/json_reader.h"
 #include "base/values.h"
 
 #include "hci.h"
-#include "os/log.h"
+#include "osi/include/log.h"
 #include "osi/include/osi.h"
 
 using std::vector;
@@ -51,17 +54,14 @@ DeviceProperties::DeviceProperties(const std::string& file_name)
       le_data_packet_length_(27), num_le_data_packets_(15), le_white_list_size_(15) {
   std::string properties_raw;
 
-  ASSERT(Address::FromString("BB:BB:BB:BB:BB:AD", address_));
-  ASSERT(Address::FromString("BB:BB:BB:BB:AD:1E", le_address_));
+  CHECK(Address::FromString("BB:BB:BB:BB:BB:AD", address_));
+  CHECK(Address::FromString("BB:BB:BB:BB:AD:1E", le_address_));
   name_ = {'D', 'e', 'f', 'a', 'u', 'l', 't'};
 
   supported_codecs_ = {0};  // Only SBC is supported.
   vendor_specific_codecs_ = {};
 
-  for (int i = 0; i < 35; i++) supported_commands_.push_back(0xff);
-  // Mark HCI_LE_Transmitter_Test[v2] and newer commands as unsupported
-  // TODO: Implement a better mapping.
-  for (int i = 35; i < 64; i++) supported_commands_.push_back(0x00);
+  for (int i = 0; i < 64; i++) supported_commands_.push_back(0xff);
 
   le_supported_features_ = 0x1f;
   le_supported_states_ = 0x3ffffffffff;
@@ -70,14 +70,15 @@ DeviceProperties::DeviceProperties(const std::string& file_name)
   if (file_name.size() == 0) {
     return;
   }
-  LOG_INFO("Reading controller properties from %s.", file_name.c_str());
+  LOG_INFO(LOG_TAG, "Reading controller properties from %s.", file_name.c_str());
   if (!base::ReadFileToString(base::FilePath(file_name), &properties_raw)) {
-    LOG_ERROR("Error reading controller properties from file.");
+    LOG_ERROR(LOG_TAG, "Error reading controller properties from file.");
     return;
   }
 
   std::unique_ptr<base::Value> properties_value_ptr = base::JSONReader::Read(properties_raw);
-  if (properties_value_ptr.get() == nullptr) LOG_INFO("Error controller properties may consist of ill-formed JSON.");
+  if (properties_value_ptr.get() == nullptr)
+    LOG_INFO(LOG_TAG, "Error controller properties may consist of ill-formed JSON.");
 
   // Get the underlying base::Value object, which is of type
   // base::Value::TYPE_DICTIONARY, and read it into member variables.
@@ -85,7 +86,7 @@ DeviceProperties::DeviceProperties(const std::string& file_name)
   base::JSONValueConverter<DeviceProperties> converter;
 
   if (!converter.Convert(properties_dictionary, this))
-    LOG_INFO("Error converting JSON properties into Properties object.");
+    LOG_INFO(LOG_TAG, "Error converting JSON properties into Properties object.");
 }
 
 // static
