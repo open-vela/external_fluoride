c5c6a7bb30d718dcf8c1d5dfaca7b64252e4cd6d
liujinye <liujinye@xiaomi.com>
2019-10-30 11:50:41 +0800

    DO NOT MERGE - qt-qpr1-dev-plus-aosp-without-vendor@5915889 into stage-aosp-master
    Bug: 142003500
    Change-Id: I9273c7462b91567750d149aee63f91635e1c3fc9

--- gd/l2cap/signal_id.h
+++ gd/l2cap/signal_id.h
@@ -48,7 +48,6 @@ struct SignalId {
 };
 
 constexpr SignalId kInvalidSignalId{0};
-constexpr SignalId kInitialSignalId{1};
 
 inline bool operator==(const SignalId& lhs, const SignalId& rhs) {
   return lhs.value_ == rhs.value_;
