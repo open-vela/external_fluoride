b3cb5790060bc55bf49f443d1f6539f5ea3ef1e5
liujinye <liujinye@xiaomi.com>
2019-11-04 19:41:00 +0800

    Merge "Connect inquiry API query implementation"

--- main/shim/btm.cc
+++ main/shim/btm.cc
@@ -36,8 +36,7 @@ static constexpr uint8_t kExtendedInquiryResultMode = 2;
 
 extern void btm_process_cancel_complete(uint8_t status, uint8_t mode);
 extern void btm_process_inq_complete(uint8_t status, uint8_t result_type);
-extern void btm_process_inq_results(uint8_t* p, uint8_t hci_evt_len,
-                                    uint8_t result_mode);
+extern void btm_process_inq_results(uint8_t* p, uint8_t result_mode);
 
 /**
  * Inquiry
@@ -46,8 +45,7 @@ void bluetooth::shim::Btm::OnInquiryResult(std::vector<const uint8_t> result) {
   CHECK(result.size() < kMaxInquiryResultSize);
 
   std::copy(result.begin(), result.end(), inquiry_result_buf);
-  btm_process_inq_results(inquiry_result_buf, result.size(),
-                          kInquiryResultMode);
+  btm_process_inq_results(inquiry_result_buf, kInquiryResultMode);
 }
 
 void bluetooth::shim::Btm::OnInquiryResultWithRssi(
@@ -55,8 +53,7 @@ void bluetooth::shim::Btm::OnInquiryResultWithRssi(
   CHECK(result.size() < kMaxInquiryResultSize);
 
   std::copy(result.begin(), result.end(), inquiry_result_buf);
-  btm_process_inq_results(inquiry_result_buf, result.size(),
-                          kInquiryResultWithRssiMode);
+  btm_process_inq_results(inquiry_result_buf, kInquiryResultWithRssiMode);
 }
 
 void bluetooth::shim::Btm::OnExtendedInquiryResult(
@@ -64,8 +61,7 @@ void bluetooth::shim::Btm::OnExtendedInquiryResult(
   CHECK(result.size() < kMaxInquiryResultSize);
 
   std::copy(result.begin(), result.end(), inquiry_result_buf);
-  btm_process_inq_results(inquiry_result_buf, result.size(),
-                          kExtendedInquiryResultMode);
+  btm_process_inq_results(inquiry_result_buf, kExtendedInquiryResultMode);
 }
 
 void bluetooth::shim::Btm::OnInquiryComplete(uint16_t status) {
@@ -232,6 +228,14 @@ bool bluetooth::shim::Btm::StartPeriodicInquiry(
 
 void bluetooth::shim::Btm::CancelPeriodicInquiry() {
   bluetooth::shim::GetInquiry()->StopPeriodicInquiry();
+}
+
+bool bluetooth::shim::Btm::IsGeneralPeriodicInquiryActive() const {
+  return bluetooth::shim::GetInquiry()->IsGeneralPeriodicInquiryActive();
+}
+
+bool bluetooth::shim::Btm::IsLimitedPeriodicInquiryActive() const {
+  return bluetooth::shim::GetInquiry()->IsLimitedPeriodicInquiryActive();
 }
 
 /**
