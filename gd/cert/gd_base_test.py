611bc581c03a5eb16a02ea46c84d9997bb7781f2
liujinye <liujinye@xiaomi.com>
2019-11-06 08:03:42 +0800

    Merge "Remove unused events in acl manager"

--- gd/cert/gd_base_test.py
+++ gd/cert/gd_base_test.py
@@ -70,6 +70,7 @@ class GdBaseTestClass(BaseTestClass):
             builtin=True)
 
     def teardown_class(self):
+        self.unregister_controllers()
         if self.rootcanal_running:
             self.rootcanal_process.send_signal(signal.SIGINT)
             rootcanal_return_code = self.rootcanal_process.wait()
