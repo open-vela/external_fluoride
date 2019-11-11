1e69918e438342972372be39ee2bbc2051142d32
openvela-robot <openvela-robot@xiaomi.com>
2019-11-11 21:09:00 +0800

    Add protection to prevent null pointer dereference.
    * Potential null pointer dereference risk to cause Bluetooth native
    crash.
    
    Bug: 144102387
    Test: Pair with BLE device
    Change-Id: I1986899a3f4e472a68a589fa2465118d66a27915

--- btif/src/btif_dm.cc
+++ btif/src/btif_dm.cc
@@ -925,8 +925,8 @@ static void btif_dm_pin_req_evt(tBTA_DM_PIN_REQ* p_pin_req) {
  ******************************************************************************/
 static void btif_dm_ssp_cfm_req_evt(tBTA_DM_SP_CFM_REQ* p_ssp_cfm_req) {
   bt_bdname_t bd_name;
-  bool is_incoming = !(pairing_cb.state == BT_BOND_STATE_BONDING);
   uint32_t cod;
+  bool is_incoming = !(pairing_cb.state == BT_BOND_STATE_BONDING);
   int dev_type;
 
   BTIF_TRACE_DEBUG("%s", __func__);
@@ -1179,7 +1179,6 @@ static void btif_dm_auth_cmpl_evt(tBTA_DM_AUTH_CMPL* p_auth_cmpl) {
       case HCI_ERR_AUTH_FAILURE:
       case HCI_ERR_KEY_MISSING:
         btif_storage_remove_bonded_device(&bd_addr);
-        [[fallthrough]];
       case HCI_ERR_HOST_REJECT_SECURITY:
       case HCI_ERR_ENCRY_MODE_NOT_ACCEPTABLE:
       case HCI_ERR_UNIT_KEY_USED:
