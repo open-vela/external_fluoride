ffd5f77dd5e1bd992d43b8bb6408b724470b618e
openvela-robot <openvela-robot@xiaomi.com>
2019-11-14 04:04:58 +0800

    Snap for 6007460 from f7edbea32e1e7d27fb4dc3b9a7d2f19081fc8540 to rvc-release
    Change-Id: I2e815c4332b77c1d45949431a8e5b5639d17f62f

--- gd/l2cap/classic/internal/link_manager.h
+++ gd/l2cap/classic/internal/link_manager.h
@@ -52,6 +52,12 @@ class LinkManager : public hci::ConnectionCallbacks {
     FixedChannelManager::OnConnectionFailureCallback on_fail_callback_;
   };
 
+  struct PendingDynamicChannelConnection {
+    os::Handler* handler_;
+    DynamicChannelManager::OnConnectionOpenCallback on_open_callback_;
+    DynamicChannelManager::OnConnectionFailureCallback on_fail_callback_;
+  };
+
   struct PendingLink {
     std::vector<PendingFixedChannelConnection> pending_fixed_channel_connections_;
   };
@@ -70,7 +76,7 @@ class LinkManager : public hci::ConnectionCallbacks {
   // DynamicChannelManager methods
 
   void ConnectDynamicChannelServices(hci::Address device,
-                                     Link::PendingDynamicChannelConnection pending_dynamic_channel_connection, Psm psm);
+                                     PendingDynamicChannelConnection pending_dynamic_channel_connection, Psm psm);
 
  private:
   // Dependencies
@@ -84,8 +90,7 @@ class LinkManager : public hci::ConnectionCallbacks {
   std::unordered_map<hci::Address, PendingLink> pending_links_;
   std::unordered_map<hci::Address, Link> links_;
   std::unordered_map<hci::Address, std::list<Psm>> pending_dynamic_channels_;
-  std::unordered_map<hci::Address, std::list<Link::PendingDynamicChannelConnection>>
-      pending_dynamic_channels_callbacks_;
+  std::unordered_map<hci::Address, std::list<PendingDynamicChannelConnection>> pending_dynamic_channels_callbacks_;
   DISALLOW_COPY_AND_ASSIGN(LinkManager);
 };
 
