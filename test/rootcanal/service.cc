e5ef67d7081d2dfa019a2c9e3f8d34a0d97db7d0
liujinye <liujinye@xiaomi.com>
2019-11-15 22:01:47 +0800

    Merge changes I4be323bc,I737488ed,Iba0c6ee1
    * changes:
      L2CAP: Simplify parameters for segmenter and reassembler
      L2CAP: Make a common ChannelImpl interface
      L2CAP Segmenter: Keep a reference to DynamicChannelImpl

--- test/rootcanal/service.cc
+++ test/rootcanal/service.cc
@@ -16,18 +16,19 @@
 
 #define LOG_TAG "android.hardware.bluetooth@1.0-service.sim"
 
+#include "os/log.h"
+
 #include <android/hardware/bluetooth/1.0/IBluetoothHci.h>
 #include <hidl/HidlSupport.h>
 #include <hidl/HidlTransportSupport.h>
-#include <utils/Log.h>
 
 #include "bluetooth_hci.h"
 
+using ::android::sp;
 using ::android::hardware::configureRpcThreadpool;
+using ::android::hardware::joinRpcThreadpool;
 using ::android::hardware::bluetooth::V1_0::IBluetoothHci;
 using ::android::hardware::bluetooth::V1_0::sim::BluetoothHci;
-using ::android::hardware::joinRpcThreadpool;
-using ::android::sp;
 
 int main(int /* argc */, char** /* argv */) {
   sp<IBluetoothHci> bluetooth = new BluetoothHci;
@@ -36,5 +37,5 @@ int main(int /* argc */, char** /* argv */) {
   if (status == android::OK)
     joinRpcThreadpool();
   else
-    ALOGE("Could not register as a service!");
+    LOG_ERROR("Could not register as a service!");
 }
