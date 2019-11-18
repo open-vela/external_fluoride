d14379d3812e800e5396c967249d221a092963f3
openvela-robot <openvela-robot@xiaomi.com>
2019-11-19 04:07:42 +0800

    Snap for 6016664 from c2cf7b183d05aae236320b6d250cdcbe012f5f8f to rvc-release
    Change-Id: I9300af41431dd3f1baba252affee258ac06ee323

--- gd/l2cap/internal/channel_impl.h
+++ gd/l2cap/internal/channel_impl.h
@@ -38,7 +38,7 @@ class ChannelImpl {
   GetQueueUpEnd() = 0;
 
   /**
-   * Return the queue end for lower layer (sender and receiver)
+   * Return the queue end for lower layer (segmenter and reassembler)
    */
   virtual common::BidiQueueEnd<packet::PacketView<packet::kLittleEndian>, packet::BasePacketBuilder>*
   GetQueueDownEnd() = 0;
--- gd/packet/parser/struct_parser_generator.cc
+++ gd/packet/parser/struct_parser_generator.cc
@@ -16,12 +16,11 @@
 
 #include "struct_parser_generator.h"
 
-StructParserGenerator::StructParserGenerator(const Declarations& decls) {
+StructParserGenerator::StructParserGenerator(Declarations& decls) {
   is_little_endian = decls.is_little_endian;
-  for (const auto& s : decls.type_defs_queue_) {
+  for (auto& s : decls.type_defs_queue_) {
     if (s.second->GetDefinitionType() == TypeDef::Type::STRUCT) {
-      const auto* struct_def = dynamic_cast<const StructDef*>(s.second);
-      variable_struct_fields_.emplace_back(struct_def);
+      variable_struct_fields_.push_back((StructDef*)s.second);
     }
   }
   for (const auto& node : variable_struct_fields_) {
@@ -37,10 +36,10 @@ StructParserGenerator::StructParserGenerator(const Declarations& decls) {
 
 void StructParserGenerator::explore_children(const TreeNode& node, std::ostream& s) const {
   auto field = node.packet_field_;
-  if (!node.children_.empty()) {
+  if (node.children_.size() > 0) {
     s << "bool " << field->GetName() << "_child_found = false; /* Greedy match */";
   }
-  for (const auto& child : node.children_) {
+  for (const auto child : node.children_) {
     s << "if (!" << field->GetName() << "_child_found && ";
     s << child->struct_def_->name_ << "::IsInstance(*" << field->GetName() << "_value.get())) {";
     s << field->GetName() << "_child_found = true;";
@@ -66,33 +65,32 @@ void StructParserGenerator::explore_children(const TreeNode& node, std::ostream&
 
 void StructParserGenerator::Generate(std::ostream& s) const {
   for (const auto& node : variable_struct_fields_) {
-    if (node.children_.empty()) {
-      continue;
-    }
-    auto field = node.packet_field_;
-    s << "inline std::unique_ptr<" << node.struct_def_->name_ << "> Parse" << node.struct_def_->name_;
-    if (is_little_endian) {
-      s << "(Iterator<kLittleEndian> to_bound) {";
-    } else {
-      s << "(Iterator<!kLittleEndian> to_bound) {";
-    }
-    s << field->GetDataType() << " " << field->GetName() << "_value = ";
-    s << "std::make_unique<" << node.struct_def_->name_ << ">();";
+    if (node.children_.size() > 0) {
+      auto field = node.packet_field_;
+      s << "inline std::unique_ptr<" << node.struct_def_->name_ << "> Parse" << node.struct_def_->name_;
+      if (is_little_endian) {
+        s << "(Iterator<kLittleEndian> to_bound) {";
+      } else {
+        s << "(Iterator<!kLittleEndian> to_bound) {";
+      }
+      s << field->GetDataType() << " " << field->GetName() << "_value = ";
+      s << "std::make_unique<" << node.struct_def_->name_ << ">();";
 
-    s << "auto " << field->GetName() << "_it = to_bound;";
-    s << "auto optional_it = ";
-    s << node.struct_def_->name_ << "::Parse( " << field->GetName() << "_value.get(), ";
-    s << field->GetName() << "_it";
-    if (node.struct_def_->parent_ != nullptr) {
-      s << ", true);";
-    } else {
-      s << ");";
-    }
-    s << "if (optional_it) {";
-    s << field->GetName() << "_it = *optional_it;";
-    s << "} else { return nullptr; }";
+      s << "auto " << field->GetName() << "_it = to_bound;";
+      s << "auto optional_it = ";
+      s << node.struct_def_->name_ << "::Parse( " << field->GetName() << "_value.get(), ";
+      s << field->GetName() << "_it";
+      if (node.struct_def_->parent_ != nullptr) {
+        s << ", true);";
+      } else {
+        s << ");";
+      }
+      s << "if (optional_it) {";
+      s << field->GetName() << "_it = *optional_it;";
+      s << "} else { return nullptr; }";
 
-    explore_children(node, s);
-    s << "return " << field->GetName() << "_value; }";
+      explore_children(node, s);
+      s << "return " << field->GetName() << "_value; }";
+    }
   }
 }
