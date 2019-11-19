a82d80e1111e56975f4c480990b3b3524ccdcbe4
openvela-robot <openvela-robot@xiaomi.com>
2019-11-20 00:36:51 +0800

    Rootcanal: Add SetExtendedProperties am: 171ccb21c9 am: 5935732557 am: b793444724
    Change-Id: I8e551d8a0e3dbd616eb0c46672c158e431c5f79d

--- vendor_libs/test_vendor_lib/model/setup/phy_layer_factory.cc
+++ vendor_libs/test_vendor_lib/model/setup/phy_layer_factory.cc
@@ -15,7 +15,6 @@
  */
 
 #include "phy_layer_factory.h"
-#include <sstream>
 
 namespace test_vendor_lib {
 
@@ -32,11 +31,10 @@ uint32_t PhyLayerFactory::GetFactoryId() {
 
 std::shared_ptr<PhyLayer> PhyLayerFactory::GetPhyLayer(
     const std::function<void(model::packets::LinkLayerPacketView)>&
-        device_receive,
-    uint32_t device_id) {
-  std::shared_ptr<PhyLayer> new_phy = std::make_shared<PhyLayerImpl>(
-      phy_type_, next_id_++, device_receive, device_id,
-      std::shared_ptr<PhyLayerFactory>(this));
+        device_receive) {
+  std::shared_ptr<PhyLayer> new_phy =
+      std::make_shared<PhyLayerImpl>(phy_type_, next_id_++, device_receive,
+                                     std::shared_ptr<PhyLayerFactory>(this));
   phy_layers_.push_back(new_phy);
   return new_phy;
 }
@@ -65,12 +63,16 @@ void PhyLayerFactory::Send(
       model::packets::LinkLayerPacketView::Create(packet_view);
   ASSERT(link_layer_packet_view.IsValid());
 
-  Send(link_layer_packet_view, id);
+  for (const auto phy : phy_layers_) {
+    if (id != phy->GetId()) {
+      phy->Receive(link_layer_packet_view);
+    }
+  }
 }
 
 void PhyLayerFactory::Send(model::packets::LinkLayerPacketView packet,
                            uint32_t id) {
-  for (const auto& phy : phy_layers_) {
+  for (const auto phy : phy_layers_) {
     if (id != phy->GetId()) {
       phy->Receive(packet);
     }
@@ -78,37 +80,30 @@ void PhyLayerFactory::Send(model::packets::LinkLayerPacketView packet,
 }
 
 void PhyLayerFactory::TimerTick() {
-  for (auto& phy : phy_layers_) {
+  for (auto phy : phy_layers_) {
     phy->TimerTick();
   }
 }
 
 std::string PhyLayerFactory::ToString() const {
-  std::stringstream factory;
   switch (phy_type_) {
     case Phy::Type::LOW_ENERGY:
-      factory << "LOW_ENERGY: ";
+      return "LOW_ENERGY";
       break;
     case Phy::Type::BR_EDR:
-      factory << "BR_EDR: ";
+      return "BR_EDR";
       break;
     default:
-      factory << "Unknown: ";
+      return "Unknown";
   }
-  for (auto& phy : phy_layers_) {
-    factory << phy->GetDeviceId();
-    factory << ",";
-  }
-
-  return factory.str();
 }
 
 PhyLayerImpl::PhyLayerImpl(
     Phy::Type phy_type, uint32_t id,
     const std::function<void(model::packets::LinkLayerPacketView)>&
         device_receive,
-    uint32_t device_id, const std::shared_ptr<PhyLayerFactory> factory)
-    : PhyLayer(phy_type, id, device_receive, device_id), factory_(factory) {}
+    const std::shared_ptr<PhyLayerFactory>& factory)
+    : PhyLayer(phy_type, id, device_receive), factory_(factory) {}
 
 PhyLayerImpl::~PhyLayerImpl() {
   Unregister();
