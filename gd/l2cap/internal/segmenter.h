45a9da71c33319d725a2a754726887de1c721683
openvela-robot <openvela-robot@xiaomi.com>
2019-11-19 18:09:49 +0800

    Merge "Modify aidl to support new connect/disconnect interface" am: 7846d989d7 am: 7f6144c069 am: f7bade9f2f
    Change-Id: I454e42ffb6b1010c70e6ef72e7b8433b73cf859e

--- gd/l2cap/internal/segmenter.h
+++ gd/l2cap/internal/segmenter.h
@@ -21,10 +21,8 @@
 
 #include "common/bidi_queue.h"
 #include "common/bind.h"
-#include "data_controller.h"
 #include "l2cap/cid.h"
 #include "l2cap/internal/channel_impl.h"
-#include "l2cap/internal/data_controller.h"
 #include "os/handler.h"
 #include "os/queue.h"
 #include "packet/base_packet_builder.h"
@@ -39,42 +37,40 @@ class Scheduler;
  * A middle layer between L2CAP channel and outgoing packet scheduler.
  * Fetches data (SDU) from an L2CAP channel queue end, handles L2CAP segmentation, and gives data to L2CAP scheduler.
  */
-class Sender {
+class Segmenter {
  public:
   using UpperEnqueue = packet::PacketView<packet::kLittleEndian>;
   using UpperDequeue = packet::BasePacketBuilder;
   using UpperQueueDownEnd = common::BidiQueueEnd<UpperEnqueue, UpperDequeue>;
 
-  Sender(os::Handler* handler, Scheduler* scheduler, std::shared_ptr<ChannelImpl> channel);
-  ~Sender();
+  Segmenter(os::Handler* handler, Scheduler* scheduler, std::shared_ptr<ChannelImpl> channel);
+  ~Segmenter();
 
   /**
    * Callback from scheduler to indicate that scheduler already dequeued a packet from segmenter's queue.
    * Segmenter can continue dequeuing from channel queue end.
    */
-  void OnPacketSent();
+  void NotifyPacketSent();
 
   /**
    * Called by the scheduler to return the next PDU to be sent
    */
   std::unique_ptr<UpperDequeue> GetNextPacket();
 
-  void SetChannelRetransmissionFlowControlMode(RetransmissionAndFlowControlModeOption mode);
-
-  DataController* GetDataController();
-
  private:
   os::Handler* handler_;
   UpperQueueDownEnd* queue_end_;
+  std::queue<std::unique_ptr<UpperDequeue>> pdu_buffer_;
   Scheduler* scheduler_;
   const Cid channel_id_;
   const Cid remote_channel_id_;
+  std::shared_ptr<ChannelImpl> channel_;
   bool is_dequeue_registered_ = false;
-  RetransmissionAndFlowControlModeOption mode_ = RetransmissionAndFlowControlModeOption::L2CAP_BASIC;
-  std::unique_ptr<DataController> data_controller_;
 
   void try_register_dequeue();
   void dequeue_callback();
+  void handle_basic_mode_sdu(std::unique_ptr<UpperDequeue> packet);
+  void handle_enhanced_retransmission_mode_sdu(std::unique_ptr<UpperDequeue> packet);
 };
 }  // namespace internal
 }  // namespace l2cap
--- gd/packet/parser/fields/struct_field.cc
+++ gd/packet/parser/fields/struct_field.cc
@@ -44,14 +44,8 @@ void StructField::GenExtractor(std::ostream& s, int, bool) const {
   s << GetDataType() << "::Parse(" << GetName() << "_ptr, " << GetName() << "_it);";
 }
 
-std::string StructField::GetGetterFunctionName() const {
-  std::stringstream ss;
-  ss << "Get" << util::UnderscoreToCamelCase(GetName());
-  return ss.str();
-}
-
 void StructField::GenGetter(std::ostream& s, Size start_offset, Size end_offset) const {
-  s << GetDataType() << " " << GetGetterFunctionName() << "() const {";
+  s << GetDataType() << " Get" << util::UnderscoreToCamelCase(GetName()) << "() const {";
   s << "ASSERT(was_validated_);";
   s << "size_t end_index = size();";
   s << "auto to_bound = begin();";
@@ -64,8 +58,9 @@ void StructField::GenGetter(std::ostream& s, Size start_offset, Size end_offset)
   s << "}\n";
 }
 
-std::string StructField::GetBuilderParameterType() const {
-  return GetDataType();
+bool StructField::GenBuilderParameter(std::ostream& s) const {
+  s << GetDataType() << " " << GetName();
+  return true;
 }
 
 bool StructField::HasParameterValidator() const {
