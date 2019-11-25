245c354c979ed3920cdde858908ec3bfcd69a19c
openvela-robot <openvela-robot@xiaomi.com>
2019-11-25 18:47:13 +0800

    Merge "Revert "Fix potential OOB write in btm_read_remote_ext_features_complete"" into qt-dev am: db7ae0ecc0 am: a9b717c00f am: 5d42028684
    Change-Id: I423ceb3e7e90060f52f49f48b648f75caea1b8d4

--- bta/hf_client/bta_hf_client_at.cc
+++ bta/hf_client/bta_hf_client_at.cc
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  *  Copyright (c) 2014 The Android Open Source Project
- *  Copyright (C) 2003-2012 Broadcom Corporation
+ *  Copyright 2003-2012 Broadcom Corporation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -40,13 +40,6 @@
 
 /* timeout (in milliseconds) for AT hold timer */
 #define BTA_HF_CLIENT_AT_HOLD_TIMEOUT 41
-
-/******************************************************************************
- *
- *          DATA TYPES AND CONTAINERS
- *
- ******************************************************************************/
-extern fixed_queue_t* btu_bta_alarm_queue;
 
 /******************************************************************************
  *       SUPPORTED EVENT MESSAGES
@@ -164,9 +157,8 @@ static void bta_hf_client_at_resp_timer_cback(void* data) {
 }
 
 static void bta_hf_client_start_at_resp_timer(tBTA_HF_CLIENT_CB* client_cb) {
-  alarm_set_on_queue(client_cb->at_cb.resp_timer, BTA_HF_CLIENT_AT_TIMEOUT,
-                     bta_hf_client_at_resp_timer_cback, (void*)client_cb,
-                     btu_bta_alarm_queue);
+  alarm_set_on_mloop(client_cb->at_cb.resp_timer, BTA_HF_CLIENT_AT_TIMEOUT,
+                     bta_hf_client_at_resp_timer_cback, (void*)client_cb);
 }
 
 static void bta_hf_client_stop_at_resp_timer(tBTA_HF_CLIENT_CB* client_cb) {
@@ -178,7 +170,7 @@ static void bta_hf_client_send_at(tBTA_HF_CLIENT_CB* client_cb,
                                   uint16_t buf_len) {
   APPL_TRACE_DEBUG("%s", __func__);
   if ((client_cb->at_cb.current_cmd == BTA_HF_CLIENT_AT_NONE ||
-       client_cb->svc_conn == false) &&
+       !client_cb->svc_conn) &&
       !alarm_is_scheduled(client_cb->at_cb.hold_timer)) {
     uint16_t len;
 
@@ -234,9 +226,8 @@ static void bta_hf_client_stop_at_hold_timer(tBTA_HF_CLIENT_CB* client_cb) {
 
 static void bta_hf_client_start_at_hold_timer(tBTA_HF_CLIENT_CB* client_cb) {
   APPL_TRACE_DEBUG("%s", __func__);
-  alarm_set_on_queue(client_cb->at_cb.hold_timer, BTA_HF_CLIENT_AT_HOLD_TIMEOUT,
-                     bta_hf_client_at_hold_timer_cback, (void*)client_cb,
-                     btu_bta_alarm_queue);
+  alarm_set_on_mloop(client_cb->at_cb.hold_timer, BTA_HF_CLIENT_AT_HOLD_TIMEOUT,
+                     bta_hf_client_at_hold_timer_cback, (void*)client_cb);
 }
 
 /******************************************************************************
@@ -266,7 +257,7 @@ static void bta_hf_client_handle_ok(tBTA_HF_CLIENT_CB* client_cb) {
       client_cb->at_cb.current_cmd = BTA_HF_CLIENT_AT_NONE;
       return;
     case BTA_HF_CLIENT_AT_CLIP:  // last cmd is post slc seq
-      if (client_cb->send_at_reply == false) {
+      if (!client_cb->send_at_reply) {
         client_cb->send_at_reply = true;
       }
       break;
@@ -305,7 +296,7 @@ static void bta_hf_client_handle_error(tBTA_HF_CLIENT_CB* client_cb,
       bta_hf_client_cback_sco(client_cb, BTA_HF_CLIENT_AUDIO_CLOSE_EVT);
       break;
     case BTA_HF_CLIENT_AT_CLIP:  // last cmd is post slc seq
-      if (client_cb->send_at_reply == false) {
+      if (!client_cb->send_at_reply) {
         client_cb->send_at_reply = true;
       }
       break;
@@ -573,7 +564,7 @@ void bta_hf_client_ind(tBTA_HF_CLIENT_CB* client_cb,
   evt.ind.type = type;
   evt.ind.value = value;
 
-  bdcpy(evt.ind.bd_addr, client_cb->peer_addr);
+  evt.ind.bd_addr = client_cb->peer_addr;
   bta_hf_client_app_callback(BTA_HF_CLIENT_IND_EVT, &evt);
 }
 
@@ -594,7 +585,7 @@ void bta_hf_client_evt_val(tBTA_HF_CLIENT_CB* client_cb,
 
   memset(&evt, 0, sizeof(evt));
 
-  bdcpy(evt.val.bd_addr, client_cb->peer_addr);
+  evt.val.bd_addr = client_cb->peer_addr;
   evt.val.value = value;
 
   bta_hf_client_app_callback(type, &evt);
@@ -618,7 +609,7 @@ void bta_hf_client_operator_name(tBTA_HF_CLIENT_CB* client_cb, char* name) {
   strlcpy(evt.operator_name.name, name, BTA_HF_CLIENT_OPERATOR_NAME_LEN + 1);
   evt.operator_name.name[BTA_HF_CLIENT_OPERATOR_NAME_LEN] = '\0';
 
-  bdcpy(evt.operator_name.bd_addr, client_cb->peer_addr);
+  evt.operator_name.bd_addr = client_cb->peer_addr;
   bta_hf_client_app_callback(BTA_HF_CLIENT_OPERATOR_NAME_EVT, &evt);
 }
 
@@ -640,7 +631,7 @@ void bta_hf_client_clip(tBTA_HF_CLIENT_CB* client_cb, char* number) {
   strlcpy(evt.number.number, number, BTA_HF_CLIENT_NUMBER_LEN + 1);
   evt.number.number[BTA_HF_CLIENT_NUMBER_LEN] = '\0';
 
-  bdcpy(evt.number.bd_addr, client_cb->peer_addr);
+  evt.number.bd_addr = client_cb->peer_addr;
   bta_hf_client_app_callback(BTA_HF_CLIENT_CLIP_EVT, &evt);
 }
 
@@ -662,7 +653,7 @@ void bta_hf_client_ccwa(tBTA_HF_CLIENT_CB* client_cb, char* number) {
   strlcpy(evt.number.number, number, BTA_HF_CLIENT_NUMBER_LEN + 1);
   evt.number.number[BTA_HF_CLIENT_NUMBER_LEN] = '\0';
 
-  bdcpy(evt.number.bd_addr, client_cb->peer_addr);
+  evt.number.bd_addr = client_cb->peer_addr;
   bta_hf_client_app_callback(BTA_HF_CLIENT_CCWA_EVT, &evt);
 }
 
@@ -685,7 +676,7 @@ void bta_hf_client_at_result(tBTA_HF_CLIENT_CB* client_cb,
   evt.result.type = type;
   evt.result.cme = cme;
 
-  bdcpy(evt.result.bd_addr, client_cb->peer_addr);
+  evt.result.bd_addr = client_cb->peer_addr;
   bta_hf_client_app_callback(BTA_HF_CLIENT_AT_RESULT_EVT, &evt);
 }
 
@@ -717,7 +708,7 @@ void bta_hf_client_clcc(tBTA_HF_CLIENT_CB* client_cb, uint32_t idx,
     evt.clcc.number[BTA_HF_CLIENT_NUMBER_LEN] = '\0';
   }
 
-  bdcpy(evt.clcc.bd_addr, client_cb->peer_addr);
+  evt.clcc.bd_addr = client_cb->peer_addr;
   bta_hf_client_app_callback(BTA_HF_CLIENT_CLCC_EVT, &evt);
 }
 
@@ -733,16 +724,26 @@ void bta_hf_client_clcc(tBTA_HF_CLIENT_CB* client_cb, uint32_t idx,
  ******************************************************************************/
 void bta_hf_client_cnum(tBTA_HF_CLIENT_CB* client_cb, char* number,
                         uint16_t service) {
-  tBTA_HF_CLIENT evt;
-
-  memset(&evt, 0, sizeof(evt));
+  tBTA_HF_CLIENT evt = {};
 
   evt.cnum.service = service;
   strlcpy(evt.cnum.number, number, BTA_HF_CLIENT_NUMBER_LEN + 1);
   evt.cnum.number[BTA_HF_CLIENT_NUMBER_LEN] = '\0';
 
-  bdcpy(evt.cnum.bd_addr, client_cb->peer_addr);
+  evt.cnum.bd_addr = client_cb->peer_addr;
   bta_hf_client_app_callback(BTA_HF_CLIENT_CNUM_EVT, &evt);
+}
+
+void bta_hf_client_unknown_response(tBTA_HF_CLIENT_CB* client_cb,
+                                    const char* evt_buffer) {
+  tBTA_HF_CLIENT evt = {};
+
+  strlcpy(evt.unknown.event_string, evt_buffer,
+          BTA_HF_CLIENT_UNKOWN_EVENT_LEN + 1);
+  evt.unknown.event_string[BTA_HF_CLIENT_UNKOWN_EVENT_LEN] = '\0';
+
+  evt.unknown.bd_addr = client_cb->peer_addr;
+  bta_hf_client_app_callback(BTA_HF_CLIENT_UNKNOWN_EVT, &evt);
 }
 
 /*******************************************************************************
@@ -763,7 +764,7 @@ void bta_hf_client_binp(tBTA_HF_CLIENT_CB* client_cb, char* number) {
   strlcpy(evt.number.number, number, BTA_HF_CLIENT_NUMBER_LEN + 1);
   evt.number.number[BTA_HF_CLIENT_NUMBER_LEN] = '\0';
 
-  bdcpy(evt.number.bd_addr, client_cb->peer_addr);
+  evt.number.bd_addr = client_cb->peer_addr;
   bta_hf_client_app_callback(BTA_HF_CLIENT_BINP_EVT, &evt);
 }
 
@@ -1445,6 +1446,36 @@ static char* bta_hf_client_skip_unknown(tBTA_HF_CLIENT_CB* client_cb,
   return buffer;
 }
 
+static char* bta_hf_client_process_unknown(tBTA_HF_CLIENT_CB* client_cb,
+                                           char* buffer) {
+  char* start = strstr(buffer, "\r\n");
+  if (start == NULL) {
+    return NULL;
+  }
+  start += sizeof("\r\n") - 1;
+
+  char* end = strstr(start, "\r\n");
+  if (end == NULL) {
+    return NULL;
+  }
+
+  int evt_size = end - start + 1;
+
+  char tmp_buf[BTA_HF_CLIENT_UNKOWN_EVENT_LEN];
+  if (evt_size < BTA_HF_CLIENT_UNKOWN_EVENT_LEN) {
+    strlcpy(tmp_buf, start, evt_size);
+    bta_hf_client_unknown_response(client_cb, tmp_buf);
+    AT_CHECK_RN(end);
+  } else {
+    APPL_TRACE_ERROR("%s: exceed event buffer size. (%d, %d)", __func__,
+                     evt_size, BTA_HF_CLIENT_UNKOWN_EVENT_LEN);
+  }
+
+  APPL_TRACE_DEBUG("%s: %s", __func__, buffer);
+
+  return end;
+}
+
 /******************************************************************************
  *       SUPPORTED EVENT MESSAGES
  ******************************************************************************/
@@ -1470,7 +1501,7 @@ static const tBTA_HF_CLIENT_PARSER_CALLBACK bta_hf_client_parser_cb[] = {
     bta_hf_client_parse_cnum,        bta_hf_client_parse_btrh,
     bta_hf_client_parse_busy,        bta_hf_client_parse_delayed,
     bta_hf_client_parse_no_carrier,  bta_hf_client_parse_no_answer,
-    bta_hf_client_parse_blacklisted, bta_hf_client_skip_unknown};
+    bta_hf_client_parse_blacklisted, bta_hf_client_process_unknown};
 
 /* calculate supported event list length */
 static const uint16_t bta_hf_client_parser_cb_count =
@@ -1596,7 +1627,7 @@ void bta_hf_client_at_parse(tBTA_HF_CLIENT_CB* client_cb, char* buf,
     client_cb->at_cb.offset += space_left;
 
     /* find end of last complete command before proceeding */
-    while (bta_hf_client_check_at_complete(client_cb) == false) {
+    while (!bta_hf_client_check_at_complete(client_cb)) {
       if (client_cb->at_cb.offset == 0) {
         APPL_TRACE_ERROR("HFPClient: AT parser buffer overrun, disconnecting");
 
@@ -1629,7 +1660,7 @@ void bta_hf_client_at_parse(tBTA_HF_CLIENT_CB* client_cb, char* buf,
   client_cb->at_cb.offset += len;
 
   /* If last event is complete, parsing can be started */
-  if (bta_hf_client_check_at_complete(client_cb) == true) {
+  if (bta_hf_client_check_at_complete(client_cb)) {
     bta_hf_client_at_parse_start(client_cb);
     bta_hf_client_at_clear_buf(client_cb);
   }
@@ -1895,7 +1926,7 @@ void bta_hf_client_send_at_btrh(tBTA_HF_CLIENT_CB* client_cb, bool query,
 
   APPL_TRACE_DEBUG("%s", __func__);
 
-  if (query == true) {
+  if (query) {
     at_len = snprintf(buf, sizeof(buf), "AT+BTRH?\r");
   } else {
     at_len = snprintf(buf, sizeof(buf), "AT+BTRH=%u\r", val);
@@ -1933,11 +1964,6 @@ void bta_hf_client_send_at_bcc(tBTA_HF_CLIENT_CB* client_cb) {
   buf = "AT+BCC\r";
 
   bta_hf_client_send_at(client_cb, BTA_HF_CLIENT_AT_BCC, buf, strlen(buf));
-
-  // At this point we should also open up an incoming SCO connection
-  tBTA_HF_CLIENT_DATA p_data;
-  p_data.hdr.layer_specific = client_cb->handle;
-  bta_hf_client_sco_listen(&p_data);
 }
 
 void bta_hf_client_send_at_cnum(tBTA_HF_CLIENT_CB* client_cb) {
@@ -2008,6 +2034,25 @@ void bta_hf_client_send_at_bia(tBTA_HF_CLIENT_CB* client_cb) {
   }
 
   bta_hf_client_send_at(client_cb, BTA_HF_CLIENT_AT_BIA, buf, at_len);
+}
+
+void bta_hf_client_send_at_vendor_specific_cmd(tBTA_HF_CLIENT_CB* client_cb,
+                                               const char* str) {
+  char buf[BTA_HF_CLIENT_AT_MAX_LEN];
+
+  APPL_TRACE_DEBUG("%s", __func__);
+
+  int at_len = snprintf(buf, sizeof(buf), "AT%s", str);
+
+  if (at_len < 1) {
+    APPL_TRACE_ERROR("%s: AT command Framing error", __func__);
+    return;
+  }
+
+  buf[at_len - 1] = '\r';
+
+  bta_hf_client_send_at(client_cb, BTA_HF_CLIENT_AT_VENDOR_SPECIFIC, buf,
+                        at_len);
 }
 
 void bta_hf_client_at_init(tBTA_HF_CLIENT_CB* client_cb) {
@@ -2100,6 +2145,9 @@ void bta_hf_client_send_at_cmd(tBTA_HF_CLIENT_DATA* p_data) {
       break;
     case BTA_HF_CLIENT_AT_CMD_NREC:
       bta_hf_client_send_at_nrec(client_cb);
+      break;
+    case BTA_HF_CLIENT_AT_CMD_VENDOR_SPECIFIC_CMD:
+      bta_hf_client_send_at_vendor_specific_cmd(client_cb, p_val->str);
       break;
     default:
       APPL_TRACE_ERROR("Default case");
--- stack/gatt/gatt_auth.cc
+++ stack/gatt/gatt_auth.cc
@@ -1,6 +1,6 @@
 /******************************************************************************
  *
- *  Copyright (C) 1999-2012 Broadcom Corporation
+ *  Copyright 1999-2012 Broadcom Corporation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -31,6 +31,8 @@
 #include "gatt_api.h"
 #include "gatt_int.h"
 #include "osi/include/osi.h"
+
+using base::StringPrintf;
 
 /*******************************************************************************
  *
@@ -98,8 +100,8 @@ void gatt_verify_signature(tGATT_TCB& tcb, BT_HDR* p_buf) {
   uint32_t counter;
 
   if (p_buf->len < GATT_AUTH_SIGN_LEN + 4) {
-    GATT_TRACE_ERROR("%s: Data length %u less than expected %u", __func__,
-                     p_buf->len, GATT_AUTH_SIGN_LEN + 4);
+    LOG(ERROR) << StringPrintf("%s: Data length %u less than expected %u",
+                               __func__, p_buf->len, GATT_AUTH_SIGN_LEN + 4);
     return;
   }
   cmd_len = p_buf->len - GATT_AUTH_SIGN_LEN + 4;
@@ -108,7 +110,7 @@ void gatt_verify_signature(tGATT_TCB& tcb, BT_HDR* p_buf) {
 
   if (!BTM_BleVerifySignature(tcb.peer_bda, p_orig, cmd_len, counter, p)) {
     /* if this is a bad signature, assume from attacker, ignore it  */
-    GATT_TRACE_ERROR("Signature Verification Failed, data ignored");
+    LOG(ERROR) << StringPrintf("Signature Verification Failed, data ignored");
     return;
   }
 
@@ -148,19 +150,21 @@ void gatt_sec_check_complete(bool sec_check_ok, tGATT_CLCB* p_clcb,
  * Returns
  *
  ******************************************************************************/
-void gatt_enc_cmpl_cback(BD_ADDR bd_addr, tBT_TRANSPORT transport,
+void gatt_enc_cmpl_cback(const RawAddress* bd_addr, tBT_TRANSPORT transport,
                          UNUSED_ATTR void* p_ref_data, tBTM_STATUS result) {
-  GATT_TRACE_DEBUG("gatt_enc_cmpl_cback");
-  tGATT_TCB* p_tcb = gatt_find_tcb_by_addr(bd_addr, transport);
+  VLOG(1) << StringPrintf("gatt_enc_cmpl_cback");
+  tGATT_TCB* p_tcb = gatt_find_tcb_by_addr(*bd_addr, transport);
   if (!p_tcb) {
-    GATT_TRACE_ERROR("%s: enc callback for unknown bd_addr", __func__);
+    LOG(ERROR) << StringPrintf("%s: enc callback for unknown bd_addr",
+                               __func__);
     return;
   }
 
   if (gatt_get_sec_act(p_tcb) == GATT_SEC_ENC_PENDING) return;
 
   if (p_tcb->pending_enc_clcb.empty()) {
-    GATT_TRACE_ERROR("%s: no operation waiting for encrypting", __func__);
+    LOG(ERROR) << StringPrintf("%s: no operation waiting for encrypting",
+                               __func__);
     return;
   }
 
@@ -171,7 +175,7 @@ void gatt_enc_cmpl_cback(BD_ADDR bd_addr, tBT_TRANSPORT transport,
   if (result == BTM_SUCCESS) {
     if (gatt_get_sec_act(p_tcb) == GATT_SEC_ENCRYPT_MITM) {
       uint8_t sec_flag = 0;
-      BTM_GetSecurityFlagsByTransport(bd_addr, &sec_flag, transport);
+      BTM_GetSecurityFlagsByTransport(*bd_addr, &sec_flag, transport);
 
       if (sec_flag & BTM_SEC_FLAG_LKEY_AUTHED) {
         status = true;
@@ -184,11 +188,13 @@ void gatt_enc_cmpl_cback(BD_ADDR bd_addr, tBT_TRANSPORT transport,
   gatt_sec_check_complete(status, p_clcb, p_tcb->sec_act);
 
   /* start all other pending operation in queue */
+  std::queue<tGATT_CLCB*> new_pending_clcbs;
   while (!p_tcb->pending_enc_clcb.empty()) {
     tGATT_CLCB* p_clcb = p_tcb->pending_enc_clcb.front();
     p_tcb->pending_enc_clcb.pop();
-    gatt_security_check_start(p_clcb);
+    if (gatt_security_check_start(p_clcb)) new_pending_clcbs.push(p_clcb);
   }
+  p_tcb->pending_enc_clcb = new_pending_clcbs;
 }
 
 /*******************************************************************************
@@ -201,10 +207,11 @@ void gatt_enc_cmpl_cback(BD_ADDR bd_addr, tBT_TRANSPORT transport,
  * Returns
  *
  ******************************************************************************/
-void gatt_notify_enc_cmpl(BD_ADDR bd_addr) {
+void gatt_notify_enc_cmpl(const RawAddress& bd_addr) {
   tGATT_TCB* p_tcb = gatt_find_tcb_by_addr(bd_addr, BT_TRANSPORT_LE);
   if (!p_tcb) {
-    GATT_TRACE_DEBUG("notify GATT for encryption completion of unknown device");
+    VLOG(1) << StringPrintf(
+        "notify GATT for encryption completion of unknown device");
     return;
   }
 
@@ -218,11 +225,13 @@ void gatt_notify_enc_cmpl(BD_ADDR bd_addr) {
   if (gatt_get_sec_act(p_tcb) == GATT_SEC_ENC_PENDING) {
     gatt_set_sec_act(p_tcb, GATT_SEC_NONE);
 
+    std::queue<tGATT_CLCB*> new_pending_clcbs;
     while (!p_tcb->pending_enc_clcb.empty()) {
       tGATT_CLCB* p_clcb = p_tcb->pending_enc_clcb.front();
       p_tcb->pending_enc_clcb.pop();
-      gatt_security_check_start(p_clcb);
+      if (gatt_security_check_start(p_clcb)) new_pending_clcbs.push(p_clcb);
     }
+    p_tcb->pending_enc_clcb = new_pending_clcbs;
   }
 }
 /*******************************************************************************
@@ -356,7 +365,8 @@ tGATT_STATUS gatt_get_link_encrypt_status(tGATT_TCB& tcb) {
       encrypt_status = GATT_ENCRYPED_MITM;
   }
 
-  GATT_TRACE_DEBUG("gatt_get_link_encrypt_status status=0x%x", encrypt_status);
+  VLOG(1) << StringPrintf("gatt_get_link_encrypt_status status=0x%x",
+                          encrypt_status);
   return encrypt_status;
 }
 
@@ -390,65 +400,51 @@ static bool gatt_convert_sec_action(tGATT_SEC_ACTION gatt_sec_act,
 
   return status;
 }
-/*******************************************************************************
- *
- * Function         gatt_check_enc_req
- *
- * Description      check link security.
- *
- * Returns          true if encrypted, otherwise false.
- *
- ******************************************************************************/
+
+/** check link security, return true if p_clcb should be added back to queue */
 bool gatt_security_check_start(tGATT_CLCB* p_clcb) {
   tGATT_TCB* p_tcb = p_clcb->p_tcb;
-  tGATT_SEC_ACTION gatt_sec_act;
-  tBTM_BLE_SEC_ACT btm_ble_sec_act;
-  bool status = true;
-  tBTM_STATUS btm_status;
   tGATT_SEC_ACTION sec_act_old = gatt_get_sec_act(p_tcb);
 
-  gatt_sec_act = gatt_determine_sec_act(p_clcb);
+  tGATT_SEC_ACTION gatt_sec_act = gatt_determine_sec_act(p_clcb);
 
   if (sec_act_old == GATT_SEC_NONE) gatt_set_sec_act(p_tcb, gatt_sec_act);
 
   switch (gatt_sec_act) {
     case GATT_SEC_SIGN_DATA:
-      GATT_TRACE_DEBUG("gatt_security_check_start: Do data signing");
+      VLOG(1) << StringPrintf("%s: Do data signing", __func__);
       gatt_sign_data(p_clcb);
       break;
     case GATT_SEC_ENCRYPT:
     case GATT_SEC_ENCRYPT_NO_MITM:
     case GATT_SEC_ENCRYPT_MITM:
       if (sec_act_old < GATT_SEC_ENCRYPT) {
-        GATT_TRACE_DEBUG(
-            "gatt_security_check_start: Encrypt now or key upgreade first");
+        VLOG(1) << StringPrintf("%s: Encrypt now or key upgreade first",
+                                __func__);
+        tBTM_BLE_SEC_ACT btm_ble_sec_act;
         gatt_convert_sec_action(gatt_sec_act, &btm_ble_sec_act);
-        btm_status =
+        tBTM_STATUS btm_status =
             BTM_SetEncryption(p_tcb->peer_bda, p_tcb->transport,
                               gatt_enc_cmpl_cback, NULL, btm_ble_sec_act);
         if ((btm_status != BTM_SUCCESS) && (btm_status != BTM_CMD_STARTED)) {
-          GATT_TRACE_ERROR(
-              "gatt_security_check_start BTM_SetEncryption failed "
-              "btm_status=%d",
+          LOG(ERROR) << StringPrintf(
+              "%s BTM_SetEncryption failed btm_status=%d", __func__,
               btm_status);
-          status = false;
+          gatt_set_sec_act(p_tcb, GATT_SEC_NONE);
+          gatt_set_ch_state(p_tcb, GATT_CH_OPEN);
+
+          gatt_end_operation(p_clcb, GATT_INSUF_ENCRYPTION, NULL);
+          return false;
         }
       }
-      if (status) p_tcb->pending_enc_clcb.push(p_clcb);
-      break;
+      return true;
     case GATT_SEC_ENC_PENDING:
-      p_tcb->pending_enc_clcb.push(p_clcb);
       /* wait for link encrypotion to finish */
-      break;
+      return true;
     default:
       gatt_sec_check_complete(true, p_clcb, gatt_sec_act);
       break;
   }
 
-  if (status == false) {
-    gatt_set_sec_act(p_tcb, GATT_SEC_NONE);
-    gatt_set_ch_state(p_tcb, GATT_CH_OPEN);
-  }
-
-  return status;
+  return false;
 }
