c3f935d08db6c2651d9bb463a7a8cd08b80dd16f
openvela-robot <openvela-robot@xiaomi.com>
2019-11-27 13:41:07 +0800

    Merge "HCI: Test padded lengths for LE Advertising" am: 7ee70515f9 am: 68a5d8f2f8
    Change-Id: Ie4fdbda16c850bcb01178592b93edfbda2100e29

--- gd/security/internal/security_manager_impl.cc
+++ gd/security/internal/security_manager_impl.cc
@@ -92,9 +92,8 @@ void SecurityManagerImpl::CreateBond(hci::AddressWithType device) {
 void SecurityManagerImpl::CancelBond(hci::AddressWithType device) {
   auto entry = pairing_handler_map_.find(device.GetAddress());
   if (entry != pairing_handler_map_.end()) {
-    auto cancel_me = entry->second;
     pairing_handler_map_.erase(entry);
-    cancel_me->Cancel();
+    entry->second->Cancel();
   }
 }
 
