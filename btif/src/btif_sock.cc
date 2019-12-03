0084886d62a2c84b451347638eedd3858118ba57
openvela-robot <openvela-robot@xiaomi.com>
2019-12-04 07:49:36 +0800

    Merge "Fix potential OOB write in btm_read_remote_ext_features_complete" into pi-dev

--- btif/src/btif_sock.cc
+++ btif/src/btif_sock.cc
@@ -22,13 +22,11 @@
 
 #include <base/logging.h>
 
-#include <frameworks/base/core/proto/android/bluetooth/enums.pb.h>
 #include <hardware/bluetooth.h>
 #include <hardware/bt_sock.h>
 
 #include "bta_api.h"
 #include "btif_common.h"
-#include "btif_config.h"
 #include "btif_sock_l2cap.h"
 #include "btif_sock_rfc.h"
 #include "btif_sock_sco.h"
@@ -36,7 +34,6 @@
 #include "btif_sock_thread.h"
 #include "btif_uid.h"
 #include "btif_util.h"
-#include "common/metrics.h"
 #include "device/include/controller.h"
 #include "osi/include/thread.h"
 
@@ -141,11 +138,6 @@ static bt_status_t btsock_listen(btsock_type_t type, const char* service_name,
   bt_status_t status = BT_STATUS_FAIL;
   int original_channel = channel;
 
-  bluetooth::common::LogSocketConnectionState(
-      RawAddress::kEmpty, 0, type,
-      android::bluetooth::SocketConnectionstateEnum::
-          SOCKET_CONNECTION_STATE_LISTENING,
-      0, 0, app_uid, channel, android::bluetooth::SOCKET_ROLE_LISTEN);
   switch (type) {
     case BTSOCK_RFCOMM:
       status = btsock_rfc_listen(service_name, service_uuid, channel, sock_fd,
@@ -182,13 +174,6 @@ static bt_status_t btsock_listen(btsock_type_t type, const char* service_name,
       status = BT_STATUS_UNSUPPORTED;
       break;
   }
-  if (status != BT_STATUS_SUCCESS) {
-    bluetooth::common::LogSocketConnectionState(
-        RawAddress::kEmpty, 0, type,
-        android::bluetooth::SocketConnectionstateEnum::
-            SOCKET_CONNECTION_STATE_DISCONNECTED,
-        0, 0, app_uid, channel, android::bluetooth::SOCKET_ROLE_LISTEN);
-  }
   return status;
 }
 
@@ -201,11 +186,6 @@ static bt_status_t btsock_connect(const RawAddress* bd_addr, btsock_type_t type,
   *sock_fd = INVALID_FD;
   bt_status_t status = BT_STATUS_FAIL;
 
-  bluetooth::common::LogSocketConnectionState(
-      *bd_addr, 0, type,
-      android::bluetooth::SocketConnectionstateEnum::
-          SOCKET_CONNECTION_STATE_CONNECTING,
-      0, 0, app_uid, channel, android::bluetooth::SOCKET_ROLE_CONNECTION);
   switch (type) {
     case BTSOCK_RFCOMM:
       status =
@@ -216,24 +196,12 @@ static bt_status_t btsock_connect(const RawAddress* bd_addr, btsock_type_t type,
       status = btsock_l2cap_connect(bd_addr, channel, sock_fd, flags, app_uid);
       break;
 
-    case BTSOCK_L2CAP_LE: {
+    case BTSOCK_L2CAP_LE:
       flags |= BTSOCK_FLAG_LE_COC;
-
-      // Ensure device is in inquiry database
-      int addr_type = 0;
-      int device_type = 0;
-
-      if (btif_get_address_type(*bd_addr, &addr_type) &&
-          btif_get_device_type(*bd_addr, &device_type) &&
-          device_type != BT_DEVICE_TYPE_BREDR) {
-        BTA_DmAddBleDevice(*bd_addr, addr_type, device_type);
-      }
-
       LOG_DEBUG(LOG_TAG, "%s: type=BTSOCK_L2CAP_LE, channel=0x%x, flags=0x%x",
                 __func__, channel, flags);
       status = btsock_l2cap_connect(bd_addr, channel, sock_fd, flags, app_uid);
       break;
-    }
 
     case BTSOCK_SCO:
       status = btsock_sco_connect(bd_addr, sock_fd, flags);
@@ -244,13 +212,6 @@ static bt_status_t btsock_connect(const RawAddress* bd_addr, btsock_type_t type,
                 type);
       status = BT_STATUS_UNSUPPORTED;
       break;
-  }
-  if (status != BT_STATUS_SUCCESS) {
-    bluetooth::common::LogSocketConnectionState(
-        *bd_addr, 0, type,
-        android::bluetooth::SocketConnectionstateEnum::
-            SOCKET_CONNECTION_STATE_DISCONNECTED,
-        0, 0, app_uid, channel, android::bluetooth::SOCKET_ROLE_CONNECTION);
   }
   return status;
 }
@@ -276,8 +237,7 @@ static void btsock_signaled(int fd, int type, int flags, uint32_t user_id) {
       btsock_l2cap_signaled(fd, flags, user_id);
       break;
     default:
-      LOG(FATAL) << "Invalid socket type! type=" << type << " fd=" << fd
-                 << " flags=" << flags << " user_id=" << user_id;
+      CHECK(false && "Invalid socket type");
       break;
   }
 }
