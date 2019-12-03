a48b0053d549f1c4ddf28e04a29a373a64ef6336
openvela-robot <openvela-robot@xiaomi.com>
2019-12-03 23:14:57 +0800

    Merge "Merge Android10 QPR1 into AOSP master"

--- utils/src/bt_utils.cc
+++ utils/src/bt_utils.cc
@@ -1,6 +1,6 @@
 /******************************************************************************
  *
- *  Copyright (C) 2012 Broadcom Corporation
+ *  Copyright 2012 Broadcom Corporation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -39,7 +39,7 @@
 
 #define A2DP_RT_PRIORITY 1
 #ifndef OS_GENERIC
-#include <cutils/sched_policy.h>
+#include <processgroup/sched_policy.h>
 #endif
 
 #include "bt_types.h"
