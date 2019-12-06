d30c31db50c4e93b51650f3cdd9b317ae8b0ff5f
liujinye <liujinye@xiaomi.com>
2019-12-06 15:54:14 +0800

    Merge "Add build config for a bluetooth apex" am: 851dc10267 am: 475962c4ed am: b2690da388
    Change-Id: I8677715f414a235b11ff5cdc1d03aa938f279d9f

--- vendor_libs/test_vendor_lib/model/devices/h4_packetizer.cc
+++ vendor_libs/test_vendor_lib/model/devices/h4_packetizer.cc
@@ -105,9 +105,6 @@ void H4Packetizer::OnDataReady(int fd) {
       if (errno == EAGAIN) {
         // No data, try again later.
         return;
-      } else if (errno == ECONNRESET) {
-        // They probably rejected our packet
-        return;
       } else {
         LOG_ALWAYS_FATAL("%s: Read packet type error: %s", __func__, strerror(errno));
       }
