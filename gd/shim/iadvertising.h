0cf6123632b82a3d164a5c358202e7b42abb9220
openvela-robot <openvela-robot@xiaomi.com>
2019-12-09 11:30:23 +0800

    Merge "L2CAP: Add LE Module mock" am: 4aa2db4db7
    Change-Id: Ic3a87ee34ea92b31f8932721650b16ee97000e3b

--- gd/shim/iadvertising.h
+++ gd/shim/iadvertising.h
@@ -15,8 +15,6 @@
  */
 #pragma once
 
-#include <cstddef>
-
 /**
  * The gd API exported to the legacy api
  */
@@ -24,11 +22,6 @@ namespace bluetooth {
 namespace shim {
 
 struct IAdvertising {
-  virtual void StartAdvertising() = 0;
-  virtual void StopAdvertising() = 0;
-
-  virtual size_t GetNumberOfAdvertisingInstances() const = 0;
-
   virtual ~IAdvertising() {}
 };
 
