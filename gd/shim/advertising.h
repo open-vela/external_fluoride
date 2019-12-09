b6c077ad1e10e8f0379369611d85021f531fe2f2
liujinye <liujinye@xiaomi.com>
2019-12-09 11:30:23 +0800

    Merge "L2CAP: Add LE Module mock" am: 4aa2db4db7
    Change-Id: Ic3a87ee34ea92b31f8932721650b16ee97000e3b

--- gd/shim/advertising.h
+++ gd/shim/advertising.h
@@ -29,11 +29,6 @@ class Advertising : public bluetooth::Module, public bluetooth::shim::IAdvertisi
   Advertising() = default;
   ~Advertising() = default;
 
-  void StartAdvertising() override;
-  void StopAdvertising() override;
-
-  size_t GetNumberOfAdvertisingInstances() const override;
-
   static const ModuleFactory Factory;
 
  protected:
