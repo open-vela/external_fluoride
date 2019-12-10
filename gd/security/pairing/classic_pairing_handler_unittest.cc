efcc08cdc93ebbcf7c436b8828f5e55d5e6ae316
openvela-robot <openvela-robot@xiaomi.com>
2019-12-10 20:28:17 +0800

    HCI: Add LeAdvertisingManager Extended tests am: 67c13486db am: 2890915ba9 am: f82deee003
    Change-Id: Iadd5cbc9360d2d44cdfd8a2f543bfb06b52fbce8

--- gd/security/pairing/classic_pairing_handler_unittest.cc
+++ gd/security/pairing/classic_pairing_handler_unittest.cc
@@ -24,7 +24,6 @@
 #include "l2cap/classic/fixed_channel_manager_mock.h"
 #include "packet/raw_builder.h"
 #include "security/channel/security_manager_channel.h"
-#include "security/initial_informations.h"
 #include "security/smp_packets.h"
 #include "security/test/fake_hci_layer.h"
 
@@ -100,14 +99,7 @@ class SecurityManagerChannelCallback : public ISecurityManagerChannelListener {
   pairing::ClassicPairingHandler* pairing_handler_ = nullptr;
 };
 
-static void pairing_complete_callback(bluetooth::hci::Address address, PairingResultOrFailure status) {
-  //  if (std::holds_alternative<PairingResult>(status)) {
-  //    auto result = status::get<PairingResult>(status);
-  //  }
-  //  if (std::holds_alternative<PairingFailure>(status)) {
-  //    auto failure = status::get<PairingFailure>(status);
-  //  }
-}
+static void pairing_complete_callback(bluetooth::hci::Address address) {}
 
 class ClassicPairingHandlerTest : public ::testing::Test {
  protected:
