1e5409b2f02f0caed5304c9b378524fdc5ad7ed7
liujinye <liujinye@xiaomi.com>
2019-12-12 05:33:56 +0800

    Merge "Race condition with channel closure"

--- main/shim/btm.cc
+++ main/shim/btm.cc
@@ -48,8 +48,7 @@ static constexpr bool kPassiveScanning = true;
 
 extern void btm_process_cancel_complete(uint8_t status, uint8_t mode);
 extern void btm_process_inq_complete(uint8_t status, uint8_t result_type);
-extern void btm_process_inq_results(uint8_t* p, uint8_t hci_evt_len,
-                                    uint8_t result_mode);
+extern void btm_process_inq_results(uint8_t* p, uint8_t result_mode);
 extern void btm_ble_process_adv_addr(RawAddress& raw_address,
                                      uint8_t* address_type);
 extern void btm_ble_process_adv_pkt_cont(
@@ -71,8 +70,7 @@ void bluetooth::shim::Btm::OnInquiryResult(std::vector<const uint8_t> result) {
   CHECK(result.size() < kMaxInquiryResultSize);
 
   std::copy(result.begin(), result.end(), inquiry_result_buf);
-  btm_process_inq_results(inquiry_result_buf, result.size(),
-                          kInquiryResultMode);
+  btm_process_inq_results(inquiry_result_buf, kInquiryResultMode);
 }
 
 void bluetooth::shim::Btm::OnInquiryResultWithRssi(
@@ -80,8 +78,7 @@ void bluetooth::shim::Btm::OnInquiryResultWithRssi(
   CHECK(result.size() < kMaxInquiryResultSize);
 
   std::copy(result.begin(), result.end(), inquiry_result_buf);
-  btm_process_inq_results(inquiry_result_buf, result.size(),
-                          kInquiryResultWithRssiMode);
+  btm_process_inq_results(inquiry_result_buf, kInquiryResultWithRssiMode);
 }
 
 void bluetooth::shim::Btm::OnExtendedInquiryResult(
@@ -89,8 +86,7 @@ void bluetooth::shim::Btm::OnExtendedInquiryResult(
   CHECK(result.size() < kMaxInquiryResultSize);
 
   std::copy(result.begin(), result.end(), inquiry_result_buf);
-  btm_process_inq_results(inquiry_result_buf, result.size(),
-                          kExtendedInquiryResultMode);
+  btm_process_inq_results(inquiry_result_buf, kExtendedInquiryResultMode);
 }
 
 void bluetooth::shim::Btm::OnInquiryComplete(uint16_t status) {
