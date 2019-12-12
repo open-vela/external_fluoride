65602287e386fb5343151e3356ae0079c52c4637
openvela-robot <openvela-robot@xiaomi.com>
2019-12-12 19:00:40 +0800

    Merge "Add LE related fields into SecurityRecord"

--- hci/src/hci_layer_android.cc
+++ hci/src/hci_layer_android.cc
@@ -54,7 +54,6 @@ extern void hci_event_received(const base::Location& from_here, BT_HDR* packet);
 extern void acl_event_received(BT_HDR* packet);
 extern void sco_data_received(BT_HDR* packet);
 extern void iso_data_received(BT_HDR* packet);
-extern void hal_service_died();
 
 android::sp<V1_0::IBluetoothHci> btHci;
 android::sp<V1_1::IBluetoothHci> btHci_1_1;
@@ -63,7 +62,7 @@ class BluetoothHciDeathRecipient : public hidl_death_recipient {
  public:
   virtual void serviceDied(uint64_t /*cookie*/, const android::wp<::android::hidl::base::V1_0::IBase>& /*who*/) {
     LOG_ERROR(LOG_TAG, "Bluetooth HAL service died!");
-    hal_service_died();
+    abort();
   }
 };
 android::sp<BluetoothHciDeathRecipient> bluetoothHciDeathRecipient = new BluetoothHciDeathRecipient();
