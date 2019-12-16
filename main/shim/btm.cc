79e83c9ce23dc7aaafb89bf970c852a6a6ab867b
openvela-robot <openvela-robot@xiaomi.com>
2019-12-16 18:19:09 +0800

    Merge "L2CAP ERTM: Fix infinite loop in retransmit_i_frame"

--- main/shim/btm.cc
+++ main/shim/btm.cc
@@ -32,6 +32,10 @@ extern tBTM_CB btm_cb;
 static constexpr size_t kMaxInquiryResultSize = 4096;
 static uint8_t inquiry_result_buf[kMaxInquiryResultSize];
 
+static constexpr uint8_t kInquiryResultMode = 0;
+static constexpr uint8_t kInquiryResultWithRssiMode = 1;
+static constexpr uint8_t kExtendedInquiryResultMode = 2;
+
 static constexpr size_t kRemoteDeviceNameLength = 248;
 
 static constexpr uint8_t kAdvDataInfoNotPresent = 0xff;
@@ -43,6 +47,7 @@ static constexpr bool kPassiveScanning = false;
 
 extern void btm_process_cancel_complete(uint8_t status, uint8_t mode);
 extern void btm_process_inq_complete(uint8_t status, uint8_t result_type);
+extern void btm_process_inq_results(uint8_t* p, uint8_t result_mode);
 extern void btm_ble_process_adv_addr(RawAddress& raw_address,
                                      uint8_t* address_type);
 extern void btm_ble_process_adv_pkt_cont(
@@ -60,6 +65,7 @@ void bluetooth::shim::Btm::OnInquiryResult(std::vector<const uint8_t> result) {
   CHECK(result.size() < kMaxInquiryResultSize);
 
   std::copy(result.begin(), result.end(), inquiry_result_buf);
+  btm_process_inq_results(inquiry_result_buf, kInquiryResultMode);
 }
 
 void bluetooth::shim::Btm::OnInquiryResultWithRssi(
@@ -67,6 +73,7 @@ void bluetooth::shim::Btm::OnInquiryResultWithRssi(
   CHECK(result.size() < kMaxInquiryResultSize);
 
   std::copy(result.begin(), result.end(), inquiry_result_buf);
+  btm_process_inq_results(inquiry_result_buf, kInquiryResultWithRssiMode);
 }
 
 void bluetooth::shim::Btm::OnExtendedInquiryResult(
@@ -74,6 +81,7 @@ void bluetooth::shim::Btm::OnExtendedInquiryResult(
   CHECK(result.size() < kMaxInquiryResultSize);
 
   std::copy(result.begin(), result.end(), inquiry_result_buf);
+  btm_process_inq_results(inquiry_result_buf, kExtendedInquiryResultMode);
 }
 
 void bluetooth::shim::Btm::OnInquiryComplete(uint16_t status) {
