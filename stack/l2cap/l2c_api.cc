fd14fafdfeb64fa72393a84d97eab9d40eb73b48
liujinye <liujinye@xiaomi.com>
2019-12-19 04:11:43 +0800

    Snap for 6084815 from 9a3f74035e19688949852c545ff56020f4d217fa to rvc-release
    Change-Id: Id99c611b748c29fd3479ee3c4ff61b3ca5a48af7

--- stack/l2cap/l2c_api.cc
+++ stack/l2cap/l2c_api.cc
@@ -61,10 +61,9 @@ using base::StringPrintf;
  *
  ******************************************************************************/
 uint16_t L2CA_Register(uint16_t psm, tL2CAP_APPL_INFO* p_cb_info,
-                       bool enable_snoop, tL2CAP_ERTM_INFO* p_ertm_info) {
+                       bool enable_snoop) {
   if (bluetooth::shim::is_gd_shim_enabled()) {
-    return bluetooth::shim::L2CA_Register(psm, p_cb_info, enable_snoop,
-                                          p_ertm_info);
+    return bluetooth::shim::L2CA_Register(psm, p_cb_info, enable_snoop);
   }
 
   tL2C_RCB* p_rcb;
