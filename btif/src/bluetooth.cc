346142832a0bc847729faac79119be62b0b4f554
liujinye <liujinye@xiaomi.com>
2019-12-21 02:11:17 +0800

    Snap for 6091187 from fb4b5ebf1dd89ca8f3903a436e39e803949735ea to qt-qpr2-release
    Change-Id: I529c233bc91e51349fa2bee5cef27644ea867bf7

--- btif/src/bluetooth.cc
+++ btif/src/bluetooth.cc
@@ -65,14 +65,12 @@
 #include "common/address_obfuscator.h"
 #include "common/metrics.h"
 #include "device/include/interop.h"
-#include "main/shim/shim.h"
 #include "osi/include/alarm.h"
 #include "osi/include/allocation_tracker.h"
 #include "osi/include/log.h"
 #include "osi/include/osi.h"
 #include "osi/include/wakelock.h"
 #include "stack/gatt/connection_manager.h"
-#include "stack/include/btu.h"
 #include "stack_manager.h"
 
 using bluetooth::hearing_aid::HearingAidInterface;
@@ -83,7 +81,7 @@ using bluetooth::hearing_aid::HearingAidInterface;
 
 bt_callbacks_t* bt_hal_cbacks = NULL;
 bool restricted_mode = false;
-bool single_user_mode = false;
+bool niap_mode = false;
 
 /*******************************************************************************
  *  Externs
@@ -136,15 +134,9 @@ static bool is_profile(const char* p1, const char* p2) {
  ****************************************************************************/
 
 static int init(bt_callbacks_t* callbacks, bool start_restricted,
-                bool is_single_user_mode) {
-  LOG_INFO(LOG_TAG, "%s: start restricted = %d ; single user = %d", __func__,
-           start_restricted, is_single_user_mode);
-
-  if (bluetooth::shim::is_gd_shim_enabled()) {
-    LOG_INFO(LOG_TAG, "%s Enable Gd bluetooth functionality", __func__);
-  } else {
-    LOG_INFO(LOG_TAG, "%s Preserving legacy bluetooth functionality", __func__);
-  }
+                bool is_niap_mode) {
+  LOG_INFO(LOG_TAG, "%s: start restricted = %d ; niap = %d", __func__,
+           start_restricted, is_niap_mode);
 
   if (interface_ready()) return BT_STATUS_DONE;
 
@@ -154,7 +146,7 @@ static int init(bt_callbacks_t* callbacks, bool start_restricted,
 
   bt_hal_cbacks = callbacks;
   restricted_mode = start_restricted;
-  single_user_mode = is_single_user_mode;
+  niap_mode = is_niap_mode;
   stack_manager_get_interface()->init_stack();
   btif_debug_init();
   return BT_STATUS_SUCCESS;
@@ -177,7 +169,7 @@ static int disable(void) {
 static void cleanup(void) { stack_manager_get_interface()->clean_up_stack(); }
 
 bool is_restricted_mode() { return restricted_mode; }
-bool is_single_user_mode() { return single_user_mode; }
+bool is_niap_mode() { return niap_mode; }
 
 static int get_adapter_properties(void) {
   /* sanity check */
@@ -320,7 +312,6 @@ static void dump(int fd, const char** arguments) {
   btif_debug_av_dump(fd);
   bta_debug_av_dump(fd);
   stack_debug_avdtp_api_dump(fd);
-  stack_debug_hwbinder_thread_dump(fd);
   bluetooth::avrcp::AvrcpService::DebugDump(fd);
   btif_debug_config_dump(fd);
   BTA_HfClientDumpStatistics(fd);
--- btif/src/btif_hh.cc
+++ btif/src/btif_hh.cc
@@ -537,8 +537,7 @@ bt_status_t btif_hh_virtual_unplug(const RawAddress* bd_addr) {
   p_dev = btif_hh_find_dev_by_bda(*bd_addr);
   if ((p_dev != NULL) && (p_dev->dev_status == BTHH_CONN_STATE_CONNECTED) &&
       (p_dev->attr_mask & HID_VIRTUAL_CABLE)) {
-    BTIF_TRACE_DEBUG("%s: Sending BTA_HH_CTRL_VIRTUAL_CABLE_UNPLUG for: %s", __func__,
-                     bd_addr->ToString().c_str());
+    BTIF_TRACE_DEBUG("%s Sending BTA_HH_CTRL_VIRTUAL_CABLE_UNPLUG", __func__);
     /* start the timer */
     btif_hh_start_vup_timer(bd_addr);
     p_dev->local_vup = true;
@@ -546,8 +545,7 @@ bt_status_t btif_hh_virtual_unplug(const RawAddress* bd_addr) {
     return BT_STATUS_SUCCESS;
   } else if ((p_dev != NULL) &&
              (p_dev->dev_status == BTHH_CONN_STATE_CONNECTED)) {
-    BTIF_TRACE_ERROR("%s: Virtual unplug not suported, disconnecting device: %s",
-                     __func__, bd_addr->ToString().c_str());
+    BTIF_TRACE_ERROR("%s: Virtual unplug not suported, disconnecting device");
     /* start the timer */
     btif_hh_start_vup_timer(bd_addr);
     p_dev->local_vup = true;
