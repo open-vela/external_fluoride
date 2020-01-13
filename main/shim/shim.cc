1232281284b0d0ac317a8afe1b973429f3cbd084
openvela-robot <openvela-robot@xiaomi.com>
2020-01-14 04:03:31 +0800

    Snap for 6132583 from 84285bf06fcdedebb3c73c27bc4a32cc60b14374 to rvc-release
    Change-Id: Iedb3f392d3a5c78ae92a7323d3a4ec16dcc15931

--- main/shim/shim.cc
+++ main/shim/shim.cc
@@ -30,7 +30,6 @@ static bluetooth::common::MessageLoopThread bt_shim_thread("bt_shim_thread");
 
 static bool gd_shim_enabled_ = false;
 static bool gd_shim_property_checked_ = false;
-static bool gd_stack_started_up_ = false;
 
 future_t* ShimModuleStartUp() {
   bt_shim_thread.StartUp();
@@ -38,12 +37,10 @@ future_t* ShimModuleStartUp() {
       << "Unable to start bt shim message loop thread.";
   module_start_up(get_module(GD_SHIM_BTM_MODULE));
   bluetooth::shim::StartGabeldorscheStack();
-  gd_stack_started_up_ = true;
   return kReturnImmediate;
 }
 
 future_t* ShimModuleShutDown() {
-  gd_stack_started_up_ = false;
   bluetooth::shim::StopGabeldorscheStack();
   module_shut_down(get_module(GD_SHIM_BTM_MODULE));
   bt_shim_thread.ShutDown();
@@ -69,5 +66,3 @@ bool bluetooth::shim::is_gd_shim_enabled() {
   }
   return gd_shim_enabled_;
 }
-
-bool bluetooth::shim::is_gd_stack_started_up() { return gd_stack_started_up_; }
