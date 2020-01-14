0b3b6572e58a209db18476391b1882e26039047d
liujinye <liujinye@xiaomi.com>
2020-01-15 03:34:40 +0800

    Merge "Use BASIC rather than LE_CREDIT_BASED channel for LE Fixed channels"

--- gd/hci/acl_manager.h
+++ gd/hci/acl_manager.h
@@ -217,9 +217,6 @@ class AclManager : public Module {
   // Should register only once when user module starts.
   virtual void RegisterAclManagerCallbacks(AclManagerCallbacks* callbacks, os::Handler* handler);
 
-  // Should register only once when user module starts.
-  virtual void RegisterLeAclManagerCallbacks(AclManagerCallbacks* callbacks, os::Handler* handler);
-
   // Generates OnConnectSuccess if connected, or OnConnectFail otherwise
   virtual void CreateConnection(Address address);
 
