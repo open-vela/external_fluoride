992e04f482c80ec7f6bc562705ebb5be0afe2ecb
liujinye <liujinye@xiaomi.com>
2020-01-15 04:18:26 +0800

    Snap for 6134912 from 47ca3d560ed68779d5ad0864d74c3e9d72621e09 to rvc-release
    Change-Id: I9650e8aec36ba1048123ea6c7c898c1cdcf551ce

--- gd/l2cap/le/internal/link.cc
+++ gd/l2cap/le/internal/link.cc
@@ -64,7 +64,8 @@ void Link::UpdateConnectionParameter(SignalId signal_id, uint16_t conn_interval_
 
 std::shared_ptr<FixedChannelImpl> Link::AllocateFixedChannel(Cid cid, SecurityPolicy security_policy) {
   auto channel = fixed_channel_allocator_.AllocateChannel(cid, security_policy);
-  data_pipeline_manager_.AttachChannel(cid, channel, l2cap::internal::DataPipelineManager::ChannelMode::BASIC);
+  data_pipeline_manager_.AttachChannel(cid, channel,
+                                       l2cap::internal::DataPipelineManager::ChannelMode::LE_CREDIT_BASED);
   return channel;
 }
 
