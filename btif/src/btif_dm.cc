fd6a25f83e7eb701e0cb1be04744b014f65f0339
openvela-robot <openvela-robot@xiaomi.com>
2020-01-16 16:20:06 +0800

    LE-COC: Free LE-COC server resource when BluetoothServerSocket is closed
    * When BluetoothServerSocket is closed in the Java layer, the native
      layer should respond by freeing all resources used by the server
      including file descriptors, structs, PSMs, and security IDs
    * We did this correctly for BR_EDR L2CAP COC channels after
      I4e37dcd858af258fbd64fbfb2fbf0083bd743e06, but the same fix did not
      apply to LE COC
    * This CL make sure LE COC server resources are freed propertly upon
      server fd closure
    
    Test: open and close LE COC server repeatedly on an Android phone
    Fixes: 144148429
    Change-Id: I16fa10e77612105d23848f71925ff6efc95bc75a
    (cherry picked from commit d3c9966624530772fbdc469179726bd9191314c5)

--- btif/src/btif_dm.cc
+++ btif/src/btif_dm.cc
@@ -697,7 +697,7 @@ static void btif_dm_cb_create_bond(const RawAddress& bd_addr,
     if (status != BT_STATUS_SUCCESS)
       bond_state_changed(status, bd_addr, BT_BOND_STATE_NONE);
   } else {
-    BTA_DmBond(bd_addr, addr_type, transport);
+    BTA_DmBondByTransport(bd_addr, transport);
   }
   /*  Track  originator of bond creation  */
   pairing_cb.is_local_initiated = true;
@@ -925,8 +925,8 @@ static void btif_dm_pin_req_evt(tBTA_DM_PIN_REQ* p_pin_req) {
  ******************************************************************************/
 static void btif_dm_ssp_cfm_req_evt(tBTA_DM_SP_CFM_REQ* p_ssp_cfm_req) {
   bt_bdname_t bd_name;
-  bool is_incoming = !(pairing_cb.state == BT_BOND_STATE_BONDING);
   uint32_t cod;
+  bool is_incoming = !(pairing_cb.state == BT_BOND_STATE_BONDING);
   int dev_type;
 
   BTIF_TRACE_DEBUG("%s", __func__);
@@ -1108,6 +1108,7 @@ static void btif_dm_auth_cmpl_evt(tBTA_DM_AUTH_CMPL* p_auth_cmpl) {
 
       LOG_WARN(LOG_TAG, "%s: Incoming HID Connection", __func__);
       bt_property_t prop;
+      RawAddress bd_addr;
       Uuid uuid = Uuid::From16Bit(UUID_SERVCLASS_HUMAN_INTERFACE);
 
       prop.type = BT_PROPERTY_UUIDS;
@@ -1178,7 +1179,6 @@ static void btif_dm_auth_cmpl_evt(tBTA_DM_AUTH_CMPL* p_auth_cmpl) {
       case HCI_ERR_AUTH_FAILURE:
       case HCI_ERR_KEY_MISSING:
         btif_storage_remove_bonded_device(&bd_addr);
-        [[fallthrough]];
       case HCI_ERR_HOST_REJECT_SECURITY:
       case HCI_ERR_ENCRY_MODE_NOT_ACCEPTABLE:
       case HCI_ERR_UNIT_KEY_USED:
