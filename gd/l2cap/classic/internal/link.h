92ec6f77974bd5e44e32f29e81b1781e2df8e337
liujinye <liujinye@xiaomi.com>
2020-01-22 18:15:24 +0800

    Merge "Zip cert resources and scripts" am: a8e924c0cf am: 5a0712ca1a
    Change-Id: Ic606cf043eb37dda526ab2fc9c10325c17cc46cb

--- gd/l2cap/classic/internal/link.h
+++ gd/l2cap/classic/internal/link.h
@@ -69,10 +69,6 @@ class Link : public l2cap::internal::ILink {
 
   virtual void Authenticate();
 
-  virtual void ReadRemoteSupportedFeatures();
-
-  virtual void ReadRemoteExtendedFeatures();
-
   // FixedChannel methods
 
   std::shared_ptr<FixedChannelImpl> AllocateFixedChannel(Cid cid, SecurityPolicy security_policy);
