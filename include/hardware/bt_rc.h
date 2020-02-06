ab26f175a9f26ba0a7dbeb507ddb31e450ec6032
liujinye <liujinye@xiaomi.com>
2020-02-07 04:14:26 +0800

    Merge "Add Support FCS for L2CAP" am: d89c493305 am: aefc6a0d48
    Change-Id: I07c106baeee4794d4de59b5fb87373f13c6da5ef

--- include/hardware/bt_rc.h
+++ include/hardware/bt_rc.h
@@ -617,8 +617,6 @@ typedef void (*btrc_ctrl_addressed_player_changed_callback)(
     const RawAddress& bd_addr, uint16_t id);
 typedef void (*btrc_ctrl_now_playing_contents_changed_callback)(
     const RawAddress& bd_addr);
-typedef void (*btrc_ctrl_available_player_changed_callback)(
-    const RawAddress& bd_addr);
 
 /** BT-RC Controller callback structure. */
 typedef struct {
@@ -645,7 +643,6 @@ typedef struct {
   btrc_ctrl_addressed_player_changed_callback addressed_player_changed_cb;
   btrc_ctrl_now_playing_contents_changed_callback
       now_playing_contents_changed_cb;
-  btrc_ctrl_available_player_changed_callback available_player_changed_cb;
 } btrc_ctrl_callbacks_t;
 
 /** Represents the standard BT-RC AVRCP Controller interface. */
