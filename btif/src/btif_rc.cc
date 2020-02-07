055ba58b2fc1d73bf3d311d32d35a6df205fdda3
liujinye <liujinye@xiaomi.com>
2020-02-07 19:42:42 +0800

    AVDTP: Prevent OOB read when parsing rejected response
    Because different AVDTP rejected response has different fields, we check
    its data length based on the signal to prevent OOB read.
    
    Bug: 79702484
    Test: PoC
    Change-Id: Iddb887c79bd8a2caa2ae5f21af15219807f9dd63
    Merged-In: Iddb887c79bd8a2caa2ae5f21af15219807f9dd63

--- btif/src/btif_rc.cc
+++ btif/src/btif_rc.cc
@@ -2535,9 +2535,9 @@ static bt_status_t set_volume(uint8_t volume) {
     BTIF_TRACE_DEBUG("%s: Peer supports absolute volume. newVolume: %d",
                      __func__, volume);
 
-    tAVRC_COMMAND avrc_cmd = {.volume = {.pdu = AVRC_PDU_SET_ABSOLUTE_VOLUME,
+    tAVRC_COMMAND avrc_cmd = {.volume = {.opcode = AVRC_OP_VENDOR,
+                                         .pdu = AVRC_PDU_SET_ABSOLUTE_VOLUME,
                                          .status = AVRC_STS_NO_ERROR,
-                                         .opcode = AVRC_OP_VENDOR,
                                          .volume = volume}};
 
     BT_HDR* p_msg = NULL;
@@ -2939,12 +2939,11 @@ static void register_for_event_notification(btif_rc_supported_event_t* p_event,
     return;
   }
   // interval is only valid for AVRC_EVT_PLAY_POS_CHANGED
-  uint32_t interval_in_seconds = 0;
+  uint32_t interval = 0;
   if (p_event->event_id == AVRC_EVT_PLAY_POS_CHANGED) {
-    interval_in_seconds = 2;
+    interval = 2000;
   }
-  status = register_notification_cmd(p_transaction->lbl, p_event->event_id,
-                                     interval_in_seconds, p_dev);
+  status = register_notification_cmd(p_transaction->lbl, p_event->event_id, interval, p_dev);
   if (status != BT_STATUS_SUCCESS) {
     BTIF_TRACE_ERROR("%s: Error in Notification registration: %d", __func__,
                      status);
@@ -3056,8 +3055,7 @@ static void handle_get_capability_response(tBTA_AV_META_MSG* pmeta_msg,
           (p_rsp->param.event_id[xx] == AVRC_EVT_APP_SETTING_CHANGE) ||
           (p_rsp->param.event_id[xx] == AVRC_EVT_NOW_PLAYING_CHANGE) ||
           (p_rsp->param.event_id[xx] == AVRC_EVT_ADDR_PLAYER_CHANGE) ||
-          (p_rsp->param.event_id[xx] == AVRC_EVT_UIDS_CHANGE) ||
-          (p_rsp->param.event_id[xx] == AVRC_EVT_AVAL_PLAYERS_CHANGE)) {
+          (p_rsp->param.event_id[xx] == AVRC_EVT_UIDS_CHANGE)) {
         p_event = (btif_rc_supported_event_t*)osi_malloc(
             sizeof(btif_rc_supported_event_t));
         p_event->event_id = p_rsp->param.event_id[xx];
@@ -3154,10 +3152,11 @@ static void handle_notification_response(tBTA_AV_META_MSG* pmeta_msg,
           break;
         } else {
           uint8_t* p_data = p_rsp->param.track;
+          /* Update the UID for current track
+           * Attributes will be fetched after the AVRCP procedure
+           */
           BE_STREAM_TO_UINT64(p_dev->rc_playing_uid, p_data);
           get_play_status_cmd(p_dev);
-          get_element_attribute_cmd(AVRC_MAX_NUM_MEDIA_ATTR_ID, attr_list,
-                                    p_dev);
         }
         break;
 
@@ -3172,11 +3171,6 @@ static void handle_notification_response(tBTA_AV_META_MSG* pmeta_msg,
         break;
 
       case AVRC_EVT_AVAL_PLAYERS_CHANGE:
-        BTIF_TRACE_DEBUG("%s: AVRC_EVT_AVAL_PLAYERS_CHANGE", __func__);
-        do_in_jni_thread(
-            FROM_HERE,
-            base::Bind(bt_rc_ctrl_callbacks->available_player_changed_cb,
-                       p_dev->rc_addr));
         break;
 
       case AVRC_EVT_ADDR_PLAYER_CHANGE:
@@ -3342,8 +3336,6 @@ static void handle_app_attr_response(tBTA_AV_META_MSG* pmeta_msg,
     rc_ctrl_procedure_complete(p_dev);
     return;
   }
-  p_dev->rc_app_settings.num_attrs = 0;
-  p_dev->rc_app_settings.num_ext_attrs = 0;
 
   for (xx = 0; xx < p_rsp->num_attr; xx++) {
     uint8_t st_index;
@@ -3817,10 +3809,6 @@ static void handle_get_playstatus_response(tBTA_AV_META_MSG* pmeta_msg,
   if (p_rsp->status == AVRC_STS_NO_ERROR) {
     do_in_jni_thread(
         FROM_HERE,
-        base::Bind(bt_rc_ctrl_callbacks->play_status_changed_cb, p_dev->rc_addr,
-                   (btrc_play_status_t)p_rsp->play_status));
-    do_in_jni_thread(
-        FROM_HERE,
         base::Bind(bt_rc_ctrl_callbacks->play_position_changed_cb,
                    p_dev->rc_addr, p_rsp->song_len, p_rsp->song_pos));
   } else {
@@ -3919,12 +3907,6 @@ static void handle_get_folder_items_response(tBTA_AV_META_MSG* pmeta_msg,
                    /* We want to make the ownership explicit in native */
                    btrc_items, item_count));
 
-    if (item_count > 0) {
-      if (btrc_items[0].item_type == AVRC_ITEM_PLAYER &&
-          (p_dev->rc_features & BTA_AV_FEAT_APP_SETTING)) {
-        list_player_app_setting_attrib_cmd(p_dev);
-      }
-    }
     /* Release the memory block for items and attributes allocated here.
      * Since the executor for do_in_jni_thread is a Single Thread Task Runner it
      * is okay to queue up the cleanup of btrc_items */
--- hci/src/hci_layer.cc
+++ hci/src/hci_layer.cc
@@ -46,7 +46,6 @@
 #include "hci_internals.h"
 #include "hcidefs.h"
 #include "hcimsgs.h"
-#include "main/shim/shim.h"
 #include "osi/include/alarm.h"
 #include "osi/include/list.h"
 #include "osi/include/log.h"
@@ -88,7 +87,6 @@ typedef struct {
 static const uint32_t COMMAND_PENDING_TIMEOUT_MS = 2000;
 static const uint32_t COMMAND_PENDING_MUTEX_ACQUIRE_TIMEOUT_MS = 500;
 static const uint32_t COMMAND_TIMEOUT_RESTART_MS = 5000;
-static const uint32_t ROOT_INFLAMMED_RESTART_MS = 5000;
 static const int HCI_UNKNOWN_COMMAND_TIMED_OUT = 0x00ffffff;
 static const int HCI_STARTUP_TIMED_OUT = 0x00eeeeee;
 
@@ -139,8 +137,6 @@ static void transmit_fragment(BT_HDR* packet, bool send_transmit_finished);
 static void dispatch_reassembled(BT_HDR* packet);
 static void fragmenter_transmit_finished(BT_HDR* packet,
                                          bool all_fragments_sent);
-static bool filter_bqr_event(int16_t bqr_parameter_length,
-                             uint8_t* p_bqr_event);
 
 static const packet_fragmenter_callbacks_t packet_fragmenter_callbacks = {
     transmit_fragment, dispatch_reassembled, fragmenter_transmit_finished};
@@ -163,11 +159,6 @@ void acl_event_received(BT_HDR* packet) {
 }
 
 void sco_data_received(BT_HDR* packet) {
-  btsnoop->capture(packet, true);
-  packet_fragmenter->reassemble_and_dispatch(packet);
-}
-
-void iso_data_received(BT_HDR* packet) {
   btsnoop->capture(packet, true);
   packet_fragmenter->reassemble_and_dispatch(packet);
 }
@@ -369,13 +360,6 @@ static void startup_timer_expired(UNUSED_ATTR void* context) {
   LOG_ERROR(LOG_TAG, "%s", __func__);
 
   LOG_EVENT_INT(BT_HCI_TIMEOUT_TAG_NUM, HCI_STARTUP_TIMED_OUT);
-
-  hci_close();
-  if (abort_timer.IsScheduled()) {
-    LOG_ERROR(LOG_TAG, "%s: waiting for abort_timer", __func__);
-    return;
-  }
-
   abort();
 }
 
@@ -537,7 +521,8 @@ static void command_timed_out(void* original_wait_entry) {
 
   uint8_t* hci_packet = reinterpret_cast<uint8_t*>(bt_hdr + 1);
 
-  UINT16_TO_STREAM(hci_packet, HCI_CONTROLLER_DEBUG_INFO);
+  UINT16_TO_STREAM(hci_packet,
+                   HCI_GRP_VENDOR_SPECIFIC | HCI_CONTROLLER_DEBUG_INFO_OCF);
   UINT8_TO_STREAM(hci_packet, 0);  // No parameters
 
   hci_firmware_log_fd = hci_open_firmware_log_file();
@@ -574,43 +559,6 @@ void process_command_credits(int credits) {
     }
     command_queue.pop();
     command_credits--;
-  }
-}
-
-bool hci_is_root_inflammation_event_received() {
-  return abort_timer.IsScheduled();
-}
-
-void handle_root_inflammation_event(uint8_t error_code,
-                                    uint8_t vendor_error_code) {
-  LOG(ERROR) << __func__
-             << ": Root inflammation event! setting timer to restart.";
-  // TODO(ugoyu) Report to bluetooth metrics here
-  {
-    // Try to stop hci command and startup timers
-    std::unique_lock<std::recursive_timed_mutex> lock(
-        commands_pending_response_mutex, std::defer_lock);
-    if (lock.try_lock_for(std::chrono::milliseconds(
-            COMMAND_PENDING_MUTEX_ACQUIRE_TIMEOUT_MS))) {
-      if (alarm_is_scheduled(startup_timer)) {
-        alarm_cancel(startup_timer);
-      }
-      if (alarm_is_scheduled(command_response_timer)) {
-        alarm_cancel(command_response_timer);
-      }
-    } else {
-      LOG(ERROR) << __func__ << ": Failed to obtain mutex";
-    }
-  }
-
-  // HwBinder thread post to hci_thread
-  if (!hci_thread.IsRunning() ||
-      !abort_timer.Schedule(
-          hci_thread.GetWeakPtr(), FROM_HERE,
-          base::Bind(hci_root_inflamed_abort, error_code, vendor_error_code),
-          base::TimeDelta::FromMilliseconds(ROOT_INFLAMMED_RESTART_MS))) {
-    LOG(ERROR) << "Failed to schedule abort_timer or hci has already closed!";
-    hci_root_inflamed_abort(error_code, vendor_error_code);
   }
 }
 
@@ -691,12 +639,46 @@ static bool filter_incoming_event(BT_HDR* packet) {
       buffer_allocator->free(packet);
       return true;
     } else if (sub_event_code == HCI_VSE_SUBCODE_BQR_SUB_EVT) {
-      // Excluding the HCI Event packet header and 1 octet sub-event code
-      int16_t bqr_parameter_length = packet->len - HCIE_PREAMBLE_SIZE - 1;
-      // The stream currently points to the BQR sub-event parameters
-      if (filter_bqr_event(bqr_parameter_length, stream)) {
-        buffer_allocator->free(packet);
-        return true;
+      uint8_t bqr_report_id;
+      STREAM_TO_UINT8(bqr_report_id, stream);
+
+      if (bqr_report_id ==
+              bluetooth::bqr::QUALITY_REPORT_ID_ROOT_INFLAMMATION &&
+          packet->len >= bluetooth::bqr::kRootInflammationPacketMinSize) {
+        uint8_t error_code;
+        uint8_t vendor_error_code;
+        STREAM_TO_UINT8(error_code, stream);
+        STREAM_TO_UINT8(vendor_error_code, stream);
+        // TODO(ugoyu) Report to bluetooth metrics here
+
+        LOG(ERROR) << __func__
+                   << ": Root inflammation event! setting timer to restart.";
+        {
+          // Try to stop hci command and startup timers
+          std::unique_lock<std::recursive_timed_mutex> lock(
+              commands_pending_response_mutex, std::defer_lock);
+          if (lock.try_lock_for(std::chrono::milliseconds(
+                  COMMAND_PENDING_MUTEX_ACQUIRE_TIMEOUT_MS))) {
+            if (alarm_is_scheduled(startup_timer)) {
+              alarm_cancel(startup_timer);
+            }
+            if (alarm_is_scheduled(command_response_timer)) {
+              alarm_cancel(command_response_timer);
+            }
+          } else {
+            LOG(ERROR) << __func__ << ": Failed to obtain mutex";
+          }
+        }
+
+        // HwBinder thread post to hci_thread
+        if (!abort_timer.Schedule(hci_thread.GetWeakPtr(), FROM_HERE,
+                                  base::Bind(hci_root_inflamed_abort,
+                                             error_code, vendor_error_code),
+                                  base::TimeDelta::FromMilliseconds(
+                                      COMMAND_TIMEOUT_RESTART_MS))) {
+          LOG(ERROR) << "Failed to schedule abort_timer!";
+          hci_root_inflamed_abort(error_code, vendor_error_code);
+        }
       }
     }
   }
@@ -771,62 +753,6 @@ static void update_command_response_timer(void) {
   }
 }
 
-// Returns true if the BQR event is handled and should not proceed to
-// higher layers.
-static bool filter_bqr_event(int16_t bqr_parameter_length,
-                             uint8_t* p_bqr_event) {
-  if (bqr_parameter_length <= 0) {
-    LOG(ERROR) << __func__ << ": Invalid parameter length : "
-               << std::to_string(bqr_parameter_length);
-    return true;
-  }
-
-  bool intercepted = false;
-  uint8_t quality_report_id = p_bqr_event[0];
-  switch (quality_report_id) {
-    case bluetooth::bqr::QUALITY_REPORT_ID_ROOT_INFLAMMATION:
-      if (bqr_parameter_length >=
-          bluetooth::bqr::kRootInflammationParamTotalLen) {
-        uint8_t error_code;
-        uint8_t vendor_error_code;
-        STREAM_TO_UINT8(quality_report_id, p_bqr_event);
-        STREAM_TO_UINT8(error_code, p_bqr_event);
-        STREAM_TO_UINT8(vendor_error_code, p_bqr_event);
-        handle_root_inflammation_event(error_code, vendor_error_code);
-      }
-      intercepted = true;
-      break;
-
-    case bluetooth::bqr::QUALITY_REPORT_ID_LMP_LL_MESSAGE_TRACE:
-      if (bqr_parameter_length >= bluetooth::bqr::kLogDumpParamTotalLen) {
-        bluetooth::bqr::DumpLmpLlMessage(bqr_parameter_length, p_bqr_event);
-      }
-      intercepted = true;
-      break;
-
-    case bluetooth::bqr::QUALITY_REPORT_ID_BT_SCHEDULING_TRACE:
-      if (bqr_parameter_length >= bluetooth::bqr::kLogDumpParamTotalLen) {
-        bluetooth::bqr::DumpBtScheduling(bqr_parameter_length, p_bqr_event);
-      }
-      intercepted = true;
-      break;
-
-    case bluetooth::bqr::QUALITY_REPORT_ID_CONTROLLER_DBG_INFO:
-      // TODO: Integrate with the HCI_VSE_SUBCODE_DEBUG_INFO_SUB_EVT
-      intercepted = true;
-      break;
-
-    case bluetooth::bqr::QUALITY_REPORT_ID_MONITOR_MODE:
-    case bluetooth::bqr::QUALITY_REPORT_ID_APPROACH_LSTO:
-    case bluetooth::bqr::QUALITY_REPORT_ID_A2DP_AUDIO_CHOPPY:
-    case bluetooth::bqr::QUALITY_REPORT_ID_SCO_VOICE_CHOPPY:
-    default:
-      break;
-  }
-
-  return intercepted;
-}
-
 static void init_layer_interface() {
   if (!interface_created) {
     // It's probably ok for this to live forever. It's small and
@@ -852,21 +778,7 @@ void hci_layer_cleanup_interface() {
   }
 }
 
-namespace bluetooth {
-namespace shim {
-const hci_t* hci_layer_get_interface();
-}  // namespace shim
-}  // namespace bluetooth
-
 const hci_t* hci_layer_get_interface() {
-  if (bluetooth::shim::is_gd_shim_enabled()) {
-    return bluetooth::shim::hci_layer_get_interface();
-  } else {
-    return bluetooth::legacy::hci_layer_get_interface();
-  }
-}
-
-const hci_t* bluetooth::legacy::hci_layer_get_interface() {
   buffer_allocator = buffer_allocator_get_interface();
   btsnoop = btsnoop_get_interface();
   packet_fragmenter = packet_fragmenter_get_interface();
