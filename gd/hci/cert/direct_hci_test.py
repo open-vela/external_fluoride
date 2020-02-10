f18fa0ff9bbe173a8a5e2e021de164f5b75b8092
liujinye <liujinye@xiaomi.com>
2020-02-11 00:02:38 +0800

    HCI: Connect without valid clock offsets am: f8df82a69a
    Change-Id: I81c4bded42fe00a64cb5143c548271868409c715

--- gd/hci/cert/direct_hci_test.py
+++ gd/hci/cert/direct_hci_test.py
@@ -48,9 +48,7 @@ class DirectHciTest(GdFacadeOnlyBaseTestClass):
         self.device_under_test.wait_channel_ready()
         self.cert_device.wait_channel_ready()
 
-        self.cert_device.hal.SendHciCommand(
-            hal_facade.HciCommandPacket(
-                payload=bytes(hci_packets.ResetBuilder().Serialize())))
+        self.cert_device.hal.SendHciResetCommand(empty_proto.Empty())
 
     def teardown_test(self):
         self.device_under_test.rootservice.StopStack(
