076d557ae9cf13d407882a174d229c51a925c718
liujinye <liujinye@xiaomi.com>
2020-02-12 19:33:51 +0800

    Merge "AVDTP: Prevent OOB read when parsing rejected response"

--- gd/security/initial_informations.h
+++ gd/security/initial_informations.h
@@ -98,8 +98,7 @@ struct InitialInformations {
   std::optional<MyOobData> my_oob_data;
 
   /* Used by Pairing Handler to present user with requests*/
-  UI* user_interface;
-  os::Handler* user_interface_handler;
+  UI* ui_handler;
 
   /* HCI interface to use */
   hci::LeSecurityInterface* le_security_interface;
