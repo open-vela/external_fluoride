c7aa55339f4db10768e2ffa9d58f68e5f395dfd2
liujinye <liujinye@xiaomi.com>
2020-02-13 11:40:44 +0800

    GattServcer: Check invalid offset
    Test: manual
    Bug: 143231677
    Change-Id: I97e2c3ae15fccc482d07d8d621c455cc74900cfd
    Merged-In: I0ca22e7c60292d61c758120c1cd67f6e6edd8ae8

--- stack/bnep/bnep_main.cc
+++ stack/bnep/bnep_main.cc
@@ -94,8 +94,7 @@ tBNEP_RESULT bnep_register_with_l2cap(void) {
   bnep_cb.reg_info.pL2CA_CongestionStatus_Cb = bnep_congestion_ind;
 
   /* Now, register with L2CAP */
-  if (!L2CA_Register(BT_PSM_BNEP, &bnep_cb.reg_info, false /* enable_snoop */,
-                     nullptr)) {
+  if (!L2CA_Register(BT_PSM_BNEP, &bnep_cb.reg_info)) {
     BNEP_TRACE_ERROR("BNEP - Registration failed");
     return BNEP_SECURITY_FAIL;
   }
