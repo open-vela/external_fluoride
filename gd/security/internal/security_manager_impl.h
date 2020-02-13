464095d57b6b2138201c1c62acd091fcaaa0ed61
liujinye <liujinye@xiaomi.com>
2020-02-14 04:31:40 +0800

    Merge "gd: Expand and log more acl events" am: 495ac9fd42 am: 7f1f790ab2 am: 350b095ca9
    Change-Id: I073609112a9ba1783cfef70406c9927ff1f05b1a

--- gd/security/internal/security_manager_impl.h
+++ gd/security/internal/security_manager_impl.h
@@ -133,8 +133,7 @@ class SecurityManagerImpl : public channel::ISecurityManagerChannelListener {
   template <class T>
   void HandleEvent(T packet);
 
-  void DispatchPairingHandler(record::SecurityRecord& record, bool locally_initiated,
-                              hci::AuthenticationRequirements authentication_requirements);
+  void DispatchPairingHandler(record::SecurityRecord& record, bool locally_initiated);
   void OnL2capRegistrationCompleteLe(l2cap::le::FixedChannelManager::RegistrationResult result,
                                      std::unique_ptr<l2cap::le::FixedChannelService> le_smp_service);
   void OnSmpCommandLe();
