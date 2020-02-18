62e73dc07e33c51c9abae27d0ca04b2304f0b24d
liujinye <liujinye@xiaomi.com>
2020-02-18 17:39:54 +0800

    Merge "gd: Add shim access to name db" am: 8962199fe6 am: 48a31f8956
    Change-Id: I7f8230e372b496147b67ff25f41b065d913e3e6b

--- gd/os/log.h
+++ gd/os/log.h
@@ -28,17 +28,11 @@
 
 #include <log/log.h>
 
-/* When including headers from legacy stack, this log definitions collide with existing logging system. Remove once we
- * get rid of legacy stack. */
-#ifndef LOG_VERBOSE
-
 #define LOG_VERBOSE(fmt, args...) ALOGV("%s:%d %s: " fmt, __FILE__, __LINE__, __func__, ##args)
 #define LOG_DEBUG(fmt, args...) ALOGD("%s:%d %s: " fmt, __FILE__, __LINE__, __func__, ##args)
 #define LOG_INFO(fmt, args...) ALOGI("%s:%d %s: " fmt, __FILE__, __LINE__, __func__, ##args)
 #define LOG_WARN(fmt, args...) ALOGW("%s:%d %s: " fmt, __FILE__, __LINE__, __func__, ##args)
 #define LOG_ERROR(fmt, args...) ALOGE("%s:%d %s: " fmt, __FILE__, __LINE__, __func__, ##args)
-
-#endif /* LOG_VERBOSE*/
 
 #else
 
@@ -46,10 +40,6 @@
 #include <chrono>
 #include <cstdio>
 #include <ctime>
-
-/* When including headers from legacy stack, this log definitions collide with existing logging system. Remove once we
- * get rid of legacy stack. */
-#ifndef LOG_VERBOSE
 
 #define LOGWRAPPER(fmt, args...)                                                                                      \
   do {                                                                                                                \
@@ -73,8 +63,6 @@
     LOGWRAPPER(__VA_ARGS__);  \
     abort();                  \
   } while (false)
-
-#endif /* LOG_VERBOE */
 
 #endif /* defined(OS_ANDROID) */
 
