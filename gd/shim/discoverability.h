bf5cf7125f662b85c6dbc6da9403d3939d01a2d1
openvela-robot <openvela-robot@xiaomi.com>
2020-02-18 21:47:56 +0800

    Get rid of the redundant shim interfaces am: c73a895fa2
    Change-Id: I68abdaf9b3e0601278c0497a581248bc0d85b034

--- gd/shim/discoverability.h
+++ gd/shim/discoverability.h
@@ -13,51 +13,41 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 #pragma once
 
 #include <memory>
+#include <string>
 
-#include "security/security_module.h"
-#include "shim/advertising.h"
-#include "shim/connectability.h"
-#include "shim/discoverability.h"
-#include "shim/dumpsys.h"
-#include "shim/hci_layer.h"
-#include "shim/inquiry.h"
-#include "shim/l2cap.h"
-#include "shim/name.h"
-#include "shim/page.h"
-#include "shim/scanning.h"
-#include "shim/security.h"
-#include "shim/storage.h"
-#include "stack_manager.h"
+#include "module.h"
 
-/**
- * The shim layer implementation on the Gd stack side.
- */
 namespace bluetooth {
 namespace shim {
 
-class Stack {
+class Discoverability : public bluetooth::Module {
  public:
-  Stack();
-  ~Stack() = default;
+  void StartGeneralDiscoverability();
+  void StartLimitedDiscoverability();
+  void StopDiscoverability();
 
-  void Start();
-  void Stop();
+  bool IsGeneralDiscoverabilityEnabled() const;
+  bool IsLimitedDiscoverabilityEnabled() const;
 
-  StackManager* GetStackManager();
+  Discoverability() = default;
+  ~Discoverability() = default;
+
+  static const ModuleFactory Factory;
+
+ protected:
+  void ListDependencies(ModuleList* list) override;  // Module
+  void Start() override;                             // Module
+  void Stop() override;                              // Module
+  std::string ToString() const override;             // Module
 
  private:
   struct impl;
   std::unique_ptr<impl> pimpl_;
-
-  Stack(const Stack&) = delete;
-  void operator=(const Stack&) = delete;
+  DISALLOW_COPY_AND_ASSIGN(Discoverability);
 };
-
-Stack* GetGabeldorscheStack();
 
 }  // namespace shim
 }  // namespace bluetooth
