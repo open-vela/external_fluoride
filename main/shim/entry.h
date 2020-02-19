b24748861d5eb6a0c540b01138724a0be196eca5
liujinye <liujinye@xiaomi.com>
2020-02-20 01:03:36 +0800

    Merge "Swap Cert and DUT index"

--- main/shim/entry.h
+++ main/shim/entry.h
@@ -39,14 +39,11 @@ class Handler;
 namespace neighbor {
 class ConnectabilityModule;
 class DiscoverabilityModule;
-class InquiryModule;
 class NameModule;
 class PageModule;
 }
 namespace hci {
 class Controller;
-class HciLayer;
-class LeAdvertisingManager;
 class LeScanningManager;
 }
 
@@ -64,13 +61,13 @@ future_t* StopGabeldorscheStack();
 /* This returns a handler that might be used in shim to receive callbacks from
  * within the stack. */
 os::Handler* GetGdShimHandler();
-hci::LeAdvertisingManager* GetAdvertising();
+Advertising* GetAdvertising();
 bluetooth::hci::Controller* GetController();
 neighbor::DiscoverabilityModule* GetDiscoverability();
 neighbor::ConnectabilityModule* GetConnectability();
 Dumpsys* GetDumpsys();
-neighbor::InquiryModule* GetInquiry();
-hci::HciLayer* GetHciLayer();
+Inquiry* GetInquiry();
+HciLayer* GetHciLayer();
 L2cap* GetL2cap();
 neighbor::NameModule* GetName();
 neighbor::PageModule* GetPage();
