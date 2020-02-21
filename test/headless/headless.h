156e682fe1b40258f6ceef3d41fcc73ddc816505
openvela-robot <openvela-robot@xiaomi.com>
2020-02-21 18:06:10 +0800

    Merge "gd: Fix legacy storage test config file" am: e4070f09a0 am: 9730313c3a am: 0520d89e1c
    Change-Id: I39bc95ee646a09651d47fc465d3c89949ff99274

--- test/headless/headless.h
+++ test/headless/headless.h
@@ -14,104 +14,32 @@
  * limitations under the License.
  */
 
-#pragma once
-
-#include <unordered_map>
-
-#include <unistd.h>
-
-#include "base/logging.h"  // LOG() stdout and android log
-#include "test/headless/get_options.h"
-
 namespace bluetooth {
 namespace test {
 namespace headless {
 
-namespace {
-
 template <typename T>
 using ExecutionUnit = std::function<T()>;
 
-constexpr char kHeadlessStartSentinel[] =
-    " START HEADLESS HEADLESS HEADLESS HEADLESS HEADLESS HEADLESS HEADLESS "
-    "HEADLESS";
-constexpr char kHeadlessStopSentinel[] =
-    " STOP HEADLESS HEADLESS HEADLESS HEADLESS HEADLESS HEADLESS HEADLESS "
-    "HEADLESS";
+class Headless {
+ public:
+  Headless() = default;
+  virtual ~Headless() = default;
 
-}  // namespace
-
-class HeadlessStack {
  protected:
-  HeadlessStack() = default;
-  virtual ~HeadlessStack() = default;
-
-  void SetUp();
-  void TearDown();
+  virtual void SetUp();
+  virtual void TearDown();
 };
 
-class HeadlessRun : public HeadlessStack {
- protected:
-  const bluetooth::test::headless::GetOpt& options_;
-  unsigned long loop_{0};
-
-  HeadlessRun(const bluetooth::test::headless::GetOpt& options)
-      : options_(options) {}
-
+class Test : public Headless {
+ public:
   template <typename T>
-  T RunOnHeadlessStack(ExecutionUnit<T> func) {
+  T Run(ExecutionUnit<T> func) {
     SetUp();
-    LOG(INFO) << kHeadlessStartSentinel;
-
-    T rc;
-    for (loop_ = 0; loop_ < options_.loop_; loop_++) {
-      rc = func();
-      if (options_.msec_ != 0) {
-        usleep(options_.msec_ * 1000);
-      }
-      if (rc) {
-        break;
-      }
-    }
-    if (rc) {
-      LOG(ERROR) << "FAIL:" << rc << " loop/loops:" << loop_ << "/"
-                 << options_.loop_;
-    } else {
-      LOG(INFO) << "PASS:" << rc << " loop/loops:" << loop_ << "/"
-                << options_.loop_;
-    }
-
-    LOG(INFO) << kHeadlessStopSentinel;
+    T rc = func();
     TearDown();
     return rc;
   }
-  virtual ~HeadlessRun() = default;
-};
-
-template <typename T>
-class HeadlessTest : public HeadlessRun {
- public:
-  virtual T Run() {
-    if (options_.non_options_.size() == 0) {
-      fprintf(stdout, "Must supply at least one subtest name\n");
-      return -1;
-    }
-
-    std::string subtest = options_.GetNextSubTest();
-    if (test_nodes_.find(subtest) == test_nodes_.end()) {
-      fprintf(stdout, "Unknown subtest module:%s\n", subtest.c_str());
-      return -1;
-    }
-    return test_nodes_.at(subtest)->Run();
-  }
-
-  virtual ~HeadlessTest() = default;
-
- protected:
-  HeadlessTest(const bluetooth::test::headless::GetOpt& options)
-      : HeadlessRun(options) {}
-
-  std::unordered_map<std::string, std::unique_ptr<HeadlessTest<T>>> test_nodes_;
 };
 
 }  // namespace headless
