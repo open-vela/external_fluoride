ceab14a71cde0ed49427f5ab6bae87047968c46e
liujinye <liujinye@xiaomi.com>
2020-02-26 17:47:17 +0800

    Mark updatable APEXes am: f3d123910b am: 4f2a2e818d
    Change-Id: Ie8b71c4d9f5dcc5c30263b06c3a90e8b8034b065

--- gd/hci/cert/controller_test.py
+++ gd/hci/cert/controller_test.py
@@ -16,7 +16,7 @@
 
 import time
 
-from mobly import asserts
+from acts import asserts
 from cert.gd_base_test_facade_only import GdFacadeOnlyBaseTestClass
 from google.protobuf import empty_pb2 as empty_proto
 from facade import rootservice_pb2 as facade_rootservice
