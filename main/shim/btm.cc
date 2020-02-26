0a87218e146ff6fe37f86c1448c604f102639822
liujinye <liujinye@xiaomi.com>
2020-02-26 12:51:54 +0800

    CertSelfTest: Fix test_l2cap_config_options am: 00eccc2531 am: 6e2ff03656 am: c9e4d629f7
    Change-Id: Ifac0a14557b0b0b58f9f552de5030d55493e0e4f

--- main/shim/btm.cc
+++ main/shim/btm.cc
@@ -22,7 +22,6 @@
 #include <cstring>
 #include <mutex>
 
-#include "bta/include/bta_api.h"
 #include "main/shim/btm.h"
 #include "main/shim/controller.h"
 #include "main/shim/entry.h"
@@ -790,16 +789,8 @@ size_t bluetooth::shim::Btm::GetNumberOfAdvertisingInstances() const {
 tBTM_STATUS bluetooth::shim::Btm::CreateBond(const RawAddress& bd_addr,
                                              tBLE_ADDR_TYPE addr_type,
                                              tBT_TRANSPORT transport,
-                                             int device_type) {
-  if (transport == BTA_TRANSPORT_UNKNOWN) {
-    if (BT_DEVICE_TYPE_BLE & BT_DEVICE_TYPE_BLE) {
-      transport = BTA_TRANSPORT_LE;
-    } else if (device_type & BT_DEVICE_TYPE_BREDR) {
-      transport = BTA_TRANSPORT_BR_EDR;
-    }
-    LOG_DEBUG(LOG_TAG, "%s guessing transport as %02x ", __func__, transport);
-  }
-
+                                             uint8_t pin_len, uint8_t* p_pin,
+                                             uint32_t trusted_mask[]) {
   auto security_manager =
       bluetooth::shim::GetSecurityModule()->GetSecurityManager();
   switch (transport) {
