5b6fc8a5c4c04786525bbc695b1ae66f3165f51a
liujinye <liujinye@xiaomi.com>
2020-02-27 07:42:10 +0800

    Security: Allow UI handlers in either order am: fc8ad63a5b
    Change-Id: I8fc3dea9fc8ab250b3653a2a7f240ab600179bc8

--- main/shim/btm_api.cc
+++ main/shim/btm_api.cc
@@ -1101,6 +1101,7 @@ bool bluetooth::shim::BTM_SecRegister(const tBTM_APPL_INFO* p_cb_info) {
     LOG_INFO(LOG_TAG, "UNIMPLEMENTED %s le_key_callback", __func__);
   }
 
+  shim_btm.SetSimplePairingCallback(p_cb_info->p_sp_callback);
   return true;
 }
 
