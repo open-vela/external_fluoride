e294e0d7a7cd575963143a641725e92cab71b55c
openvela-robot <openvela-robot@xiaomi.com>
2020-02-29 00:07:09 +0800

    Merge "Add a truth module, to make cert asserts more fluent" am: 678b95cb77
    Change-Id: I97197df03178522a011df84c0bad0acdaf6c5698

--- gd/cert/truth.py
+++ gd/cert/truth.py
@@ -20,7 +20,6 @@ from mobly.asserts import assert_true
 from mobly.asserts import assert_false
 
 from mobly import signals
-from cert.event_stream import EventStream
 
 import sys, traceback
 
@@ -54,53 +53,6 @@ class ObjectSubject(object):
                 "Expected \"%s\" to not be None" % self._value, extras=None)
 
 
-class EventStreamSubject(ObjectSubject):
-
-    def __init__(self, value):
-        super().__init__(value)
-
-    def emits(self, *match_fns):
-        if len(match_fns) == 0:
-            raise signals.TestFailure("Must specify a match function")
-        elif len(match_fns) == 1:
-            self._value.assert_event_occurs(match_fns[0])
-            return EventStreamContinuationSubject(self._value)
-        else:
-            return MultiMatchStreamSubject(self._value, match_fns)
-
-
-class MultiMatchStreamSubject(object):
-
-    def __init__(self, stream, match_fns):
-        self._stream = stream
-        self._match_fns = match_fns
-
-    def inAnyOrder(self):
-        self._stream.assert_all_events_occur(
-            self._match_fns, order_matters=False)
-        return EventStreamContinuationSubject(self._stream)
-
-    def inOrder(self):
-        self._stream.assert_all_events_occur(
-            self._match_fns, order_matters=True)
-        return EventStreamContinuationSubject(self._stream)
-
-
-class EventStreamContinuationSubject(ObjectSubject):
-
-    def __init__(self, value):
-        super().__init__(value)
-
-    def then(self, *match_fns):
-        if len(match_fns) == 0:
-            raise signals.TestFailure("Must specify a match function")
-        elif len(match_fns) == 1:
-            self._value.assert_event_occurs(match_fns[0])
-            return EventStreamContinuationSubject(self._value)
-        else:
-            return MultiMatchStreamSubject(self._value, match_fns)
-
-
 class BooleanSubject(ObjectSubject):
 
     def __init__(self, value):
@@ -116,7 +68,5 @@ class BooleanSubject(ObjectSubject):
 def assertThat(subject):
     if type(subject) is bool:
         return BooleanSubject(subject)
-    elif isinstance(subject, EventStream):
-        return EventStreamSubject(subject)
     else:
         return ObjectSubject(subject)
