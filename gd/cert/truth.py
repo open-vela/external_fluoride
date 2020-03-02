f4c3339d18e813a1688de1b298891c56b06d78f2
liujinye <liujinye@xiaomi.com>
2020-03-02 23:50:09 +0800

    Merge "GD SMP: Fix logic error in picking dialog type" am: 2fcfaca906 am: 19cc38a512 am: 734a47ac2a
    Change-Id: Ic3cad886dc52826668659bfa06c9aecaf3dda206

--- gd/cert/truth.py
+++ gd/cert/truth.py
@@ -24,8 +24,6 @@ from mobly import signals
 from cert.event_stream import IEventStream
 from cert.event_stream import NOT_FOR_YOU_assert_event_occurs
 from cert.event_stream import NOT_FOR_YOU_assert_all_events_occur
-from cert.event_stream import NOT_FOR_YOU_assert_none_matching
-from cert.event_stream import NOT_FOR_YOU_assert_none
 
 import sys, traceback
 
@@ -80,17 +78,6 @@ class EventStreamSubject(ObjectSubject):
         else:
             return MultiMatchStreamSubject(self._value, match_fns, timeout)
 
-    def emitsNone(self, *match_fns, timeout=DEFAULT_TIMEOUT):
-        if len(match_fns) == 0:
-            NOT_FOR_YOU_assert_none(self._value, timeout=timeout)
-            return EventStreamContinuationSubject(self._value)
-        elif len(match_fns) == 1:
-            NOT_FOR_YOU_assert_none_matching(
-                self._value, match_fns[0], timeout=timeout)
-            return EventStreamContinuationSubject(self._value)
-        else:
-            raise signals.TestFailure("Cannot specify multiple match functions")
-
 
 class MultiMatchStreamSubject(object):
 
@@ -133,17 +120,6 @@ class EventStreamContinuationSubject(ObjectSubject):
             return EventStreamContinuationSubject(self._value)
         else:
             return MultiMatchStreamSubject(self._value, match_fns, timeout)
-
-    def thenNone(self, *match_fns, timeout=DEFAULT_TIMEOUT):
-        if len(match_fns) == 0:
-            NOT_FOR_YOU_assert_none(self._value, timeout=timeout)
-            return EventStreamContinuationSubject(self._value)
-        elif len(match_fns) == 1:
-            NOT_FOR_YOU_assert_none_matching(
-                self._value, match_fns[0], timeout=timeout)
-            return EventStreamContinuationSubject(self._value)
-        else:
-            raise signals.TestFailure("Cannot specify multiple match functions")
 
 
 class BooleanSubject(ObjectSubject):
