aaeb6b9da3b37d5dde9050dd7a2e196c55adfbd6
liujinye <liujinye@xiaomi.com>
2020-03-02 23:27:12 +0800

    Merge "GD SMP: Fix logic error in picking dialog type" am: 2fcfaca906
    Change-Id: I92fa7b06e84f4b3ea49b742b15190aa5444584f8

--- gd/module.cc
+++ gd/module.cc
@@ -21,7 +21,7 @@ using ::bluetooth::os::Thread;
 
 namespace bluetooth {
 
-constexpr std::chrono::milliseconds kModuleStopTimeout = std::chrono::milliseconds(2000);
+constexpr std::chrono::milliseconds kModuleStopTimeout = std::chrono::milliseconds(20);
 
 ModuleFactory::ModuleFactory(std::function<Module*()> ctor) : ctor_(ctor) {
 }
