fb102100c5e4d6db75e68a9d729bd3a274eb2e82
openvela-robot <openvela-robot@xiaomi.com>
2020-03-04 00:01:01 +0800

    AAC Decoder: Use osi_free() to free buffers allocated by osi_malloc() am: 7a6fd5459e am: c4d6c95107 am: 0dd50b24a3
    Change-Id: Ief856194be69171831a004f1bcb283d09aba2364

--- gd/module_unittest.cc
+++ gd/module_unittest.cc
@@ -15,8 +15,6 @@
  */
 
 #include "module.h"
-#include "os/handler.h"
-#include "os/thread.h"
 
 #include "gtest/gtest.h"
 
@@ -41,8 +39,6 @@ class ModuleTest : public ::testing::Test {
   Thread* thread_;
 };
 
-os::Handler* test_module_no_dependency_handler = nullptr;
-
 class TestModuleNoDependency : public Module {
  public:
   static const ModuleFactory Factory;
@@ -54,7 +50,6 @@ class TestModuleNoDependency : public Module {
   void Start() override {
     // A module is not considered started until Start() finishes
     EXPECT_FALSE(GetModuleRegistry()->IsStarted<TestModuleNoDependency>());
-    test_module_no_dependency_handler = GetHandler();
   }
 
   void Stop() override {
@@ -66,8 +61,6 @@ class TestModuleNoDependency : public Module {
 const ModuleFactory TestModuleNoDependency::Factory = ModuleFactory([]() {
   return new TestModuleNoDependency();
 });
-
-os::Handler* test_module_one_dependency_handler = nullptr;
 
 class TestModuleOneDependency : public Module {
  public:
@@ -83,7 +76,6 @@ class TestModuleOneDependency : public Module {
 
     // A module is not considered started until Start() finishes
     EXPECT_FALSE(GetModuleRegistry()->IsStarted<TestModuleOneDependency>());
-    test_module_one_dependency_handler = GetHandler();
   }
 
   void Stop() override {
@@ -205,19 +197,6 @@ TEST_F(ModuleTest, two_dependencies) {
   EXPECT_FALSE(registry_->IsStarted<TestModuleOneDependency>());
   EXPECT_FALSE(registry_->IsStarted<TestModuleNoDependencyTwo>());
   EXPECT_FALSE(registry_->IsStarted<TestModuleTwoDependencies>());
-}
-
-void post_two_module_one_handler() {
-  std::this_thread::sleep_for(std::chrono::milliseconds(100));
-  test_module_one_dependency_handler->Post(common::BindOnce([] { FAIL(); }));
-}
-
-TEST_F(ModuleTest, shutdown_with_unhandled_callback) {
-  ModuleList list;
-  list.add<TestModuleOneDependency>();
-  registry_->Start(&list, thread_);
-  test_module_no_dependency_handler->Post(common::BindOnce(&post_two_module_one_handler));
-  registry_->StopAll();
 }
 
 }  // namespace
