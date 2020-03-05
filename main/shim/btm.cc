5b18b75d7af216c077235b82de188eb1dd7bd1db
openvela-robot <openvela-robot@xiaomi.com>
2020-03-05 15:02:46 +0800

    BluetoothMetricid: Add metric allocator api in java
    Bug: 142563377
    Test: Manual atest AdapterServiceTest
    
    Change-Id: I46d1c1d5bf0c8beea1f46b1024cbd5ceb9c0b562
    Merged-In: I46d1c1d5bf0c8beea1f46b1024cbd5ceb9c0b562

--- main/shim/btm.cc
+++ main/shim/btm.cc
@@ -22,6 +22,7 @@
 #include <cstring>
 #include <mutex>
 
+#include "bta/include/bta_api.h"
 #include "main/shim/btm.h"
 #include "main/shim/controller.h"
 #include "main/shim/entry.h"
@@ -789,8 +790,16 @@ size_t bluetooth::shim::Btm::GetNumberOfAdvertisingInstances() const {
 tBTM_STATUS bluetooth::shim::Btm::CreateBond(const RawAddress& bd_addr,
                                              tBLE_ADDR_TYPE addr_type,
                                              tBT_TRANSPORT transport,
-                                             uint8_t pin_len, uint8_t* p_pin,
-                                             uint32_t trusted_mask[]) {
+                                             int device_type) {
+  if (transport == BTA_TRANSPORT_UNKNOWN) {
+    if (device_type & BT_DEVICE_TYPE_BLE) {
+      transport = BTA_TRANSPORT_LE;
+    } else if (device_type & BT_DEVICE_TYPE_BREDR) {
+      transport = BTA_TRANSPORT_BR_EDR;
+    }
+    LOG_DEBUG(LOG_TAG, "%s guessing transport as %02x ", __func__, transport);
+  }
+
   auto security_manager =
       bluetooth::shim::GetSecurityModule()->GetSecurityManager();
   switch (transport) {
@@ -820,11 +829,4 @@ bool bluetooth::shim::Btm::RemoveBond(const RawAddress& bd_addr) {
       bluetooth::shim::GetSecurityModule()->GetSecurityManager();
   security_manager->RemoveBond(ToAddressWithType(bd_addr, BLE_ADDR_PUBLIC));
   return true;
-}
-
-void bluetooth::shim::Btm::SetSimplePairingCallback(
-    tBTM_SP_CALLBACK* callback) {
-  auto security_manager =
-      bluetooth::shim::GetSecurityModule()->GetSecurityManager();
-  simple_pairing_callback_ = callback;
 }
--- main/shim/btm_api.cc
+++ main/shim/btm_api.cc
@@ -1064,10 +1064,8 @@ bool bluetooth::shim::BTM_BleLocalPrivacyEnabled(void) {
 tBTM_STATUS bluetooth::shim::BTM_SecBond(const RawAddress& bd_addr,
                                          tBLE_ADDR_TYPE addr_type,
                                          tBT_TRANSPORT transport,
-                                         uint8_t pin_len, uint8_t* p_pin,
-                                         uint32_t trusted_mask[]) {
-  return shim_btm.CreateBond(bd_addr, addr_type, transport, pin_len, p_pin,
-                             trusted_mask);
+                                         int device_type) {
+  return shim_btm.CreateBond(bd_addr, addr_type, transport, device_type);
 }
 
 bool bluetooth::shim::BTM_SecRegister(const tBTM_APPL_INFO* p_cb_info) {
@@ -1103,7 +1101,6 @@ bool bluetooth::shim::BTM_SecRegister(const tBTM_APPL_INFO* p_cb_info) {
     LOG_INFO(LOG_TAG, "UNIMPLEMENTED %s le_key_callback", __func__);
   }
 
-  shim_btm.SetSimplePairingCallback(p_cb_info->p_sp_callback);
   return true;
 }
 
