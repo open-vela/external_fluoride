696546a8c0e04dd502cc4598b41f99730ebd7e95
openvela-robot <openvela-robot@xiaomi.com>
2020-03-11 00:14:15 +0800

    Fix L2capTest:test_continuation_flag am: 99e7558fda am: 6d82244866 am: 5447cc13a1
    Change-Id: Ibd939fea1efa1e6d3d5b4d227a4c08d0336fa51e

--- hci/src/hci_layer_android.cc
+++ hci/src/hci_layer_android.cc
@@ -32,29 +32,44 @@
 #include <android/hardware/bluetooth/1.0/IBluetoothHci.h>
 #include <android/hardware/bluetooth/1.0/IBluetoothHciCallbacks.h>
 #include <android/hardware/bluetooth/1.0/types.h>
+#include <android/hardware/bluetooth/1.1/IBluetoothHci.h>
+#include <android/hardware/bluetooth/1.1/IBluetoothHciCallbacks.h>
 #include <hwbinder/ProcessState.h>
 
 #define LOG_PATH "/data/misc/bluetooth/logs/firmware_events.log"
 #define LAST_LOG_PATH "/data/misc/bluetooth/logs/firmware_events.log.last"
 
-using android::hardware::bluetooth::V1_0::IBluetoothHci;
-using android::hardware::bluetooth::V1_0::IBluetoothHciCallbacks;
-using android::hardware::bluetooth::V1_0::HciPacket;
-using android::hardware::bluetooth::V1_0::Status;
-using android::hardware::ProcessState;
+using ::android::hardware::hidl_death_recipient;
+using ::android::hardware::hidl_vec;
+using ::android::hardware::ProcessState;
 using ::android::hardware::Return;
 using ::android::hardware::Void;
-using ::android::hardware::hidl_vec;
+using ::android::hardware::bluetooth::V1_0::HciPacket;
+using ::android::hardware::bluetooth::V1_0::Status;
+
+using namespace ::android::hardware::bluetooth;
 
 extern void initialization_complete();
-extern void hci_event_received(const tracked_objects::Location& from_here,
-                               BT_HDR* packet);
+extern void hci_event_received(const base::Location& from_here, BT_HDR* packet);
 extern void acl_event_received(BT_HDR* packet);
 extern void sco_data_received(BT_HDR* packet);
+extern void iso_data_received(BT_HDR* packet);
+extern void hal_service_died();
+extern bool hci_is_root_inflammation_event_received();
 
-android::sp<IBluetoothHci> btHci;
+android::sp<V1_0::IBluetoothHci> btHci;
+android::sp<V1_1::IBluetoothHci> btHci_1_1;
 
-class BluetoothHciCallbacks : public IBluetoothHciCallbacks {
+class BluetoothHciDeathRecipient : public hidl_death_recipient {
+ public:
+  virtual void serviceDied(uint64_t /*cookie*/, const android::wp<::android::hidl::base::V1_0::IBase>& /*who*/) {
+    LOG_ERROR("Bluetooth HAL service died!");
+    hal_service_died();
+  }
+};
+android::sp<BluetoothHciDeathRecipient> bluetoothHciDeathRecipient = new BluetoothHciDeathRecipient();
+
+class BluetoothHciCallbacks : public V1_1::IBluetoothHciCallbacks {
  public:
   BluetoothHciCallbacks() {
     buffer_allocator = buffer_allocator_get_interface();
@@ -74,27 +89,41 @@ class BluetoothHciCallbacks : public IBluetoothHciCallbacks {
     return packet;
   }
 
-  Return<void> initializationComplete(Status status) {
+  Return<void> initializationComplete(Status status) override {
+    if (hci_is_root_inflammation_event_received()) {
+      // Ignore the initializationComplete here as we have already received
+      // root inflammation event earlier.
+      LOG_ERROR(
+          "initializationComplete after root inflammation event! status=%d",
+          status);
+      return Void();
+    }
     CHECK(status == Status::SUCCESS);
     initialization_complete();
     return Void();
   }
 
-  Return<void> hciEventReceived(const hidl_vec<uint8_t>& event) {
+  Return<void> hciEventReceived(const hidl_vec<uint8_t>& event) override {
     BT_HDR* packet = WrapPacketAndCopy(MSG_HC_TO_STACK_HCI_EVT, event);
     hci_event_received(FROM_HERE, packet);
     return Void();
   }
 
-  Return<void> aclDataReceived(const hidl_vec<uint8_t>& data) {
+  Return<void> aclDataReceived(const hidl_vec<uint8_t>& data) override {
     BT_HDR* packet = WrapPacketAndCopy(MSG_HC_TO_STACK_HCI_ACL, data);
     acl_event_received(packet);
     return Void();
   }
 
-  Return<void> scoDataReceived(const hidl_vec<uint8_t>& data) {
+  Return<void> scoDataReceived(const hidl_vec<uint8_t>& data) override {
     BT_HDR* packet = WrapPacketAndCopy(MSG_HC_TO_STACK_HCI_SCO, data);
     sco_data_received(packet);
+    return Void();
+  }
+
+  Return<void> isoDataReceived(const hidl_vec<uint8_t>& data) override {
+    BT_HDR* packet = WrapPacketAndCopy(MSG_HC_TO_STACK_HCI_ISO, data);
+    iso_data_received(packet);
     return Void();
   }
 
@@ -102,24 +131,52 @@ class BluetoothHciCallbacks : public IBluetoothHciCallbacks {
 };
 
 void hci_initialize() {
-  LOG_INFO(LOG_TAG, "%s", __func__);
+  LOG_INFO("%s", __func__);
 
-  btHci = IBluetoothHci::getService();
+  btHci_1_1 = V1_1::IBluetoothHci::getService();
+
+  if (btHci_1_1 != nullptr) {
+    btHci = btHci_1_1;
+  } else {
+    btHci = V1_0::IBluetoothHci::getService();
+  }
+
   // If android.hardware.bluetooth* is not found, Bluetooth can not continue.
   CHECK(btHci != nullptr);
-  LOG_INFO(LOG_TAG, "%s: IBluetoothHci::getService() returned %p (%s)",
-           __func__, btHci.get(), (btHci->isRemote() ? "remote" : "local"));
+  auto death_link = btHci->linkToDeath(bluetoothHciDeathRecipient, 0);
+  if (!death_link.isOk()) {
+    LOG_ERROR("%s: Unable to set the death recipient for the Bluetooth HAL",
+              __func__);
+    abort();
+  }
+  LOG_INFO("%s: IBluetoothHci::getService() returned %p (%s)", __func__,
+           btHci.get(), (btHci->isRemote() ? "remote" : "local"));
 
   // Block allows allocation of a variable that might be bypassed by goto.
   {
-    android::sp<IBluetoothHciCallbacks> callbacks = new BluetoothHciCallbacks();
-    btHci->initialize(callbacks);
+    android::sp<V1_1::IBluetoothHciCallbacks> callbacks =
+        new BluetoothHciCallbacks();
+    if (btHci_1_1 != nullptr) {
+      btHci_1_1->initialize(callbacks);
+    } else {
+      btHci->initialize(callbacks);
+    }
   }
 }
 
 void hci_close() {
-  btHci->close();
-  btHci = nullptr;
+  if (btHci != nullptr) {
+    auto death_unlink = btHci->unlinkToDeath(bluetoothHciDeathRecipient);
+    if (!death_unlink.isOk()) {
+      LOG_ERROR("%s: Error unlinking death recipient from the Bluetooth HAL",
+                __func__);
+    }
+    auto close_status = btHci->close();
+    if (!close_status.isOk()) {
+      LOG_ERROR("%s: Error closing the Bluetooth HAL", __func__);
+    }
+    btHci = nullptr;
+  }
 }
 
 void hci_transmit(BT_HDR* packet) {
@@ -137,16 +194,23 @@ void hci_transmit(BT_HDR* packet) {
     case MSG_STACK_TO_HC_HCI_SCO:
       btHci->sendScoData(data);
       break;
+    case MSG_STACK_TO_HC_HCI_ISO:
+      if (btHci_1_1 != nullptr) {
+        btHci_1_1->sendIsoData(data);
+      } else {
+        LOG_ERROR("ISO is not supported in HAL v1.0");
+      }
+      break;
     default:
-      LOG_ERROR(LOG_TAG, "Unknown packet type (%d)", event);
+      LOG_ERROR("Unknown packet type (%d)", event);
       break;
   }
 }
 
 int hci_open_firmware_log_file() {
   if (rename(LOG_PATH, LAST_LOG_PATH) == -1 && errno != ENOENT) {
-    LOG_ERROR(LOG_TAG, "%s unable to rename '%s' to '%s': %s", __func__,
-              LOG_PATH, LAST_LOG_PATH, strerror(errno));
+    LOG_ERROR("%s unable to rename '%s' to '%s': %s", __func__, LOG_PATH,
+              LAST_LOG_PATH, strerror(errno));
   }
 
   mode_t prevmask = umask(0);
@@ -154,7 +218,7 @@ int hci_open_firmware_log_file() {
                         S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
   umask(prevmask);
   if (logfile_fd == INVALID_FD) {
-    LOG_ERROR(LOG_TAG, "%s unable to open '%s': %s", __func__, LOG_PATH,
+    LOG_ERROR("%s unable to open '%s': %s", __func__, LOG_PATH,
               strerror(errno));
   }
 
