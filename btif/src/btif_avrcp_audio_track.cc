2ccb9604bfecdbd78ebdd00b5c3f610e02f32a79
liujinye <liujinye@xiaomi.com>
2020-03-10 21:03:33 +0800

    Merge "L2CAP: Add test for L2CAP 2/2 Support of configuration process" am: 87c1bac341
    Change-Id: I37aacaf5a0eddae7992031316954f0360085f78c

--- btif/src/btif_avrcp_audio_track.cc
+++ btif/src/btif_avrcp_audio_track.cc
@@ -19,8 +19,8 @@
 
 #include "btif_avrcp_audio_track.h"
 
+#include <aaudio/AAudio.h>
 #include <base/logging.h>
-#include <media/AudioTrack.h>
 #include <utils/StrongPointer.h>
 
 #include "bt_target.h"
@@ -28,70 +28,86 @@
 
 using namespace android;
 
-typedef struct { android::sp<android::AudioTrack> track; } BtifAvrcpAudioTrack;
+typedef struct {
+  AAudioStream* stream;
+  int bitsPerSample;
+  int channelCount;
+  float* buffer;
+  size_t bufferLength;
+} BtifAvrcpAudioTrack;
 
 #if (DUMP_PCM_DATA == TRUE)
 FILE* outputPcmSampleFile;
 char outputFilename[50] = "/data/misc/bluedroid/output_sample.pcm";
 #endif
 
-void* BtifAvrcpAudioTrackCreate(int trackFreq, int channelType) {
-  LOG_VERBOSE(LOG_TAG, "%s Track.cpp: btCreateTrack freq %d  channel %d ",
-              __func__, trackFreq, channelType);
-  sp<android::AudioTrack> track = new android::AudioTrack(
-      AUDIO_STREAM_MUSIC, trackFreq, AUDIO_FORMAT_PCM_16_BIT, channelType,
-      (size_t)0 /*frameCount*/, (audio_output_flags_t)AUDIO_OUTPUT_FLAG_FAST,
-      NULL /*callback_t*/, NULL /*void* user*/, 0 /*notificationFrames*/,
-      AUDIO_SESSION_ALLOCATE, android::AudioTrack::TRANSFER_SYNC);
-  CHECK(track != NULL);
+void* BtifAvrcpAudioTrackCreate(int trackFreq, int bitsPerSample,
+                                int channelCount) {
+  LOG_VERBOSE("%s Track.cpp: btCreateTrack freq %d bps %d channel %d ",
+              __func__, trackFreq, bitsPerSample, channelCount);
+
+  AAudioStreamBuilder* builder;
+  AAudioStream* stream;
+  aaudio_result_t result = AAudio_createStreamBuilder(&builder);
+  AAudioStreamBuilder_setSampleRate(builder, trackFreq);
+  AAudioStreamBuilder_setFormat(builder, AAUDIO_FORMAT_PCM_FLOAT);
+  AAudioStreamBuilder_setChannelCount(builder, channelCount);
+  AAudioStreamBuilder_setSessionId(builder, AAUDIO_SESSION_ID_ALLOCATE);
+  AAudioStreamBuilder_setPerformanceMode(builder,
+                                         AAUDIO_PERFORMANCE_MODE_LOW_LATENCY);
+  result = AAudioStreamBuilder_openStream(builder, &stream);
+  CHECK(result == AAUDIO_OK);
+  AAudioStreamBuilder_delete(builder);
 
   BtifAvrcpAudioTrack* trackHolder = new BtifAvrcpAudioTrack;
   CHECK(trackHolder != NULL);
-  trackHolder->track = track;
-
-  if (trackHolder->track->initCheck() != 0) {
-    return nullptr;
-  }
+  trackHolder->stream = stream;
+  trackHolder->bitsPerSample = bitsPerSample;
+  trackHolder->channelCount = channelCount;
+  trackHolder->bufferLength =
+      trackHolder->channelCount * AAudioStream_getBufferSizeInFrames(stream);
+  trackHolder->buffer = new float[trackHolder->bufferLength]();
 
 #if (DUMP_PCM_DATA == TRUE)
   outputPcmSampleFile = fopen(outputFilename, "ab");
 #endif
-  trackHolder->track->setVolume(1, 1);
   return (void*)trackHolder;
 }
 
 void BtifAvrcpAudioTrackStart(void* handle) {
   if (handle == NULL) {
-    LOG_ERROR(LOG_TAG, "%s: handle is null!", __func__);
+    LOG_ERROR("%s: handle is null!", __func__);
     return;
   }
   BtifAvrcpAudioTrack* trackHolder = static_cast<BtifAvrcpAudioTrack*>(handle);
   CHECK(trackHolder != NULL);
-  CHECK(trackHolder->track != NULL);
-  LOG_VERBOSE(LOG_TAG, "%s Track.cpp: btStartTrack", __func__);
-  trackHolder->track->start();
+  CHECK(trackHolder->stream != NULL);
+  LOG_VERBOSE("%s Track.cpp: btStartTrack", __func__);
+  AAudioStream_requestStart(trackHolder->stream);
 }
 
 void BtifAvrcpAudioTrackStop(void* handle) {
   if (handle == NULL) {
-    LOG_DEBUG(LOG_TAG, "%s handle is null.", __func__);
+    LOG_DEBUG("%s handle is null.", __func__);
     return;
   }
   BtifAvrcpAudioTrack* trackHolder = static_cast<BtifAvrcpAudioTrack*>(handle);
-  if (trackHolder != NULL && trackHolder->track != NULL) {
-    LOG_VERBOSE(LOG_TAG, "%s Track.cpp: btStartTrack", __func__);
-    trackHolder->track->stop();
+  if (trackHolder != NULL && trackHolder->stream != NULL) {
+    LOG_VERBOSE("%s Track.cpp: btStartTrack", __func__);
+    AAudioStream_requestStop(trackHolder->stream);
   }
 }
 
 void BtifAvrcpAudioTrackDelete(void* handle) {
   if (handle == NULL) {
-    LOG_DEBUG(LOG_TAG, "%s handle is null.", __func__);
+    LOG_DEBUG("%s handle is null.", __func__);
     return;
   }
   BtifAvrcpAudioTrack* trackHolder = static_cast<BtifAvrcpAudioTrack*>(handle);
-  if (trackHolder != NULL && trackHolder->track != NULL) {
-    LOG_VERBOSE(LOG_TAG, "%s Track.cpp: btStartTrack", __func__);
+  if (trackHolder != NULL && trackHolder->stream != NULL) {
+    LOG_VERBOSE("%s Track.cpp: btStartTrack", __func__);
+    AAudioStream_close(trackHolder->stream);
+    delete trackHolder->buffer;
     delete trackHolder;
   }
 
@@ -105,42 +121,106 @@ void BtifAvrcpAudioTrackDelete(void* handle) {
 
 void BtifAvrcpAudioTrackPause(void* handle) {
   if (handle == NULL) {
-    LOG_DEBUG(LOG_TAG, "%s handle is null.", __func__);
+    LOG_DEBUG("%s handle is null.", __func__);
     return;
   }
   BtifAvrcpAudioTrack* trackHolder = static_cast<BtifAvrcpAudioTrack*>(handle);
-  if (trackHolder != NULL && trackHolder->track != NULL) {
-    LOG_VERBOSE(LOG_TAG, "%s Track.cpp: btStartTrack", __func__);
-    trackHolder->track->pause();
-    trackHolder->track->flush();
+  if (trackHolder != NULL && trackHolder->stream != NULL) {
+    LOG_VERBOSE("%s Track.cpp: btPauseTrack", __func__);
+    AAudioStream_requestPause(trackHolder->stream);
+    AAudioStream_requestFlush(trackHolder->stream);
   }
 }
 
 void BtifAvrcpSetAudioTrackGain(void* handle, float gain) {
   if (handle == NULL) {
-    LOG_DEBUG(LOG_TAG, "%s handle is null.", __func__);
+    LOG_DEBUG("%s handle is null.", __func__);
     return;
   }
-  BtifAvrcpAudioTrack* trackHolder = static_cast<BtifAvrcpAudioTrack*>(handle);
-  if (trackHolder != NULL && trackHolder->track != NULL) {
-    LOG_VERBOSE(LOG_TAG, "%s set gain %f", __func__, gain);
-    trackHolder->track->setVolume(gain);
-  }
+  // Does nothing right now
 }
 
+constexpr float kScaleQ15ToFloat = 1.0f / 32768.0f;
+constexpr float kScaleQ23ToFloat = 1.0f / 8388608.0f;
+constexpr float kScaleQ31ToFloat = 1.0f / 2147483648.0f;
+
+static size_t sampleSizeFor(BtifAvrcpAudioTrack* trackHolder) {
+  return trackHolder->bitsPerSample / 8;
+}
+
+static size_t transcodeQ15ToFloat(uint8_t* buffer, size_t length,
+                                  BtifAvrcpAudioTrack* trackHolder) {
+  size_t sampleSize = sampleSizeFor(trackHolder);
+  size_t i = 0;
+  for (; i <= length / sampleSize; i++) {
+    trackHolder->buffer[i] = ((int16_t*)buffer)[i] * kScaleQ15ToFloat;
+  }
+  return i * sampleSize;
+}
+
+static size_t transcodeQ23ToFloat(uint8_t* buffer, size_t length,
+                                  BtifAvrcpAudioTrack* trackHolder) {
+  size_t sampleSize = sampleSizeFor(trackHolder);
+  size_t i = 0;
+  for (; i <= length / sampleSize; i++) {
+    size_t offset = i * sampleSize;
+    int32_t sample = *((int32_t*)(buffer + offset - 1)) & 0x00FFFFFF;
+    trackHolder->buffer[i] = sample * kScaleQ23ToFloat;
+  }
+  return i * sampleSize;
+}
+
+static size_t transcodeQ31ToFloat(uint8_t* buffer, size_t length,
+                                  BtifAvrcpAudioTrack* trackHolder) {
+  size_t sampleSize = sampleSizeFor(trackHolder);
+  size_t i = 0;
+  for (; i <= length / sampleSize; i++) {
+    trackHolder->buffer[i] = ((int32_t*)buffer)[i] * kScaleQ31ToFloat;
+  }
+  return i * sampleSize;
+}
+
+static size_t transcodeToPcmFloat(uint8_t* buffer, size_t length,
+                                  BtifAvrcpAudioTrack* trackHolder) {
+  switch (trackHolder->bitsPerSample) {
+    case 16:
+      return transcodeQ15ToFloat(buffer, length, trackHolder);
+    case 24:
+      return transcodeQ23ToFloat(buffer, length, trackHolder);
+    case 32:
+      return transcodeQ31ToFloat(buffer, length, trackHolder);
+  }
+  return -1;
+}
+
+constexpr int64_t kTimeoutNanos = 100 * 1000 * 1000;  // 100 ms
+
 int BtifAvrcpAudioTrackWriteData(void* handle, void* audioBuffer,
-                                 int bufferlen) {
+                                 int bufferLength) {
   BtifAvrcpAudioTrack* trackHolder = static_cast<BtifAvrcpAudioTrack*>(handle);
   CHECK(trackHolder != NULL);
-  CHECK(trackHolder->track != NULL);
-  int retval = -1;
+  CHECK(trackHolder->stream != NULL);
+  aaudio_result_t retval = -1;
 #if (DUMP_PCM_DATA == TRUE)
   if (outputPcmSampleFile) {
-    fwrite((audioBuffer), 1, (size_t)bufferlen, outputPcmSampleFile);
+    fwrite((audioBuffer), 1, (size_t)bufferLength, outputPcmSampleFile);
   }
 #endif
-  retval = trackHolder->track->write(audioBuffer, (size_t)bufferlen);
-  LOG_VERBOSE(LOG_TAG, "%s Track.cpp: btWriteData len = %d ret = %d", __func__,
-              bufferlen, retval);
-  return retval;
+
+  size_t sampleSize = sampleSizeFor(trackHolder);
+  int transcodedCount = 0;
+  do {
+    transcodedCount +=
+        transcodeToPcmFloat(((uint8_t*)audioBuffer) + transcodedCount,
+                            bufferLength - transcodedCount, trackHolder);
+
+    retval = AAudioStream_write(
+        trackHolder->stream, trackHolder->buffer,
+        transcodedCount / (sampleSize * trackHolder->channelCount),
+        kTimeoutNanos);
+    LOG_VERBOSE("%s Track.cpp: btWriteData len = %d ret = %d", __func__,
+                bufferLength, retval);
+  } while (transcodedCount < bufferLength);
+
+  return transcodedCount;
 }
--- stack/include/btm_ble_api_types.h
+++ stack/include/btm_ble_api_types.h
@@ -36,10 +36,12 @@ typedef uint8_t tBTM_BLE_CHNL_MAP[CHNL_MAP_LEN];
 #define BTM_BLE_NON_CONNECT_EVT 0x03
 /* Connectable low duty cycle directed advertising  */
 #define BTM_BLE_CONNECT_LO_DUTY_DIR_EVT 0x04
-/* 0x00 - 0x05 can be received on adv event type */
+/* 0x00 - 0x04 can be received on adv event type */
+#define BTM_BLE_ADV_IND_EVT  0x00
+#define BTM_BLE_ADV_DIRECT_IND_EVT  0x01
+#define BTM_BLE_ADV_SCAN_IND_EVT  0x02
+#define BTM_BLE_ADV_NONCONN_IND_EVT  0x03
 #define BTM_BLE_SCAN_RSP_EVT 0x04
-#define BTM_BLE_SCAN_REQ_EVT 0x05
-#define BTM_BLE_UNKNOWN_EVT 0xff
 
 #define BTM_BLE_UNKNOWN_EVT 0xff
 
@@ -135,7 +137,7 @@ typedef uint8_t tBTM_BLE_SFP;
 #define BTM_BLE_CONN_SUP_TOUT_MAX 0x0C80
 /* use this value when a specific value not to be overwritten */
 #define BTM_BLE_CONN_PARAM_UNDEF 0xffff
-#define BTM_BLE_SCAN_PARAM_UNDEF 0xffffffff
+#define BTM_BLE_SCAN_PARAM_UNDEF 0xffff
 
 /* default connection parameters if not configured, use GAP recommended value
  * for auto/selective connection */
@@ -201,13 +203,6 @@ typedef uint8_t tBTM_BLE_SFP;
  * device */
 #ifndef BTM_BLE_CONN_INT_MIN_HEARINGAID
 #define BTM_BLE_CONN_INT_MIN_HEARINGAID 0x0010
-#endif
-
-#define BTM_BLE_DIR_CONN_FALLBACK_UNDIR 1
-#define BTM_BLE_DIR_CONN_FALLBACK_NO_ADV 2
-
-#ifndef BTM_BLE_DIR_CONN_FALLBACK
-#define BTM_BLE_DIR_CONN_FALLBACK BTM_BLE_DIR_CONN_FALLBACK_UNDIR
 #endif
 
 #define BTM_CMAC_TLEN_SIZE 8 /* 64 bits */
@@ -279,7 +274,7 @@ typedef struct {
   uint8_t status;
   uint8_t param_len;
   uint16_t opcode;
-  uint8_t param_buf[BT_OCTET16_LEN];
+  uint8_t param_buf[OCTET16_LEN];
 } tBTM_RAND_ENC;
 
 /* General callback function for notifying an application that a synchronous
@@ -335,7 +330,8 @@ typedef void(tBTM_RAND_ENC_CB)(tBTM_RAND_ENC* p1);
 /*  Preferred maximum number of microseconds that the local Controller
     should use to transmit a single Link Layer Data Channel PDU. */
 #define BTM_BLE_DATA_TX_TIME_MIN 0x0148
-#define BTM_BLE_DATA_TX_TIME_MAX 0x0848
+#define BTM_BLE_DATA_TX_TIME_MAX_LEGACY  0x0848
+#define BTM_BLE_DATA_TX_TIME_MAX         0x4290
 
 /* adv tx power in dBm */
 typedef struct {
@@ -493,9 +489,6 @@ typedef uint8_t BTM_BLE_ADV_STATE;
 typedef uint8_t BTM_BLE_ADV_INFO_PRESENT;
 typedef uint8_t BTM_BLE_RSSI_VALUE;
 typedef uint16_t BTM_BLE_ADV_INFO_TIMESTAMP;
-
-enum { BTM_BLE_CONN_NONE, BTM_BLE_CONN_AUTO };
-typedef uint8_t tBTM_BLE_CONN_TYPE;
 
 #define ADV_INFO_PRESENT 0x00
 #define NO_ADV_INFO_PRESENT 0x01
