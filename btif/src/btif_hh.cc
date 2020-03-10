f05cbe2f67c9f3ab2b3d17ae8ae6dcd75ccb4ae7
liujinye <liujinye@xiaomi.com>
2020-03-11 00:14:09 +0800

    [automerger skipped] GattServcer: Check invalid offset am: 30a2860ed1 am: 2865a273f2 -s ours am: b9e373e364 -s ours
    am skip reason: Change-Id I0396380f431cdb7f91c78db6de9043ea0f373dfe with SHA-1 c14c1fb864 is in history
    
    Change-Id: I19ce865f59ffe8bee30c81401a480d1b32ee632b

--- btif/src/btif_hh.cc
+++ btif/src/btif_hh.cc
@@ -79,6 +79,10 @@ static int btif_hh_keylockstates = 0;  // The current key state of each key
 #define BTUI_HH_SECURITY (BTA_SEC_AUTHENTICATE | BTA_SEC_ENCRYPT)
 #endif
 
+#ifndef BTUI_HH_MOUSE_SECURITY
+#define BTUI_HH_MOUSE_SECURITY (BTA_SEC_NONE)
+#endif
+
 /* HH request events */
 typedef enum {
   BTIF_HH_CONNECT_REQ_EVT = 0,
@@ -533,21 +537,11 @@ bt_status_t btif_hh_virtual_unplug(const RawAddress* bd_addr) {
   p_dev = btif_hh_find_dev_by_bda(*bd_addr);
   if ((p_dev != NULL) && (p_dev->dev_status == BTHH_CONN_STATE_CONNECTED) &&
       (p_dev->attr_mask & HID_VIRTUAL_CABLE)) {
-    BTIF_TRACE_DEBUG("%s: Sending BTA_HH_CTRL_VIRTUAL_CABLE_UNPLUG for: %s", __func__,
-                     bd_addr->ToString().c_str());
+    BTIF_TRACE_DEBUG("%s Sending BTA_HH_CTRL_VIRTUAL_CABLE_UNPLUG", __func__);
     /* start the timer */
     btif_hh_start_vup_timer(bd_addr);
     p_dev->local_vup = true;
     BTA_HhSendCtrl(p_dev->dev_handle, BTA_HH_CTRL_VIRTUAL_CABLE_UNPLUG);
-    return BT_STATUS_SUCCESS;
-  } else if ((p_dev != NULL) &&
-             (p_dev->dev_status == BTHH_CONN_STATE_CONNECTED)) {
-    BTIF_TRACE_ERROR("%s: Virtual unplug not suported, disconnecting device: %s",
-                     __func__, bd_addr->ToString().c_str());
-    /* start the timer */
-    btif_hh_start_vup_timer(bd_addr);
-    p_dev->local_vup = true;
-    BTA_HhClose(p_dev->dev_handle);
     return BT_STATUS_SUCCESS;
   } else {
     BTIF_TRACE_ERROR("%s: Error, device %s not opened, status = %d", __func__,
@@ -609,9 +603,10 @@ bt_status_t btif_hh_connect(const RawAddress* bd_addr) {
    request from host, for subsequent user initiated connection. If the remote is
    not in
    pagescan mode, we will do 2 retries to connect before giving up */
+  tBTA_SEC sec_mask = BTUI_HH_SECURITY;
   btif_hh_cb.status = BTIF_HH_DEV_CONNECTING;
   btif_hh_cb.pending_conn_address = *bd_addr;
-  BTA_HhOpen(*bd_addr, BTA_HH_PROTO_RPT_MODE, BTUI_HH_SECURITY);
+  BTA_HhOpen(*bd_addr, BTA_HH_PROTO_RPT_MODE, sec_mask);
 
   // TODO(jpawlowski); make cback accept const and remove tmp!
   auto tmp = *bd_addr;
@@ -685,21 +680,6 @@ void btif_hh_service_registration(bool enable) {
     btif_hh_cb.service_dereg_active = TRUE;
     BTA_HhDisable();
   }
-}
-
-/*******************************************************************************
- *
- *
- * Function         btif_hh_getreport
- *
- * Description      getreport initiated from the BTIF thread context
- *
- * Returns          void
- *
- ******************************************************************************/
-void btif_hh_getreport(btif_hh_device_t* p_dev, bthh_report_type_t r_type,
-                       uint8_t reportId, uint16_t bufferSize) {
-  BTA_HhGetReport(p_dev->dev_handle, r_type, reportId, bufferSize);
 }
 
 /*****************************************************************************
@@ -960,7 +940,7 @@ static void btif_hh_upstreams_evt(uint16_t event, char* p_param) {
       }
       if (p_dev->fd < 0) {
         LOG_ERROR(
-
+            LOG_TAG,
             "BTA_HH_GET_DSCP_EVT: Error, failed to find the uhid driver...");
         return;
       }
@@ -1084,7 +1064,7 @@ static void btif_hh_upstreams_evt(uint16_t event, char* p_param) {
       break;
 
     case BTA_HH_API_ERR_EVT:
-      LOG_INFO("BTA_HH API_ERR");
+      LOG_INFO(LOG_TAG, "BTA_HH API_ERR");
       break;
 
     default:
@@ -1550,7 +1530,7 @@ static bt_status_t set_report(RawAddress* bd_addr,
     /* Build a SetReport data buffer */
     // TODO
     hex_bytes_filled = ascii_2_hex(report, len, hexbuf);
-    LOG_INFO("Hex bytes filled, hex value: %d", hex_bytes_filled);
+    LOG_INFO(LOG_TAG, "Hex bytes filled, hex value: %d", hex_bytes_filled);
     if (hex_bytes_filled) {
       BT_HDR* p_buf = create_pbuf(hex_bytes_filled, hexbuf);
       if (p_buf == NULL) {
--- stack/avrc/avrc_pars_ct.cc
+++ stack/avrc/avrc_pars_ct.cc
@@ -184,16 +184,6 @@ tAVRC_STS avrc_parse_notification_rsp(uint8_t* p_stream, uint16_t len,
       break;
 
     case AVRC_EVT_ADDR_PLAYER_CHANGE:
-      min_len += 4;
-      if (len < min_len) goto length_error;
-      BE_STREAM_TO_UINT16(p_rsp->param.addr_player.player_id, p_stream);
-      BE_STREAM_TO_UINT16(p_rsp->param.addr_player.uid_counter, p_stream);
-      break;
-
-    case AVRC_EVT_PLAY_POS_CHANGED:
-      min_len += 4;
-      if (len < min_len) goto length_error;
-      BE_STREAM_TO_UINT32(p_rsp->param.play_pos, p_stream);
       break;
 
     case AVRC_EVT_UIDS_CHANGE:
@@ -201,6 +191,7 @@ tAVRC_STS avrc_parse_notification_rsp(uint8_t* p_stream, uint16_t len,
 
     case AVRC_EVT_TRACK_REACHED_END:
     case AVRC_EVT_TRACK_REACHED_START:
+    case AVRC_EVT_PLAY_POS_CHANGED:
     case AVRC_EVT_BATTERY_STATUS_CHANGE:
     case AVRC_EVT_SYSTEM_STATUS_CHANGE:
     default:
@@ -451,7 +442,7 @@ static tAVRC_STS avrc_pars_browse_rsp(tAVRC_MSG_BROWSE* p_msg,
           set_br_pl_rsp->charset_id, set_br_pl_rsp->folder_depth);
 
       set_br_pl_rsp->p_folders = (tAVRC_NAME*)osi_malloc(
-          set_br_pl_rsp->folder_depth * sizeof(tAVRC_NAME));
+          set_br_pl_rsp->num_items * sizeof(tAVRC_NAME));
 
       /* Read each of the folder in the depth */
       for (uint32_t i = 0; i < set_br_pl_rsp->folder_depth; i++) {
@@ -806,7 +797,7 @@ static tAVRC_STS avrc_ctrl_pars_vendor_rsp(tAVRC_MSG_VENDOR* p_msg,
       if (len < min_len) goto length_error;
       BE_STREAM_TO_UINT32(p_result->get_play_status.song_len, p);
       BE_STREAM_TO_UINT32(p_result->get_play_status.song_pos, p);
-      BE_STREAM_TO_UINT8(p_result->get_play_status.play_status, p);
+      BE_STREAM_TO_UINT8(p_result->get_play_status.status, p);
       break;
 
     case AVRC_PDU_SET_ADDRESSED_PLAYER:
