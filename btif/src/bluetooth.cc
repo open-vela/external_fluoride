f2bd7de135ec3877c9197e921860f701afeef56a
liujinye <liujinye@xiaomi.com>
2020-03-12 02:20:28 +0800

    LeL2capTest initial commit am: 4761c3e8b7 am: ec787f5324
    Change-Id: Ie99c819ba63d2c3f268f7cba3a7d9f78e8d9eda4

--- btif/src/bluetooth.cc
+++ btif/src/bluetooth.cc
@@ -63,7 +63,6 @@
 #include "btsnoop.h"
 #include "btsnoop_mem.h"
 #include "common/address_obfuscator.h"
-#include "common/metric_id_allocator.h"
 #include "common/metrics.h"
 #include "device/include/interop.h"
 #include "main/shim/dumpsys.h"
@@ -461,11 +460,6 @@ static std::string obfuscate_address(const RawAddress& address) {
       address);
 }
 
-static int get_metric_id(const RawAddress& address) {
-  return bluetooth::common::MetricIdAllocator::GetInstance().AllocateId(
-      address);
-}
-
 EXPORT_SYMBOL bt_interface_t bluetoothInterface = {
     sizeof(bluetoothInterface),
     init,
@@ -502,5 +496,4 @@ EXPORT_SYMBOL bt_interface_t bluetoothInterface = {
     interop_database_add,
     get_avrcp_service,
     obfuscate_address,
-    get_metric_id,
 };
