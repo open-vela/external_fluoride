77070062f1d4cb65eb4a18346561b7c3c6e6f027
openvela-robot <openvela-robot@xiaomi.com>
2020-03-11 20:47:51 +0800

    Merge "Clear LE cache before starting le scan or the report is skipped" am: daedecb474 am: 0bc677a4bb am: 8bb821c9bb
    Change-Id: Id024609f37f41d5c95033371b8e54fe7e69900d6

--- gd/packet/parser/struct_def.cc
+++ gd/packet/parser/struct_def.cc
@@ -45,6 +45,37 @@ void StructDef::GenSpecialize(std::ostream& s) const {
   s << "}";
 }
 
+void StructDef::GenToString(std::ostream& s) const {
+  s << "std::string ToString() {";
+  s << "std::stringstream ss;";
+  s << "ss << std::hex << std::showbase << \"" << name_ << " { \";";
+
+  if (fields_.size() > 0) {
+    s << "ss";
+    bool firstfield = true;
+    for (const auto& field : fields_) {
+      if (field->GetFieldType() == ReservedField::kFieldType ||
+          field->GetFieldType() == ChecksumStartField::kFieldType ||
+          field->GetFieldType() == FixedScalarField::kFieldType || field->GetFieldType() == CountField::kFieldType ||
+          field->GetFieldType() == SizeField::kFieldType)
+        continue;
+
+      s << (firstfield ? " << \"" : " << \", ") << field->GetName() << " = \" << ";
+
+      field->GenStringRepresentation(s, field->GetName() + "_");
+
+      if (firstfield) {
+        firstfield = false;
+      }
+    }
+    s << ";";
+  }
+
+  s << "ss << \" }\";";
+  s << "return ss.str();";
+  s << "}\n";
+}
+
 void StructDef::GenParse(std::ostream& s) const {
   std::string iterator = (is_little_endian_ ? "Iterator<kLittleEndian>" : "Iterator<!kLittleEndian>");
 
@@ -92,7 +123,7 @@ void StructDef::GenParse(std::ostream& s) const {
   Size total_bits{0};
   for (const auto& field : fields_) {
     if (field->GetFieldType() != ReservedField::kFieldType && field->GetFieldType() != BodyField::kFieldType &&
-        field->GetFieldType() != FixedScalarField::kFieldType && field->GetFieldType() != SizeField::kFieldType &&
+        field->GetFieldType() != FixedScalarField::kFieldType &&
         field->GetFieldType() != ChecksumStartField::kFieldType && field->GetFieldType() != ChecksumField::kFieldType &&
         field->GetFieldType() != CountField::kFieldType) {
       total_bits += field->GetSize().bits();
@@ -119,17 +150,10 @@ void StructDef::GenParse(std::ostream& s) const {
       s << "}";
     }
     if (field->GetFieldType() == CountField::kFieldType || field->GetFieldType() == SizeField::kFieldType) {
-      s << field->GetDataType() << " " << field->GetName() << "_extracted;";
       s << "{";
-      s << "if (to_bound.NumBytesRemaining() < " << field->GetSize().bytes() << ")";
-      if (!fields_.HasBody()) {
-        s << "{ return to_bound.Subrange(to_bound.NumBytesRemaining(),0);}";
-      } else {
-        s << "{ return {};}";
-      }
       int num_leading_bits =
           field->GenBounds(s, GetStructOffsetForField(field->GetName()), Size(), field->GetStructSize());
-      s << "auto " << field->GetName() << "_ptr = &" << field->GetName() << "_extracted;";
+      s << "auto " << field->GetName() << "_ptr = &to_fill->" << field->GetName() << "_extracted_;";
       field->GenExtractor(s, num_leading_bits, true);
       s << "}";
     }
@@ -182,7 +206,16 @@ void StructDef::GenDefinition(std::ostream& s) const {
   GenSpecialize(s);
   s << "\n";
 
+  GenToString(s);
+  s << "\n";
+
   GenMembers(s);
+  for (const auto& field : fields_) {
+    if (field->GetFieldType() == CountField::kFieldType || field->GetFieldType() == SizeField::kFieldType) {
+      s << "\n private:\n";
+      s << " mutable " << field->GetDataType() << " " << field->GetName() << "_extracted_{0};";
+    }
+  }
   s << "};\n";
 
   if (fields_.HasBody()) {
