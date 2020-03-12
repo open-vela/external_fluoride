330c540b79e851910a6a71424efd93cfcf04a3a5
liujinye <liujinye@xiaomi.com>
2020-03-12 18:12:11 +0800

    Merge "LeL2capTest: Add COC PTS test"

--- btif/src/bluetooth.cc
+++ btif/src/bluetooth.cc
@@ -63,7 +63,6 @@
 #include "btsnoop.h"
 #include "btsnoop_mem.h"
 #include "common/address_obfuscator.h"
-#include "common/metric_id_allocator.h"
 #include "common/metrics.h"
 #include "device/include/interop.h"
 #include "main/shim/dumpsys.h"
@@ -138,13 +137,13 @@ static bool is_profile(const char* p1, const char* p2) {
 
 static int init(bt_callbacks_t* callbacks, bool start_restricted,
                 bool is_single_user_mode) {
-  LOG_INFO(LOG_TAG, "%s: start restricted = %d ; single user = %d", __func__,
+  LOG_INFO("%s: start restricted = %d ; single user = %d", __func__,
            start_restricted, is_single_user_mode);
 
   if (bluetooth::shim::is_gd_shim_enabled()) {
-    LOG_INFO(LOG_TAG, "%s Enable Gd bluetooth functionality", __func__);
+    LOG_INFO("%s Enable Gd bluetooth functionality", __func__);
   } else {
-    LOG_INFO(LOG_TAG, "%s Preserving legacy bluetooth functionality", __func__);
+    LOG_INFO("%s Preserving legacy bluetooth functionality", __func__);
   }
 
   if (interface_ready()) return BT_STATUS_DONE;
@@ -344,7 +343,7 @@ static void dumpMetrics(std::string* output) {
 }
 
 static const void* get_profile_interface(const char* profile_id) {
-  LOG_INFO(LOG_TAG, "%s: id = %s", __func__, profile_id);
+  LOG_INFO("%s: id = %s", __func__, profile_id);
 
   /* sanity check */
   if (!interface_ready()) return NULL;
@@ -392,7 +391,7 @@ static const void* get_profile_interface(const char* profile_id) {
 }
 
 int dut_mode_configure(uint8_t enable) {
-  LOG_INFO(LOG_TAG, "%s", __func__);
+  LOG_INFO("%s", __func__);
 
   /* sanity check */
   if (!interface_ready()) return BT_STATUS_NOT_READY;
@@ -401,7 +400,7 @@ int dut_mode_configure(uint8_t enable) {
 }
 
 int dut_mode_send(uint16_t opcode, uint8_t* buf, uint8_t len) {
-  LOG_INFO(LOG_TAG, "%s", __func__);
+  LOG_INFO("%s", __func__);
 
   /* sanity check */
   if (!interface_ready()) return BT_STATUS_NOT_READY;
@@ -410,7 +409,7 @@ int dut_mode_send(uint16_t opcode, uint8_t* buf, uint8_t len) {
 }
 
 int le_test_mode(uint16_t opcode, uint8_t* buf, uint8_t len) {
-  LOG_INFO(LOG_TAG, "%s", __func__);
+  LOG_INFO("%s", __func__);
 
   /* sanity check */
   if (!interface_ready()) return BT_STATUS_NOT_READY;
@@ -448,7 +447,7 @@ static int set_os_callouts(bt_os_callouts_t* callouts) {
 }
 
 static int config_clear(void) {
-  LOG_INFO(LOG_TAG, "%s", __func__);
+  LOG_INFO("%s", __func__);
   return btif_config_clear() ? BT_STATUS_SUCCESS : BT_STATUS_FAIL;
 }
 
@@ -458,11 +457,6 @@ static bluetooth::avrcp::ServiceInterface* get_avrcp_service(void) {
 
 static std::string obfuscate_address(const RawAddress& address) {
   return bluetooth::common::AddressObfuscator::GetInstance()->Obfuscate(
-      address);
-}
-
-static int get_metric_id(const RawAddress& address) {
-  return bluetooth::common::MetricIdAllocator::GetInstance().AllocateId(
       address);
 }
 
@@ -502,5 +496,4 @@ EXPORT_SYMBOL bt_interface_t bluetoothInterface = {
     interop_database_add,
     get_avrcp_service,
     obfuscate_address,
-    get_metric_id,
 };
