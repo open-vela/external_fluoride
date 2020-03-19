468dbe52d5c8baa4803fec7dd83190f4b6769831
liujinye <liujinye@xiaomi.com>
2020-03-20 03:55:20 +0800

    Cert: Add name to gd_device am: 88e12958e6
    Change-Id: Ia77ec3d6db0004137ae3ee6f51d584fd202b04ea

--- gd/security/pairing/classic_pairing_handler_unittest.cc
+++ gd/security/pairing/classic_pairing_handler_unittest.cc
@@ -182,8 +182,8 @@ class ClassicPairingHandlerTest : public ::testing::Test {
 //  <- IoCapabilityResponse
 //  <- UserConfirmationRequest
 //  -> UserConfirmationRequestReply (auto)
-//  <- LinkKeyNotification
 //  <- SimplePairingComplete
+//  <- LinkKeyNotification
 //  <- AuthenticationComplete
 //  -> SetConnectionEncryption
 //  <- EncryptionChange
