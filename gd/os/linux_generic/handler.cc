3e4ad4b95a9330c770726f5f79d919e536bdd4b7
liujinye <liujinye@xiaomi.com>
2020-03-20 03:43:16 +0800

    Merge "avrc: Validating msg size before accessing fields" into qt-qpr1-dev am: 49437a62bd
    Change-Id: I6ff012808218c6026584d4dc9217b99d8c6a3169

--- gd/os/linux_generic/handler.cc
+++ gd/os/linux_generic/handler.cc
@@ -55,7 +55,6 @@ void Handler::Post(OnceClosure closure) {
   {
     std::lock_guard<std::mutex> lock(mutex_);
     if (was_cleared()) {
-      LOG_WARN("Posting to a handler which has been cleared");
       return;
     }
     tasks_->emplace(std::move(closure));
