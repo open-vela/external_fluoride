e2e9e43703504d485b47790210aacc08f53a0a16
liujinye <liujinye@xiaomi.com>
2020-03-26 19:43:52 +0800

    [automerger skipped] Merge "DO NOT MERGE  Ensure hci command status event has sufficient packet length" into qt-qpr1-dev am: 466138189c -s ours
    am skip reason: subject contains skip directive
    
    Change-Id: I3e56d0584e861ec214834a7568cf48c8b2d65f19

--- btif/src/btif_config.cc
+++ btif/src/btif_config.cc
@@ -147,7 +147,8 @@ bool btif_get_device_type(const RawAddress& bda, int* p_device_type) {
 
   if (!btif_config_get_int(bd_addr_str, "DevType", p_device_type)) return false;
 
-  LOG_DEBUG("%s: Device [%s] type %d", __func__, bd_addr_str, *p_device_type);
+  LOG_DEBUG(LOG_TAG, "%s: Device [%s] type %d", __func__, bd_addr_str,
+            *p_device_type);
   return true;
 }
 
@@ -159,7 +160,7 @@ bool btif_get_address_type(const RawAddress& bda, int* p_addr_type) {
 
   if (!btif_config_get_int(bd_addr_str, "AddrType", p_addr_type)) return false;
 
-  LOG_DEBUG("%s: Device [%s] address type %d", __func__, bd_addr_str,
+  LOG_DEBUG(LOG_TAG, "%s: Device [%s] address type %d", __func__, bd_addr_str,
             *p_addr_type);
   return true;
 }
@@ -276,15 +277,16 @@ static future_t* init(void) {
   config = btif_config_open(CONFIG_FILE_PATH, CONFIG_FILE_CHECKSUM_PATH);
   btif_config_source = ORIGINAL;
   if (!config) {
-    LOG_WARN("%s unable to load config file: %s; using backup.", __func__,
-             CONFIG_FILE_PATH);
+    LOG_WARN(LOG_TAG, "%s unable to load config file: %s; using backup.",
+             __func__, CONFIG_FILE_PATH);
     remove(CONFIG_FILE_CHECKSUM_PATH);
     config = btif_config_open(CONFIG_BACKUP_PATH, CONFIG_BACKUP_CHECKSUM_PATH);
     btif_config_source = BACKUP;
     file_source = "Backup";
   }
   if (!config) {
-    LOG_WARN("%s unable to load backup; attempting to transcode legacy file.",
+    LOG_WARN(LOG_TAG,
+             "%s unable to load backup; attempting to transcode legacy file.",
              __func__);
     remove(CONFIG_BACKUP_CHECKSUM_PATH);
     config = btif_config_transcode(CONFIG_LEGACY_FILE_PATH);
@@ -292,7 +294,8 @@ static future_t* init(void) {
     file_source = "Legacy";
   }
   if (!config) {
-    LOG_ERROR("%s unable to transcode legacy file; creating empty config.",
+    LOG_ERROR(LOG_TAG,
+              "%s unable to transcode legacy file; creating empty config.",
               __func__);
     config = storage_config_get_interface()->config_new_empty();
     btif_config_source = NEW_FILE;
@@ -334,7 +337,7 @@ static future_t* init(void) {
   // write back to disk.
   config_timer = alarm_new("btif.config");
   if (!config_timer) {
-    LOG_ERROR("%s unable to create alarm.", __func__);
+    LOG_ERROR(LOG_TAG, "%s unable to create alarm.", __func__);
     goto error;
   }
 
@@ -374,7 +377,7 @@ static std::unique_ptr<config_t> btif_config_open(const char* filename, const ch
   if (!config) return nullptr;
 
   if (!config_has_section(*config, "Adapter")) {
-    LOG_ERROR("Config is missing adapter section");
+    LOG_ERROR(LOG_TAG, "Config is missing adapter section");
     return nullptr;
   }
 
