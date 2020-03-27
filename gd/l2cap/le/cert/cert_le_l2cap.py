d7b0f1cac6151898726369a94fe63d32465fad8b
openvela-robot <openvela-robot@xiaomi.com>
2020-03-27 17:21:14 +0800

    LeL2capTest: Add disconnect request and credit am: 3ce698287f
    Change-Id: I9c2b87f35999d8edfce54e6bdb67957a466c395f

--- gd/l2cap/le/cert/cert_le_l2cap.py
+++ gd/l2cap/le/cert/cert_le_l2cap.py
@@ -131,12 +131,6 @@ class CertLeL2cap(Closable):
             control_channel=None)
         self._get_acl_stream().register_callback(self._handle_control_packet)
 
-    def open_fixed_channel(self, cid=4):
-        channel = CertLeL2capChannel(self._device, cid, cid,
-                                     self._get_acl_stream(), self._le_acl, None,
-                                     0)
-        return channel
-
     def open_channel(self,
                      signal_id,
                      psm,
@@ -148,7 +142,7 @@ class CertLeL2cap(Closable):
             l2cap_packets.LeCreditBasedConnectionRequestBuilder(
                 signal_id, psm, scid, mtu, mps, initial_credit))
 
-        response = L2capCaptures.CreditBasedConnectionResponse()
+        response = L2capCaptures.CreditBasedConnectionResponse(scid)
         assertThat(self.control_channel).emits(response)
         channel = CertLeL2capChannel(self._device, scid,
                                      response.get().GetDestinationCid(),
