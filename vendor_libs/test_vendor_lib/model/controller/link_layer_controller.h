7638d60d57e083e1e3816b0adcc89a0e6a38a739
liujinye <liujinye@xiaomi.com>
2020-03-31 18:38:49 +0800

    A2DP: Do the connection task in the stack main thread
    There is a profile queue to handle connection tasks sequentially, and
    invokes the connect API in JNI thread while the previous task finishes.
    However, since BtifAvStateMachine is running in the stack main thread,
    it brings a race condition about the BtifAVPeer object between the JNI
    and stack main threads. This change does A2DP connection task in the
    main thread, so has no such race conditions.
    
    Bug: 152595833
    Test: make sure the opening event is in main thread manually
    Change-Id: Ib40dc7c5fa3e839a894d6f7947ac8266eacd2022
    Merged-In: Ib40dc7c5fa3e839a894d6f7947ac8266eacd2022
    (cherry picked from commit 367c497abbc0ff5551a1b3210db35ad6540e2dfe)

--- vendor_libs/test_vendor_lib/model/controller/link_layer_controller.h
+++ vendor_libs/test_vendor_lib/model/controller/link_layer_controller.h
@@ -124,15 +124,12 @@ class LinkLayerController {
 
   void RegisterTaskCancel(std::function<void(AsyncTaskId)> cancel);
   void Reset();
+  void AddControllerEvent(std::chrono::milliseconds delay, const TaskCallback& task);
 
+  void PageScan();
   void Connections();
 
   void LeAdvertising();
-
-  void LeConnectionUpdateComplete(
-      bluetooth::hci::LeConnectionUpdateView connection_update_view);
-  ErrorCode LeConnectionUpdate(
-      bluetooth::hci::LeConnectionUpdateView connection_update_view);
 
   void HandleLeConnection(AddressWithType addr, AddressWithType own_addr,
                           uint8_t role, uint16_t connection_interval,
@@ -365,7 +362,7 @@ class LinkLayerController {
   SecurityManager security_manager_{10};
   std::chrono::steady_clock::time_point last_inquiry_;
   model::packets::InquiryType inquiry_mode_;
-  AsyncTaskId inquiry_timer_task_id_ = kInvalidTaskId;
+  Inquiry::InquiryState inquiry_state_;
   uint64_t inquiry_lap_;
   uint8_t inquiry_max_responses_;
 
