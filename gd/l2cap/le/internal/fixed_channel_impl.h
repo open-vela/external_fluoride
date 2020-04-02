ea2ebb6b50eb6d351d4af0f86a71867c4d061d19
openvela-robot <openvela-robot@xiaomi.com>
2020-04-02 17:02:36 +0800

    Add rahulsabnis@ as an owner of bluetooth code to allow for owner code reviews
    Bug: 153115116
    Test: Manual
    Merged-In: I8153358384bbf18102f84f544551d358d95b45e7
    Change-Id: I8153358384bbf18102f84f544551d358d95b45e7

--- gd/l2cap/le/internal/fixed_channel_impl.h
+++ gd/l2cap/le/internal/fixed_channel_impl.h
@@ -20,7 +20,6 @@
 #include "l2cap/cid.h"
 #include "l2cap/internal/channel_impl.h"
 #include "l2cap/le/fixed_channel.h"
-#include "l2cap/le/link_options.h"
 #include "os/handler.h"
 #include "os/log.h"
 
@@ -73,8 +72,6 @@ class FixedChannelImpl : public l2cap::internal::ChannelImpl {
   common::BidiQueueEnd<packet::PacketView<packet::kLittleEndian>, packet::BasePacketBuilder>* GetQueueDownEnd() {
     return channel_queue_.GetDownEnd();
   }
-
-  LinkOptions* GetLinkOptions();
 
  private:
   // Constructor states
