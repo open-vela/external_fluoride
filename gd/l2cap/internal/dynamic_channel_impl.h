26ff80c287ebedb7731b55278e3956b26b3513ac
openvela-robot <openvela-robot@xiaomi.com>
2020-04-06 14:49:41 +0800

    Merge "A2DP: Do the connection task in the stack main thread" into rvc-dev

--- gd/l2cap/internal/dynamic_channel_impl.h
+++ gd/l2cap/internal/dynamic_channel_impl.h
@@ -38,7 +38,7 @@ class DynamicChannelImpl : public l2cap::internal::ChannelImpl {
 
   virtual ~DynamicChannelImpl() = default;
 
-  hci::AddressWithType GetDevice() const;
+  hci::Address GetDevice() const;
 
   virtual void RegisterOnCloseCallback(os::Handler* user_handler, DynamicChannel::OnCloseCallback on_close_callback);
 
