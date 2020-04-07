6be4273a895832aa2af5e11bd86b943557e99ec2
openvela-robot <openvela-robot@xiaomi.com>
2020-04-07 19:40:52 +0800

    Fix potential stack overflow caused by integer overflow am: 4c0c93eb61 am: b0a2c73279 am: 987d2aafd0
    Change-Id: Ic7455dcd7a083494f4c263859b427967ecf12db9

--- gd/security/pairing_handler_le.cc
+++ gd/security/pairing_handler_le.cc
@@ -161,14 +161,6 @@ void PairingHandlerLe::PairingMain(InitialInformations i) {
     return;
   }
 
-  // If it's secure connections pairing, do cross-transport key derivation
-  DistributedKeys distributed_keys = std::get<DistributedKeys>(keyExchangeStatus);
-  if ((pairing_response.GetAuthReq() & AuthReqMaskSc) && distributed_keys.ltk.has_value()) {
-    bool use_h7 = (pairing_response.GetAuthReq() & AuthReqMaskCt2);
-    Octet16 link_key = crypto_toolbox::ltk_to_link_key(*(distributed_keys.ltk), use_h7);
-    distributed_keys.link_key = link_key;
-  }
-
   // bool bonding = pairing_request.GetAuthReq() & pairing_response.GetAuthReq() & AuthReqMaskBondingFlag;
 
   i.OnPairingFinished(PairingResult{
@@ -331,7 +323,8 @@ DistributedKeysOrFailure PairingHandlerLe::ReceiveKeys(const uint8_t& keys_i_rec
   std::optional<Octet16> ltk;                 /* Legacy only */
   std::optional<uint16_t> ediv;               /* Legacy only */
   std::optional<std::array<uint8_t, 8>> rand; /* Legacy only */
-  std::optional<hci::AddressWithType> identity_address;
+  std::optional<Address> identity_address;
+  AddrType identity_address_type;
   std::optional<Octet16> irk;
   std::optional<Octet16> signature_key;
 
@@ -374,10 +367,8 @@ DistributedKeysOrFailure PairingHandlerLe::ReceiveKeys(const uint8_t& keys_i_rec
       return std::get<PairingFailure>(iapacket);
     }
     LOG_INFO("Received Identity Address Information");
-    auto iapacketview = std::get<IdentityAddressInformationView>(iapacket);
-    identity_address = hci::AddressWithType(iapacketview.GetBdAddr(), iapacketview.GetAddrType() == AddrType::PUBLIC
-                                                                          ? hci::AddressType::PUBLIC_DEVICE_ADDRESS
-                                                                          : hci::AddressType::RANDOM_DEVICE_ADDRESS);
+    identity_address = std::get<IdentityAddressInformationView>(iapacket).GetBdAddr();
+    identity_address_type = std::get<IdentityAddressInformationView>(iapacket).GetAddrType();
   }
 
   if (keys_i_receive & KeyMaskSign) {
@@ -391,12 +382,7 @@ DistributedKeysOrFailure PairingHandlerLe::ReceiveKeys(const uint8_t& keys_i_rec
     signature_key = std::get<SigningInformationView>(packet).GetSignatureKey();
   }
 
-  return DistributedKeys{.ltk = ltk,
-                         .ediv = ediv,
-                         .rand = rand,
-                         .identity_address = identity_address,
-                         .irk = irk,
-                         .signature_key = signature_key};
+  return DistributedKeys{ltk, ediv, rand, identity_address, identity_address_type, irk, signature_key};
 }
 
 void PairingHandlerLe::SendKeys(const InitialInformations& i, const uint8_t& keys_i_send, Octet16 ltk, uint16_t ediv,
--- vendor_libs/test_vendor_lib/model/controller/security_manager.h
+++ vendor_libs/test_vendor_lib/model/controller/security_manager.h
@@ -90,20 +90,18 @@ class SecurityManager {
   std::unordered_map<std::string, std::array<uint8_t, 16>> key_store_;
 
   bool peer_capabilities_valid_{false};
-  IoCapabilityType peer_io_capability_{IoCapabilityType::DISPLAY_ONLY};
+  IoCapabilityType peer_io_capability_;
   bool peer_oob_present_flag_{false};
-  AuthenticationType peer_authentication_requirements_{
-      AuthenticationType::NO_BONDING};
+  AuthenticationType peer_authentication_requirements_;
 
   bool host_capabilities_valid_{false};
-  IoCapabilityType host_io_capability_{IoCapabilityType::DISPLAY_ONLY};
+  IoCapabilityType host_io_capability_;
   bool host_oob_present_flag_{false};
-  AuthenticationType host_authentication_requirements_{
-      AuthenticationType::NO_BONDING};
+  AuthenticationType host_authentication_requirements_;
 
   bool authenticating_{false};
-  uint16_t current_handle_{};
-  Address peer_address_{};
+  uint16_t current_handle_;
+  Address peer_address_;
 };
 
 }  // namespace test_vendor_lib
