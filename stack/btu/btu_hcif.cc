22f99a85942a5c814162a38d076d15aa8dff4b8f
liujinye <liujinye@xiaomi.com>
2020-04-07 22:31:00 +0800

    Fix potential stack overflow caused by integer overflow am: ec0d507ed2
    Change-Id: Ia8ae7d5d144e3a5e94728dc236d9e7af6ddd3dd2

--- stack/btu/btu_hcif.cc
+++ stack/btu/btu_hcif.cc
@@ -43,7 +43,6 @@
 #include "bt_common.h"
 #include "bt_types.h"
 #include "bt_utils.h"
-#include "btif_config.h"
 #include "btm_api.h"
 #include "btm_int.h"
 #include "btu.h"
@@ -59,7 +58,6 @@
 using base::Location;
 
 extern void btm_process_cancel_complete(uint8_t status, uint8_t mode);
-extern void btm_process_inq_results2(uint8_t* p, uint8_t inq_res_mode);
 extern void btm_ble_test_command_complete(uint8_t* p);
 extern void smp_cancel_start_encryption_attempt();
 
@@ -193,44 +191,6 @@ void btu_hcif_log_event_metrics(uint8_t evt_code, uint8_t* p_event) {
       bluetooth::common::LogLinkLayerConnectionEvent(
           &bda, handle, android::bluetooth::DIRECTION_UNKNOWN, link_type, cmd,
           evt_code, android::bluetooth::hci::BLE_EVT_UNKNOWN, status, reason);
-
-      // Read SDP_DI manufacturer, model, HW version from config,
-      // and log them
-      int sdp_di_manufacturer_id = 0;
-      int sdp_di_model_id = 0;
-      int sdp_di_hw_version = 0;
-      int sdp_di_vendor_id_source = 0;
-      std::string bda_string = bda.ToString();
-      btif_config_get_int(bda_string, BT_CONFIG_KEY_SDP_DI_MANUFACTURER,
-                          &sdp_di_manufacturer_id);
-      btif_config_get_int(bda_string, BT_CONFIG_KEY_SDP_DI_MODEL,
-                          &sdp_di_model_id);
-      btif_config_get_int(bda_string, BT_CONFIG_KEY_SDP_DI_HW_VERSION,
-                          &sdp_di_hw_version);
-      btif_config_get_int(bda_string, BT_CONFIG_KEY_SDP_DI_VENDOR_ID_SRC,
-                          &sdp_di_vendor_id_source);
-
-      std::stringstream ss;
-      // [N - native]::SDP::[DIP - Device ID Profile]
-      ss << "N:SDP::DIP::" << loghex(sdp_di_vendor_id_source);
-      bluetooth::common::LogManufacturerInfo(
-          bda, android::bluetooth::DeviceInfoSrcEnum::DEVICE_INFO_INTERNAL,
-          ss.str(), loghex(sdp_di_manufacturer_id), loghex(sdp_di_model_id),
-          loghex(sdp_di_hw_version), "");
-
-      // Read LMP version, subversion and  manufacturer from config,
-      // and log them
-      int lmp_version = -1;
-      int lmp_subversion = -1;
-      int lmp_manufacturer_id = -1;
-      btif_config_get_int(bda_string, BT_CONFIG_KEY_REMOTE_VER_VER,
-                          &lmp_version);
-      btif_config_get_int(bda_string, BT_CONFIG_KEY_REMOTE_VER_SUBVER,
-                          &lmp_subversion);
-      btif_config_get_int(bda_string, BT_CONFIG_KEY_REMOTE_VER_MFCT,
-                          &lmp_manufacturer_id);
-      bluetooth::common::LogRemoteVersionInfo(
-          handle, status, lmp_version, lmp_manufacturer_id, lmp_subversion);
       break;
     }
     case HCI_CONNECTION_REQUEST_EVT: {
@@ -354,16 +314,16 @@ void btu_hcif_process_event(UNUSED_ATTR uint8_t controller_id, BT_HDR* p_msg) {
       btu_hcif_qos_setup_comp_evt(p);
       break;
     case HCI_COMMAND_COMPLETE_EVT:
-      LOG_ERROR(
-          "%s should not have received a command complete event. "
-          "Someone didn't go through the hci transmit_command function.",
-          __func__);
+      LOG_ERROR(LOG_TAG,
+                "%s should not have received a command complete event. "
+                "Someone didn't go through the hci transmit_command function.",
+                __func__);
       break;
     case HCI_COMMAND_STATUS_EVT:
-      LOG_ERROR(
-          "%s should not have received a command status event. "
-          "Someone didn't go through the hci transmit_command function.",
-          __func__);
+      LOG_ERROR(LOG_TAG,
+                "%s should not have received a command status event. "
+                "Someone didn't go through the hci transmit_command function.",
+                __func__);
       break;
     case HCI_HARDWARE_ERROR_EVT:
       btu_hcif_hardware_error_evt(p);
@@ -1704,12 +1664,6 @@ static void btu_hcif_command_status_evt(uint8_t status, BT_HDR* command,
  ******************************************************************************/
 static void btu_hcif_hardware_error_evt(uint8_t* p) {
   HCI_TRACE_ERROR("Ctlr H/w error event - code:0x%x", *p);
-  if (hci_is_root_inflammation_event_received()) {
-    // Ignore the hardware error event here as we have already received
-    // root inflammation event earlier.
-    HCI_TRACE_ERROR("H/w error event after root inflammation event!");
-    return;
-  }
 
   /* If anyone wants device status notifications, give him one. */
   btm_report_device_status(BTM_DEV_STATUS_DOWN);
