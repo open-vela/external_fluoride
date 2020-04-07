57282bafeb7dbce2972b360b1a3b013259484e2e
liujinye <liujinye@xiaomi.com>
2020-04-08 00:24:48 +0800

    [automerger skipped] Fix potential stack overflow caused by integer overflow am: 1570b62c88 am: 5b4bf745e9 am: 6360e91bb3 am: 3cd74938d8 -s ours am: db0c360dce -s ours am: ca7a1eabe0 -s ours am: 67fa765f93 -s ours
    am skip reason: Change-Id I0655b0b62301f78cd8705cc7b0e4fc11522f00ca with SHA-1 ec0d507ed2 is in history
    
    Change-Id: I55b319557d78efdf8bf3da7b24cf5bcb8ca91523

--- gd/packet/iterator.h
+++ gd/packet/iterator.h
@@ -18,6 +18,7 @@
 
 #include <cstdint>
 #include <forward_list>
+#include <memory>
 
 #include "packet/view.h"
 
@@ -60,11 +61,13 @@ class Iterator : public std::iterator<std::random_access_iterator_tag, uint8_t> 
 
   size_t NumBytesRemaining() const;
 
+  Iterator Subrange(size_t index, size_t length) const;
+
   // Get the next sizeof(FixedWidthPODType) bytes and return the filled type
   template <typename FixedWidthPODType>
   FixedWidthPODType extract() {
-    static_assert(std::is_pod<FixedWidthPODType>::value, "Iterator::extract requires an fixed type.");
-    FixedWidthPODType extracted_value;
+    static_assert(std::is_pod<FixedWidthPODType>::value, "Iterator::extract requires a fixed-width type.");
+    FixedWidthPODType extracted_value{};
     uint8_t* value_ptr = (uint8_t*)&extracted_value;
 
     for (size_t i = 0; i < sizeof(FixedWidthPODType); i++) {
@@ -77,7 +80,8 @@ class Iterator : public std::iterator<std::random_access_iterator_tag, uint8_t> 
  private:
   std::forward_list<View> data_;
   size_t index_;
-  size_t length_;
+  size_t begin_;
+  size_t end_;
 };
 
 }  // namespace packet
