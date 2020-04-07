c9891ace0f329a39c9c2f1a3559b8f00cc5bd1f3
liujinye <liujinye@xiaomi.com>
2020-04-07 23:38:42 +0800

    [automerger skipped] Fix potential stack overflow caused by integer overflow am: ec0d507ed2 am: 8ccc0c8d5f -s ours am: 7a1190ef0c -s ours am: 35d64d233a -s ours am: 649f2314c1 -s ours am: 657cc6a8b9 -s ours
    am skip reason: Change-Id I0655b0b62301f78cd8705cc7b0e4fc11522f00ca with SHA-1 ec0d507ed2 is in history
    
    Change-Id: I57c567985ac67fe176bb49550c7ee3a5fa4b78e0

--- btif/src/bluetooth.cc
+++ btif/src/bluetooth.cc
@@ -63,8 +63,11 @@
 #include "btsnoop.h"
 #include "btsnoop_mem.h"
 #include "common/address_obfuscator.h"
+#include "common/metric_id_allocator.h"
 #include "common/metrics.h"
 #include "device/include/interop.h"
+#include "main/shim/dumpsys.h"
+#include "main/shim/shim.h"
 #include "osi/include/alarm.h"
 #include "osi/include/allocation_tracker.h"
 #include "osi/include/log.h"
@@ -135,8 +138,14 @@ static bool is_profile(const char* p1, const char* p2) {
 
 static int init(bt_callbacks_t* callbacks, bool start_restricted,
                 bool is_single_user_mode) {
-  LOG_INFO(LOG_TAG, "%s: start restricted = %d ; single user = %d", __func__,
+  LOG_INFO("%s: start restricted = %d ; single user = %d", __func__,
            start_restricted, is_single_user_mode);
+
+  if (bluetooth::shim::is_gd_shim_enabled()) {
+    LOG_INFO("%s Enable Gd bluetooth functionality", __func__);
+  } else {
+    LOG_INFO("%s Preserving legacy bluetooth functionality", __func__);
+  }
 
   if (interface_ready()) return BT_STATUS_DONE;
 
@@ -321,9 +330,13 @@ static void dump(int fd, const char** arguments) {
   HearingAid::DebugDump(fd);
   connection_manager::dump(fd);
   bluetooth::bqr::DebugDump(fd);
+  if (bluetooth::shim::is_gd_shim_enabled()) {
+    bluetooth::shim::Dump(fd);
+  } else {
 #if (BTSNOOP_MEM == TRUE)
-  btif_debug_btsnoop_dump(fd);
+    btif_debug_btsnoop_dump(fd);
 #endif
+  }
 }
 
 static void dumpMetrics(std::string* output) {
@@ -331,7 +344,7 @@ static void dumpMetrics(std::string* output) {
 }
 
 static const void* get_profile_interface(const char* profile_id) {
-  LOG_INFO(LOG_TAG, "%s: id = %s", __func__, profile_id);
+  LOG_INFO("%s: id = %s", __func__, profile_id);
 
   /* sanity check */
   if (!interface_ready()) return NULL;
@@ -379,7 +392,7 @@ static const void* get_profile_interface(const char* profile_id) {
 }
 
 int dut_mode_configure(uint8_t enable) {
-  LOG_INFO(LOG_TAG, "%s", __func__);
+  LOG_INFO("%s", __func__);
 
   /* sanity check */
   if (!interface_ready()) return BT_STATUS_NOT_READY;
@@ -388,7 +401,7 @@ int dut_mode_configure(uint8_t enable) {
 }
 
 int dut_mode_send(uint16_t opcode, uint8_t* buf, uint8_t len) {
-  LOG_INFO(LOG_TAG, "%s", __func__);
+  LOG_INFO("%s", __func__);
 
   /* sanity check */
   if (!interface_ready()) return BT_STATUS_NOT_READY;
@@ -397,7 +410,7 @@ int dut_mode_send(uint16_t opcode, uint8_t* buf, uint8_t len) {
 }
 
 int le_test_mode(uint16_t opcode, uint8_t* buf, uint8_t len) {
-  LOG_INFO(LOG_TAG, "%s", __func__);
+  LOG_INFO("%s", __func__);
 
   /* sanity check */
   if (!interface_ready()) return BT_STATUS_NOT_READY;
@@ -435,7 +448,7 @@ static int set_os_callouts(bt_os_callouts_t* callouts) {
 }
 
 static int config_clear(void) {
-  LOG_INFO(LOG_TAG, "%s", __func__);
+  LOG_INFO("%s", __func__);
   return btif_config_clear() ? BT_STATUS_SUCCESS : BT_STATUS_FAIL;
 }
 
@@ -445,6 +458,11 @@ static bluetooth::avrcp::ServiceInterface* get_avrcp_service(void) {
 
 static std::string obfuscate_address(const RawAddress& address) {
   return bluetooth::common::AddressObfuscator::GetInstance()->Obfuscate(
+      address);
+}
+
+static int get_metric_id(const RawAddress& address) {
+  return bluetooth::common::MetricIdAllocator::GetInstance().AllocateId(
       address);
 }
 
@@ -484,4 +502,5 @@ EXPORT_SYMBOL bt_interface_t bluetoothInterface = {
     interop_database_add,
     get_avrcp_service,
     obfuscate_address,
+    get_metric_id,
 };
