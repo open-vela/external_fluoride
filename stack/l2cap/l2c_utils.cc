4a6eccf75c4fe2bb4767305eb24b8a86e48516ed
openvela-robot <openvela-robot@xiaomi.com>
2020-04-07 23:38:36 +0800

    [automerger skipped] Fix potential stack overflow caused by integer overflow am: 1570b62c88 am: 5b4bf745e9 am: 6360e91bb3 am: 3cd74938d8 -s ours
    am skip reason: Change-Id I0655b0b62301f78cd8705cc7b0e4fc11522f00ca with SHA-1 ec0d507ed2 is in history
    
    Change-Id: Ife4a1d737088dbf51ac29f0a6411d9468b456593

--- stack/l2cap/l2c_utils.cc
+++ stack/l2cap/l2c_utils.cc
@@ -1902,13 +1902,13 @@ uint8_t l2cu_process_peer_cfg_req(tL2C_CCB* p_ccb, tL2CAP_CFG_INFO* p_cfg) {
     /* Make sure service type is not a reserved value; otherwise let upper
        layer decide if acceptable
     */
-    if (p_cfg->qos.service_type <= SVC_TYPE_GUARANTEED) {
+    if (p_cfg->qos.service_type <= GUARANTEED) {
       p_ccb->peer_cfg.qos = p_cfg->qos;
       p_ccb->peer_cfg.qos_present = true;
       p_ccb->peer_cfg_bits |= L2CAP_CH_CFG_MASK_QOS;
     } else /* Illegal service type value */
     {
-      p_cfg->qos.service_type = SVC_TYPE_BEST_EFFORT;
+      p_cfg->qos.service_type = BEST_EFFORT;
       qos_type_ok = false;
     }
   }
@@ -2016,6 +2016,9 @@ void l2cu_process_our_cfg_req(tL2C_CCB* p_ccb, tL2CAP_CFG_INFO* p_cfg) {
       /*                 timer value in config response shall be greater than
        * received processing time */
       p_cfg->fcr.mon_tout = p_cfg->fcr.rtrans_tout = 0;
+
+      if (p_cfg->fcr.mode == L2CAP_FCR_STREAM_MODE)
+        p_cfg->fcr.max_transmit = p_cfg->fcr.tx_win_sz = 0;
     }
 
     /* Set the threshold to send acks (may be updated in the cfg response) */
