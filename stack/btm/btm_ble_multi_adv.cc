e40fe6332ed4848f93b9bf2e95ec165e8f1bf63b
liujinye <liujinye@xiaomi.com>
2020-04-08 17:00:45 +0800

    Merge "btm: fixing oob write in multi-adv SetData." into qt-qpr1-dev am: aeeb427c94
    Change-Id: Ibb621c8984997088b6ec96cc61fc65733e73c673

--- stack/btm/btm_ble_multi_adv.cc
+++ stack/btm/btm_ble_multi_adv.cc
@@ -24,6 +24,7 @@
 #include "ble_advertiser.h"
 #include "ble_advertiser_hci_interface.h"
 #include "btm_int_types.h"
+#include "stack/btm/btm_ble_int.h"
 
 #include <string.h>
 #include <queue>
@@ -170,7 +171,7 @@ class BleAdvertisingManagerImpl
                    weak_factory_.GetWeakPtr()));
   }
 
-  ~BleAdvertisingManagerImpl() { adv_inst.clear(); }
+  ~BleAdvertisingManagerImpl() override { adv_inst.clear(); }
 
   void GetOwnAddress(uint8_t inst_id, GetAddressCallback cb) override {
     cb.Run(adv_inst[inst_id].own_address_type, adv_inst[inst_id].own_address);
@@ -257,7 +258,7 @@ class BleAdvertisingManagerImpl
               p_inst->own_address = bda;
 
               alarm_set_on_mloop(p_inst->adv_raddr_timer,
-                                 BTM_BLE_PRIVATE_ADDR_INT_MS,
+                                 btm_get_next_private_addrress_interval_ms(),
                                  btm_ble_adv_raddr_timer_timeout, p_inst);
               cb.Run(p_inst->inst_id, BTM_BLE_MULTI_ADV_SUCCESS);
             },
