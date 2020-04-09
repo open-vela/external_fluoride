f03322cf02bb61adda67d64c0871aaf2e9b54d7c
openvela-robot <openvela-robot@xiaomi.com>
2020-04-09 23:15:30 +0800

    Merge "Cert: Improve gd/cert/run performance" am: b93c3944a7 am: 25ef358edd am: b36f588b09
    Change-Id: I907fde6dc7a1c63374e3dc641df7c75ac3ee78a9

--- gd/os/linux_generic/reactor.cc
+++ gd/os/linux_generic/reactor.cc
@@ -105,7 +105,6 @@ void Reactor::Run() {
       }
       auto* reactable = static_cast<Reactor::Reactable*>(event.data.ptr);
       std::unique_lock<std::mutex> lock(mutex_);
-      executing_reactable_finished_ = nullptr;
       // See if this reactable has been removed in the meantime.
       if (std::find(invalidation_list_.begin(), invalidation_list_.end(), reactable) != invalidation_list_.end()) {
         continue;
@@ -195,11 +194,11 @@ void Reactor::Unregister(Reactor::Reactable* reactable) {
 
 bool Reactor::WaitForUnregisteredReactable(std::chrono::milliseconds timeout) {
   std::lock_guard<std::mutex> lock(mutex_);
-  std::shared_ptr<std::future<void>> reactable_finished_future = executing_reactable_finished_;
-  if (reactable_finished_future == nullptr) {
+  if (executing_reactable_finished_ == nullptr) {
     return true;
   }
-  auto stop_status = reactable_finished_future->wait_for(timeout);
+  auto stop_status = executing_reactable_finished_->wait_for(timeout);
+  executing_reactable_finished_ = nullptr;
   if (stop_status != std::future_status::ready) {
     LOG_ERROR("Unregister reactable timed out");
   }
