c3b8246b71b27dd2783dbd62b1c8d05452b7c002
liujinye <liujinye@xiaomi.com>
2020-04-10 09:45:13 +0800

    Add SentinelWorkItem, to allow posting a work item and waiting for completion
    Stopgap until we have a better way to handle threads and fuzzing.
    Test: fuzz test I'm working on
    
    Change-Id: Ie1b3c0775dfbf40045d0322bd98cadc845d411dc

--- gd/fuzzing/helpers.h
+++ gd/fuzzing/helpers.h
@@ -14,15 +14,25 @@
  * limitations under the License.
  */
 
-#include "service/common/bluetooth/a2dp_codec_config.h"
-#include "stack/include/a2dp_vendor_ldac.h"
+#pragma once
 
-bluetooth::A2dpCodecConfig* bta_av_get_a2dp_current_codec(void) {
-  return nullptr;
-}
+#include <cstdint>
+#include <vector>
+#include "os/handler.h"
 
-int A2DP_VendorGetTrackSampleRateLdac(const uint8_t* p_codec_info) { return 0; }
-int A2DP_VendorGetTrackBitsPerSampleLdac(const uint8_t* p_codec_info) {
-  return 0;
+namespace bluetooth {
+namespace fuzzing {
+
+std::vector<std::vector<uint8_t>> SplitInput(const uint8_t* data, size_t size, const uint8_t* separator,
+                                             size_t separatorSize);
+
+class SentinelWorkItem {
+ public:
+  void WaitUntilFinishedOn(os::Handler* handler);
+
+ private:
+  void notify_handler_quiesced();
+  std::unique_ptr<std::promise<void>> quiesce_promise_;
+};
 }
-int A2DP_VendorGetChannelModeCodeLdac(const uint8_t* p_codec_info) { return 0; }
+}  // namespace bluetooth
