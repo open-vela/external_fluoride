6e7ff80df2183954a209c9f8d99bd3bd49b52788
openvela-robot <openvela-robot@xiaomi.com>
2020-04-13 02:48:23 +0800

    Cert: Generate backing process code coverage report after test run
    * Generate line-by-line coverage report after test run using
      llvm-profdata and llvm-cov in JSON format
    * Iteratively merge coverage report to the same file for the backing
      processes of both cert stack and stack under test even when they
      are using the same binary
    * Since we cannot run a method after all tests, we have to iteratively
      merge coverage report into a common file after each test run
    * llvm-cov and llvm-profdata tool is packaged together with the test
      ZIP as they have to be the same version when indexing .profraw data
    
    Outputs:
    
    * Two indexed data files per test run:
      - GdDevice_cert_stock_backing_process_coverage.profdata
      - GdDevice_stack_under_test_backing_process_coverage.profdata
    * Two reports per test run:
      - GdDevice_cert_stock_backing_process_coverage.json
      - GdDevice_stack_under_test_backing_process_coverage.json
    * Two summaries per test run:
      - GdDevice_cert_stock_backing_process_coverage_summary.txt
      - GdDevice_stack_under_test_backing_process_coverage_summary.txt
    * Two raw data files per test class
      - GdDevice_cert_stock_backing_process_coverage.profraw
      - GdDevice_stack_under_test_backing_process_coverage.profraw
    
    Performance Analysis:
    
    * File Sizes:
      - profraw: around 6MB, 2 generated per test class
      - profdata: around 400KB, 2 generated per test class
      - coverage report: around 40MB, 2 generated per test run
    * Each coverage report takes about 2 seconds and hence 4 seconds
      per test class. 11 test classes would result in 44 seconds for
      the whole test suite
    * Hence, additional 80MB storage and about 50 seconds run time per
      test run
    
    Bug: 153384476
    Test: gd/cert/run --host
    Change-Id: If488f1d1816fb1f9379729ace9d2b0e1a0343233

--- stack/btm/btm_ble_multi_adv.cc
+++ stack/btm/btm_ble_multi_adv.cc
@@ -430,7 +430,8 @@ class BleAdvertisingManagerImpl
             c->self->adv_inst[c->inst_id].tx_power = tx_power;
 
             if (c->self->adv_inst[c->inst_id].own_address_type == BLE_ADDR_PUBLIC) {
-              c->self->StartAdvertisingSetAfterAddressPart(std::move(c));
+              auto self = c->self;
+              self->StartAdvertisingSetAfterAddressPart(std::move(c));
               return;
             }
 
@@ -450,7 +451,8 @@ class BleAdvertisingManagerImpl
                   return;
                 }
 
-                c->self->StartAdvertisingSetAfterAddressPart(std::move(c));
+                auto self = c->self;
+                self->StartAdvertisingSetAfterAddressPart(std::move(c));
           }, base::Passed(&c)));
         }, base::Passed(&c)));
     }, base::Passed(&c)));
@@ -493,11 +495,11 @@ class BleAdvertisingManagerImpl
                           return;
                         }
 
+                        auto self = c->self;
                         if (c->periodic_params.enable) {
-                          c->self->StartAdvertisingSetPeriodicPart(
-                              std::move(c));
+                          self->StartAdvertisingSetPeriodicPart(std::move(c));
                         } else {
-                          c->self->StartAdvertisingSetFinish(std::move(c));
+                          self->StartAdvertisingSetFinish(std::move(c));
                         }
                       },
                       base::Passed(&c)));
@@ -551,7 +553,8 @@ class BleAdvertisingManagerImpl
                   return;
                 }
 
-                c->self->StartAdvertisingSetFinish(std::move(c));
+                auto self = c->self;
+                self->StartAdvertisingSetFinish(std::move(c));
 
               }, base::Passed(&c)));
         }, base::Passed(&c)));
@@ -729,13 +732,16 @@ class BleAdvertisingManagerImpl
       data.insert(data.begin(), flags.begin(), flags.end());
     }
 
-    // Find and fill TX Power with the correct value.
-    // The TX Power section is a 3 byte section.
-    for (size_t i = 0; (i + 2) < data.size();) {
-      if (data[i + 1] == HCI_EIR_TX_POWER_LEVEL_TYPE) {
-        data[i + 2] = adv_inst[inst_id].tx_power;
+    // Find and fill TX Power with the correct value
+    if (data.size()) {
+      size_t i = 0;
+      while (i < data.size()) {
+        uint8_t type = data[i + 1];
+        if (type == HCI_EIR_TX_POWER_LEVEL_TYPE) {
+          data[i + 2] = adv_inst[inst_id].tx_power;
+        }
+        i += data[i] + 1;
       }
-      i += data[i] + 1;
     }
 
     VLOG(1) << "data is: " << base::HexEncode(data.data(), data.size());
@@ -789,8 +795,9 @@ class BleAdvertisingManagerImpl
     int length = moreThanOnePacket ? ADV_DATA_LEN_MAX : dataSize - offset;
     int newOffset = offset + length;
 
+    auto dataData = data.data();
     sender.Run(
-        inst_id, operation, length, data.data() + offset,
+        inst_id, operation, length, dataData + offset,
         Bind(&BleAdvertisingManagerImpl::DivideAndSendDataRecursively, false,
              inst_id, std::move(data), newOffset, std::move(done_cb), sender));
   }
