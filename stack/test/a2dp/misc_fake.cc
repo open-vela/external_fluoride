622122ac950f46b7ac472751d0eed8814c96156b
liujinye <liujinye@xiaomi.com>
2020-04-13 17:18:24 +0800

    L2capTest stability improvement am: 018bf6b05c
    Change-Id: I33176698c5e68bfcc46f854b4edc9309bb2fa383

--- stack/test/a2dp/misc_fake.cc
+++ stack/test/a2dp/misc_fake.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 The Android Open Source Project
+ * Copyright 2020 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,17 +14,15 @@
  * limitations under the License.
  */
 
-syntax = "proto3";
+#include "service/common/bluetooth/a2dp_codec_config.h"
+#include "stack/include/a2dp_vendor_ldac.h"
 
-package bluetooth;
-
-import "google/protobuf/any.proto";
-
-message ModuleDumpState {
-  string name = 1;
-  google.protobuf.Any data = 2;
+bluetooth::A2dpCodecConfig* bta_av_get_a2dp_current_codec(void) {
+  return nullptr;
 }
 
-message Dumpmod {
-  map<string, ModuleDumpState> module_dump_states = 1;
+int A2DP_VendorGetTrackSampleRateLdac(const uint8_t* p_codec_info) { return 0; }
+int A2DP_VendorGetTrackBitsPerSampleLdac(const uint8_t* p_codec_info) {
+  return 0;
 }
+int A2DP_VendorGetChannelModeCodeLdac(const uint8_t* p_codec_info) { return 0; }
