33c35a81e684e94597f85d100503b73c5411ad2d
openvela-robot <openvela-robot@xiaomi.com>
2020-04-14 15:48:59 +0800

    A2DP: Be more careful in allocating a BtifAvPeer instance am: f92966049c am: 250f99a734 am: afbff640ab
    Change-Id: If8af7236593a8932a7bb3b0c7ad12cecfcf6b261

--- gd/packet/parser/fields/custom_field_fixed_size.cc
+++ gd/packet/parser/fields/custom_field_fixed_size.cc
@@ -62,8 +62,3 @@ void CustomFieldFixedSize::GenInserter(std::ostream& s) const {
 void CustomFieldFixedSize::GenValidator(std::ostream&) const {
   // Do nothing.
 }
-
-void CustomFieldFixedSize::GenStringRepresentation(std::ostream& s, std::string accessor) const {
-  // We assume that custom fields will have a ToString() method
-  s << accessor << ".ToString()";
-}
