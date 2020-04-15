71c7d040fd207e1d282da0db2c281025d4504bc8
openvela-robot <openvela-robot@xiaomi.com>
2020-04-15 22:45:45 +0800

    Merge "Add fuzzer concepts to inject builders into a layer" am: 8102f10ec0 am: a262971b13
    Change-Id: Ib8166bf80a98525919f1c402749a8b2feaec6b7d

--- gd/hal/fuzz/fuzz_hci_hal.cc
+++ gd/hal/fuzz/fuzz_hci_hal.cc
@@ -16,7 +16,6 @@
 
 #include "hal/fuzz/fuzz_hci_hal.h"
 #include "fuzz/helpers.h"
-#include "hci/fuzz/status_vs_complete_commands.h"
 
 namespace bluetooth {
 namespace hal {
@@ -38,7 +37,8 @@ void FuzzHciHal::sendHciCommand(HciPacket packet) {
   }
 
   waiting_opcode_ = command.GetOpCode();
-  waiting_for_status_ = hci::fuzz::uses_command_status(waiting_opcode_);
+  // TODO: expand list or find better way to associate opcodes needing status vs complete
+  waiting_for_status_ = waiting_opcode_ == hci::OpCode::RESET;
 }
 
 void FuzzHciHal::injectHciEvent(std::vector<uint8_t> data) {
