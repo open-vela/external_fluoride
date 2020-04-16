bdecb70e918c3dbbe84a6382b03187b0c2d44fc4
openvela-robot <openvela-robot@xiaomi.com>
2020-04-16 18:40:02 +0800

    Merge "Behavior: Add wait for timeout" am: d60ffedad9 am: 0ac688a2a7 am: a5c0caee96
    Change-Id: I23d1d0dd8daefe76af43a2f7c3bafd70b22b09c5

--- gd/hci/fuzz/hci_layer_fuzz_test.cc
+++ gd/hci/fuzz/hci_layer_fuzz_test.cc
@@ -16,9 +16,8 @@
 
 #include <stddef.h>
 #include <stdint.h>
-#include "fuzz/helpers.h"
 #include "hal/fuzz/fuzz_hci_hal.h"
-#include "hci/fuzz/hci_layer_fuzz_client.h"
+#include "hci/fuzz/dev_null_hci.h"
 #include "hci/hci_layer.h"
 #include "module.h"
 #include "os/fuzz/fake_timerfd.h"
@@ -26,22 +25,28 @@
 
 #include <fuzzer/FuzzedDataProvider.h>
 
-using bluetooth::FuzzTestModuleRegistry;
-using bluetooth::fuzz::GetArbitraryBytes;
+using bluetooth::TestModuleRegistry;
 using bluetooth::hal::HciHal;
 using bluetooth::hal::fuzz::FuzzHciHal;
-using bluetooth::hci::fuzz::HciLayerFuzzClient;
+using bluetooth::hci::fuzz::DevNullHci;
 using bluetooth::os::fuzz::fake_timerfd_advance;
 using bluetooth::os::fuzz::fake_timerfd_cap_at;
 using bluetooth::os::fuzz::fake_timerfd_reset;
+
+static std::vector<uint8_t> GetArbitraryBytes(FuzzedDataProvider* fdp) {
+  return fdp->ConsumeBytes<uint8_t>(fdp->ConsumeIntegral<size_t>());
+}
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   FuzzedDataProvider dataProvider(data, size);
   fake_timerfd_cap_at(1999);  // prevent command timeouts
 
-  static FuzzTestModuleRegistry moduleRegistry = FuzzTestModuleRegistry();
-  FuzzHciHal* fuzzHal = moduleRegistry.Inject<FuzzHciHal>(&HciHal::Factory);
-  HciLayerFuzzClient* fuzzClient = moduleRegistry.Start<HciLayerFuzzClient>();
+  static TestModuleRegistry moduleRegistry = TestModuleRegistry();
+  FuzzHciHal* fuzzHal = new FuzzHciHal();
+
+  moduleRegistry.InjectTestModule(&HciHal::Factory, fuzzHal);
+  moduleRegistry.Start<DevNullHci>(&moduleRegistry.GetTestThread());
+  DevNullHci* devNullHci = moduleRegistry.GetModuleUnderTest<DevNullHci>();
 
   while (dataProvider.remaining_bytes() > 0) {
     const uint8_t action = dataProvider.ConsumeIntegralInRange(0, 12);
@@ -59,33 +64,36 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
         fuzzHal->injectScoData(GetArbitraryBytes(&dataProvider));
         break;
       case 5:
-        fuzzClient->injectAclData(GetArbitraryBytes(&dataProvider));
+        devNullHci->injectAclData(GetArbitraryBytes(&dataProvider));
         break;
       case 6:
-        fuzzClient->injectHciCommand(GetArbitraryBytes(&dataProvider));
+        devNullHci->injectHciCommand(GetArbitraryBytes(&dataProvider));
         break;
       case 7:
-        // TODO: fuzzClient->injectSecurityCommand(GetArbitraryBytes(&dataProvider));
+        // TODO: devNullHci->injectSecurityCommand(GetArbitraryBytes(&dataProvider));
         break;
       case 8:
-        fuzzClient->injectLeSecurityCommand(GetArbitraryBytes(&dataProvider));
+        devNullHci->injectLeSecurityCommand(GetArbitraryBytes(&dataProvider));
         break;
       case 9:
-        fuzzClient->injectAclConnectionCommand(GetArbitraryBytes(&dataProvider));
+        devNullHci->injectAclConnectionCommand(GetArbitraryBytes(&dataProvider));
         break;
       case 10:
-        fuzzClient->injectLeAclConnectionCommand(GetArbitraryBytes(&dataProvider));
+        devNullHci->injectLeAclConnectionCommand(GetArbitraryBytes(&dataProvider));
         break;
       case 11:
-        fuzzClient->injectLeAdvertisingCommand(GetArbitraryBytes(&dataProvider));
+        devNullHci->injectLeAdvertisingCommand(GetArbitraryBytes(&dataProvider));
         break;
       case 12:
-        fuzzClient->injectLeScanningCommand(GetArbitraryBytes(&dataProvider));
+        devNullHci->injectLeScanningCommand(GetArbitraryBytes(&dataProvider));
         break;
     }
   }
 
-  moduleRegistry.WaitForIdleAndStopAll();
+  if (!moduleRegistry.GetTestThread().GetReactor()->WaitForIdle(std::chrono::milliseconds(100))) {
+    LOG_ERROR("idle timed out");
+  }
+  moduleRegistry.StopAll();
   fake_timerfd_reset();
   return 0;
 }
