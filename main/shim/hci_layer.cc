0461f27e0a943f89a74b6e01d49e105caa1d56f4
liujinye <liujinye@xiaomi.com>
2020-04-17 16:06:17 +0800

    GAP: Use proper page scan rep mode for connection
    Issue: Page time out seen when user initiated pairing from LE scan response
    
    Steps to reproduce
    ==================
    Steps:
    1. Turn on BT
    2. inquiry any dual mode device
    3. Initiate pairing (only LE scan response received but br/edr
       inquiry response not received)
    
    Actual Result:
    Remote name request or create connection got page timeout some times
    
    Expected result:
    Remote name request and create connection should be successful
    
    Root Cause:
    If user finds only LE scan response for dual mode  devices, then stack
    treats it as dual mode based on br/edr not supported bit. If pairing
    initiated from LE scan response, then stack initiates RNR and ACL connection
    on br/edr transport with wrong page scan response from LE inquiry database,
    it leads to page time out.
    
    Fix:
    Use page scan response from inquiry data base if br/edr inquiry response
    received, else use default page scan response R1 in RNR and create connection.
    
    Test: Follow steps above
    Bug: 154286616
    Change-Id: I281ebc6d0e67d88c30328cb67df7f28793f354c1

--- main/shim/hci_layer.cc
+++ main/shim/hci_layer.cc
@@ -180,13 +180,13 @@ static void transmit_command(BT_HDR* command,
   if (IsCommandStatusOpcode(op_code)) {
     bluetooth::shim::GetHciLayer()->EnqueueCommand(
         std::move(packet),
-        bluetooth::shim::GetGdShimHandler()->BindOnce(
-            OnTransmitPacketStatus, status_callback, context));
+        BindOnce(OnTransmitPacketStatus, status_callback, context),
+        bluetooth::shim::GetGdShimHandler());
   } else {
     bluetooth::shim::GetHciLayer()->EnqueueCommand(
         std::move(packet),
-        bluetooth::shim::GetGdShimHandler()->BindOnce(
-            OnTransmitPacketCommandComplete, complete_callback, context));
+        BindOnce(OnTransmitPacketCommandComplete, complete_callback, context),
+        bluetooth::shim::GetGdShimHandler());
   }
 }
 
