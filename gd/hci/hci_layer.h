f5cde1e653180e336477523c883615b885e31439
openvela-robot <openvela-robot@xiaomi.com>
2020-04-20 18:29:18 +0800

    Merge "Separate LE connection state from callbacks" am: d1c6ee49a5 am: 4963c887eb am: 55a6af5ac5 am: ff3e6ee7d0
    Change-Id: Ic2b922e598db40e8f713b64f87ef68f0aee8e7c2

--- gd/hci/hci_layer.h
+++ gd/hci/hci_layer.h
@@ -82,30 +82,22 @@ class HciLayer : public Module, public CommandInterface<CommandPacketBuilder> {
   virtual LeScanningInterface* GetLeScanningInterface(common::Callback<void(LeMetaEventView)> event_handler,
                                                       os::Handler* handler);
 
-  os::Handler* GetHciHandler() {
-    return GetHandler();
-  }
-
-  std::string ToString() const override {
-    return "Hci Layer";
-  }
-
-  static constexpr std::chrono::milliseconds kHciTimeoutMs = std::chrono::milliseconds(2000);
-
   static const ModuleFactory Factory;
 
- protected:
   void ListDependencies(ModuleList* list) override;
 
   void Start() override;
 
   void Stop() override;
 
+  os::Handler* GetHciHandler();
+
+  std::string ToString() const override;
+  static constexpr std::chrono::milliseconds kHciTimeoutMs = std::chrono::milliseconds(2000);
+
  private:
   struct impl;
-  struct hal_callbacks;
-  impl* impl_;
-  hal_callbacks* hal_callbacks_;
+  std::unique_ptr<impl> impl_;
 };
 }  // namespace hci
 }  // namespace bluetooth
