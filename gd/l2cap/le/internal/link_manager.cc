663ce1ffb54486cb7c82012473d7c1189a5c5e67
liujinye <liujinye@xiaomi.com>
2020-04-22 01:17:31 +0800

    Merge "Fix bugprone-posix-return warnings" am: af528ec5a6 am: 76cfacdb3a am: 8191798682 am: 84347cf7f1
    Change-Id: I59622fc803d07a7c9d9f1b8d19f393a02d259298

--- gd/l2cap/le/internal/link_manager.cc
+++ gd/l2cap/le/internal/link_manager.cc
@@ -101,17 +101,13 @@ Link* LinkManager::GetLink(hci::AddressWithType address_with_type) {
 }
 
 void LinkManager::OnLeConnectSuccess(hci::AddressWithType connecting_address_with_type,
-                                     std::unique_ptr<hci::AclConnection> acl_connection) {
+                                     std::unique_ptr<hci::LeAclConnection> acl_connection) {
   // Same link should not be connected twice
-  hci::AddressWithType connected_address_with_type(acl_connection->GetAddress(), acl_connection->GetAddressType());
+  hci::AddressWithType connected_address_with_type = acl_connection->GetRemoteAddress();
   ASSERT_LOG(GetLink(connected_address_with_type) == nullptr, "%s is connected twice without disconnection",
-             acl_connection->GetAddress().ToString().c_str());
-  // Register ACL disconnection callback in LinkManager so that we can clean up link resource properly
-  acl_connection->RegisterDisconnectCallback(
-      common::BindOnce(&LinkManager::OnDisconnect, common::Unretained(this), connected_address_with_type),
-      l2cap_handler_);
+             acl_connection->GetRemoteAddress().ToString().c_str());
   links_.try_emplace(connected_address_with_type, l2cap_handler_, std::move(acl_connection), parameter_provider_,
-                     dynamic_channel_service_manager_, fixed_channel_service_manager_);
+                     dynamic_channel_service_manager_, fixed_channel_service_manager_, this);
   auto* link = GetLink(connected_address_with_type);
   // Allocate and distribute channels for all registered fixed channel services
   auto fixed_channel_services = fixed_channel_service_manager_->GetRegisteredServices();
@@ -128,13 +124,7 @@ void LinkManager::OnLeConnectSuccess(hci::AddressWithType connecting_address_wit
   }
 
   // Remove device from pending links list, if any
-  auto pending_link = pending_links_.find(connecting_address_with_type);
-  if (pending_link == pending_links_.end()) {
-    // This an incoming connection, exit
-    return;
-  }
-  // This is an outgoing connection, remove entry in pending link list
-  pending_links_.erase(pending_link);
+  pending_links_.erase(connecting_address_with_type);
 }
 
 void LinkManager::OnLeConnectFail(hci::AddressWithType address_with_type, hci::ErrorCode reason) {
@@ -155,11 +145,10 @@ void LinkManager::OnLeConnectFail(hci::AddressWithType address_with_type, hci::E
   pending_links_.erase(pending_link);
 }
 
-void LinkManager::OnDisconnect(hci::AddressWithType address_with_type, hci::ErrorCode status) {
+void LinkManager::OnDisconnect(bluetooth::hci::AddressWithType address_with_type) {
   auto* link = GetLink(address_with_type);
-  ASSERT_LOG(link != nullptr, "Device %s is disconnected with reason 0x%x, but not in local database",
-             address_with_type.ToString().c_str(), static_cast<uint8_t>(status));
-  link->OnAclDisconnected(status);
+  ASSERT_LOG(link != nullptr, "Device %s is disconnected but not in local database",
+             address_with_type.ToString().c_str());
   links_.erase(address_with_type);
 }
 
