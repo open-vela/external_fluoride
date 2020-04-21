b4657da3556f759394c96b328c030592ee43d5c0
liujinye <liujinye@xiaomi.com>
2020-04-21 09:25:39 +0800

    Merge "Into the BG connection when the HA disconnects from remote side" am: bf1727f553 am: 5379b24033 am: 7e69c0b3eb am: 783e661a55
    Change-Id: I8ad364bf53616bc1c3b1cd7f9d8474fc852d0f76

--- bta/dm/bta_dm_act.cc
+++ bta/dm/bta_dm_act.cc
@@ -1754,9 +1754,7 @@ void bta_dm_search_cancel_notify(UNUSED_ATTR tBTA_DM_MSG* p_data) {
   if (bta_dm_search_cb.p_search_cback) {
     bta_dm_search_cb.p_search_cback(BTA_DM_SEARCH_CANCEL_CMPL_EVT, NULL);
   }
-  if (!bta_dm_search_cb.name_discover_done &&
-      (bta_dm_search_cb.state == BTA_DM_SEARCH_ACTIVE ||
-       bta_dm_search_cb.state == BTA_DM_SEARCH_CANCELLING)) {
+  if (!bta_dm_search_cb.name_discover_done) {
     BTM_CancelRemoteDeviceName();
   }
   if (bta_dm_search_cb.gatt_disc_active) {
