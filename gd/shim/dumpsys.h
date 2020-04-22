1642f7d56452b6d96a3e62bd730bce04d76a3acf
openvela-robot <openvela-robot@xiaomi.com>
2020-04-22 21:16:32 +0800

    Into the BG connection when the HA disconnects from remote side am: 645730da0a
    Change-Id: Ifc4096e20f78405129188783e4d1bc136415fc5a

--- gd/shim/dumpsys.h
+++ gd/shim/dumpsys.h
@@ -27,8 +27,7 @@ using DumpsysFunction = std::function<void(int fd)>;
 
 class Dumpsys : public bluetooth::Module {
  public:
-  void Dump(int fd, const char** args);
-
+  void Dump(int fd);
   void RegisterDumpsysFunction(const void* token, DumpsysFunction func);
   void UnregisterDumpsysFunction(const void* token);
 
