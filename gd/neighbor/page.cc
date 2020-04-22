1ba02c121d6c75131f16db443f0b00450dc5d80c
openvela-robot <openvela-robot@xiaomi.com>
2020-04-22 21:43:03 +0800

    Into the BG connection when the HA disconnects from remote side am: 645730da0a am: efaa7482dc
    Change-Id: I149ff2621c5021ba0323847fe8bbbd0aa3633c69

--- gd/neighbor/page.cc
+++ gd/neighbor/page.cc
@@ -112,13 +112,13 @@ void neighbor::PageModule::impl::Start() {
   handler_ = module_.GetHandler();
 
   hci_layer_->EnqueueCommand(hci::ReadPageScanActivityBuilder::Create(),
-                             handler_->BindOnceOn(this, &impl::OnCommandComplete));
+                             common::BindOnce(&impl::OnCommandComplete, common::Unretained(this)), handler_);
 
   hci_layer_->EnqueueCommand(hci::ReadPageScanTypeBuilder::Create(),
-                             handler_->BindOnceOn(this, &impl::OnCommandComplete));
+                             common::BindOnce(&impl::OnCommandComplete, common::Unretained(this)), handler_);
 
   hci_layer_->EnqueueCommand(hci::ReadPageTimeoutBuilder::Create(),
-                             handler_->BindOnceOn(this, &impl::OnCommandComplete));
+                             common::BindOnce(&impl::OnCommandComplete, common::Unretained(this)), handler_);
 }
 
 void neighbor::PageModule::impl::Stop() {
@@ -128,10 +128,10 @@ void neighbor::PageModule::impl::Stop() {
 
 void neighbor::PageModule::impl::SetScanActivity(ScanParameters params) {
   hci_layer_->EnqueueCommand(hci::WritePageScanActivityBuilder::Create(params.interval, params.window),
-                             handler_->BindOnceOn(this, &impl::OnCommandComplete));
+                             common::BindOnce(&impl::OnCommandComplete, common::Unretained(this)), handler_);
 
   hci_layer_->EnqueueCommand(hci::ReadPageScanActivityBuilder::Create(),
-                             handler_->BindOnceOn(this, &impl::OnCommandComplete));
+                             common::BindOnce(&impl::OnCommandComplete, common::Unretained(this)), handler_);
   LOG_DEBUG("Set page scan activity interval:0x%x/%.02fms window:0x%x/%.02fms", params.interval,
             ScanIntervalTimeMs(params.interval), params.window, ScanWindowTimeMs(params.window));
 }
@@ -142,19 +142,19 @@ ScanParameters neighbor::PageModule::impl::GetScanActivity() const {
 
 void neighbor::PageModule::impl::SetScanType(hci::PageScanType scan_type) {
   hci_layer_->EnqueueCommand(hci::WritePageScanTypeBuilder::Create(scan_type),
-                             handler_->BindOnceOn(this, &impl::OnCommandComplete));
+                             common::BindOnce(&impl::OnCommandComplete, common::Unretained(this)), handler_);
 
   hci_layer_->EnqueueCommand(hci::ReadPageScanTypeBuilder::Create(),
-                             handler_->BindOnceOn(this, &impl::OnCommandComplete));
+                             common::BindOnce(&impl::OnCommandComplete, common::Unretained(this)), handler_);
   LOG_DEBUG("Set page scan type:%s", hci::PageScanTypeText(scan_type).c_str());
 }
 
 void neighbor::PageModule::impl::SetTimeout(PageTimeout timeout) {
   hci_layer_->EnqueueCommand(hci::WritePageTimeoutBuilder::Create(timeout),
-                             handler_->BindOnceOn(this, &impl::OnCommandComplete));
+                             common::BindOnce(&impl::OnCommandComplete, common::Unretained(this)), handler_);
 
   hci_layer_->EnqueueCommand(hci::ReadPageTimeoutBuilder::Create(),
-                             handler_->BindOnceOn(this, &impl::OnCommandComplete));
+                             common::BindOnce(&impl::OnCommandComplete, common::Unretained(this)), handler_);
   LOG_DEBUG("Set page scan timeout:0x%x/%.02fms", timeout, PageTimeoutMs(timeout));
 }
 
