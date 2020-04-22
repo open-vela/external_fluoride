a64ed499fcaf215f15dc3d69750abe49eae67454
liujinye <liujinye@xiaomi.com>
2020-04-22 21:43:03 +0800

    Into the BG connection when the HA disconnects from remote side am: 645730da0a am: efaa7482dc
    Change-Id: I149ff2621c5021ba0323847fe8bbbd0aa3633c69

--- gd/l2cap/classic/internal/signalling_manager.h
+++ gd/l2cap/classic/internal/signalling_manager.h
@@ -66,7 +66,7 @@ class ClassicSignallingManager {
 
   void SendConnectionRequest(Psm psm, Cid local_cid);
 
-  void SendInitialConfigRequest(Cid local_cid);
+  void SendConfigurationRequest(Cid remote_cid, std::vector<std::unique_ptr<ConfigurationOption>> config);
 
   void SendDisconnectionRequest(Cid local_cid, Cid remote_cid);
 
@@ -105,11 +105,6 @@ class ClassicSignallingManager {
                                 ConnectionResponseStatus status);
   void on_command_timeout();
   void handle_send_next_command();
-
-  void negotiate_configuration(Cid cid, Continuation is_continuation,
-                               std::vector<std::unique_ptr<ConfigurationOption>>);
-
-  void send_configuration_request(Cid remote_cid, std::vector<std::unique_ptr<ConfigurationOption>> config);
 
   os::Handler* handler_;
   Link* link_;
