74c7911d52b3d0cfe879f40d3740dde1cc14b126
openvela-robot <openvela-robot@xiaomi.com>
2020-04-28 20:06:48 +0800

    Remove using statements from handler.h am: e68592e696 am: b08bb3c290 am: 7125946a17
    Change-Id: I14d8d8451ce37a3beb3b728ca4e85a0e48754772

--- gd/common/contextual_callback.h
+++ gd/common/contextual_callback.h
@@ -49,16 +49,6 @@ class ContextualOnceCallback<R(Args...)> {
     context_->Post(common::BindOnce(std::move(callback_), std::forward<Args>(args)...));
   }
 
-  void InvokeIfNotEmpty(Args... args) {
-    if (context_ != nullptr) {
-      context_->Post(common::BindOnce(std::move(callback_), std::forward<Args>(args)...));
-    }
-  }
-
-  bool IsEmpty() {
-    return context_ == nullptr;
-  }
-
  private:
   common::OnceCallback<R(Args...)> callback_;
   IPostableContext* context_;
@@ -83,16 +73,6 @@ class ContextualCallback<R(Args...)> {
 
   void Invoke(Args... args) {
     context_->Post(common::BindOnce(callback_, std::forward<Args>(args)...));
-  }
-
-  void InvokeIfNotEmpty(Args... args) {
-    if (context_ != nullptr) {
-      context_->Post(common::BindOnce(callback_, std::forward<Args>(args)...));
-    }
-  }
-
-  bool IsEmpty() {
-    return context_ == nullptr;
   }
 
  private:
