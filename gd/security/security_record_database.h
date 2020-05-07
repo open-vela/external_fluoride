edc108b3c28129f5c434b9c2ca7fa11d60b2f57c
liujinye <liujinye@xiaomi.com>
2020-05-08 01:19:46 +0800

    Merge "Dump cert & dut stack crashes during testing" am: 921c71a0cf am: 78c4ae3bce
    Change-Id: Idd6f1f16a2be4da4ee99ab2b788e3756f013d440

--- gd/security/security_record_database.h
+++ gd/security/security_record_database.h
@@ -20,7 +20,6 @@
 
 namespace bluetooth {
 namespace security {
-namespace record {
 
 class SecurityRecordDatabase {
  public:
@@ -60,6 +59,5 @@ class SecurityRecordDatabase {
   std::vector<record::SecurityRecord> records_;
 };
 
-}  // namespace record
 }  // namespace security
 }  // namespace bluetooth
