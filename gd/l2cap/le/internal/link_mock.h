3b7a12a0fcb5deb8b8b17741bfb930f651b486dd
liujinye <liujinye@xiaomi.com>
2020-05-09 01:05:32 +0800

    Snap for 6477313 from d39a5ba44ae86dcd4ccbb1ea19d8e5b137334fee to rvc-release
    Change-Id: I22381d0a7e82a21af7f1e3952296bdac182d6975

--- gd/l2cap/le/internal/link_mock.h
+++ gd/l2cap/le/internal/link_mock.h
@@ -29,13 +29,13 @@ namespace le {
 namespace internal {
 namespace testing {
 
-using hci::testing::MockLeAclConnection;
+using hci::testing::MockAclConnection;
 
 class MockLink : public Link {
  public:
   explicit MockLink(os::Handler* handler, l2cap::internal::ParameterProvider* parameter_provider,
-                    std::unique_ptr<MockLeAclConnection> mock_acl_connection, LinkManager* link_manager)
-      : Link(handler, std::move(mock_acl_connection), parameter_provider, nullptr, nullptr, link_manager) {}
+                    std::unique_ptr<MockAclConnection> mock_acl_connection)
+      : Link(handler, std::move(mock_acl_connection), parameter_provider, nullptr, nullptr){};
 
   MOCK_METHOD(hci::AddressWithType, GetDevice, (), (override));
   MOCK_METHOD(hci::Role, GetRole, (), (override));
--- gd/neighbor/inquiry.cc
+++ gd/neighbor/inquiry.cc
@@ -238,13 +238,13 @@ void neighbor::InquiryModule::impl::RegisterCallbacks(InquiryCallbacks callbacks
   inquiry_callbacks_ = callbacks;
 
   hci_layer_->RegisterEventHandler(hci::EventCode::INQUIRY_RESULT,
-                                   handler_->BindOn(this, &InquiryModule::impl::OnEvent));
+                                   common::Bind(&InquiryModule::impl::OnEvent, common::Unretained(this)), handler_);
   hci_layer_->RegisterEventHandler(hci::EventCode::INQUIRY_RESULT_WITH_RSSI,
-                                   handler_->BindOn(this, &InquiryModule::impl::OnEvent));
+                                   common::Bind(&InquiryModule::impl::OnEvent, common::Unretained(this)), handler_);
   hci_layer_->RegisterEventHandler(hci::EventCode::EXTENDED_INQUIRY_RESULT,
-                                   handler_->BindOn(this, &InquiryModule::impl::OnEvent));
+                                   common::Bind(&InquiryModule::impl::OnEvent, common::Unretained(this)), handler_);
   hci_layer_->RegisterEventHandler(hci::EventCode::INQUIRY_COMPLETE,
-                                   handler_->BindOn(this, &InquiryModule::impl::OnEvent));
+                                   common::Bind(&InquiryModule::impl::OnEvent, common::Unretained(this)), handler_);
 }
 
 void neighbor::InquiryModule::impl::UnregisterCallbacks() {
@@ -257,18 +257,21 @@ void neighbor::InquiryModule::impl::UnregisterCallbacks() {
 }
 
 void neighbor::InquiryModule::impl::EnqueueCommandComplete(std::unique_ptr<hci::CommandPacketBuilder> command) {
-  hci_layer_->EnqueueCommand(std::move(command), handler_->BindOnceOn(this, &impl::OnCommandComplete));
+  hci_layer_->EnqueueCommand(std::move(command), common::BindOnce(&impl::OnCommandComplete, common::Unretained(this)),
+                             handler_);
 }
 
 void neighbor::InquiryModule::impl::EnqueueCommandStatus(std::unique_ptr<hci::CommandPacketBuilder> command) {
-  hci_layer_->EnqueueCommand(std::move(command), handler_->BindOnceOn(this, &impl::OnCommandStatus));
+  hci_layer_->EnqueueCommand(std::move(command), common::BindOnce(&impl::OnCommandStatus, common::Unretained(this)),
+                             handler_);
 }
 
 void neighbor::InquiryModule::impl::EnqueueCommandCompleteSync(std::unique_ptr<hci::CommandPacketBuilder> command) {
   ASSERT(command_sync_ == nullptr);
   command_sync_ = new std::promise<void>();
   auto command_received = command_sync_->get_future();
-  hci_layer_->EnqueueCommand(std::move(command), handler_->BindOnceOn(this, &impl::OnCommandCompleteSync));
+  hci_layer_->EnqueueCommand(std::move(command),
+                             common::BindOnce(&impl::OnCommandCompleteSync, common::Unretained(this)), handler_);
   command_received.wait();
   delete command_sync_;
   command_sync_ = nullptr;
