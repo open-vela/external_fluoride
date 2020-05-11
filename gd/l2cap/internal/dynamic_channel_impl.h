51de7c20091d8622cc7519870119d5e5b86ae7d1
openvela-robot <openvela-robot@xiaomi.com>
2020-05-12 01:14:54 +0800

    Snap for 6484097 from 466f60ad978a60e72fb85fbf37f9a52b33a5256f to rvc-release
    Change-Id: Id1b5a2b55f91e8b1d9899b459561d310bfda599a

--- gd/l2cap/internal/dynamic_channel_impl.h
+++ gd/l2cap/internal/dynamic_channel_impl.h
@@ -38,7 +38,7 @@ class DynamicChannelImpl : public l2cap::internal::ChannelImpl {
 
   virtual ~DynamicChannelImpl() = default;
 
-  hci::AddressWithType GetDevice() const;
+  hci::Address GetDevice() const;
 
   virtual void RegisterOnCloseCallback(os::Handler* user_handler, DynamicChannel::OnCloseCallback on_close_callback);
 
@@ -84,7 +84,7 @@ class DynamicChannelImpl : public l2cap::internal::ChannelImpl {
   // Internal states
   bool closed_ = false;
   hci::ErrorCode close_reason_ = hci::ErrorCode::SUCCESS;
-  static constexpr size_t kChannelQueueSize = 5;
+  static constexpr size_t kChannelQueueSize = 10;
   common::BidiQueue<packet::PacketView<packet::kLittleEndian>, packet::BasePacketBuilder> channel_queue_{
       kChannelQueueSize};
 
