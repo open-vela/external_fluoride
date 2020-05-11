b0a331a4c1c6b4ffe475ef1581a5ec0e49b11d5b
liujinye <liujinye@xiaomi.com>
2020-05-12 01:14:54 +0800

    Snap for 6484097 from 466f60ad978a60e72fb85fbf37f9a52b33a5256f to rvc-release
    Change-Id: Id1b5a2b55f91e8b1d9899b459561d310bfda599a

--- gd/hci/controller.h
+++ gd/hci/controller.h
@@ -34,8 +34,6 @@ class Controller : public Module {
   virtual void RegisterCompletedAclPacketsCallback(
       common::Callback<void(uint16_t /* handle */, uint16_t /* num_packets */)> cb, os::Handler* handler);
 
-  virtual void UnregisterCompletedAclPacketsCallback();
-
   virtual std::string GetControllerLocalName() const;
 
   virtual LocalVersionInformation GetControllerLocalVersionInformation() const;
