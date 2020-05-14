62d50ce9f2f5746c65f86eef62694ce2bbb85135
liujinye <liujinye@xiaomi.com>
2020-05-15 01:06:27 +0800

    Snap for 6496549 from bb0b7952833714e56e7d45a711eb16c0a9fcee55 to rvc-release
    Change-Id: Ia2d47586af9ea4d0e0f27ad09cff77a3c2e25a64

--- gd/packet/parser/struct_def.cc
+++ gd/packet/parser/struct_def.cc
@@ -45,37 +45,6 @@ void StructDef::GenSpecialize(std::ostream& s) const {
   s << "}";
 }
 
-void StructDef::GenToString(std::ostream& s) const {
-  s << "std::string ToString() {";
-  s << "std::stringstream ss;";
-  s << "ss << std::hex << std::showbase << \"" << name_ << " { \";";
-
-  if (fields_.size() > 0) {
-    s << "ss";
-    bool firstfield = true;
-    for (const auto& field : fields_) {
-      if (field->GetFieldType() == ReservedField::kFieldType ||
-          field->GetFieldType() == ChecksumStartField::kFieldType ||
-          field->GetFieldType() == FixedScalarField::kFieldType || field->GetFieldType() == CountField::kFieldType ||
-          field->GetFieldType() == SizeField::kFieldType)
-        continue;
-
-      s << (firstfield ? " << \"" : " << \", ") << field->GetName() << " = \" << ";
-
-      field->GenStringRepresentation(s, field->GetName() + "_");
-
-      if (firstfield) {
-        firstfield = false;
-      }
-    }
-    s << ";";
-  }
-
-  s << "ss << \" }\";";
-  s << "return ss.str();";
-  s << "}\n";
-}
-
 void StructDef::GenParse(std::ostream& s) const {
   std::string iterator = (is_little_endian_ ? "Iterator<kLittleEndian>" : "Iterator<!kLittleEndian>");
 
@@ -112,14 +81,18 @@ void StructDef::GenParse(std::ostream& s) const {
 
   if (!fields_.HasBody()) {
     s << "size_t end_index = struct_begin_it.NumBytesRemaining();";
-    s << "if (end_index < " << GetSize().bytes() << ")";
+    if (parent_ != nullptr) {
+      s << "if (end_index < " << GetSize().bytes() << " - to_fill->" << parent_->name_ << "::size())";
+    } else {
+      s << "if (end_index < " << GetSize().bytes() << ")";
+    }
     s << "{ return struct_begin_it.Subrange(0,0);}";
   }
 
   Size total_bits{0};
   for (const auto& field : fields_) {
     if (field->GetFieldType() != ReservedField::kFieldType && field->GetFieldType() != BodyField::kFieldType &&
-        field->GetFieldType() != FixedScalarField::kFieldType &&
+        field->GetFieldType() != FixedScalarField::kFieldType && field->GetFieldType() != SizeField::kFieldType &&
         field->GetFieldType() != ChecksumStartField::kFieldType && field->GetFieldType() != ChecksumField::kFieldType &&
         field->GetFieldType() != CountField::kFieldType) {
       total_bits += field->GetSize().bits();
@@ -146,15 +119,22 @@ void StructDef::GenParse(std::ostream& s) const {
       s << "}";
     }
     if (field->GetFieldType() == CountField::kFieldType || field->GetFieldType() == SizeField::kFieldType) {
+      s << field->GetDataType() << " " << field->GetName() << "_extracted;";
       s << "{";
+      s << "if (to_bound.NumBytesRemaining() < " << field->GetSize().bytes() << ")";
+      if (!fields_.HasBody()) {
+        s << "{ return to_bound.Subrange(to_bound.NumBytesRemaining(),0);}";
+      } else {
+        s << "{ return {};}";
+      }
       int num_leading_bits =
           field->GenBounds(s, GetStructOffsetForField(field->GetName()), Size(), field->GetStructSize());
-      s << "auto " << field->GetName() << "_ptr = &to_fill->" << field->GetName() << "_extracted_;";
+      s << "auto " << field->GetName() << "_ptr = &" << field->GetName() << "_extracted;";
       field->GenExtractor(s, num_leading_bits, true);
       s << "}";
     }
   }
-  s << "return struct_begin_it + to_fill->size();";
+  s << "return struct_begin_it + to_fill->" << name_ << "::size();";
   s << "}";
 }
 
@@ -202,16 +182,7 @@ void StructDef::GenDefinition(std::ostream& s) const {
   GenSpecialize(s);
   s << "\n";
 
-  GenToString(s);
-  s << "\n";
-
   GenMembers(s);
-  for (const auto& field : fields_) {
-    if (field->GetFieldType() == CountField::kFieldType || field->GetFieldType() == SizeField::kFieldType) {
-      s << "\n private:\n";
-      s << " mutable " << field->GetDataType() << " " << field->GetName() << "_extracted_{0};";
-    }
-  }
   s << "};\n";
 
   if (fields_.HasBody()) {
