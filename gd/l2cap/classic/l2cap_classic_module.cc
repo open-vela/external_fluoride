773c20742bf373f26e419fb9aa87621968678f59
openvela-robot <openvela-robot@xiaomi.com>
2020-05-20 23:48:51 +0800

    Merge "gd HCI: Split out LeAddressInitiator"

--- gd/l2cap/classic/l2cap_classic_module.cc
+++ gd/l2cap/classic/l2cap_classic_module.cc
@@ -59,7 +59,6 @@ struct L2capClassicModule::impl {
     void RegisterLinkSecurityInterfaceListener(os::Handler* handler, LinkSecurityInterfaceListener* listener) {
       ASSERT(!registered_);
       module_impl_->link_manager_.RegisterLinkSecurityInterfaceListener(handler, listener);
-      registered_ = true;
     }
 
     void InitiateConnectionForSecurity(hci::Address remote) override {
@@ -70,7 +69,6 @@ struct L2capClassicModule::impl {
     void Unregister() override {
       ASSERT(registered_);
       module_impl_->link_manager_.RegisterLinkSecurityInterfaceListener(nullptr, nullptr);
-      registered_ = false;
     }
     impl* module_impl_;
     bool registered_ = false;
