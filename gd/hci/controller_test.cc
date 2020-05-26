fe82642c23d80a2201eb8fe9844532fae122295d
liujinye <liujinye@xiaomi.com>
2020-05-27 03:37:56 +0800

    Remove ASSERT from tests am: 7ae58794e4 am: 6028dd56b8
    Change-Id: I55b58c822475a62d407542e9b62b26b5d58474a5

--- gd/hci/controller_test.cc
+++ gd/hci/controller_test.cc
@@ -235,10 +235,7 @@ class TestHciLayer : public HciLayer {
         break;
       }
     }
-    EXPECT_TRUE(command_queue_.size() > 0);
-    if (command_queue_.empty()) {
-      return CommandPacketView::Create(std::make_shared<std::vector<uint8_t>>());
-    }
+    ASSERT(command_queue_.size() > 0);
     CommandPacketView command = command_queue_.front();
     EXPECT_EQ(command.GetOpCode(), op_code);
     command_queue_.pop();
