870ca54f2aa2d51a62d3a24ba134db822f0d5e82
openvela-robot <openvela-robot@xiaomi.com>
2020-05-27 03:45:10 +0800

    btif_config: Fix pointer to freed memory am: d96e89cd9c
    Change-Id: I52aac3213e98a32d74e9828dffdf409b0bc9793b

--- gd/security/facade.cc
+++ gd/security/facade.cc
@@ -21,7 +21,6 @@
 #include "security/facade.grpc.pb.h"
 #include "security/security_manager_listener.h"
 #include "security/security_module.h"
-#include "security/ui.h"
 
 namespace bluetooth {
 namespace security {
@@ -31,7 +30,6 @@ class SecurityModuleFacadeService : public SecurityModuleFacade::Service, public
   SecurityModuleFacadeService(SecurityModule* security_module, ::bluetooth::os::Handler* security_handler)
       : security_module_(security_module), security_handler_(security_handler) {
     security_module_->GetSecurityManager()->RegisterCallbackListener(this, security_handler_);
-    security_module_->GetSecurityManager()->SetUserInterfaceHandler(this, security_handler_);
   }
 
   ::grpc::Status CreateBond(::grpc::ServerContext* context, const facade::BluetoothAddressWithType* request,
@@ -40,15 +38,6 @@ class SecurityModuleFacadeService : public SecurityModuleFacade::Service, public
     ASSERT(hci::Address::FromString(request->address().address(), peer));
     hci::AddressType peer_type = hci::AddressType::PUBLIC_DEVICE_ADDRESS;
     security_module_->GetSecurityManager()->CreateBond(hci::AddressWithType(peer, peer_type));
-    return ::grpc::Status::OK;
-  }
-
-  ::grpc::Status CreateBondLe(::grpc::ServerContext* context, const facade::BluetoothAddressWithType* request,
-                              ::google::protobuf::Empty* response) override {
-    hci::Address peer;
-    ASSERT(hci::Address::FromString(request->address().address(), peer));
-    hci::AddressType peer_type = static_cast<hci::AddressType>(request->type());
-    security_module_->GetSecurityManager()->CreateBondLe(hci::AddressWithType(peer, peer_type));
     return ::grpc::Status::OK;
   }
 
@@ -77,17 +66,12 @@ class SecurityModuleFacadeService : public SecurityModuleFacade::Service, public
 
   ::grpc::Status SendUiCallback(::grpc::ServerContext* context, const UiCallbackMsg* request,
                                 ::google::protobuf::Empty* response) override {
-    hci::Address peer;
-    ASSERT(hci::Address::FromString(request->address().address().address(), peer));
-    hci::AddressType remote_type = hci::AddressType::PUBLIC_DEVICE_ADDRESS;
-
     switch (request->message_type()) {
       case UiCallbackType::PASSKEY:
         // TODO: security_module_->GetSecurityManager()->OnPasskeyEntry();
         break;
       case UiCallbackType::YES_NO:
-        security_module_->GetSecurityManager()->OnConfirmYesNo(hci::AddressWithType(peer, remote_type),
-                                                               request->boolean());
+        // TODO: security_module_->GetSecurityManager()->OnConfirmYesNo(request->boolean());
         break;
       default:
         LOG_ERROR("Unknown UiCallbackType %d", static_cast<int>(request->message_type()));
@@ -101,28 +85,6 @@ class SecurityModuleFacadeService : public SecurityModuleFacade::Service, public
     return bond_events_.RunLoop(context, writer);
   }
 
-  ::grpc::Status SetIoCapability(::grpc::ServerContext* context, const IoCapabilityMessage* request,
-                                 ::google::protobuf::Empty* response) override {
-    security_module_->GetFacadeConfigurationApi()->SetIoCapability(
-        static_cast<hci::IoCapability>(request->capability()));
-    return ::grpc::Status::OK;
-  }
-
-  ::grpc::Status SetAuthenticationRequirements(::grpc::ServerContext* context,
-                                               const AuthenticationRequirementsMessage* request,
-                                               ::google::protobuf::Empty* response) override {
-    security_module_->GetFacadeConfigurationApi()->SetAuthenticationRequirements(
-        static_cast<hci::AuthenticationRequirements>(request->requirement()));
-    return ::grpc::Status::OK;
-  }
-
-  ::grpc::Status SetOobDataPresent(::grpc::ServerContext* context, const OobDataMessage* request,
-                                   ::google::protobuf::Empty* response) override {
-    security_module_->GetFacadeConfigurationApi()->SetOobData(
-        static_cast<hci::OobDataPresent>(request->data_present()));
-    return ::grpc::Status::OK;
-  }
-
   void DisplayPairingPrompt(const bluetooth::hci::AddressWithType& peer, std::string name) {
     LOG_INFO("%s", peer.ToString().c_str());
     UiMsg display_yes_no;
@@ -130,7 +92,6 @@ class SecurityModuleFacadeService : public SecurityModuleFacade::Service, public
     display_yes_no.mutable_peer()->set_type(facade::BluetoothAddressTypeEnum::PUBLIC_DEVICE_ADDRESS);
     display_yes_no.set_message_type(UiMsgType::DISPLAY_YES_NO);
     display_yes_no.set_unique_id(unique_id++);
-    ui_events_.OnIncomingEvent(display_yes_no);
   }
 
   virtual void DisplayConfirmValue(const bluetooth::hci::AddressWithType& peer, std::string name,
@@ -152,7 +113,6 @@ class SecurityModuleFacadeService : public SecurityModuleFacade::Service, public
     display_yes_no.mutable_peer()->set_type(facade::BluetoothAddressTypeEnum::PUBLIC_DEVICE_ADDRESS);
     display_yes_no.set_message_type(UiMsgType::DISPLAY_YES_NO);
     display_yes_no.set_unique_id(unique_id++);
-    ui_events_.OnIncomingEvent(display_yes_no);
   }
 
   void DisplayPasskey(const bluetooth::hci::AddressWithType& peer, std::string name, uint32_t passkey) override {
@@ -194,8 +154,6 @@ class SecurityModuleFacadeService : public SecurityModuleFacade::Service, public
     bonded.set_message_type(BondMsgType::DEVICE_BONDED);
     bond_events_.OnIncomingEvent(bonded);
   }
-
-  void OnEncryptionStateChanged(hci::EncryptionChangeView encryption_change_view) override {}
 
   void OnDeviceUnbonded(hci::AddressWithType peer) override {
     LOG_INFO("%s", peer.ToString().c_str());
