174301796d6ae9b21a0bf05e5c1e44168b9251ca
openvela-robot <openvela-robot@xiaomi.com>
2020-05-27 22:22:30 +0800

    Merge "L2cap: Remove duplicated OnAclDisconnected invocation"

--- gd/cert/matchers.py
+++ gd/cert/matchers.py
@@ -89,10 +89,6 @@ class HciMatchers(object):
     def SimplePairingComplete():
         return lambda event: HciMatchers.EventWithCode(EventCode.SIMPLE_PAIRING_COMPLETE)
 
-    @staticmethod
-    def Disconnect():
-        return lambda event: HciMatchers.EventWithCode(EventCode.DISCONNECT)
-
 
 class NeighborMatchers(object):
 
