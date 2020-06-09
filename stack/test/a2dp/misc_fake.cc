e16ac37ce28f8adcd58d5ee3f6d2808c61b08367
openvela-robot <openvela-robot@xiaomi.com>
2020-06-10 01:25:23 +0800

    Merge "bt HCI: Make sure the device numbers match"

--- stack/test/a2dp/misc_fake.cc
+++ stack/test/a2dp/misc_fake.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 The Android Open Source Project
+ * Copyright 2020 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,18 +14,15 @@
  * limitations under the License.
  */
 
-#include "os/system_properties.h"
+#include "service/common/bluetooth/a2dp_codec_config.h"
+#include "stack/include/a2dp_vendor_ldac.h"
 
-namespace bluetooth {
-namespace os {
-
-std::optional<std::string> GetSystemProperty(const std::string& property) {
-  return std::nullopt;
+bluetooth::A2dpCodecConfig* bta_av_get_a2dp_current_codec(void) {
+  return nullptr;
 }
 
-bool SetSystemProperty(const std::string& property, const std::string& value) {
-  return false;
+int A2DP_VendorGetTrackSampleRateLdac(const uint8_t* p_codec_info) { return 0; }
+int A2DP_VendorGetTrackBitsPerSampleLdac(const uint8_t* p_codec_info) {
+  return 0;
 }
-
-}  // namespace os
-}  // namespace bluetooth
+int A2DP_VendorGetChannelModeCodeLdac(const uint8_t* p_codec_info) { return 0; }
