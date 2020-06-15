e0e8e24f8ed49579a49a48b35bead887fce68097
liujinye <liujinye@xiaomi.com>
2020-06-15 23:22:53 +0800

    Merge "RootCanal: Remove duplicated function name logging" am: 131b7e64bb am: ea74da204d am: 5bf959e6f1 am: 3250807a46
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1334973
    
    Change-Id: Ic69d3739fe78eda342a02833421ae1aac927521f

--- vendor_libs/test_vendor_lib/model/controller/dual_mode_controller.cc
+++ vendor_libs/test_vendor_lib/model/controller/dual_mode_controller.cc
@@ -2000,12 +2000,15 @@ void DualModeController::LeStartEncryption(CommandPacketView command) {
       gd_hci::LeSecurityCommandView::Create(command));
   ASSERT(command_view.IsValid());
 
-  ErrorCode status = link_layer_controller_.LeEnableEncryption(
-      command_view.GetConnectionHandle(), command_view.GetRand(),
-      command_view.GetEdiv(), command_view.GetLtk());
+  uint16_t handle = command_view.GetConnectionHandle();
 
-  send_event_(bluetooth::hci::LeStartEncryptionStatusBuilder::Create(
-      status, kNumCommandPackets));
+  auto status_packet = bluetooth::hci::LeStartEncryptionStatusBuilder::Create(
+      ErrorCode::SUCCESS, kNumCommandPackets);
+  send_event_(std::move(status_packet));
+
+  auto complete_packet = bluetooth::hci::EncryptionChangeBuilder::Create(
+      ErrorCode::SUCCESS, handle, bluetooth::hci::EncryptionEnabled::OFF);
+  send_event_(std::move(complete_packet));
 }
 
 void DualModeController::ReadLoopbackMode(CommandPacketView command) {
