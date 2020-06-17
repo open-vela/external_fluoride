94582d4add13219ad282bc65190dc761c5beff98
openvela-robot <openvela-robot@xiaomi.com>
2020-06-17 17:12:14 +0800

    L2cap: Remove FCR option in FixedChannel registration am: 7cd49d9847 am: f78a3bb8d0 am: 27b448c624
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1337979
    
    Change-Id: I3e7180a981e4b82aaef92de60dc20700e90303db

--- gd/cert/py_le_acl_manager.py
+++ gd/cert/py_le_acl_manager.py
@@ -43,7 +43,13 @@ class PyLeAclManagerAclConnection(IEventStream, Closable):
         self.our_acl_stream = acl_stream
 
         if remote_addr:
-            self.connection_event_stream = EventStream(self.device.hci_le_acl_manager.CreateConnection(remote_addr))
+            remote_addr_bytes = bytes(remote_addr.address.address,
+                                      'utf8') if type(remote_addr.address.address) is str else bytes(
+                                          remote_addr.address.address)
+            self.connection_event_stream = EventStream(
+                self.device.hci_le_acl_manager.CreateConnection(
+                    le_acl_manager_facade.LeConnectionMsg(
+                        address_type=int(remote_addr.type), address=remote_addr_bytes)))
         else:
             self.connection_event_stream = None
 
