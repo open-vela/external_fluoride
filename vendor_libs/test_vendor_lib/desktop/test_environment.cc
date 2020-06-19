40cdb24db7665214a0f9e9e81bcf9a386eea2520
openvela-robot <openvela-robot@xiaomi.com>
2020-06-19 23:53:25 +0800

    Merge "Don't query for metadata on playing status" into rvc-dev am: 12d6bc5db8
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/bt/+/11921967
    
    Change-Id: I7a1e935dd2c78282ac4ade3bdd6cfe3b36a32384

--- vendor_libs/test_vendor_lib/desktop/test_environment.cc
+++ vendor_libs/test_vendor_lib/desktop/test_environment.cc
@@ -126,7 +126,7 @@ int TestEnvironment::ConnectToRemoteServer(const std::string& server, int port) 
     return -1;
   }
 
-  struct sockaddr_in serv_addr {};
+  struct sockaddr_in serv_addr;
   memset((void*)&serv_addr, 0, sizeof(serv_addr));
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_addr.s_addr = INADDR_ANY;
@@ -149,7 +149,7 @@ void TestEnvironment::SetUpTestChannel() {
   int socket_fd = test_channel_transport_.SetUp(test_port_);
   test_channel_.AddPhy({"BR_EDR"});
   test_channel_.AddPhy({"LOW_ENERGY"});
-  test_channel_.SetTimerPeriod({"5"});
+  test_channel_.SetTimerPeriod({"10"});
   test_channel_.StartTimer({});
 
   test_channel_.RegisterSendResponse(
