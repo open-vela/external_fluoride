425d555756980f0ad7f2db4aad932b7c7c03efd6
liujinye <liujinye@xiaomi.com>
2020-06-23 06:24:13 +0800

    [automerger skipped] A2DP: Enable AAC VBR for A2DP source feature am: cb53324dfa -s ours
    am skip reason: Change-Id I4271154248af5ea82c3de2c97bb39526e9f904ba with SHA-1 d201d55d2b is in history
    
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/bt/+/11929819
    
    Change-Id: Ifdd24c2dfcb835f2429a0cff4f59cf715f315a01

--- gd/l2cap/classic/internal/signalling_manager.h
+++ gd/l2cap/classic/internal/signalling_manager.h
@@ -66,7 +66,7 @@ class ClassicSignallingManager {
 
   void SendConnectionRequest(Psm psm, Cid local_cid);
 
-  void SendConfigurationRequest(Cid remote_cid, std::vector<std::unique_ptr<ConfigurationOption>> config);
+  void SendInitialConfigRequest(Cid local_cid);
 
   void SendDisconnectionRequest(Cid local_cid, Cid remote_cid);
 
@@ -101,10 +101,18 @@ class ClassicSignallingManager {
 
  private:
   void on_incoming_packet();
+  void handle_one_command(ControlView control_view);
   void send_connection_response(SignalId signal_id, Cid remote_cid, Cid local_cid, ConnectionResponseResult result,
                                 ConnectionResponseStatus status);
   void on_command_timeout();
   void handle_send_next_command();
+
+  void negotiate_configuration(Cid cid, Continuation is_continuation,
+                               std::vector<std::unique_ptr<ConfigurationOption>>);
+
+  void send_configuration_request(Cid remote_cid, std::vector<std::unique_ptr<ConfigurationOption>> config);
+  void on_security_result_for_incoming(Psm psm, bool result);
+  void on_security_result_for_outgoing(Psm psm, bool result);
 
   os::Handler* handler_;
   Link* link_;
@@ -119,6 +127,13 @@ class ClassicSignallingManager {
   os::Alarm alarm_;
   SignalId next_signal_id_ = kInitialSignalId;
   std::unordered_map<Cid, ChannelConfigurationState> channel_configuration_;
+
+  struct PendingConnection {
+    Cid local_cid;
+    Cid remote_cid;
+    SignalId incoming_signal_id;
+  };
+  std::unordered_map<Psm, PendingConnection> pending_security_requests_;
 };
 
 }  // namespace internal
