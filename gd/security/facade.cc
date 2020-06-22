e9cdc4f428409e2b143eacda9537906f3a4c952d
liujinye <liujinye@xiaomi.com>
2020-06-23 06:47:19 +0800

    Merge "Add Honda High End carkit to IOP table to only use AVRCP 1.4" into rvc-d1-dev

--- gd/security/facade.cc
+++ gd/security/facade.cc
@@ -17,13 +17,10 @@
 
 #include "grpc/grpc_event_queue.h"
 #include "hci/address_with_type.h"
-#include "hci/le_address_manager.h"
-#include "l2cap/classic/security_policy.h"
 #include "os/handler.h"
 #include "security/facade.grpc.pb.h"
 #include "security/security_manager_listener.h"
 #include "security/security_module.h"
-#include "security/ui.h"
 
 namespace bluetooth {
 namespace security {
@@ -33,7 +30,6 @@ class SecurityModuleFacadeService : public SecurityModuleFacade::Service, public
   SecurityModuleFacadeService(SecurityModule* security_module, ::bluetooth::os::Handler* security_handler)
       : security_module_(security_module), security_handler_(security_handler) {
     security_module_->GetSecurityManager()->RegisterCallbackListener(this, security_handler_);
-    security_module_->GetSecurityManager()->SetUserInterfaceHandler(this, security_handler_);
   }
 
   ::grpc::Status CreateBond(::grpc::ServerContext* context, const facade::BluetoothAddressWithType* request,
@@ -42,15 +38,6 @@ class SecurityModuleFacadeService : public SecurityModuleFacade::Service, public
     ASSERT(hci::Address::FromString(request->address().address(), peer));
     hci::AddressType peer_type = hci::AddressType::PUBLIC_DEVICE_ADDRESS;
     security_module_->GetSecurityManager()->CreateBond(hci::AddressWithType(peer, peer_type));
-    return ::grpc::Status::OK;
-  }
-
-  ::grpc::Status CreateBondLe(::grpc::ServerContext* context, const facade::BluetoothAddressWithType* request,
-                              ::google::protobuf::Empty* response) override {
-    hci::Address peer;
-    ASSERT(hci::Address::FromString(request->address().address(), peer));
-    hci::AddressType peer_type = static_cast<hci::AddressType>(request->type());
-    security_module_->GetSecurityManager()->CreateBondLe(hci::AddressWithType(peer, peer_type));
     return ::grpc::Status::OK;
   }
 
@@ -79,21 +66,12 @@ class SecurityModuleFacadeService : public SecurityModuleFacade::Service, public
 
   ::grpc::Status SendUiCallback(::grpc::ServerContext* context, const UiCallbackMsg* request,
                                 ::google::protobuf::Empty* response) override {
-    hci::Address peer;
-    ASSERT(hci::Address::FromString(request->address().address().address(), peer));
-    hci::AddressType remote_type = static_cast<hci::AddressType>(request->address().type());
-
     switch (request->message_type()) {
       case UiCallbackType::PASSKEY:
         // TODO: security_module_->GetSecurityManager()->OnPasskeyEntry();
         break;
       case UiCallbackType::YES_NO:
-        security_module_->GetSecurityManager()->OnConfirmYesNo(hci::AddressWithType(peer, remote_type),
-                                                               request->boolean());
-        break;
-      case UiCallbackType::PAIRING_PROMPT:
-        security_module_->GetSecurityManager()->OnPairingPromptAccepted(
-            hci::AddressWithType(peer, remote_type), request->boolean());
+        // TODO: security_module_->GetSecurityManager()->OnConfirmYesNo(request->boolean());
         break;
       default:
         LOG_ERROR("Unknown UiCallbackType %d", static_cast<int>(request->message_type()));
@@ -107,86 +85,13 @@ class SecurityModuleFacadeService : public SecurityModuleFacade::Service, public
     return bond_events_.RunLoop(context, writer);
   }
 
-  ::grpc::Status SetIoCapability(::grpc::ServerContext* context, const IoCapabilityMessage* request,
-                                 ::google::protobuf::Empty* response) override {
-    security_module_->GetFacadeConfigurationApi()->SetIoCapability(
-        static_cast<hci::IoCapability>(request->capability()));
-    return ::grpc::Status::OK;
-  }
-
-  ::grpc::Status SetLeIoCapability(
-      ::grpc::ServerContext* context,
-      const LeIoCapabilityMessage* request,
-      ::google::protobuf::Empty* response) override {
-    security_module_->GetFacadeConfigurationApi()->SetLeIoCapability(
-        static_cast<security::IoCapability>(request->capabilities()));
-    return ::grpc::Status::OK;
-  }
-
-  ::grpc::Status SetAuthenticationRequirements(::grpc::ServerContext* context,
-                                               const AuthenticationRequirementsMessage* request,
-                                               ::google::protobuf::Empty* response) override {
-    security_module_->GetFacadeConfigurationApi()->SetAuthenticationRequirements(
-        static_cast<hci::AuthenticationRequirements>(request->requirement()));
-    return ::grpc::Status::OK;
-  }
-
-  ::grpc::Status SetOobDataPresent(::grpc::ServerContext* context, const OobDataMessage* request,
-                                   ::google::protobuf::Empty* response) override {
-    security_module_->GetFacadeConfigurationApi()->SetOobData(
-        static_cast<hci::OobDataPresent>(request->data_present()));
-    return ::grpc::Status::OK;
-  }
-
-  ::grpc::Status SetLeAuthReq(
-      ::grpc::ServerContext* context, const LeAuthReqMsg* request, ::google::protobuf::Empty* response) override {
-    security_module_->GetFacadeConfigurationApi()->SetLeAuthReq(request->auth_req());
-    return ::grpc::Status::OK;
-  }
-
-  ::grpc::Status SetLeInitiatorAddressPolicy(
-      ::grpc::ServerContext* context, const hci::PrivacyPolicy* request, ::google::protobuf::Empty* response) override {
-    Address address = Address::kEmpty;
-    hci::LeAddressManager::AddressPolicy address_policy =
-        static_cast<hci::LeAddressManager::AddressPolicy>(request->address_policy());
-    if (address_policy == hci::LeAddressManager::AddressPolicy::USE_STATIC_ADDRESS) {
-      ASSERT(Address::FromString(request->address_with_type().address().address(), address));
-    }
-    hci::AddressWithType address_with_type(address, static_cast<hci::AddressType>(request->address_with_type().type()));
-    crypto_toolbox::Octet16 irk = {};
-    auto request_irk_length = request->rotation_irk().end() - request->rotation_irk().begin();
-    if (request_irk_length == crypto_toolbox::OCTET16_LEN) {
-      std::vector<uint8_t> irk_data(request->rotation_irk().begin(), request->rotation_irk().end());
-      std::copy_n(irk_data.begin(), crypto_toolbox::OCTET16_LEN, irk.begin());
-    } else {
-      ASSERT(request_irk_length == 0);
-    }
-    auto minimum_rotation_time = std::chrono::milliseconds(request->minimum_rotation_time());
-    auto maximum_rotation_time = std::chrono::milliseconds(request->maximum_rotation_time());
-    security_module_->GetSecurityManager()->SetLeInitiatorAddressPolicy(
-        address_policy, address_with_type, irk, minimum_rotation_time, maximum_rotation_time);
-    return ::grpc::Status::OK;
-  }
-
-  ::grpc::Status EnforceSecurityPolicy(
-      ::grpc::ServerContext* context,
-      const SecurityPolicyMessage* request,
-      ::google::protobuf::Empty* response) override {
-    hci::Address peer;
-    ASSERT(hci::Address::FromString(request->address().address().address(), peer));
-    hci::AddressType peer_type = static_cast<hci::AddressType>(request->address().type());
-    hci::AddressWithType peer_with_type(peer, peer_type);
-    return ::grpc::Status::OK;
-  }
-
   void DisplayPairingPrompt(const bluetooth::hci::AddressWithType& peer, std::string name) {
     LOG_INFO("%s", peer.ToString().c_str());
     UiMsg display_yes_no;
     display_yes_no.mutable_peer()->mutable_address()->set_address(peer.ToString());
-    display_yes_no.mutable_peer()->set_type(static_cast<facade::BluetoothAddressTypeEnum>(peer.GetAddressType()));
-    display_yes_no.set_message_type(UiMsgType::DISPLAY_PAIRING_PROMPT);
+    display_yes_no.mutable_peer()->set_type(facade::BluetoothAddressTypeEnum::PUBLIC_DEVICE_ADDRESS);
+    display_yes_no.set_message_type(UiMsgType::DISPLAY_YES_NO);
     display_yes_no.set_unique_id(unique_id++);
-    ui_events_.OnIncomingEvent(display_yes_no);
   }
 
   virtual void DisplayConfirmValue(const bluetooth::hci::AddressWithType& peer, std::string name,
@@ -194,7 +99,7 @@ class SecurityModuleFacadeService : public SecurityModuleFacade::Service, public
     LOG_INFO("%s value = 0x%x", peer.ToString().c_str(), numeric_value);
     UiMsg display_with_value;
     display_with_value.mutable_peer()->mutable_address()->set_address(peer.ToString());
-    display_with_value.mutable_peer()->set_type(static_cast<facade::BluetoothAddressTypeEnum>(peer.GetAddressType()));
+    display_with_value.mutable_peer()->set_type(facade::BluetoothAddressTypeEnum::PUBLIC_DEVICE_ADDRESS);
     display_with_value.set_message_type(UiMsgType::DISPLAY_YES_NO_WITH_VALUE);
     display_with_value.set_numeric_value(numeric_value);
     display_with_value.set_unique_id(unique_id++);
@@ -205,17 +110,16 @@ class SecurityModuleFacadeService : public SecurityModuleFacade::Service, public
     LOG_INFO("%s", peer.ToString().c_str());
     UiMsg display_yes_no;
     display_yes_no.mutable_peer()->mutable_address()->set_address(peer.ToString());
-    display_yes_no.mutable_peer()->set_type(static_cast<facade::BluetoothAddressTypeEnum>(peer.GetAddressType()));
+    display_yes_no.mutable_peer()->set_type(facade::BluetoothAddressTypeEnum::PUBLIC_DEVICE_ADDRESS);
     display_yes_no.set_message_type(UiMsgType::DISPLAY_YES_NO);
     display_yes_no.set_unique_id(unique_id++);
-    ui_events_.OnIncomingEvent(display_yes_no);
   }
 
   void DisplayPasskey(const bluetooth::hci::AddressWithType& peer, std::string name, uint32_t passkey) override {
     LOG_INFO("%s value = 0x%x", peer.ToString().c_str(), passkey);
     UiMsg display_passkey;
     display_passkey.mutable_peer()->mutable_address()->set_address(peer.ToString());
-    display_passkey.mutable_peer()->set_type(static_cast<facade::BluetoothAddressTypeEnum>(peer.GetAddressType()));
+    display_passkey.mutable_peer()->set_type(facade::BluetoothAddressTypeEnum::PUBLIC_DEVICE_ADDRESS);
     display_passkey.set_message_type(UiMsgType::DISPLAY_PASSKEY);
     display_passkey.set_numeric_value(passkey);
     display_passkey.set_unique_id(unique_id++);
@@ -226,8 +130,7 @@ class SecurityModuleFacadeService : public SecurityModuleFacade::Service, public
     LOG_INFO("%s", peer.ToString().c_str());
     UiMsg display_passkey_input;
     display_passkey_input.mutable_peer()->mutable_address()->set_address(peer.ToString());
-    display_passkey_input.mutable_peer()->set_type(
-        static_cast<facade::BluetoothAddressTypeEnum>(peer.GetAddressType()));
+    display_passkey_input.mutable_peer()->set_type(facade::BluetoothAddressTypeEnum::PUBLIC_DEVICE_ADDRESS);
     display_passkey_input.set_message_type(UiMsgType::DISPLAY_PASSKEY_ENTRY);
     display_passkey_input.set_unique_id(unique_id++);
     ui_events_.OnIncomingEvent(display_passkey_input);
@@ -237,7 +140,7 @@ class SecurityModuleFacadeService : public SecurityModuleFacade::Service, public
     LOG_INFO("%s", peer.ToString().c_str());
     UiMsg display_cancel;
     display_cancel.mutable_peer()->mutable_address()->set_address(peer.ToString());
-    display_cancel.mutable_peer()->set_type(static_cast<facade::BluetoothAddressTypeEnum>(peer.GetAddressType()));
+    display_cancel.mutable_peer()->set_type(facade::BluetoothAddressTypeEnum::PUBLIC_DEVICE_ADDRESS);
     display_cancel.set_message_type(UiMsgType::DISPLAY_CANCEL);
     display_cancel.set_unique_id(unique_id++);
     ui_events_.OnIncomingEvent(display_cancel);
@@ -247,18 +150,16 @@ class SecurityModuleFacadeService : public SecurityModuleFacade::Service, public
     LOG_INFO("%s", peer.ToString().c_str());
     BondMsg bonded;
     bonded.mutable_peer()->mutable_address()->set_address(peer.ToString());
-    bonded.mutable_peer()->set_type(static_cast<facade::BluetoothAddressTypeEnum>(peer.GetAddressType()));
+    bonded.mutable_peer()->set_type(facade::BluetoothAddressTypeEnum::PUBLIC_DEVICE_ADDRESS);
     bonded.set_message_type(BondMsgType::DEVICE_BONDED);
     bond_events_.OnIncomingEvent(bonded);
   }
-
-  void OnEncryptionStateChanged(hci::EncryptionChangeView encryption_change_view) override {}
 
   void OnDeviceUnbonded(hci::AddressWithType peer) override {
     LOG_INFO("%s", peer.ToString().c_str());
     BondMsg unbonded;
     unbonded.mutable_peer()->mutable_address()->set_address(peer.ToString());
-    unbonded.mutable_peer()->set_type(static_cast<facade::BluetoothAddressTypeEnum>(peer.GetAddressType()));
+    unbonded.mutable_peer()->set_type(facade::BluetoothAddressTypeEnum::PUBLIC_DEVICE_ADDRESS);
     unbonded.set_message_type(BondMsgType::DEVICE_UNBONDED);
     bond_events_.OnIncomingEvent(unbonded);
   }
@@ -267,7 +168,7 @@ class SecurityModuleFacadeService : public SecurityModuleFacade::Service, public
     LOG_INFO("%s", peer.ToString().c_str());
     BondMsg bond_failed;
     bond_failed.mutable_peer()->mutable_address()->set_address(peer.ToString());
-    bond_failed.mutable_peer()->set_type(static_cast<facade::BluetoothAddressTypeEnum>(peer.GetAddressType()));
+    bond_failed.mutable_peer()->set_type(facade::BluetoothAddressTypeEnum::PUBLIC_DEVICE_ADDRESS);
     bond_failed.set_message_type(BondMsgType::DEVICE_BOND_FAILED);
     bond_events_.OnIncomingEvent(bond_failed);
   }
--- gd/shim/dumpsys.cc
+++ gd/shim/dumpsys.cc
@@ -15,8 +15,6 @@
  */
 #define LOG_TAG "bt_gd_shim"
 
-#include "shim/dumpsys.h"
-
 #include <algorithm>
 #include <functional>
 #include <future>
@@ -25,12 +23,10 @@
 #include <unordered_map>
 #include <utility>
 
-#include "bundler_generated.h"
-#include "flatbuffers/idl.h"
-#include "flatbuffers/reflection_generated.h"
 #include "module.h"
 #include "os/handler.h"
 #include "os/log.h"
+#include "shim/dumpsys.h"
 
 namespace bluetooth {
 namespace shim {
@@ -39,121 +35,50 @@ namespace {
 constexpr char kModuleName[] = "shim::Dumpsys";
 }  // namespace
 
-constexpr char kArgumentDeveloper[] = "--dev";
-
-class ParsedDumpsysArgs {
- public:
-  ParsedDumpsysArgs(const char** args) {
-    if (args == nullptr) return;
-    const char* p = *args;
-    while (p != nullptr) {
-      num_args_++;
-      if (!strcmp(p, kArgumentDeveloper)) {
-        dev_arg_ = true;
-      } else {
-        // silently ignore unexpected option
-      }
-      if (++args == nullptr) break;
-      p = *args;
-    }
-  }
-  bool IsDeveloper() const {
-    return dev_arg_;
-  }
-
- private:
-  unsigned num_args_{0};
-  bool dev_arg_{false};
-};
-
 struct Dumpsys::impl {
  public:
-  void DumpWithArgs(int fd, const char** args, std::promise<void> promise);
+  void Dump(int fd, std::promise<void> promise);
+  void RegisterDumpsysFunction(const void* token, DumpsysFunction func);
+  void UnregisterDumpsysFunction(const void* token);
 
-  void RegisterDumpsysFunction(const void* token, DumpsysFunction func);  // OBSOLETE
-  void UnregisterDumpsysFunction(const void* token);                      // OBSOLETE
-
-  impl(const Dumpsys& dumpsys_module);
   ~impl() = default;
 
- protected:
-  void FilterAsUser(std::string* output);
-  void FilterAsDeveloper(std::string* output);
-  std::string PrintAsJson(std::string* output) const;
-
  private:
-  std::unordered_map<const void*, DumpsysFunction> dumpsys_functions_;  // OBSOLETE
-  const reflection::Schema* FindBundledSchema(
-      const dumpsys::BundleSchema& bundle_schema, const std::string& name) const;
-  const Dumpsys& dumpsys_module_;
+  std::unordered_map<const void*, DumpsysFunction> dumpsys_functions_;
 };
 
 const ModuleFactory Dumpsys::Factory = ModuleFactory([]() { return new Dumpsys(); });
 
-Dumpsys::impl::impl(const Dumpsys& dumpsys_module) : dumpsys_module_(dumpsys_module) {}
-
-void Dumpsys::impl::FilterAsDeveloper(std::string* output) {
-  ASSERT(output != nullptr);
-  LOG_INFO("%s UNIMPLEMENTED", __func__);
-}
-
-void Dumpsys::impl::FilterAsUser(std::string* output) {
-  ASSERT(output != nullptr);
-  LOG_INFO("%s UNIMPLEMENTED", __func__);
-}
-
-const reflection::Schema* Dumpsys::impl::FindBundledSchema(
-    const dumpsys::BundleSchema& bundle_schema, const std::string& name) const {
-  // TODO(cmanton) Return proper schema given schema container and name to index
-  return nullptr;
-}
-
-std::string Dumpsys::impl::PrintAsJson(std::string* output) const {
-  return std::string("UNIMPLEMENTED");
-}
-
-void Dumpsys::impl::DumpWithArgs(int fd, const char** args, std::promise<void> promise) {
-  ParsedDumpsysArgs parsed_dumpsys_args(args);
-  const auto registry = dumpsys_module_.GetModuleRegistry();
-
-  ModuleDumper dumper(*registry);
-  std::string output;
-  // Get the dumpstate into out string
-  dumper.DumpState(&output);
-
-  if (parsed_dumpsys_args.IsDeveloper()) {
-    FilterAsDeveloper(&output);
-  } else {
-    FilterAsUser(&output);
-  }
-
-  dprintf(fd, "%s", PrintAsJson(&output).c_str());
+void Dumpsys::impl::Dump(int fd, std::promise<void> promise) {
+  dprintf(fd, "%s Registered submodules:%zd\n", kModuleName, dumpsys_functions_.size());
+  std::for_each(dumpsys_functions_.begin(), dumpsys_functions_.end(),
+                [fd](std::pair<const void*, DumpsysFunction> element) { element.second(fd); });
   promise.set_value();
 }
 
-void Dumpsys::impl::RegisterDumpsysFunction(const void* token, DumpsysFunction func) {  // OBSOLETE
+void Dumpsys::impl::RegisterDumpsysFunction(const void* token, DumpsysFunction func) {
   ASSERT(dumpsys_functions_.find(token) == dumpsys_functions_.end());
   dumpsys_functions_[token] = func;
 }
 
-void Dumpsys::impl::UnregisterDumpsysFunction(const void* token) {  // OBSOLETE
+void Dumpsys::impl::UnregisterDumpsysFunction(const void* token) {
   ASSERT(dumpsys_functions_.find(token) != dumpsys_functions_.end());
   dumpsys_functions_.erase(token);
 }
 
-void Dumpsys::Dump(int fd, const char** args) {
+void Dumpsys::Dump(int fd) {
   std::promise<void> promise;
   auto future = promise.get_future();
-  CallOn(pimpl_.get(), &Dumpsys::impl::DumpWithArgs, fd, args, std::move(promise));
+  GetHandler()->Post(common::BindOnce(&Dumpsys::impl::Dump, common::Unretained(pimpl_.get()), fd, std::move(promise)));
   future.get();
 }
 
-void Dumpsys::RegisterDumpsysFunction(const void* token, DumpsysFunction func) {  // OBSOLETE
+void Dumpsys::RegisterDumpsysFunction(const void* token, DumpsysFunction func) {
   GetHandler()->Post(
       common::BindOnce(&Dumpsys::impl::RegisterDumpsysFunction, common::Unretained(pimpl_.get()), token, func));
 }
 
-void Dumpsys::UnregisterDumpsysFunction(const void* token) {  // OBSOLETE
+void Dumpsys::UnregisterDumpsysFunction(const void* token) {
   GetHandler()->Post(
       common::BindOnce(&Dumpsys::impl::UnregisterDumpsysFunction, common::Unretained(pimpl_.get()), token));
 }
@@ -168,7 +93,7 @@ os::Handler* Dumpsys::GetGdShimHandler() {
 void Dumpsys::ListDependencies(ModuleList* list) {}
 
 void Dumpsys::Start() {
-  pimpl_ = std::make_unique<impl>(*this);
+  pimpl_ = std::make_unique<impl>();
 }
 
 void Dumpsys::Stop() {
