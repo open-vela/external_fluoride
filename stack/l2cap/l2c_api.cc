721ee9b2192de183ed9318dc2015c69deebec501
openvela-robot <openvela-robot@xiaomi.com>
2020-06-25 00:40:42 +0800

    Merge "Implement a shim hack to get the ACL handle" am: 873f4ef8cd am: 5115443693 am: bb64258d62 am: 5e902b6e60
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1348965
    
    Change-Id: I1ad2d0978ae1f5e5e170b429e9908264f5dba8be

--- stack/l2cap/l2c_api.cc
+++ stack/l2cap/l2c_api.cc
@@ -992,15 +992,16 @@ bool L2CA_DisconnectRsp(uint16_t cid) {
   return (true);
 }
 
-bool L2CA_GetRemoteCid(uint16_t lcid, uint16_t* rcid) {
+bool L2CA_GetIdentifiers(uint16_t lcid, uint16_t* rcid, uint16_t* handle) {
   if (bluetooth::shim::is_gd_shim_enabled()) {
-    return bluetooth::shim::L2CA_GetRemoteCid(lcid, rcid);
+    return bluetooth::shim::L2CA_GetIdentifiers(lcid, rcid, handle);
   }
 
   tL2C_CCB* control_block = l2cu_find_ccb_by_cid(NULL, lcid);
   if (!control_block) return false;
 
   if (rcid) *rcid = control_block->remote_cid;
+  if (handle) *handle = control_block->p_lcb->handle;
 
   return true;
 }
