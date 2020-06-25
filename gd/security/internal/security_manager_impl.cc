022ccdf4d53fcf95d8a0b08cd873fc4da78b569f
openvela-robot <openvela-robot@xiaomi.com>
2020-06-26 01:02:16 +0800

    [automerger skipped] Merge "HF_CLIENT: Upgrade version from 1.6 to 1.7" into rvc-dev am: ab6c52b3b5 -s ours
    am skip reason: skipped by user sgaurav
    
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/bt/+/11988411
    
    Change-Id: I44a1fa2b6fcb20a373101283682facf34587af2b

--- gd/security/internal/security_manager_impl.cc
+++ gd/security/internal/security_manager_impl.cc
@@ -313,16 +313,10 @@ void SecurityManagerImpl::OnPairingHandlerComplete(hci::Address address, Pairing
     pairing_handler_map_.erase(entry);
     security_manager_channel_->Release(address);
   }
-  auto remote = hci::AddressWithType(address, hci::AddressType::PUBLIC_DEVICE_ADDRESS);
-  auto cb_entry = enforce_security_policy_callback_map_.find(remote);
-  if (cb_entry != enforce_security_policy_callback_map_.end()) {
-    this->InternalEnforceSecurityPolicy(remote, cb_entry->second.first, std::move(cb_entry->second.second), false);
-    enforce_security_policy_callback_map_.erase(cb_entry);
-  }
   if (!std::holds_alternative<PairingFailure>(status)) {
-    NotifyDeviceBonded(remote);
+    NotifyDeviceBonded(hci::AddressWithType(address, hci::AddressType::PUBLIC_DEVICE_ADDRESS));
   } else {
-    NotifyDeviceBondFailed(remote, status);
+    NotifyDeviceBondFailed(hci::AddressWithType(address, hci::AddressType::PUBLIC_DEVICE_ADDRESS), status);
   }
 }
 
@@ -571,11 +565,9 @@ void SecurityManagerImpl::SetOobDataPresent(hci::OobDataPresent data_present) {
   this->local_oob_data_present_ = data_present;
 }
 
-void SecurityManagerImpl::InternalEnforceSecurityPolicy(
-    hci::AddressWithType remote,
-    l2cap::classic::SecurityPolicy policy,
-    l2cap::classic::SecurityEnforcementInterface::ResultCallback result_callback,
-    bool try_meet_requirements) {
+void SecurityManagerImpl::EnforceSecurityPolicy(
+    hci::AddressWithType remote, l2cap::classic::SecurityPolicy policy,
+    l2cap::classic::SecurityEnforcementInterface::ResultCallback result_callback) {
   bool result = false;
   auto record = this->security_database_.FindOrCreate(remote);
   switch (policy) {
@@ -590,27 +582,10 @@ void SecurityManagerImpl::InternalEnforceSecurityPolicy(
       result = true;
       break;
   }
-  if (!result && try_meet_requirements) {
-    auto entry = enforce_security_policy_callback_map_.find(remote);
-    if (entry != enforce_security_policy_callback_map_.end()) {
-      LOG_WARN("Callback already pending for remote: '%s' !", remote.ToString().c_str());
-    } else {
-      enforce_security_policy_callback_map_.emplace(
-          remote,
-          std::pair<l2cap::classic::SecurityPolicy, l2cap::classic::SecurityEnforcementInterface::ResultCallback>(
-              policy, std::move(result_callback)));
-      DispatchPairingHandler(record, true);
-    }
-    return;
+  if (!result) {
+    // TODO(optedoblivion): Start pairing process to meet requirements
   }
   result_callback.Invoke(result);
-}
-
-void SecurityManagerImpl::EnforceSecurityPolicy(
-    hci::AddressWithType remote,
-    l2cap::classic::SecurityPolicy policy,
-    l2cap::classic::SecurityEnforcementInterface::ResultCallback result_callback) {
-  this->InternalEnforceSecurityPolicy(remote, policy, std::move(result_callback), true);
 }
 
 void SecurityManagerImpl::EnforceLeSecurityPolicy(
