893a32101990291f379199aaa4b697f9091ab238
openvela-robot <openvela-robot@xiaomi.com>
2020-06-26 01:13:37 +0800

    [automerger skipped] Merge "HF_CLIENT: Upgrade version from 1.6 to 1.7" into rvc-dev am: ab6c52b3b5 am: 4b90bcf35a -s ours am: 37741f9d56
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/bt/+/11988411
    
    Change-Id: I8946eea691549bccb2ddde04533e2f9572c0cb9e

--- gd/shim/dumpsys.cc
+++ gd/shim/dumpsys.cc
@@ -15,6 +15,8 @@
  */
 #define LOG_TAG "bt_gd_shim"
 
+#include "shim/dumpsys.h"
+
 #include <algorithm>
 #include <functional>
 #include <future>
@@ -23,62 +25,136 @@
 #include <unordered_map>
 #include <utility>
 
+#include "bundler_generated.h"
+#include "flatbuffers/idl.h"
+#include "flatbuffers/reflection_generated.h"
 #include "module.h"
 #include "os/handler.h"
 #include "os/log.h"
-#include "shim/dumpsys.h"
 
 namespace bluetooth {
 namespace shim {
 
 namespace {
 constexpr char kModuleName[] = "shim::Dumpsys";
+constexpr char kDumpsysTitle[] = "----- Gd Dumpsys ------";
 }  // namespace
+
+constexpr char kArgumentDeveloper[] = "--dev";
+
+class ParsedDumpsysArgs {
+ public:
+  ParsedDumpsysArgs(const char** args) {
+    if (args == nullptr) return;
+    const char* p = *args;
+    while (p != nullptr) {
+      num_args_++;
+      if (!strcmp(p, kArgumentDeveloper)) {
+        dev_arg_ = true;
+      } else {
+        // silently ignore unexpected option
+      }
+      if (++args == nullptr) break;
+      p = *args;
+    }
+  }
+  bool IsDeveloper() const {
+    return dev_arg_;
+  }
+
+ private:
+  unsigned num_args_{0};
+  bool dev_arg_{false};
+};
 
 struct Dumpsys::impl {
  public:
-  void Dump(int fd, std::promise<void> promise);
-  void RegisterDumpsysFunction(const void* token, DumpsysFunction func);
-  void UnregisterDumpsysFunction(const void* token);
+  void DumpWithArgs(int fd, const char** args, std::promise<void> promise);
 
+  void RegisterDumpsysFunction(const void* token, DumpsysFunction func);  // OBSOLETE
+  void UnregisterDumpsysFunction(const void* token);                      // OBSOLETE
+
+  impl(const Dumpsys& dumpsys_module);
   ~impl() = default;
 
+ protected:
+  void FilterAsUser(std::string* output);
+  void FilterAsDeveloper(std::string* output);
+  std::string PrintAsJson(std::string* output) const;
+
  private:
-  std::unordered_map<const void*, DumpsysFunction> dumpsys_functions_;
+  std::unordered_map<const void*, DumpsysFunction> dumpsys_functions_;  // OBSOLETE
+  const reflection::Schema* FindBundledSchema(
+      const dumpsys::BundleSchema& bundle_schema, const std::string& name) const;
+  const Dumpsys& dumpsys_module_;
 };
 
 const ModuleFactory Dumpsys::Factory = ModuleFactory([]() { return new Dumpsys(); });
 
-void Dumpsys::impl::Dump(int fd, std::promise<void> promise) {
-  dprintf(fd, "%s Registered submodules:%zd\n", kModuleName, dumpsys_functions_.size());
-  std::for_each(dumpsys_functions_.begin(), dumpsys_functions_.end(),
-                [fd](std::pair<const void*, DumpsysFunction> element) { element.second(fd); });
+Dumpsys::impl::impl(const Dumpsys& dumpsys_module) : dumpsys_module_(dumpsys_module) {}
+
+void Dumpsys::impl::FilterAsDeveloper(std::string* output) {
+  ASSERT(output != nullptr);
+  LOG_INFO("%s UNIMPLEMENTED", __func__);
+}
+
+void Dumpsys::impl::FilterAsUser(std::string* output) {
+  ASSERT(output != nullptr);
+  LOG_INFO("%s UNIMPLEMENTED", __func__);
+}
+
+const reflection::Schema* Dumpsys::impl::FindBundledSchema(
+    const dumpsys::BundleSchema& bundle_schema, const std::string& name) const {
+  // TODO(cmanton) Return proper schema given schema container and name to index
+  return nullptr;
+}
+
+std::string Dumpsys::impl::PrintAsJson(std::string* output) const {
+  return std::string("UNIMPLEMENTED");
+}
+
+void Dumpsys::impl::DumpWithArgs(int fd, const char** args, std::promise<void> promise) {
+  ParsedDumpsysArgs parsed_dumpsys_args(args);
+  const auto registry = dumpsys_module_.GetModuleRegistry();
+
+  ModuleDumper dumper(*registry, kDumpsysTitle);
+  std::string output;
+  // Get the dumpstate into out string
+  dumper.DumpState(&output);
+
+  if (parsed_dumpsys_args.IsDeveloper()) {
+    FilterAsDeveloper(&output);
+  } else {
+    FilterAsUser(&output);
+  }
+
+  dprintf(fd, "%s", PrintAsJson(&output).c_str());
   promise.set_value();
 }
 
-void Dumpsys::impl::RegisterDumpsysFunction(const void* token, DumpsysFunction func) {
+void Dumpsys::impl::RegisterDumpsysFunction(const void* token, DumpsysFunction func) {  // OBSOLETE
   ASSERT(dumpsys_functions_.find(token) == dumpsys_functions_.end());
   dumpsys_functions_[token] = func;
 }
 
-void Dumpsys::impl::UnregisterDumpsysFunction(const void* token) {
+void Dumpsys::impl::UnregisterDumpsysFunction(const void* token) {  // OBSOLETE
   ASSERT(dumpsys_functions_.find(token) != dumpsys_functions_.end());
   dumpsys_functions_.erase(token);
 }
 
-void Dumpsys::Dump(int fd) {
+void Dumpsys::Dump(int fd, const char** args) {
   std::promise<void> promise;
   auto future = promise.get_future();
-  GetHandler()->Post(common::BindOnce(&Dumpsys::impl::Dump, common::Unretained(pimpl_.get()), fd, std::move(promise)));
+  CallOn(pimpl_.get(), &Dumpsys::impl::DumpWithArgs, fd, args, std::move(promise));
   future.get();
 }
 
-void Dumpsys::RegisterDumpsysFunction(const void* token, DumpsysFunction func) {
+void Dumpsys::RegisterDumpsysFunction(const void* token, DumpsysFunction func) {  // OBSOLETE
   GetHandler()->Post(
       common::BindOnce(&Dumpsys::impl::RegisterDumpsysFunction, common::Unretained(pimpl_.get()), token, func));
 }
 
-void Dumpsys::UnregisterDumpsysFunction(const void* token) {
+void Dumpsys::UnregisterDumpsysFunction(const void* token) {  // OBSOLETE
   GetHandler()->Post(
       common::BindOnce(&Dumpsys::impl::UnregisterDumpsysFunction, common::Unretained(pimpl_.get()), token));
 }
@@ -93,7 +169,7 @@ os::Handler* Dumpsys::GetGdShimHandler() {
 void Dumpsys::ListDependencies(ModuleList* list) {}
 
 void Dumpsys::Start() {
-  pimpl_ = std::make_unique<impl>();
+  pimpl_ = std::make_unique<impl>(*this);
 }
 
 void Dumpsys::Stop() {
