9139f827f0911771a266b899fe1266320c75c984
openvela-robot <openvela-robot@xiaomi.com>
2020-07-01 22:33:50 +0800

    Merge "gd HCI: Remove device from LE connect list when unbond" am: f6d3de1f4d am: dfcfff3751 am: 05c1404e63
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1349182
    
    Change-Id: Ifbe76ae51738eb4ab20f64b5e87fbf5ef04db2e0

--- stack/test/a2dp/misc_fake.cc
+++ stack/test/a2dp/misc_fake.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 The Android Open Source Project
+ * Copyright 2020 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,19 +14,15 @@
  * limitations under the License.
  */
 
-/**
- * Gd shim layer to legacy hci layer stack
- */
-#pragma once
+#include "service/common/bluetooth/a2dp_codec_config.h"
+#include "stack/include/a2dp_vendor_ldac.h"
 
-#include "hci/include/hci_layer.h"
+bluetooth::A2dpCodecConfig* bta_av_get_a2dp_current_codec(void) {
+  return nullptr;
+}
 
-static const char GD_HCI_MODULE[] = "gd_hci_module";
-
-namespace bluetooth {
-namespace shim {
-
-const hci_t* hci_layer_get_interface();
-
-}  // namespace shim
-}  // namespace bluetooth
+int A2DP_VendorGetTrackSampleRateLdac(const uint8_t* p_codec_info) { return 0; }
+int A2DP_VendorGetTrackBitsPerSampleLdac(const uint8_t* p_codec_info) {
+  return 0;
+}
+int A2DP_VendorGetChannelModeCodeLdac(const uint8_t* p_codec_info) { return 0; }
