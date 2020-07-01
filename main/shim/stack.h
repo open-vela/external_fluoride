d7a5a036ee260df726e0ac92892fc4306bd81ce3
openvela-robot <openvela-robot@xiaomi.com>
2020-07-01 22:49:53 +0800

    Merge "gd HCI: Remove device from LE connect list when unbond" am: f6d3de1f4d am: dfcfff3751 am: 05c1404e63 am: b351e50727
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1349182
    
    Change-Id: I0a03fc02ee84186cee383d129f711eb6c74a6d28

--- main/shim/stack.h
+++ main/shim/stack.h
@@ -16,11 +16,8 @@
 
 #pragma once
 
-#include <mutex>
-
 #include "main/shim/btm.h"
 
-#include "gd/module.h"
 #include "gd/os/handler.h"
 #include "gd/os/thread.h"
 #include "gd/os/utils.h"
@@ -30,7 +27,6 @@
 namespace bluetooth {
 namespace shim {
 
-// GD shim stack, having modes corresponding to legacy stack
 class Stack {
  public:
   static Stack* GetInstance();
@@ -38,11 +34,7 @@ class Stack {
   Stack() = default;
   ~Stack() = default;
 
-  // Idle mode, config is loaded, but controller is not enabled
-  void StartIdleMode();
-  // Running mode, everything is up
-  void StartEverything();
-
+  void Start();
   void Stop();
   bool IsRunning();
 
@@ -53,14 +45,11 @@ class Stack {
   DISALLOW_COPY_AND_ASSIGN(Stack);
 
  private:
-  std::mutex mutex_;
   StackManager stack_manager_;
   bool is_running_ = false;
   os::Thread* stack_thread_ = nullptr;
   os::Handler* stack_handler_ = nullptr;
   Btm* btm_ = nullptr;
-
-  void Start(ModuleList* modules);
 };
 
 }  // namespace shim
