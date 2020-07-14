b4d41b44554691931c8c6d4641b8a5ba8280a100
openvela-robot <openvela-robot@xiaomi.com>
2020-07-14 23:08:12 +0800

    Snap for 6680110 from 510308441dc22b9eafc453a1398c203404f0a3aa to rvc-qpr1-release
    Change-Id: I9b3b06e08c59dde3efc45550c18783297519fb44

--- gd/l2cap/classic/internal/dynamic_channel_service_impl.h
+++ gd/l2cap/classic/internal/dynamic_channel_service_impl.h
@@ -22,7 +22,7 @@
 #include "l2cap/classic/dynamic_channel_configuration_option.h"
 #include "l2cap/classic/dynamic_channel_manager.h"
 #include "l2cap/classic/dynamic_channel_service.h"
-#include "l2cap/classic/security_policy.h"
+#include "l2cap/security_policy.h"
 
 namespace bluetooth {
 namespace l2cap {
@@ -34,21 +34,21 @@ class DynamicChannelServiceImpl {
 
   struct PendingRegistration {
     os::Handler* user_handler_ = nullptr;
-    classic::SecurityPolicy security_policy_;
+    SecurityPolicy security_policy_;
     DynamicChannelManager::OnRegistrationCompleteCallback on_registration_complete_callback_;
     DynamicChannelManager::OnConnectionOpenCallback on_connection_open_callback_;
     DynamicChannelConfigurationOption configuration_;
   };
 
   virtual void NotifyChannelCreation(std::unique_ptr<DynamicChannel> channel) {
-    on_connection_open_callback_.Invoke(std::move(channel));
+    user_handler_->Post(common::BindOnce(on_connection_open_callback_, std::move(channel)));
   }
 
-  virtual DynamicChannelConfigurationOption GetConfigOption() const {
+  DynamicChannelConfigurationOption GetConfigOption() const {
     return config_option_;
   }
 
-  virtual SecurityPolicy GetSecurityPolicy() const {
+  SecurityPolicy GetSecurityPolicy() const {
     return security_policy_;
   }
 
@@ -56,16 +56,16 @@ class DynamicChannelServiceImpl {
 
  protected:
   // protected access for mocking
-  DynamicChannelServiceImpl(
-      classic::SecurityPolicy security_policy,
-      DynamicChannelManager::OnConnectionOpenCallback on_connection_open_callback,
-      DynamicChannelConfigurationOption config_option)
-      : security_policy_(security_policy),
-        on_connection_open_callback_(std::move(on_connection_open_callback)),
+  DynamicChannelServiceImpl(os::Handler* user_handler,
+                            SecurityPolicy security_policy,
+                            DynamicChannelManager::OnConnectionOpenCallback on_connection_open_callback,
+                            DynamicChannelConfigurationOption config_option)
+      : user_handler_(user_handler), security_policy_(security_policy), on_connection_open_callback_(std::move(on_connection_open_callback)),
         config_option_(config_option) {}
 
  private:
-  classic::SecurityPolicy security_policy_;
+  os::Handler* user_handler_ = nullptr;
+  SecurityPolicy security_policy_;
   DynamicChannelManager::OnConnectionOpenCallback on_connection_open_callback_;
   DynamicChannelConfigurationOption config_option_;
 };
--- gd/shim/dumpsys.h
+++ gd/shim/dumpsys.h
@@ -23,14 +23,18 @@
 namespace bluetooth {
 namespace shim {
 
+using DumpsysFunction = std::function<void(int fd)>;
+
 class Dumpsys : public bluetooth::Module {
  public:
-  void Dump(int fd, const char** args);
+  void Dump(int fd);
+  void RegisterDumpsysFunction(const void* token, DumpsysFunction func);
+  void UnregisterDumpsysFunction(const void* token);
 
-  // Convenience thread used by shim layer for task execution
+  /* This is not a dumpsys-specific method, we just must grab thread from of one modules */
   os::Handler* GetGdShimHandler();
 
-  Dumpsys(const std::string& pre_bundled_schema);
+  Dumpsys() = default;
   ~Dumpsys() = default;
 
   static const ModuleFactory Factory;
@@ -40,12 +44,10 @@ class Dumpsys : public bluetooth::Module {
   void Start() override;                             // Module
   void Stop() override;                              // Module
   std::string ToString() const override;             // Module
-  DumpsysDataFinisher GetDumpsysData(flatbuffers::FlatBufferBuilder* builder) const override;  // Module
 
  private:
   struct impl;
   std::unique_ptr<impl> pimpl_;
-  const std::string& pre_bundled_schema_;
   DISALLOW_COPY_AND_ASSIGN(Dumpsys);
 };
 
