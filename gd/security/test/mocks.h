cfd6489f206b73a773bf57a1bec25b62e1c95794
openvela-robot <openvela-robot@xiaomi.com>
2020-07-14 17:53:06 +0800

    Make libbluetooth-types-header available for native_bridge am: a6d57793a6 am: 7fb5ae1827
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/bt/+/12128049
    
    Change-Id: I01d4987b7e67106473630462a07e2a96fe27451b

--- gd/security/test/mocks.h
+++ gd/security/test/mocks.h
@@ -45,10 +45,12 @@ class UIMock : public UI {
 
 class LeSecurityInterfaceMock : public hci::LeSecurityInterface {
  public:
-  MOCK_METHOD2(EnqueueCommand, void(std::unique_ptr<hci::LeSecurityCommandBuilder> command,
-                                    common::ContextualOnceCallback<void(hci::CommandCompleteView)> on_complete));
-  MOCK_METHOD2(EnqueueCommand, void(std::unique_ptr<hci::LeSecurityCommandBuilder> command,
-                                    common::ContextualOnceCallback<void(hci::CommandStatusView)> on_status));
+  MOCK_METHOD3(EnqueueCommand,
+               void(std::unique_ptr<hci::LeSecurityCommandBuilder> command,
+                    common::OnceCallback<void(hci::CommandCompleteView)> on_complete, os::Handler* handler));
+  MOCK_METHOD3(EnqueueCommand,
+               void(std::unique_ptr<hci::LeSecurityCommandBuilder> command,
+                    common::OnceCallback<void(hci::CommandStatusView)> on_status, os::Handler* handler));
 };
 
 }  // namespace security
