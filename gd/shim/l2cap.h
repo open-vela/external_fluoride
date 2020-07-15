dba5eb922fc0a40d27a782e4f2576c12ba4c1796
openvela-robot <openvela-robot@xiaomi.com>
2020-07-15 17:40:22 +0800

    SMP: Remove never used API am: e7ed558930 am: 91d1a2a058 am: a47175ee07
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1361821
    
    Change-Id: I9f030967cb4035e25ef152a3fd1f2d30dc2b53f0

--- gd/shim/l2cap.h
+++ gd/shim/l2cap.h
@@ -37,20 +37,20 @@ using CreateConnectionPromise = std::promise<uint16_t>;
 
 class L2cap : public bluetooth::Module {
  public:
-  void RegisterClassicService(
+  void RegisterService(
       uint16_t psm,
       bool use_ertm,
       uint16_t mtu,
       ConnectionCompleteCallback on_complete,
       RegisterServicePromise register_promise);
-  void UnregisterClassicService(uint16_t psm, UnregisterServicePromise unregister_promise);
+  void UnregisterService(uint16_t psm, UnregisterServicePromise unregister_promise);
 
-  void CreateClassicConnection(
+  void CreateConnection(
       uint16_t psm,
       const std::string address_string,
       ConnectionCompleteCallback on_complete,
       CreateConnectionPromise create_promise);
-  void CloseClassicConnection(uint16_t cid);
+  void CloseConnection(uint16_t cid);
 
   void SetReadDataReadyCallback(uint16_t cid, ReadDataReadyCallback on_data_ready);
   void SetConnectionClosedCallback(uint16_t cid, ConnectionClosedCallback on_closed);
