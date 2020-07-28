a25d8a46222aba9e218748855d12a56a0fe8a3e2
liujinye <liujinye@xiaomi.com>
2020-07-28 18:28:48 +0800

    Merge "gd HCI: Fix flaky test invoke_registered_callback_le_queue_disconnect"

--- gd/hci/hci_packets_test.cc
+++ gd/hci/hci_packets_test.cc
@@ -603,8 +603,7 @@ TEST(HciPacketsTest, testLeSetAdvertisingDataBuilderLength) {
   packet_bytes->reserve(builder->size());
   BitInserter bit_inserter(*packet_bytes);
   builder->Serialize(bit_inserter);
-  auto command_view =
-      LeAdvertisingCommandView::Create(CommandPacketView::Create(PacketView<kLittleEndian>(packet_bytes)));
+  auto command_view = LeAdvertisingCommandView::Create(CommandPacketView::Create(packet_bytes));
   ASSERT_TRUE(command_view.IsValid());
   ASSERT_EQ(1 /* data_length */ + 31 /* data */, command_view.GetPayload().size());
   auto view = LeSetAdvertisingDataView::Create(command_view);
@@ -622,8 +621,7 @@ TEST(HciPacketsTest, testLeSetScanResponseDataBuilderLength) {
   packet_bytes->reserve(builder->size());
   BitInserter bit_inserter(*packet_bytes);
   builder->Serialize(bit_inserter);
-  auto command_view =
-      LeAdvertisingCommandView::Create(CommandPacketView::Create(PacketView<kLittleEndian>(packet_bytes)));
+  auto command_view = LeAdvertisingCommandView::Create(CommandPacketView::Create(packet_bytes));
   ASSERT_TRUE(command_view.IsValid());
   ASSERT_EQ(1 /* data_length */ + 31 /* data */, command_view.GetPayload().size());
   auto view = LeSetScanResponseDataView::Create(command_view);
@@ -642,8 +640,8 @@ TEST(HciPacketsTest, testLeMultiAdvSetAdvertisingDataBuilderLength) {
   packet_bytes->reserve(builder->size());
   BitInserter bit_inserter(*packet_bytes);
   builder->Serialize(bit_inserter);
-  auto command_view = LeMultiAdvtView::Create(
-      LeAdvertisingCommandView::Create(CommandPacketView::Create(PacketView<kLittleEndian>(packet_bytes))));
+  auto command_view =
+      LeMultiAdvtView::Create(LeAdvertisingCommandView::Create(CommandPacketView::Create(packet_bytes)));
   ASSERT_TRUE(command_view.IsValid());
   EXPECT_EQ(1 /* data_length */ + 31 /* data */ + 1 /* set */, command_view.GetPayload().size());
   auto view = LeMultiAdvtSetDataView::Create(command_view);
@@ -662,8 +660,8 @@ TEST(HciPacketsTest, testLeMultiAdvSetScanResponseDataBuilderLength) {
   packet_bytes->reserve(builder->size());
   BitInserter bit_inserter(*packet_bytes);
   builder->Serialize(bit_inserter);
-  auto command_view = LeMultiAdvtView::Create(
-      LeAdvertisingCommandView::Create(CommandPacketView::Create(PacketView<kLittleEndian>(packet_bytes))));
+  auto command_view =
+      LeMultiAdvtView::Create(LeAdvertisingCommandView::Create(CommandPacketView::Create(packet_bytes)));
   ASSERT_TRUE(command_view.IsValid());
   ASSERT_EQ(1 /* data_length */ + 31 /* data */ + 1 /* set */, command_view.GetPayload().size());
   auto view = LeMultiAdvtSetScanRespView::Create(command_view);
