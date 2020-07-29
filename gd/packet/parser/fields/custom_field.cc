8e04beaf8dde7606582295fe5531199364b99ac7
liujinye <liujinye@xiaomi.com>
2020-07-30 00:06:57 +0800

    Shows a consent dialog on the local device when pairing a bluetooth low am: 03580fc440
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/bt/+/12010399
    
    Change-Id: I4491e2a0bda3765831ba363f7b98cdfaea2938a1

--- gd/packet/parser/fields/custom_field.cc
+++ gd/packet/parser/fields/custom_field.cc
@@ -91,11 +91,3 @@ void CustomField::GenInserter(std::ostream& s) const {
 void CustomField::GenValidator(std::ostream&) const {
   // Do nothing.
 }
-
-void CustomField::GenStringRepresentation(std::ostream& s, std::string accessor) const {
-  s << accessor << "->ToString()";
-}
-
-void CustomField::GenBuilderParameterFromView(std::ostream& s) const {
-  s << "*view.Get" << util::UnderscoreToCamelCase(GetName()) << "()";
-}
