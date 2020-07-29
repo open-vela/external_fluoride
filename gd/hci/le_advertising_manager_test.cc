5ef6dc9647a3213ba28b3b42b46deb8720e5225f
liujinye <liujinye@xiaomi.com>
2020-07-30 00:30:01 +0800

    [automerger skipped] Shows a consent dialog on the local device when pairing a bluetooth low am: b6bcc43c7b -s ours am: 5cf5a4c43b -s ours
    am skip reason: Change-Id I7de396230beb84bd0fa2b0cea346523b6824472a with SHA-1 fe287947a1 is in history
    
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/bt/+/12040224
    
    Change-Id: If552b7dc90a1a4cb003bea09d6d67587eeec47b3

--- gd/hci/le_advertising_manager_test.cc
+++ gd/hci/le_advertising_manager_test.cc
@@ -25,7 +25,6 @@
 #include <gtest/gtest.h>
 
 #include "common/bind.h"
-#include "hci/acl_manager.h"
 #include "hci/address.h"
 #include "hci/controller.h"
 #include "hci/hci_layer.h"
@@ -75,10 +74,17 @@ class TestController : public Controller {
 
 class TestHciLayer : public HciLayer {
  public:
+  TestHciLayer() {
+    RegisterEventHandler(EventCode::COMMAND_COMPLETE,
+                         base::Bind(&TestHciLayer::CommandCompleteCallback, common::Unretained(this)), nullptr);
+    RegisterEventHandler(EventCode::COMMAND_STATUS,
+                         base::Bind(&TestHciLayer::CommandStatusCallback, common::Unretained(this)), nullptr);
+  }
+
   void EnqueueCommand(std::unique_ptr<CommandPacketBuilder> command,
-                      common::ContextualOnceCallback<void(CommandStatusView)> on_status) override {
+                      common::OnceCallback<void(CommandStatusView)> on_status, os::Handler* handler) override {
     auto packet_view = CommandPacketView::Create(GetPacketView(std::move(command)));
-    ASSERT_TRUE(packet_view.IsValid());
+    ASSERT(packet_view.IsValid());
     command_queue_.push_back(packet_view);
     command_status_callbacks.push_back(std::move(on_status));
     if (command_promise_ != nullptr &&
@@ -92,16 +98,16 @@ class TestHciLayer : public HciLayer {
   }
 
   void EnqueueCommand(std::unique_ptr<CommandPacketBuilder> command,
-                      common::ContextualOnceCallback<void(CommandCompleteView)> on_complete) override {
+                      common::OnceCallback<void(CommandCompleteView)> on_complete, os::Handler* handler) override {
     auto packet_view = CommandPacketView::Create(GetPacketView(std::move(command)));
-    ASSERT_TRUE(packet_view.IsValid());
+    ASSERT(packet_view.IsValid());
     command_queue_.push_back(packet_view);
     command_complete_callbacks.push_back(std::move(on_complete));
     if (command_promise_ != nullptr &&
         (command_op_code_ == OpCode::NONE || command_op_code_ == packet_view.GetOpCode())) {
       if (command_op_code_ == OpCode::LE_MULTI_ADVT) {
         auto sub_view = LeMultiAdvtView::Create(LeAdvertisingCommandView::Create(packet_view));
-        ASSERT_TRUE(sub_view.IsValid());
+        ASSERT(sub_view.IsValid());
         if (sub_view.GetSubCmd() != command_sub_ocf_) {
           return;
         }
@@ -127,26 +133,23 @@ class TestHciLayer : public HciLayer {
   }
 
   ConnectionManagementCommandView GetCommandPacket(OpCode op_code) {
-    if (command_queue_.empty()) {
-      return ConnectionManagementCommandView::Create(
-          CommandPacketView::Create(std::make_shared<std::vector<uint8_t>>()));
-    }
+    ASSERT(!command_queue_.empty());
     CommandPacketView command_packet_view = CommandPacketView::Create(command_queue_.front());
     command_queue_.pop_front();
     ConnectionManagementCommandView command = ConnectionManagementCommandView::Create(command_packet_view);
-    EXPECT_TRUE(command.IsValid());
+    ASSERT(command.IsValid());
     EXPECT_EQ(command.GetOpCode(), op_code);
 
     return command;
   }
 
-  void RegisterEventHandler(EventCode event_code,
-                            common::ContextualCallback<void(EventPacketView)> event_handler) override {
+  void RegisterEventHandler(EventCode event_code, common::Callback<void(EventPacketView)> event_handler,
+                            os::Handler* handler) override {
     registered_events_[event_code] = event_handler;
   }
 
-  void RegisterLeEventHandler(SubeventCode subevent_code,
-                              common::ContextualCallback<void(LeMetaEventView)> event_handler) override {
+  void RegisterLeEventHandler(SubeventCode subevent_code, common::Callback<void(LeMetaEventView)> event_handler,
+                              os::Handler* handler) override {
     registered_le_events_[subevent_code] = event_handler;
   }
 
@@ -155,8 +158,8 @@ class TestHciLayer : public HciLayer {
     EventPacketView event = EventPacketView::Create(packet);
     ASSERT_TRUE(event.IsValid());
     EventCode event_code = event.GetEventCode();
-    ASSERT_NE(registered_events_.find(event_code), registered_events_.end()) << EventCodeText(event_code);
-    registered_events_[event_code].Invoke(event);
+    ASSERT_TRUE(registered_events_.find(event_code) != registered_events_.end()) << EventCodeText(event_code);
+    registered_events_[event_code].Run(event);
   }
 
   void IncomingLeMetaEvent(std::unique_ptr<LeMetaEventBuilder> event_builder) {
@@ -165,38 +168,34 @@ class TestHciLayer : public HciLayer {
     LeMetaEventView meta_event_view = LeMetaEventView::Create(event);
     ASSERT_TRUE(meta_event_view.IsValid());
     SubeventCode subevent_code = meta_event_view.GetSubeventCode();
-    ASSERT_NE(registered_le_events_.find(subevent_code), registered_le_events_.end())
+    ASSERT_TRUE(registered_le_events_.find(subevent_code) != registered_le_events_.end())
         << SubeventCodeText(subevent_code);
-    registered_le_events_[subevent_code].Invoke(meta_event_view);
+    registered_le_events_[subevent_code].Run(meta_event_view);
   }
 
   void CommandCompleteCallback(EventPacketView event) {
     CommandCompleteView complete_view = CommandCompleteView::Create(event);
-    ASSERT_TRUE(complete_view.IsValid());
-    std::move(command_complete_callbacks.front()).Invoke(complete_view);
+    ASSERT(complete_view.IsValid());
+    std::move(command_complete_callbacks.front()).Run(complete_view);
     command_complete_callbacks.pop_front();
   }
 
   void CommandStatusCallback(EventPacketView event) {
     CommandStatusView status_view = CommandStatusView::Create(event);
-    ASSERT_TRUE(status_view.IsValid());
-    std::move(command_status_callbacks.front()).Invoke(status_view);
+    ASSERT(status_view.IsValid());
+    std::move(command_status_callbacks.front()).Run(status_view);
     command_status_callbacks.pop_front();
   }
 
   void ListDependencies(ModuleList* list) override {}
-  void Start() override {
-    RegisterEventHandler(EventCode::COMMAND_COMPLETE,
-                         GetHandler()->BindOn(this, &TestHciLayer::CommandCompleteCallback));
-    RegisterEventHandler(EventCode::COMMAND_STATUS, GetHandler()->BindOn(this, &TestHciLayer::CommandStatusCallback));
-  }
+  void Start() override {}
   void Stop() override {}
 
  private:
-  std::map<EventCode, common::ContextualCallback<void(EventPacketView)>> registered_events_;
-  std::map<SubeventCode, common::ContextualCallback<void(LeMetaEventView)>> registered_le_events_;
-  std::list<common::ContextualOnceCallback<void(CommandCompleteView)>> command_complete_callbacks;
-  std::list<common::ContextualOnceCallback<void(CommandStatusView)>> command_status_callbacks;
+  std::map<EventCode, common::Callback<void(EventPacketView)>> registered_events_;
+  std::map<SubeventCode, common::Callback<void(LeMetaEventView)>> registered_le_events_;
+  std::list<base::OnceCallback<void(CommandCompleteView)>> command_complete_callbacks;
+  std::list<base::OnceCallback<void(CommandStatusView)>> command_status_callbacks;
 
   std::list<CommandPacketView> command_queue_;
   mutable std::mutex mutex_;
@@ -205,77 +204,20 @@ class TestHciLayer : public HciLayer {
   SubOcf command_sub_ocf_;
 };
 
-class TestLeAddressManager : public LeAddressManager {
- public:
-  TestLeAddressManager(
-      common::Callback<void(std::unique_ptr<CommandPacketBuilder>)> enqueue_command,
-      os::Handler* handler,
-      Address public_address,
-      uint8_t connect_list_size,
-      uint8_t resolving_list_size)
-      : LeAddressManager(enqueue_command, handler, public_address, connect_list_size, resolving_list_size) {}
-
-  AddressPolicy Register(LeAddressManagerCallback* callback) override {
-    return AddressPolicy::USE_STATIC_ADDRESS;
-  }
-
-  void Unregister(LeAddressManagerCallback* callback) override {}
-
-  AddressWithType GetAnotherAddress() override {
-    hci::Address address;
-    Address::FromString("05:04:03:02:01:00", address);
-    auto random_address = AddressWithType(address, AddressType::RANDOM_DEVICE_ADDRESS);
-    return random_address;
-  }
-};
-
-class TestAclManager : public AclManager {
- public:
-  LeAddressManager* GetLeAddressManager() override {
-    return test_le_address_manager_;
-  }
-
- protected:
-  void Start() override {
-    thread_ = new os::Thread("thread", os::Thread::Priority::NORMAL);
-    handler_ = new os::Handler(thread_);
-    Address address({0x01, 0x02, 0x03, 0x04, 0x05, 0x06});
-    test_le_address_manager_ = new TestLeAddressManager(
-        common::Bind(&TestAclManager::enqueue_command, common::Unretained(this)), handler_, address, 0x3F, 0x3F);
-  }
-
-  void Stop() override {
-    delete test_le_address_manager_;
-    handler_->Clear();
-    delete handler_;
-    delete thread_;
-  }
-
-  void ListDependencies(ModuleList* list) override {}
-
-  void SetRandomAddress(Address address) {}
-
-  void enqueue_command(std::unique_ptr<CommandPacketBuilder> command_packet){};
-
-  os::Thread* thread_;
-  os::Handler* handler_;
-  TestLeAddressManager* test_le_address_manager_;
-};
-
 class LeAdvertisingManagerTest : public ::testing::Test {
  protected:
   void SetUp() override {
     test_hci_layer_ = new TestHciLayer;  // Ownership is transferred to registry
     test_controller_ = new TestController;
-    test_acl_manager_ = new TestAclManager;
     test_controller_->AddSupported(param_opcode_);
     fake_registry_.InjectTestModule(&HciLayer::Factory, test_hci_layer_);
     fake_registry_.InjectTestModule(&Controller::Factory, test_controller_);
-    fake_registry_.InjectTestModule(&AclManager::Factory, test_acl_manager_);
     client_handler_ = fake_registry_.GetTestModuleHandler(&HciLayer::Factory);
     ASSERT_NE(client_handler_, nullptr);
     test_controller_->num_advertisers = 1;
-    le_advertising_manager_ = fake_registry_.Start<LeAdvertisingManager>(&thread_);
+    fake_registry_.Start<LeAdvertisingManager>(&thread_);
+    le_advertising_manager_ =
+        static_cast<LeAdvertisingManager*>(fake_registry_.GetModuleUnderTest(&LeAdvertisingManager::Factory));
   }
 
   void TearDown() override {
@@ -286,7 +228,6 @@ class LeAdvertisingManagerTest : public ::testing::Test {
   TestModuleRegistry fake_registry_;
   TestHciLayer* test_hci_layer_ = nullptr;
   TestController* test_controller_ = nullptr;
-  TestAclManager* test_acl_manager_ = nullptr;
   os::Thread& thread_ = fake_registry_.GetTestThread();
   LeAdvertisingManager* le_advertising_manager_ = nullptr;
   os::Handler* client_handler_ = nullptr;
@@ -354,7 +295,7 @@ TEST_F(LeExtendedAdvertisingManagerTest, startup_teardown) {}
 
 TEST_F(LeAdvertisingManagerTest, create_advertiser_test) {
   AdvertisingConfig advertising_config{};
-  advertising_config.event_type = AdvertisingType::ADV_IND;
+  advertising_config.event_type = AdvertisingEventType::ADV_IND;
   advertising_config.address_type = AddressType::PUBLIC_DEVICE_ADDRESS;
   std::vector<GapData> gap_data{};
   GapData data_item{};
@@ -372,10 +313,8 @@ TEST_F(LeAdvertisingManagerTest, create_advertiser_test) {
                                                       client_handler_);
   ASSERT_NE(LeAdvertisingManager::kInvalidId, id);
   std::vector<OpCode> adv_opcodes = {
-      OpCode::LE_SET_ADVERTISING_PARAMETERS,
-      OpCode::LE_SET_SCAN_RESPONSE_DATA,
-      OpCode::LE_SET_ADVERTISING_DATA,
-      OpCode::LE_SET_ADVERTISING_ENABLE,
+      OpCode::LE_SET_ADVERTISING_PARAMETERS, OpCode::LE_SET_RANDOM_ADDRESS,     OpCode::LE_SET_SCAN_RESPONSE_DATA,
+      OpCode::LE_SET_ADVERTISING_DATA,       OpCode::LE_SET_ADVERTISING_ENABLE,
   };
   std::vector<uint8_t> success_vector{static_cast<uint8_t>(ErrorCode::SUCCESS)};
   auto result = last_command_future.wait_for(std::chrono::duration(std::chrono::milliseconds(100)));
@@ -397,7 +336,7 @@ TEST_F(LeAdvertisingManagerTest, create_advertiser_test) {
 
 TEST_F(LeAndroidHciAdvertisingManagerTest, create_advertiser_test) {
   AdvertisingConfig advertising_config{};
-  advertising_config.event_type = AdvertisingType::ADV_IND;
+  advertising_config.event_type = AdvertisingEventType::ADV_IND;
   advertising_config.address_type = AddressType::PUBLIC_DEVICE_ADDRESS;
   std::vector<GapData> gap_data{};
   GapData data_item{};
@@ -423,7 +362,7 @@ TEST_F(LeAndroidHciAdvertisingManagerTest, create_advertiser_test) {
   for (size_t i = 0; i < sub_ocf.size(); i++) {
     auto packet = test_hci_layer_->GetCommandPacket(OpCode::LE_MULTI_ADVT);
     auto sub_packet = LeMultiAdvtView::Create(LeAdvertisingCommandView::Create(packet));
-    ASSERT_TRUE(sub_packet.IsValid());
+    ASSERT(sub_packet.IsValid());
     test_hci_layer_->IncomingEvent(LeMultiAdvtCompleteBuilder::Create(uint8_t{1}, ErrorCode::SUCCESS, sub_ocf[i]));
     num_commands -= 1;
   }
@@ -438,7 +377,7 @@ TEST_F(LeAndroidHciAdvertisingManagerTest, create_advertiser_test) {
 
 TEST_F(LeExtendedAdvertisingManagerTest, create_advertiser_test) {
   ExtendedAdvertisingConfig advertising_config{};
-  advertising_config.event_type = AdvertisingType::ADV_IND;
+  advertising_config.event_type = AdvertisingEventType::ADV_IND;
   advertising_config.address_type = AddressType::PUBLIC_DEVICE_ADDRESS;
   std::vector<GapData> gap_data{};
   GapData data_item{};
@@ -451,7 +390,6 @@ TEST_F(LeExtendedAdvertisingManagerTest, create_advertiser_test) {
   advertising_config.advertisement = gap_data;
   advertising_config.scan_response = gap_data;
   advertising_config.channel_map = 1;
-  advertising_config.sid = 0x01;
 
   auto last_command_future = test_hci_layer_->GetCommandFuture(OpCode::LE_SET_EXTENDED_ADVERTISING_ENABLE);
   auto id = le_advertising_manager_->ExtendedCreateAdvertiser(advertising_config, scan_callback,
