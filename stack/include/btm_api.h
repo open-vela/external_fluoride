647473f657e83cf41b03a91e174c9e7e07e88e41
openvela-robot <openvela-robot@xiaomi.com>
2020-07-31 02:26:40 +0800

    Merge "Adjust authentication bits to match legacy behavior" am: 9799146e11 am: 58e84d526e am: 8e24582b85
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1377218
    
    Change-Id: Ia2c966cec1a76acb27f8511f2995b4aa3b9893c5

--- stack/include/btm_api.h
+++ stack/include/btm_api.h
@@ -1205,8 +1205,9 @@ extern void BTM_PINCodeReply(const RawAddress& bd_addr, uint8_t res,
  ******************************************************************************/
 extern tBTM_STATUS BTM_SecBond(const RawAddress& bd_addr,
                                tBLE_ADDR_TYPE addr_type,
-                               tBT_TRANSPORT transport, uint8_t pin_len,
-                               uint8_t* p_pin, uint32_t trusted_mask[]);
+                               tBT_TRANSPORT transport, int device_type,
+                               uint8_t pin_len, uint8_t* p_pin,
+                               uint32_t trusted_mask[]);
 
 /*******************************************************************************
  *
@@ -1284,23 +1285,6 @@ extern void BTM_ConfirmReqReply(tBTM_STATUS res, const RawAddress& bd_addr);
  ******************************************************************************/
 extern void BTM_PasskeyReqReply(tBTM_STATUS res, const RawAddress& bd_addr,
                                 uint32_t passkey);
-
-/*******************************************************************************
- *
- * Function         BTM_SendKeypressNotif
- *
- * Description      This function is used during the passkey entry model
- *                  by a device with KeyboardOnly IO capabilities
- *                  (very likely to be a HID Device).
- *                  It is called by a HID Device to inform the remote device
- *                  when a key has been entered or erased.
- *
- * Parameters:      bd_addr - Address of the peer device
- *                  type - notification type
- *
- ******************************************************************************/
-extern void BTM_SendKeypressNotif(const RawAddress& bd_addr,
-                                  tBTM_SP_KEY_TYPE type);
 
 /*******************************************************************************
  *
@@ -1629,5 +1613,10 @@ extern uint8_t BTM_GetEirUuidList(uint8_t* p_eir, size_t eir_len,
  *
  ******************************************************************************/
 extern tBTM_CONTRL_STATE BTM_PM_ReadControllerState(void);
+
+/**
+ * Send remote name request, either to legacy HCI, or to GD shim Name module
+ */
+extern void SendRemoteNameRequest(const RawAddress& raw_address);
 
 #endif /* BTM_API_H */
