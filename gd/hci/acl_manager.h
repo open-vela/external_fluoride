8db85ba0c898e4b1b756ea0247d5354947ef4178
openvela-robot <openvela-robot@xiaomi.com>
2020-07-31 00:51:24 +0800

    Merge "btif_rc: Check AVRC attribute before casting" am: e09259558c am: abf0bc3fa5
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1377811
    
    Change-Id: Icdd4e988fa6d00d46950579bc9a40b9a91422f20

---
+++ gd/hci/acl_manager.h
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <memory>
+
+#include "common/bidi_queue.h"
+#include "common/callback.h"
+#include "hci/acl_manager/connection_callbacks.h"
+#include "hci/acl_manager/le_connection_callbacks.h"
+#include "hci/address.h"
+#include "hci/address_with_type.h"
+#include "hci/hci_layer.h"
+#include "hci/hci_packets.h"
+#include "hci/le_address_manager.h"
+#include "module.h"
+#include "os/handler.h"
+
+namespace bluetooth {
+
+namespace security {
+class SecurityModule;
+}
+namespace shim {
+class Btm;
+}
+
+namespace hci {
+
+class AclManager : public Module {
+ friend class bluetooth::shim::Btm;
+ public:
+  AclManager();
+  // NOTE: It is necessary to forward declare a default destructor that overrides the base class one, because
+  // "struct impl" is forwarded declared in .cc and compiler needs a concrete definition of "struct impl" when
+  // compiling AclManager's destructor. Hence we need to forward declare the destructor for AclManager to delay
+  // compiling AclManager's destructor until it starts linking the .cc file.
+  ~AclManager() override;
+
+  // Should register only once when user module starts.
+  // Generates OnConnectSuccess when an incoming connection is established.
+  virtual void RegisterCallbacks(acl_manager::ConnectionCallbacks* callbacks, os::Handler* handler);
+
+  // Should register only once when user module starts.
+  virtual void RegisterLeCallbacks(acl_manager::LeConnectionCallbacks* callbacks, os::Handler* handler);
+
+  // Generates OnConnectSuccess if connected, or OnConnectFail otherwise
+  virtual void CreateConnection(Address address);
+
+  // Generates OnLeConnectSuccess if connected, or OnLeConnectFail otherwise
+  virtual void CreateLeConnection(AddressWithType address_with_type);
+
+  virtual void SetPrivacyPolicyForInitiatorAddress(
+      LeAddressManager::AddressPolicy address_policy,
+      AddressWithType fixed_address,
+      crypto_toolbox::Octet16 rotation_irk,
+      std::chrono::milliseconds minimum_rotation_time,
+      std::chrono::milliseconds maximum_rotation_time);
+
+  // TODO(jpawlowski): remove once we have config file abstraction in cert tests
+  virtual void SetPrivacyPolicyForInitiatorAddressForTest(
+      LeAddressManager::AddressPolicy address_policy,
+      AddressWithType fixed_address,
+      crypto_toolbox::Octet16 rotation_irk,
+      std::chrono::milliseconds minimum_rotation_time,
+      std::chrono::milliseconds maximum_rotation_time);
+
+  // Generates OnConnectFail with error code "terminated by local host 0x16" if cancelled, or OnConnectSuccess if not
+  // successfully cancelled and already connected
+  virtual void CancelConnect(Address address);
+
+  virtual void CancelLeConnect(AddressWithType address_with_type);
+  virtual void AddDeviceToConnectList(AddressWithType address_with_type);
+  virtual void RemoveDeviceFromConnectList(AddressWithType address_with_type);
+
+  virtual void MasterLinkKey(KeyFlag key_flag);
+  virtual void SwitchRole(Address address, Role role);
+  virtual uint16_t ReadDefaultLinkPolicySettings();
+  virtual void WriteDefaultLinkPolicySettings(uint16_t default_link_policy_settings);
+
+  // In order to avoid circular dependency use setter rather than module dependency.
+  virtual void SetSecurityModule(security::SecurityModule* security_module);
+
+  virtual LeAddressManager* GetLeAddressManager();
+
+  static const ModuleFactory Factory;
+
+ protected:
+  void ListDependencies(ModuleList* list) override;
+
+  void Start() override;
+
+  void Stop() override;
+
+  std::string ToString() const override;
+
+ private:
+  virtual uint16_t HACK_GetHandle(const Address address);
+  virtual uint16_t HACK_GetLeHandle(const Address address);
+
+  struct impl;
+  std::unique_ptr<impl> pimpl_;
+
+  DISALLOW_COPY_AND_ASSIGN(AclManager);
+};
+
+}  // namespace hci
+}  // namespace bluetooth
--- stack/bnep/bnep_main.cc
+++ stack/bnep/bnep_main.cc
@@ -1,6 +1,6 @@
 /******************************************************************************
  *
- *  Copyright (C) 2001-2012 Broadcom Corporation
+ *  Copyright 2001-2012 Broadcom Corporation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -94,7 +94,8 @@ tBNEP_RESULT bnep_register_with_l2cap(void) {
   bnep_cb.reg_info.pL2CA_CongestionStatus_Cb = bnep_congestion_ind;
 
   /* Now, register with L2CAP */
-  if (!L2CA_Register(BT_PSM_BNEP, &bnep_cb.reg_info)) {
+  if (!L2CA_Register(BT_PSM_BNEP, &bnep_cb.reg_info, false /* enable_snoop */,
+                     nullptr, bnep_cb.l2cap_my_cfg.mtu)) {
     BNEP_TRACE_ERROR("BNEP - Registration failed");
     return BNEP_SECURITY_FAIL;
   }
@@ -184,7 +185,7 @@ static void bnep_connect_cfm(uint16_t l2cap_cid, uint16_t result) {
     BNEP_TRACE_WARNING("BNEP - Rcvd conn cnf with error: 0x%x  CID 0x%x",
                        result, p_bcb->l2cap_cid);
 
-    /* Tell the upper layer, if he has a callback */
+    /* Tell the upper layer, if there is a callback */
     if (bnep_cb.p_conn_state_cb && p_bcb->con_flags & BNEP_FLAGS_IS_ORIG) {
       (*bnep_cb.p_conn_state_cb)(p_bcb->handle, p_bcb->rem_bda,
                                  BNEP_CONN_FAILED, false);
@@ -255,9 +256,9 @@ static void bnep_config_ind(uint16_t l2cap_cid, tL2CAP_CFG_INFO* p_cfg) {
                        bnep_conn_timer_timeout, p_bcb);
 
     if (p_bcb->con_flags & BNEP_FLAGS_IS_ORIG) {
-      btm_sec_mx_access_request(
-          p_bcb->rem_bda, BT_PSM_BNEP, true, BTM_SEC_PROTO_BNEP,
-          bnep_get_uuid32(&(p_bcb->src_uuid)), &bnep_sec_check_complete, p_bcb);
+      btm_sec_mx_access_request(p_bcb->rem_bda, BT_PSM_BNEP, true,
+                                BTM_SEC_PROTO_BNEP, p_bcb->src_uuid.As32Bit(),
+                                &bnep_sec_check_complete, p_bcb);
     }
   }
 }
@@ -299,13 +300,12 @@ static void bnep_config_cfm(uint16_t l2cap_cid, tL2CAP_CFG_INFO* p_cfg) {
 
       if (p_bcb->con_flags & BNEP_FLAGS_IS_ORIG) {
         btm_sec_mx_access_request(p_bcb->rem_bda, BT_PSM_BNEP, true,
-                                  BTM_SEC_PROTO_BNEP,
-                                  bnep_get_uuid32(&(p_bcb->src_uuid)),
+                                  BTM_SEC_PROTO_BNEP, p_bcb->src_uuid.As32Bit(),
                                   &bnep_sec_check_complete, p_bcb);
       }
     }
   } else {
-    /* Tell the upper layer, if he has a callback */
+    /* Tell the upper layer, if there is a callback */
     if ((p_bcb->con_flags & BNEP_FLAGS_IS_ORIG) && (bnep_cb.p_conn_state_cb)) {
       (*bnep_cb.p_conn_state_cb)(p_bcb->handle, p_bcb->rem_bda,
                                  BNEP_CONN_FAILED_CFG, false);
@@ -341,7 +341,7 @@ static void bnep_disconnect_ind(uint16_t l2cap_cid, bool ack_needed) {
 
   BNEP_TRACE_EVENT("BNEP - Rcvd L2CAP disc, CID: 0x%x", l2cap_cid);
 
-  /* Tell the user if he has a callback */
+  /* Tell the user if there is a callback */
   if (p_bcb->con_state == BNEP_STATE_CONNECTED) {
     if (bnep_cb.p_conn_state_cb)
       (*bnep_cb.p_conn_state_cb)(p_bcb->handle, p_bcb->rem_bda,
@@ -623,7 +623,6 @@ static void bnep_data_ind(uint16_t l2cap_cid, BT_HDR* p_buf) {
   if (bnep_cb.p_data_buf_cb) {
     (*bnep_cb.p_data_buf_cb)(p_bcb->handle, *p_src_addr, *p_dst_addr, protocol,
                              p_buf, fw_ext_present);
-    osi_free(p_buf);
   } else if (bnep_cb.p_data_ind_cb) {
     (*bnep_cb.p_data_ind_cb)(p_bcb->handle, *p_src_addr, *p_dst_addr, protocol,
                              p, rem_len, fw_ext_present);
@@ -681,7 +680,7 @@ void bnep_conn_timer_timeout(void* data) {
 
     L2CA_DisconnectReq(p_bcb->l2cap_cid);
 
-    /* Tell the user if he has a callback */
+    /* Tell the user if there is a callback */
     if ((p_bcb->con_flags & BNEP_FLAGS_IS_ORIG) && (bnep_cb.p_conn_state_cb))
       (*bnep_cb.p_conn_state_cb)(p_bcb->handle, p_bcb->rem_bda,
                                  BNEP_CONN_FAILED, false);
@@ -695,7 +694,7 @@ void bnep_conn_timer_timeout(void* data) {
     } else {
       L2CA_DisconnectReq(p_bcb->l2cap_cid);
 
-      /* Tell the user if he has a callback */
+      /* Tell the user if there is a callback */
       if (bnep_cb.p_conn_state_cb)
         (*bnep_cb.p_conn_state_cb)(p_bcb->handle, p_bcb->rem_bda,
                                    BNEP_SET_FILTER_FAIL, false);
@@ -711,7 +710,7 @@ void bnep_conn_timer_timeout(void* data) {
     } else {
       L2CA_DisconnectReq(p_bcb->l2cap_cid);
 
-      /* Tell the user if he has a callback */
+      /* Tell the user if there is a callback */
       if (bnep_cb.p_conn_state_cb)
         (*bnep_cb.p_conn_state_cb)(p_bcb->handle, p_bcb->rem_bda,
                                    BNEP_SET_FILTER_FAIL, false);
@@ -748,7 +747,7 @@ void bnep_connected(tBNEP_CONN* p_bcb) {
   alarm_cancel(p_bcb->conn_timer);
   p_bcb->re_transmits = 0;
 
-  /* Tell the upper layer, if he has a callback */
+  /* Tell the upper layer, if there is a callback */
   if (bnep_cb.p_conn_state_cb)
     (*bnep_cb.p_conn_state_cb)(p_bcb->handle, p_bcb->rem_bda, BNEP_SUCCESS,
                                is_role_change);
