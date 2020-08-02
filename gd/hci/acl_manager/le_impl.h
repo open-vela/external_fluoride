80dd83f3734055444a764b660efe6618ea6d303b
liujinye <liujinye@xiaomi.com>
2020-08-02 17:23:38 +0800

    Merge "Add initial l2cap dumpsys capabilities" am: aa02b9da77 am: 8ec05b7f2f
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1384669
    
    Change-Id: I67baaa65ff161a402c2c907401fa0d2f8d1b0395

--- gd/hci/acl_manager/le_impl.h
+++ gd/hci/acl_manager/le_impl.h
@@ -321,7 +321,8 @@ struct le_impl : public bluetooth::hci::LeAddressManagerCallback {
 
     connecting_le_.insert(address_with_type);
 
-    if (controller_->IsSupported(OpCode::LE_EXTENDED_CREATE_CONNECTION)) {
+    // TODO: make features check nicer, like HCI_LE_EXTENDED_ADVERTISING_SUPPORTED
+    if (controller_->GetControllerLeLocalSupportedFeatures() & 0x0010) {
       LeCreateConnPhyScanParameters tmp;
       tmp.scan_interval_ = le_scan_interval;
       tmp.scan_window_ = le_scan_window;
