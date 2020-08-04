b88d7650e8ae192b491221c7f4d847a39046f325
openvela-robot <openvela-robot@xiaomi.com>
2020-08-05 03:37:05 +0800

    Merge "Remove BTM_ALLOW_CONN_IF_NONDISCOVER" am: a2532fca4a am: 01ab6b5a64
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1384666
    
    Change-Id: Ida165571fe3d0c1323ab47517477830c797917bb

--- gd/dumpsys/internal/filter_internal.cc
+++ gd/dumpsys/internal/filter_internal.cc
@@ -105,13 +105,6 @@ const reflection::Object* internal::FindReflectionObject(
   return nullptr;
 }
 
-bool internal::FilterTypeBool(const reflection::Field& field, flatbuffers::Table* table, PrivacyLevel privacy_level) {
-  ASSERT(table != nullptr);
-
-  // TODO(cmanton) Figure out boolean filtering
-  return kFieldHasBeenFiltered;
-}
-
 bool internal::FilterTypeInteger(
     const reflection::Field& field, flatbuffers::Table* table, PrivacyLevel privacy_level) {
   ASSERT(table != nullptr);
