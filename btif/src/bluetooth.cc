f5fb13fae48e0823c916456c1799b974735118bc
openvela-robot <openvela-robot@xiaomi.com>
2020-08-05 22:45:34 +0800

    [automerger skipped] Merge "Check whether local device is an ATV device to determine whether to show the consent dialog for BLE pairing in JUSTWORKS and ENCRYPTION_ONLY mode" into oc-dev am: b2adda8177 am: 00fd683aec -s ours am: 14ae20b072 -s ours am: 1f504a4d1c -s ours am: 2f1a14d58e -s ours am: 02ed5a5e79 -s ours
    am skip reason: Change-Id I6d06f5996da71e5a1407e544b0023d82924aa56f with SHA-1 0b4c1014f7 is in history
    
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/bt/+/12309020
    
    Change-Id: I365e3e41698a11d27dd023e569dc54371380e966

--- btif/src/bluetooth.cc
+++ btif/src/bluetooth.cc
@@ -67,7 +67,6 @@
 #include "common/metric_id_allocator.h"
 #include "common/metrics.h"
 #include "device/include/interop.h"
-#include "gd/common/init_flags.h"
 #include "main/shim/dumpsys.h"
 #include "main/shim/shim.h"
 #include "osi/include/alarm.h"
@@ -142,12 +141,16 @@ static bool is_profile(const char* p1, const char* p2) {
  ****************************************************************************/
 
 static int init(bt_callbacks_t* callbacks, bool start_restricted,
-                bool is_niap_mode, int config_compare_result,
-                const char** init_flags, bool is_atv) {
-  LOG_INFO("%s: start restricted = %d ; niap = %d, config compare result = %d",
+                bool is_niap_mode, int config_compare_result, bool is_atv) {
+  LOG_INFO(LOG_TAG,
+           "%s: start restricted = %d ; niap = %d, config compare result = %d",
            __func__, start_restricted, is_niap_mode, config_compare_result);
 
-  bluetooth::common::InitFlags::Load(init_flags);
+  if (bluetooth::shim::is_gd_shim_enabled()) {
+    LOG_INFO(LOG_TAG, "%s Enable Gd bluetooth functionality", __func__);
+  } else {
+    LOG_INFO(LOG_TAG, "%s Preserving legacy bluetooth functionality", __func__);
+  }
 
   if (interface_ready()) return BT_STATUS_DONE;
 
@@ -343,7 +346,7 @@ static void dump(int fd, const char** arguments) {
   connection_manager::dump(fd);
   bluetooth::bqr::DebugDump(fd);
   if (bluetooth::shim::is_gd_shim_enabled()) {
-    bluetooth::shim::Dump(fd, arguments);
+    bluetooth::shim::Dump(fd);
   } else {
 #if (BTSNOOP_MEM == TRUE)
     btif_debug_btsnoop_dump(fd);
@@ -356,7 +359,7 @@ static void dumpMetrics(std::string* output) {
 }
 
 static const void* get_profile_interface(const char* profile_id) {
-  LOG_INFO("%s: id = %s", __func__, profile_id);
+  LOG_INFO(LOG_TAG, "%s: id = %s", __func__, profile_id);
 
   /* sanity check */
   if (!interface_ready()) return NULL;
@@ -407,7 +410,7 @@ static const void* get_profile_interface(const char* profile_id) {
 }
 
 int dut_mode_configure(uint8_t enable) {
-  LOG_INFO("%s", __func__);
+  LOG_INFO(LOG_TAG, "%s", __func__);
 
   /* sanity check */
   if (!interface_ready()) return BT_STATUS_NOT_READY;
@@ -416,7 +419,7 @@ int dut_mode_configure(uint8_t enable) {
 }
 
 int dut_mode_send(uint16_t opcode, uint8_t* buf, uint8_t len) {
-  LOG_INFO("%s", __func__);
+  LOG_INFO(LOG_TAG, "%s", __func__);
 
   /* sanity check */
   if (!interface_ready()) return BT_STATUS_NOT_READY;
@@ -425,7 +428,7 @@ int dut_mode_send(uint16_t opcode, uint8_t* buf, uint8_t len) {
 }
 
 int le_test_mode(uint16_t opcode, uint8_t* buf, uint8_t len) {
-  LOG_INFO("%s", __func__);
+  LOG_INFO(LOG_TAG, "%s", __func__);
 
   /* sanity check */
   if (!interface_ready()) return BT_STATUS_NOT_READY;
@@ -463,7 +466,7 @@ static int set_os_callouts(bt_os_callouts_t* callouts) {
 }
 
 static int config_clear(void) {
-  LOG_INFO("%s", __func__);
+  LOG_INFO(LOG_TAG, "%s", __func__);
   return btif_config_clear() ? BT_STATUS_SUCCESS : BT_STATUS_FAIL;
 }
 
