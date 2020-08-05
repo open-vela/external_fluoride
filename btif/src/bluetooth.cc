61e0aceee70b409422d8f6380cae551cae500565
liujinye <liujinye@xiaomi.com>
2020-08-05 19:21:51 +0800

    [automerger skipped] Merge "Check whether local device is an ATV device to determine whether to show the consent dialog for BLE pairing in JUSTWORKS and ENCRYPTION_ONLY mode" into pi-dev am: 2b178f7896 -s ours am: 40f09f54ec -s ours
    am skip reason: Change-Id I6d06f5996da71e5a1407e544b0023d82924aa56f with SHA-1 cd3398b4e2 is in history
    
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/bt/+/12307728
    
    Change-Id: Ic0ff9254155d0fc13c57274fa836346fbe97fc50

--- btif/src/bluetooth.cc
+++ btif/src/bluetooth.cc
@@ -81,7 +81,7 @@ using bluetooth::hearing_aid::HearingAidInterface;
 
 bt_callbacks_t* bt_hal_cbacks = NULL;
 bool restricted_mode = false;
-bool single_user_mode = false;
+bool niap_mode = false;
 bool is_local_device_atv = false;
 
 /*******************************************************************************
@@ -135,9 +135,9 @@ static bool is_profile(const char* p1, const char* p2) {
  ****************************************************************************/
 
 static int init(bt_callbacks_t* callbacks, bool start_restricted,
-                bool is_single_user_mode, bool is_atv) {
-  LOG_INFO(LOG_TAG, "%s: start restricted = %d ; single user = %d", __func__,
-           start_restricted, is_single_user_mode);
+                bool is_niap_mode, bool is_atv) {
+  LOG_INFO(LOG_TAG, "%s: start restricted = %d ; niap = %d", __func__,
+           start_restricted, is_niap_mode);
 
   if (interface_ready()) return BT_STATUS_DONE;
 
@@ -147,7 +147,7 @@ static int init(bt_callbacks_t* callbacks, bool start_restricted,
 
   bt_hal_cbacks = callbacks;
   restricted_mode = start_restricted;
-  single_user_mode = is_single_user_mode;
+  niap_mode = is_niap_mode;
   is_local_device_atv = is_atv;
   stack_manager_get_interface()->init_stack();
   btif_debug_init();
@@ -171,7 +171,7 @@ static int disable(void) {
 static void cleanup(void) { stack_manager_get_interface()->clean_up_stack(); }
 
 bool is_restricted_mode() { return restricted_mode; }
-bool is_single_user_mode() { return single_user_mode; }
+bool is_niap_mode() { return niap_mode; }
 
 bool is_atv_device() { return is_local_device_atv; }
 
