7db57fec3672fcf9438dcbc9b1204fe7e553abcf
liujinye <liujinye@xiaomi.com>
2020-08-05 22:04:12 +0800

    Merge "L2cap: Change some functions to return void" am: acf5905640 am: dfc6a48905 am: 5621ecfb98 am: 9f7d516d7f
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1391640
    
    Change-Id: I22c78d760092fc77f8660557ddbb0072485272bb

--- stack/gatt/gatt_main.cc
+++ stack/gatt/gatt_main.cc
@@ -73,12 +73,10 @@ static void gatt_l2cif_congest_cback(uint16_t cid, bool congested);
 
 static const tL2CAP_APPL_INFO dyn_info = {gatt_l2cif_connect_ind_cback,
                                           gatt_l2cif_connect_cfm_cback,
-                                          NULL,
                                           gatt_l2cif_config_ind_cback,
                                           gatt_l2cif_config_cfm_cback,
                                           gatt_l2cif_disconnect_ind_cback,
                                           gatt_l2cif_disconnect_cfm_cback,
-                                          NULL,
                                           gatt_l2cif_data_ind_cback,
                                           gatt_l2cif_congest_cback,
                                           NULL,
@@ -109,13 +107,6 @@ void gatt_init(void) {
   gatt_cb.sign_op_queue = fixed_queue_new(SIZE_MAX);
   gatt_cb.srv_chg_clt_q = fixed_queue_new(SIZE_MAX);
   /* First, register fixed L2CAP channel for ATT over BLE */
-  fixed_reg.fixed_chnl_opts.mode = L2CAP_FCR_BASIC_MODE;
-  fixed_reg.fixed_chnl_opts.max_transmit = 0xFF;
-  fixed_reg.fixed_chnl_opts.rtrans_tout = 2000;
-  fixed_reg.fixed_chnl_opts.mon_tout = 12000;
-  fixed_reg.fixed_chnl_opts.mps = 670;
-  fixed_reg.fixed_chnl_opts.tx_win_sz = 1;
-
   fixed_reg.pL2CA_FixedConn_Cb = gatt_le_connect_cback;
   fixed_reg.pL2CA_FixedData_Cb = gatt_le_data_ind;
   fixed_reg.pL2CA_FixedCong_Cb = gatt_le_cong_cback; /* congestion callback */
@@ -125,7 +116,7 @@ void gatt_init(void) {
 
   /* Now, register with L2CAP for ATT PSM over BR/EDR */
   if (!L2CA_Register(BT_PSM_ATT, (tL2CAP_APPL_INFO*)&dyn_info,
-                     false /* enable_snoop */)) {
+                     false /* enable_snoop */, nullptr, gatt_cb.def_mtu_size)) {
     LOG(ERROR) << "ATT Dynamic Registration failed";
   }
 
