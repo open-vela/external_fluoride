4de4fdcddafc24aaedf51b7e69d7cdcdc25c55c5
openvela-robot <openvela-robot@xiaomi.com>
2020-08-05 19:58:17 +0800

    [automerger skipped] Merge "Check whether local device is an ATV device to determine whether to show the consent dialog for BLE pairing in JUSTWORKS and ENCRYPTION_ONLY mode" into qt-qpr1-dev am: cca9f79c88 -s ours am: 2727610c3d -s ours am: 9b12267581 -s ours
    am skip reason: Change-Id I6d06f5996da71e5a1407e544b0023d82924aa56f with SHA-1 e198eb931d is in history
    
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/bt/+/12307313
    
    Change-Id: I44e3224fcb1a5b9fc2c94ca7ce3d74b019581518

--- vendor_libs/test_vendor_lib/model/setup/phy_layer_factory.h
+++ vendor_libs/test_vendor_lib/model/setup/phy_layer_factory.h
@@ -44,15 +44,13 @@ class PhyLayerFactory {
 
   void UnregisterPhyLayer(uint32_t id);
 
-  void UnregisterAllPhyLayers();
-
   virtual void TimerTick();
 
   virtual std::string ToString() const;
 
  protected:
   virtual void Send(
-      std::shared_ptr<model::packets::LinkLayerPacketBuilder> packet,
+      const std::shared_ptr<model::packets::LinkLayerPacketBuilder> packet,
       uint32_t id);
   virtual void Send(model::packets::LinkLayerPacketView packet, uint32_t id);
 
@@ -68,19 +66,22 @@ class PhyLayerImpl : public PhyLayer {
   PhyLayerImpl(Phy::Type phy_type, uint32_t id,
                const std::function<void(model::packets::LinkLayerPacketView)>&
                    device_receive,
-               uint32_t device_id, PhyLayerFactory* factory);
-  ~PhyLayerImpl() override;
+               uint32_t device_id,
+               const std::shared_ptr<PhyLayerFactory> factory);
+  virtual ~PhyLayerImpl() override;
 
-  void Send(
-      std::shared_ptr<model::packets::LinkLayerPacketBuilder> packet) override;
+  virtual void Send(
+      const std::shared_ptr<model::packets::LinkLayerPacketBuilder> packet)
+      override;
   void Send(model::packets::LinkLayerPacketView packet) override;
   void Receive(model::packets::LinkLayerPacketView packet) override;
   void Unregister() override;
   bool IsFactoryId(uint32_t factory_id) override;
   void TimerTick() override;
 
+  uint32_t device_id_;
 
  private:
-  PhyLayerFactory* factory_;
+  std::shared_ptr<PhyLayerFactory> factory_;
 };
 }  // namespace test_vendor_lib
