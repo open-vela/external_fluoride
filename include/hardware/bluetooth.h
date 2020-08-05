31348003e80e5a02131abe95b72c158f682b272e
liujinye <liujinye@xiaomi.com>
2020-08-05 19:10:47 +0800

    [automerger skipped] Merge "Check whether local device is an ATV device to determine whether to show the consent dialog for BLE pairing in JUSTWORKS and ENCRYPTION_ONLY mode" into qt-dev am: 5667640792 -s ours
    am skip reason: Change-Id I6d06f5996da71e5a1407e544b0023d82924aa56f with SHA-1 b763abb99e is in history
    
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/bt/+/12307474
    
    Change-Id: Ia8237e641ef3644f78bb9845e51e5634d76b1b65

--- include/hardware/bluetooth.h
+++ include/hardware/bluetooth.h
@@ -48,7 +48,6 @@
 #define BT_PROFILE_AV_RC_ID "avrcp"
 #define BT_PROFILE_AV_RC_CTRL_ID "avrcp_ctrl"
 #define BT_PROFILE_HEARING_AID_ID "hearing_aid"
-#define BT_KEYSTORE_ID "bluetooth_keystore"
 
 /** Bluetooth Device Name */
 typedef struct { uint8_t name[249]; } __attribute__((packed)) bt_bdname_t;
@@ -471,13 +470,9 @@ typedef struct {
    * restricted mode, bonds that are created are marked as restricted in the
    * config file. These devices are deleted upon leaving restricted mode.
    * The |is_niap_mode| flag inits the adapter in NIAP mode.
-   * The |config_compare_result| flag show the config checksum check result if
-   * is in NIAP mode.
-   * The |init_flags| are config flags that cannot change during run.
    * The |is_atv| flag indicates whether the local device is an Android TV
    */
-  int (*init)(bt_callbacks_t* callbacks, bool guest_mode, bool is_niap_mode,
-              int config_compare_result, const char** init_flags, bool is_atv);
+  int (*init)(bt_callbacks_t* callbacks, bool guest_mode, bool is_niap_mode, bool is_atv);
 
   /** Enable Bluetooth. */
   int (*enable)();
@@ -630,14 +625,6 @@ typedef struct {
    * @return a string of uint8_t that is unique to this MAC address
    */
   std::string (*obfuscate_address)(const RawAddress& address);
-
-  /**
-   * Get an incremental id for as primary key for Bluetooth metric and log
-   *
-   * @param address Bluetooth MAC address of Bluetooth device
-   * @return int incremental Bluetooth id
-   */
-  int (*get_metric_id)(const RawAddress& address);
 } bt_interface_t;
 
 #define BLUETOOTH_INTERFACE_STRING "bluetoothInterface"
