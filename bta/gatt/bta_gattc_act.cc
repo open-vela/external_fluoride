0e45a9d398be553d4079294270ce5f0d604d951b
liujinye <liujinye@xiaomi.com>
2020-08-05 19:58:48 +0800

    [automerger skipped] Merge "Check whether local device is an ATV device to determine whether to show the consent dialog for BLE pairing in JUSTWORKS and ENCRYPTION_ONLY mode" into qt-dev am: 5667640792 -s ours am: 26511f3923 -s ours am: 6836ded86a -s ours am: aec0292a2e -s ours
    am skip reason: Change-Id I6d06f5996da71e5a1407e544b0023d82924aa56f with SHA-1 b763abb99e is in history
    
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/bt/+/12307474
    
    Change-Id: I4f659ca569e770a8b012b020addbf29be21aad16

--- bta/gatt/bta_gattc_act.cc
+++ bta/gatt/bta_gattc_act.cc
@@ -37,6 +37,7 @@
 #include "osi/include/log.h"
 #include "osi/include/osi.h"
 #include "stack/include/btu.h"
+#include "stack/l2cap/l2c_int.h"
 #include "utl.h"
 
 #if (BTA_HH_LE_INCLUDED == TRUE)
@@ -711,7 +712,8 @@ void bta_gattc_disc_cmpl(tBTA_GATTC_CLCB* p_clcb,
   else if (p_q_cmd != NULL) {
     p_clcb->p_q_cmd = NULL;
     /* execute pending operation of link block still present */
-    if (L2CA_IsLinkEstablished(p_clcb->p_srcb->server_bda, p_clcb->transport)) {
+    if (l2cu_find_lcb_by_bd_addr(p_clcb->p_srcb->server_bda,
+                                 p_clcb->transport)) {
       bta_gattc_sm_execute(p_clcb, p_q_cmd->hdr.event, p_q_cmd);
     }
     /* if the command executed requeued the cmd, we don't
