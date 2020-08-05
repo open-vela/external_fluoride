203e7a000c63e49427736529864708273b4c354e
liujinye <liujinye@xiaomi.com>
2020-08-05 19:38:44 +0800

    [automerger skipped] Merge "Check whether local device is an ATV device to determine whether to show the consent dialog for BLE pairing in JUSTWORKS and ENCRYPTION_ONLY mode" into pi-dev am: 2b178f7896 -s ours am: 40f09f54ec -s ours am: e1660e8e98 -s ours am: e2e784ea69 -s ours
    am skip reason: Change-Id I6d06f5996da71e5a1407e544b0023d82924aa56f with SHA-1 cd3398b4e2 is in history
    
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/bt/+/12307728
    
    Change-Id: Ic1335b16359f49cfe9062b0fab5b0e1594c1689d

--- stack/include/btm_api.h
+++ stack/include/btm_api.h
@@ -133,17 +133,6 @@ extern uint8_t* BTM_ReadDeviceClass(void);
 
 /*******************************************************************************
  *
- * Function         BTM_ReadLocalFeatures
- *
- * Description      This function is called to read the local features
- *
- * Returns          pointer to the local features string
- *
- ******************************************************************************/
-extern uint8_t* BTM_ReadLocalFeatures(void);
-
-/*******************************************************************************
- *
  * Function         BTM_RegisterForDeviceStatusNotif
  *
  * Description      This function is called to register for device status
@@ -438,38 +427,9 @@ extern uint16_t BTM_ReadConnectability(uint16_t* p_window,
  ******************************************************************************/
 extern tBTM_STATUS BTM_SetInquiryMode(uint8_t mode);
 
-/*******************************************************************************
- *
- * Function         BTM_SetInquiryScanType
- *
- * Description      This function is called to set the iquiry scan-type to
- *                  standard or interlaced.
- *
- * Input Params:    BTM_SCAN_TYPE_STANDARD or BTM_SCAN_TYPE_INTERLACED
- *
- * Returns          BTM_SUCCESS if successful
- *                  BTM_MODE_UNSUPPORTED if not a 1.2 device
- *                  BTM_WRONG_MODE if the device is not up.
- *
- ******************************************************************************/
-extern tBTM_STATUS BTM_SetInquiryScanType(uint16_t scan_type);
+extern void BTM_EnableInterlacedInquiryScan();
 
-/*******************************************************************************
- *
- * Function         BTM_SetPageScanType
- *
- * Description      This function is called to set the page scan-type to
- *                  standard or interlaced.
- *
- * Input Params:    BTM_SCAN_TYPE_STANDARD or BTM_SCAN_TYPE_INTERLACED
- *
- * Returns          BTM_SUCCESS if successful
- *                  BTM_MODE_UNSUPPORTED if not a 1.2 device
- *                  BTM_WRONG_MODE if the device is not up.
- *
- ******************************************************************************/
-
-extern tBTM_STATUS BTM_SetPageScanType(uint16_t scan_type);
+extern void BTM_EnableInterlacedPageScan();
 
 /*******************************************************************************
  *
@@ -1205,8 +1165,9 @@ extern void BTM_PINCodeReply(const RawAddress& bd_addr, uint8_t res,
  ******************************************************************************/
 extern tBTM_STATUS BTM_SecBond(const RawAddress& bd_addr,
                                tBLE_ADDR_TYPE addr_type,
-                               tBT_TRANSPORT transport, uint8_t pin_len,
-                               uint8_t* p_pin, uint32_t trusted_mask[]);
+                               tBT_TRANSPORT transport, int device_type,
+                               uint8_t pin_len, uint8_t* p_pin,
+                               uint32_t trusted_mask[]);
 
 /*******************************************************************************
  *
@@ -1284,23 +1245,6 @@ extern void BTM_ConfirmReqReply(tBTM_STATUS res, const RawAddress& bd_addr);
  ******************************************************************************/
 extern void BTM_PasskeyReqReply(tBTM_STATUS res, const RawAddress& bd_addr,
                                 uint32_t passkey);
-
-/*******************************************************************************
- *
- * Function         BTM_SendKeypressNotif
- *
- * Description      This function is used during the passkey entry model
- *                  by a device with KeyboardOnly IO capabilities
- *                  (very likely to be a HID Device).
- *                  It is called by a HID Device to inform the remote device
- *                  when a key has been entered or erased.
- *
- * Parameters:      bd_addr - Address of the peer device
- *                  type - notification type
- *
- ******************************************************************************/
-extern void BTM_SendKeypressNotif(const RawAddress& bd_addr,
-                                  tBTM_SP_KEY_TYPE type);
 
 /*******************************************************************************
  *
@@ -1629,5 +1573,10 @@ extern uint8_t BTM_GetEirUuidList(uint8_t* p_eir, size_t eir_len,
  *
  ******************************************************************************/
 extern tBTM_CONTRL_STATE BTM_PM_ReadControllerState(void);
+
+/**
+ * Send remote name request, either to legacy HCI, or to GD shim Name module
+ */
+extern void SendRemoteNameRequest(const RawAddress& raw_address);
 
 #endif /* BTM_API_H */
