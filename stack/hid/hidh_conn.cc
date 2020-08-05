869653c454fa70ea2a8a75d96f53b3a1a9e0ad63
liujinye <liujinye@xiaomi.com>
2020-08-05 23:40:31 +0800

    Merge "bta_gatt_api: Make BTA_GATTC_ServiceSearchRequest argument const" am: 839add25db am: 37ff8eaa66
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1391257
    
    Change-Id: I4a874c2932b7dc313715cacd02cacf0761ea0253

--- stack/hid/hidh_conn.cc
+++ stack/hid/hidh_conn.cc
@@ -65,12 +65,10 @@ static void hidh_l2cif_cong_ind(uint16_t l2cap_cid, bool congested);
 static const tL2CAP_APPL_INFO hst_reg_info = {
     hidh_l2cif_connect_ind,
     hidh_l2cif_connect_cfm,
-    NULL,
     hidh_l2cif_config_ind,
     hidh_l2cif_config_cfm,
     hidh_l2cif_disconnect_ind,
     hidh_l2cif_disconnect_cfm,
-    NULL,
     hidh_l2cif_data_ind,
     hidh_l2cif_cong_ind,
     NULL, /* tL2CA_TX_COMPLETE_CB */
@@ -98,12 +96,12 @@ tHID_STATUS hidh_conn_reg(void) {
 
   /* Now, register with L2CAP */
   if (!L2CA_Register(HID_PSM_CONTROL, (tL2CAP_APPL_INFO*)&hst_reg_info,
-                     false /* enable_snoop */, nullptr)) {
+                     false /* enable_snoop */, nullptr, hh_cb.l2cap_cfg.mtu)) {
     HIDH_TRACE_ERROR("HID-Host Control Registration failed");
     return (HID_ERR_L2CAP_FAILED);
   }
   if (!L2CA_Register(HID_PSM_INTERRUPT, (tL2CAP_APPL_INFO*)&hst_reg_info,
-                     false /* enable_snoop */, nullptr)) {
+                     false /* enable_snoop */, nullptr, hh_cb.l2cap_cfg.mtu)) {
     L2CA_Deregister(HID_PSM_CONTROL);
     HIDH_TRACE_ERROR("HID-Host Interrupt Registration failed");
     return (HID_ERR_L2CAP_FAILED);
