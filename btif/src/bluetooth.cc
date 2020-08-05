a39d39a855f8db490113b0114e7c063fdf419164
openvela-robot <openvela-robot@xiaomi.com>
2020-08-05 23:21:49 +0800

    Merge "bta_gatt_api: Make BTA_GATTC_ServiceSearchRequest argument const" am: 839add25db
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1391257
    
    Change-Id: Ia5464510c9caa927626434e3842fa55e234ec74d

--- btif/src/bluetooth.cc
+++ btif/src/bluetooth.cc
@@ -89,7 +89,6 @@ bool restricted_mode = false;
 bool niap_mode = false;
 const int CONFIG_COMPARE_ALL_PASS = 0b11;
 int niap_config_compare_result = CONFIG_COMPARE_ALL_PASS;
-bool is_local_device_atv = false;
 
 /*******************************************************************************
  *  Externs
@@ -143,7 +142,7 @@ static bool is_profile(const char* p1, const char* p2) {
 
 static int init(bt_callbacks_t* callbacks, bool start_restricted,
                 bool is_niap_mode, int config_compare_result,
-                const char** init_flags, bool is_atv) {
+                const char** init_flags) {
   LOG_INFO("%s: start restricted = %d ; niap = %d, config compare result = %d",
            __func__, start_restricted, is_niap_mode, config_compare_result);
 
@@ -159,7 +158,6 @@ static int init(bt_callbacks_t* callbacks, bool start_restricted,
   restricted_mode = start_restricted;
   niap_mode = is_niap_mode;
   niap_config_compare_result = config_compare_result;
-  is_local_device_atv = is_atv;
 
   stack_manager_get_interface()->init_stack();
   btif_debug_init();
@@ -189,8 +187,6 @@ bool is_niap_mode() { return niap_mode; }
 int get_niap_config_compare_result() {
   return niap_mode ? niap_config_compare_result : CONFIG_COMPARE_ALL_PASS;
 }
-
-bool is_atv_device() { return is_local_device_atv; }
 
 static int get_adapter_properties(void) {
   /* sanity check */
