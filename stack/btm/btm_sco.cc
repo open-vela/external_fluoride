5c0af5a0efc76d88a2d2b31bdd256ac6dd1ea4e7
liujinye <liujinye@xiaomi.com>
2020-08-05 23:11:33 +0800

    Snap for 6740034 from a6ed63fb83e379fb56af0510eebbd5701b26f8e3 to rvc-qpr1-release
    Change-Id: Idaf257e530ef54262d7bbf8b90eea0f8c20d6cfe

--- stack/btm/btm_sco.cc
+++ stack/btm/btm_sco.cc
@@ -264,7 +264,7 @@ static tBTM_STATUS btm_send_connect_request(uint16_t acl_handle,
     btm_handle_to_acl_index(acl_handle);
     uint8_t acl_index = btm_handle_to_acl_index(acl_handle);
     if (acl_index < MAX_L2CAP_LINKS) {
-      p_acl = &btm_cb.acl_cb_.acl_db[acl_index];
+      p_acl = &btm_cb.acl_db[acl_index];
       if (!HCI_EDR_ESCO_2MPS_SUPPORTED(p_acl->peer_lmp_feature_pages[0])) {
         BTM_TRACE_DEBUG("BTM Remote does not support 2-EDR eSCO");
         temp_packet_types |=
@@ -698,7 +698,7 @@ void btm_sco_conn_req(const RawAddress& bda, DEV_CLASS dev_class,
     if (((p->state == SCO_ST_CONNECTING) && rem_bd_matches) ||
         ((p->state == SCO_ST_LISTENING) &&
          (rem_bd_matches || !p->rem_bd_known))) {
-      /* If this was a wildcard, it is not one any more */
+      /* If this guy was a wildcard, he is not one any more */
       p->rem_bd_known = true;
       p->esco.data.link_type = link_type;
       p->state = SCO_ST_W4_CONN_RSP;
