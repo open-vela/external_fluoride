f1b92c8cb480a79750f86ad336304009c31c9418
openvela-robot <openvela-robot@xiaomi.com>
2020-08-05 22:14:51 +0800

    [automerger skipped] Merge "Check whether local device is an ATV device to determine whether to show the consent dialog for BLE pairing in JUSTWORKS and ENCRYPTION_ONLY mode" into oc-dev am: b2adda8177 am: 00fd683aec -s ours am: 14ae20b072 -s ours am: 1f504a4d1c -s ours
    am skip reason: Change-Id I6d06f5996da71e5a1407e544b0023d82924aa56f with SHA-1 0b4c1014f7 is in history
    
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/bt/+/12309020
    
    Change-Id: I84b756a054c2e0ef864b575b6cc9ef3226e63985

--- stack/gatt/gatt_int.h
+++ stack/gatt/gatt_int.h
@@ -26,7 +26,6 @@
 #include "gatt_api.h"
 #include "osi/include/fixed_queue.h"
 
-#include <base/bind.h>
 #include <base/strings/stringprintf.h>
 #include <string.h>
 #include <list>
@@ -324,8 +323,6 @@ struct tGATT_CLCB {
   bool in_use;
   alarm_t* gatt_rsp_timer_ent; /* peer response timer */
   uint8_t retry_count;
-  uint16_t read_req_current_mtu; /* This is the MTU value that the read was
-                                    initiated with */
 };
 
 typedef struct {
@@ -380,16 +377,6 @@ typedef struct {
   tGATT_PROFILE_CLCB profile_clcb[GATT_MAX_APPS];
   uint16_t
       handle_of_h_r; /* Handle of the handles reused characteristic value */
-  uint16_t handle_cl_supported_feat;
-  uint16_t handle_sr_supported_feat;
-  uint8_t
-      gatt_svr_supported_feat_mask; /* Local supported features as a server */
-
-  /* Supported features as a client. To be written to remote device.
-   * Note this is NOT a value of the characteristic with handle
-   * handle_cl_support_feat, as that one should be written by remote device.
-   */
-  uint8_t gatt_cl_supported_feat_mask;
 
   tGATT_APPL_INFO cb_info;
 
@@ -428,10 +415,6 @@ extern void gatt_add_a_bonded_dev_for_srv_chg(const RawAddress& bda);
 
 /* from gatt_attr.cc */
 extern uint16_t gatt_profile_find_conn_id_by_bd_addr(const RawAddress& bda);
-
-extern bool gatt_profile_get_eatt_support(
-    const RawAddress& remote_bda,
-    base::OnceCallback<void(const RawAddress&, bool)> cb);
 
 /* Functions provided by att_protocol.cc */
 extern tGATT_STATUS attp_send_cl_msg(tGATT_TCB& tcb, tGATT_CLCB* p_clcb,
