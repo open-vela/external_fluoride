2dff8ab05b17034e1e67e89cf59123ec978ae3aa
openvela-robot <openvela-robot@xiaomi.com>
2020-08-05 19:02:14 +0800

    [automerger skipped] Merge "Check whether local device is an ATV device to determine whether to show the consent dialog for BLE pairing in JUSTWORKS and ENCRYPTION_ONLY mode" into rvc-dev am: 272bfa7988 -s ours
    am skip reason: Change-Id I6d06f5996da71e5a1407e544b0023d82924aa56f with SHA-1 6dbd64d0bb is in history
    
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/bt/+/12287594
    
    Change-Id: I7efd14f412d33bee344108beaacbf4265b6464e3

--- stack/avdt/avdt_l2c.cc
+++ stack/avdt/avdt_l2c.cc
@@ -198,9 +198,8 @@ void avdt_l2c_connect_ind_cback(const RawAddress& bd_addr, uint16_t lcid,
         tACL_CONN* p_acl_cb = btm_bda_to_acl(bd_addr, BT_TRANSPORT_BR_EDR);
         btm_set_packet_types(
             p_acl_cb,
-            (btm_cb.acl_cb_.btm_acl_pkt_types_supported |
-             HCI_PKT_TYPES_MASK_NO_3_DH1 | HCI_PKT_TYPES_MASK_NO_3_DH3 |
-             HCI_PKT_TYPES_MASK_NO_3_DH5));
+            (btm_cb.btm_acl_pkt_types_supported | HCI_PKT_TYPES_MASK_NO_3_DH1 |
+             HCI_PKT_TYPES_MASK_NO_3_DH3 | HCI_PKT_TYPES_MASK_NO_3_DH5));
       }
 
       /* Check the security */
@@ -318,7 +317,7 @@ void avdt_l2c_connect_cfm_cback(uint16_t lcid, uint16_t result) {
                   btm_bda_to_acl(p_ccb->peer_addr, BT_TRANSPORT_BR_EDR);
               btm_set_packet_types(
                   p_acl_cb,
-                  (btm_cb.acl_cb_.btm_acl_pkt_types_supported |
+                  (btm_cb.btm_acl_pkt_types_supported |
                    HCI_PKT_TYPES_MASK_NO_3_DH1 | HCI_PKT_TYPES_MASK_NO_3_DH3 |
                    HCI_PKT_TYPES_MASK_NO_3_DH5));
             }
