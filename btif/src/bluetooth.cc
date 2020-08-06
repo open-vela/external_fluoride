28ef15e04c5bb86b17d25fa909b0daa6409114b6
openvela-robot <openvela-robot@xiaomi.com>
2020-08-06 23:56:06 +0800

    Merge "Remove unused code in btm and unused import in l2cap" am: 3fe9311d12 am: f14c1053eb am: fbd4be5b88 am: edff642b38
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1393546
    
    Change-Id: I16a8e1d59194ea859fd4366269be3d0a728cc282

--- btif/src/bluetooth.cc
+++ btif/src/bluetooth.cc
@@ -27,11 +27,6 @@
 #define LOG_TAG "bt_btif"
 
 #include <base/logging.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-
 #include <hardware/bluetooth.h>
 #include <hardware/bluetooth_headset_interface.h>
 #include <hardware/bt_av.h>
@@ -45,6 +40,10 @@
 #include <hardware/bt_rc.h>
 #include <hardware/bt_sdp.h>
 #include <hardware/bt_sock.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #include "bt_utils.h"
 #include "bta/include/bta_hearing_aid_api.h"
@@ -89,6 +88,7 @@ bool restricted_mode = false;
 bool niap_mode = false;
 const int CONFIG_COMPARE_ALL_PASS = 0b11;
 int niap_config_compare_result = CONFIG_COMPARE_ALL_PASS;
+bool is_local_device_atv = false;
 
 /*******************************************************************************
  *  Externs
@@ -142,7 +142,7 @@ static bool is_profile(const char* p1, const char* p2) {
 
 static int init(bt_callbacks_t* callbacks, bool start_restricted,
                 bool is_niap_mode, int config_compare_result,
-                const char** init_flags) {
+                const char** init_flags, bool is_atv) {
   LOG_INFO("%s: start restricted = %d ; niap = %d, config compare result = %d",
            __func__, start_restricted, is_niap_mode, config_compare_result);
 
@@ -158,6 +158,7 @@ static int init(bt_callbacks_t* callbacks, bool start_restricted,
   restricted_mode = start_restricted;
   niap_mode = is_niap_mode;
   niap_config_compare_result = config_compare_result;
+  is_local_device_atv = is_atv;
 
   stack_manager_get_interface()->init_stack();
   btif_debug_init();
@@ -187,6 +188,8 @@ bool is_niap_mode() { return niap_mode; }
 int get_niap_config_compare_result() {
   return niap_mode ? niap_config_compare_result : CONFIG_COMPARE_ALL_PASS;
 }
+
+bool is_atv_device() { return is_local_device_atv; }
 
 static int get_adapter_properties(void) {
   /* sanity check */
@@ -338,7 +341,7 @@ static void dump(int fd, const char** arguments) {
   HearingAid::DebugDump(fd);
   connection_manager::dump(fd);
   bluetooth::bqr::DebugDump(fd);
-  if (bluetooth::shim::is_gd_shim_enabled()) {
+  if (bluetooth::shim::is_any_gd_enabled()) {
     bluetooth::shim::Dump(fd, arguments);
   } else {
 #if (BTSNOOP_MEM == TRUE)
