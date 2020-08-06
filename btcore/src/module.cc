dcc04e5cc4a9b7e6d557b55fe0ee485776e53e93
openvela-robot <openvela-robot@xiaomi.com>
2020-08-06 22:49:56 +0800

    Merge "Remove unused code in btm and unused import in l2cap"

--- btcore/src/module.cc
+++ btcore/src/module.cc
@@ -18,8 +18,6 @@
 
 #define LOG_TAG "bt_core_module"
 
-#include "btcore/include/module.h"
-
 #include <base/logging.h>
 #include <dlfcn.h>
 #include <string.h>
@@ -27,6 +25,7 @@
 #include <mutex>
 #include <unordered_map>
 
+#include "btcore/include/module.h"
 #include "common/message_loop_thread.h"
 #include "osi/include/allocator.h"
 #include "osi/include/log.h"
@@ -51,7 +50,9 @@ static void set_module_state(const module_t* module, module_state_t state);
 
 void module_management_start(void) {}
 
-void module_management_stop(void) { metadata.clear(); }
+void module_management_stop(void) {
+  metadata.clear();
+}
 
 const module_t* get_module(const char* name) {
   module_t* module = (module_t*)dlsym(RTLD_DEFAULT, name);
@@ -162,9 +163,7 @@ class CallbackWrapper {
                            MessageLoopThread* callback_thread,
                            thread_fn callback)
       : module(module),
-        lifecycle_thread(std::string("bt_module_lifecycle_thread[") +
-                         (module->name != nullptr ? module->name : "unknown") +
-                         "]"),
+        lifecycle_thread("bt_module_lifecycle_thread"),
         callback_thread(callback_thread),
         callback(callback),
         success(false) {}
