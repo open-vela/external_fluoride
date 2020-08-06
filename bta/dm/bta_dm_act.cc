9e030124e95a0234cec81e90359e89717c58b481
liujinye <liujinye@xiaomi.com>
2020-08-06 21:16:11 +0800

    Merge changes I1685fabd,Ia789132e,I4b0ed942,I9607c58f,Ie9efd0e2
    * changes:
      RootCanal: Add Reset command to handler
      Track phys in Device in a list
      RootCanal: Schedule device and phy deletions
      RootCanal: Use CancelAsyncTask in test_environment
      RootCanal: Use Device::Send in devices

--- bta/dm/bta_dm_act.cc
+++ bta/dm/bta_dm_act.cc
@@ -2625,6 +2625,10 @@ static uint8_t bta_dm_sp_cback(tBTM_SP_EVT event, tBTM_SP_EVT_DATA* p_data) {
       bta_dm_cb.p_sec_cback(BTA_DM_SP_KEYPRESS_EVT, &sec_event);
       break;
 
+    case BTM_SP_UPGRADE_EVT:
+      bta_dm_co_lk_upgrade(p_data->upgrade.bd_addr, &p_data->upgrade.upgrade);
+      break;
+
     default:
       status = BTM_NOT_AUTHORIZED;
       break;
@@ -3798,16 +3802,6 @@ static uint8_t bta_dm_ble_smp_cback(tBTM_LE_EVT event, const RawAddress& bda,
       APPL_TRACE_EVENT("io mitm: %d oob_data:%d", p_data->io_req.auth_req,
                        p_data->io_req.oob_data);
 
-      break;
-
-    case BTM_LE_CONSENT_REQ_EVT:
-      sec_event.ble_req.bd_addr = bda;
-      p_name = BTM_SecReadDevName(bda);
-      if (p_name != NULL)
-        strlcpy((char*)sec_event.ble_req.bd_name, p_name, BD_NAME_LEN);
-      else
-        sec_event.ble_req.bd_name[0] = 0;
-      bta_dm_cb.p_sec_cback(BTA_DM_BLE_CONSENT_REQ_EVT, &sec_event);
       break;
 
     case BTM_LE_SEC_REQUEST_EVT:
--- stack/include/btm_api_types.h
+++ stack/include/btm_api_types.h
@@ -1086,6 +1086,8 @@ enum {
   BTM_SP_LOC_OOB_EVT,   /* received result for READ_LOCAL_OOB_DATA command */
   BTM_SP_RMT_OOB_EVT,   /* received REMOTE_OOB_DATA_REQUEST event */
   BTM_SP_COMPLT_EVT,    /* received SIMPLE_PAIRING_COMPLETE event */
+  BTM_SP_UPGRADE_EVT /* check if the application wants to upgrade the link key
+                        */
 };
 typedef uint8_t tBTM_SP_EVT;
 
@@ -1219,6 +1221,12 @@ typedef struct {
   tBTM_STATUS status;   /* status of the simple pairing process */
 } tBTM_SP_COMPLT;
 
+/* data type for BTM_SP_UPGRADE_EVT */
+typedef struct {
+  RawAddress bd_addr; /* peer address */
+  bool upgrade;    /* true, to upgrade the link key */
+} tBTM_SP_UPGRADE;
+
 typedef union {
   tBTM_SP_IO_REQ io_req;       /* BTM_SP_IO_REQ_EVT      */
   tBTM_SP_IO_RSP io_rsp;       /* BTM_SP_IO_RSP_EVT      */
@@ -1229,6 +1237,7 @@ typedef union {
   tBTM_SP_LOC_OOB loc_oob;     /* BTM_SP_LOC_OOB_EVT     */
   tBTM_SP_RMT_OOB rmt_oob;     /* BTM_SP_RMT_OOB_EVT     */
   tBTM_SP_COMPLT complt;       /* BTM_SP_COMPLT_EVT      */
+  tBTM_SP_UPGRADE upgrade;     /* BTM_SP_UPGRADE_EVT      */
 } tBTM_SP_EVT_DATA;
 
 /* Simple Pairing Events.  Called by the stack when Simple Pairing related
@@ -1282,7 +1291,6 @@ typedef void(tBTM_BOND_CANCEL_CMPL_CALLBACK)(tBTM_STATUS result);
 #define BTM_LE_LAST_FROM_SMP BTM_LE_BR_KEYS_REQ_EVT
 /* KEY update event */
 #define BTM_LE_KEY_EVT (BTM_LE_LAST_FROM_SMP + 1)
-#define BTM_LE_CONSENT_REQ_EVT SMP_CONSENT_REQ_EVT
 typedef uint8_t tBTM_LE_EVT;
 
 #define BTM_LE_KEY_NONE 0
