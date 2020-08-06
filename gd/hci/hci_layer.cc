99d4de4d90feb1ae8a62fb4dbc143422b69ab3a1
openvela-robot <openvela-robot@xiaomi.com>
2020-08-07 00:51:14 +0800

    Merge "Remove unused definitions"

--- gd/hci/hci_layer.cc
+++ gd/hci/hci_layer.cc
@@ -17,7 +17,6 @@
 #include "hci/hci_layer.h"
 
 #include "common/bind.h"
-#include "common/init_flags.h"
 #include "os/alarm.h"
 #include "os/queue.h"
 #include "packet/packet_builder.h"
@@ -363,11 +362,9 @@ void HciLayer::Start() {
   impl_->acl_queue_.GetDownEnd()->RegisterDequeue(handler, BindOn(impl_, &impl::on_outbound_acl_ready));
   RegisterEventHandler(EventCode::COMMAND_COMPLETE, handler->BindOn(impl_, &impl::on_command_complete));
   RegisterEventHandler(EventCode::COMMAND_STATUS, handler->BindOn(impl_, &impl::on_command_status));
-  if (bluetooth::common::InitFlags::GdCoreEnabled()) {
-    RegisterEventHandler(EventCode::LE_META_EVENT, handler->BindOn(impl_, &impl::on_le_meta_event));
-    RegisterEventHandler(
-        EventCode::DISCONNECTION_COMPLETE, handler->BindOn(this, &HciLayer::on_disconnection_complete));
-  }
+  RegisterEventHandler(EventCode::LE_META_EVENT, handler->BindOn(impl_, &impl::on_le_meta_event));
+  RegisterEventHandler(EventCode::DISCONNECTION_COMPLETE, handler->BindOn(this, &HciLayer::on_disconnection_complete));
+  // TODO find the right place
   auto drop_packet = handler->BindOn(impl_, &impl::drop);
   RegisterEventHandler(EventCode::PAGE_SCAN_REPETITION_MODE_CHANGE, drop_packet);
   RegisterEventHandler(EventCode::MAX_SLOTS_CHANGE, drop_packet);
