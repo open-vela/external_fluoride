5ba3307d569203c3c85f2d793094cdee3f9a36a6
liujinye <liujinye@xiaomi.com>
2020-08-07 04:41:08 +0800

    Merge "L2cap: Clean up some dependency on btm_int" am: 1448049800
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1393578
    
    Change-Id: I4fdd2bb8d3cdcacbefa8ce4141a2439e2e8b3617

--- embdrv/sbc/decoder/srce/decoder-sbc.c
+++ embdrv/sbc/decoder/srce/decoder-sbc.c
@@ -317,6 +317,7 @@ OI_STATUS OI_CODEC_SBC_DecodeFrame(OI_CODEC_SBC_DECODER_CONTEXT* context,
     return OI_CODEC_SBC_CHECKSUM_MISMATCH;
   }
 
+#ifdef OI_DEBUG
   /*
    * Make sure the bitpool values are sane.
    */
@@ -333,6 +334,7 @@ OI_STATUS OI_CODEC_SBC_DecodeFrame(OI_CODEC_SBC_DECODER_CONTEXT* context,
            OI_SBC_MaxBitpool(&context->common.frameInfo)));
     return OI_STATUS_INVALID_PARAMETERS;
   }
+#endif
 
   /*
    * Now decode the SBC data. Partial decode is not yet implemented for an SBC
