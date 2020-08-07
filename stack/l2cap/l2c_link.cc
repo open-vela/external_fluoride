c7271381f5014dc2e6f6c15f2586302d52a7c72e
openvela-robot <openvela-robot@xiaomi.com>
2020-08-07 20:23:02 +0800

    Merge changes I282fc94d,I3fdac96c am: b6f234c4c8
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1393466
    
    Change-Id: If1a4c404673909fd1e3f5d41aae1e122aa5fbac8

--- stack/l2cap/l2c_link.cc
+++ stack/l2cap/l2c_link.cc
@@ -130,21 +130,24 @@ bool l2c_link_hci_conn_req(const RawAddress& bd_addr) {
   return (false);
 }
 
-void btm_acl_connected(const RawAddress& bda, uint16_t handle, uint8_t status,
-                       uint8_t enc_mode) {
-  btm_sec_connected(bda, handle, status, enc_mode);
-
-  btm_acl_update_busy_level(BTM_BLI_PAGE_DONE_EVT);
-
-  l2c_link_hci_conn_comp(status, handle, bda);
-}
-
-void l2c_link_hci_conn_comp(uint8_t status, uint16_t handle,
+/*******************************************************************************
+ *
+ * Function         l2c_link_hci_conn_comp
+ *
+ * Description      This function is called when an HCI Connection Complete
+ *                  event is received.
+ *
+ * Returns          void
+ *
+ ******************************************************************************/
+bool l2c_link_hci_conn_comp(uint8_t status, uint16_t handle,
                             const RawAddress& p_bda) {
   tL2C_CONN_INFO ci;
   tL2C_LCB* p_lcb;
   tL2C_CCB* p_ccb;
   tBTM_SEC_DEV_REC* p_dev_info = NULL;
+
+  btm_acl_update_busy_level(BTM_BLI_PAGE_DONE_EVT);
 
   /* Save the parameters */
   ci.status = status;
@@ -159,7 +162,7 @@ void l2c_link_hci_conn_comp(uint8_t status, uint16_t handle,
     p_lcb = l2cu_allocate_lcb(ci.bd_addr, false, BT_TRANSPORT_BR_EDR);
     if (p_lcb == nullptr) {
       L2CAP_TRACE_WARNING("%s: Failed to allocate an LCB", __func__);
-      return;
+      return (false);
     }
     p_lcb->link_state = LST_CONNECTING;
   }
@@ -168,14 +171,14 @@ void l2c_link_hci_conn_comp(uint8_t status, uint16_t handle,
       (status == HCI_ERR_CONNECTION_EXISTS)) {
     L2CAP_TRACE_WARNING("%s: An ACL connection already exists. Handle:%d",
                         __func__, handle);
-    return;
+    return (true);
   } else if (p_lcb->link_state != LST_CONNECTING) {
     L2CAP_TRACE_ERROR("L2CAP got conn_comp in bad state: %d  status: 0x%d",
                       p_lcb->link_state, status);
 
     if (status != HCI_SUCCESS) l2c_link_hci_disc_comp(p_lcb->handle, status);
 
-    return;
+    return (false);
   }
 
   /* Save the handle */
@@ -202,7 +205,7 @@ void l2c_link_hci_conn_comp(uint8_t status, uint16_t handle,
 
     /* If dedicated bonding do not process any further */
     if (p_lcb->is_bonding) {
-      if (l2cu_start_post_bond_timer(handle)) return;
+      if (l2cu_start_post_bond_timer(handle)) return (true);
     }
 
     /* Update the timeouts in the hold queue */
@@ -261,7 +264,7 @@ void l2c_link_hci_conn_comp(uint8_t status, uint16_t handle,
       }
     }
   }
-  return;
+  return (true);
 }
 
 /*******************************************************************************
