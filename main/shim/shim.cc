70395564afa8f4e7faffb219b7eff524eefa2037
liujinye <liujinye@xiaomi.com>
2020-08-07 17:48:05 +0800

    Create stack/include/acl_api.h am: c72dff94f3 am: 2909989edc am: b57accb784 am: 79cb0cde86
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1393577
    
    Change-Id: Icbdb75f709f0f27c000351a8d655292baa0fec5e

--- main/shim/shim.cc
+++ main/shim/shim.cc
@@ -58,10 +58,6 @@ bool bluetooth::shim::is_gd_hci_enabled() {
   return bluetooth::common::InitFlags::GdHciEnabled();
 }
 
-bool bluetooth::shim::is_gd_controller_enabled() {
-  return bluetooth::common::InitFlags::GdControllerEnabled();
-}
-
 bool bluetooth::shim::is_gd_shim_enabled() {
   return bluetooth::common::InitFlags::GdCoreEnabled();
 }
