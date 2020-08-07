fbe2b38e989f91a26259eac2786348755c2baca4
liujinye <liujinye@xiaomi.com>
2020-08-07 23:28:09 +0800

    Move role switch api header inclusion location am: 41806112da am: 7adc14c12d am: b086d063c9
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1393467
    
    Change-Id: I24bea1bd934a8586a784388041266b1dc238f7f9

--- stack/acl/acl.h
+++ stack/acl/acl.h
@@ -17,10 +17,8 @@
 #pragma once
 
 #include <stdint.h>
-#include "internal_include/bt_target.h"
 #include "stack/include/acl_api_types.h"
 #include "stack/include/bt_types.h"
-#include "stack/include/hcidefs.h"
 #include "types/raw_address.h"
 
 #define BTM_MAX_SCN_ 31  // PORT_MAX_RFC_PORTS system/bt/stack/include/rfcdefs.h
