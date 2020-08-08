38b61c7a53beca2b25e78bf9d736c1e63430607d
liujinye <liujinye@xiaomi.com>
2020-08-09 03:00:12 +0800

    Telescope busy level update events am: cf4903f36f
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1393561
    
    Change-Id: Iff7fa40559d13559382f7c0207d4f0a14c7cc66e

--- btif/src/btif_dm.cc
+++ btif/src/btif_dm.cc
@@ -986,8 +986,8 @@ static void btif_dm_pin_req_evt(tBTA_DM_PIN_REQ* p_pin_req) {
  ******************************************************************************/
 static void btif_dm_ssp_cfm_req_evt(tBTA_DM_SP_CFM_REQ* p_ssp_cfm_req) {
   bt_bdname_t bd_name;
-  bool is_incoming = !(pairing_cb.state == BT_BOND_STATE_BONDING);
   uint32_t cod;
+  bool is_incoming = !(pairing_cb.state == BT_BOND_STATE_BONDING);
   int dev_type;
 
   BTIF_TRACE_DEBUG("%s", __func__);
@@ -1641,20 +1641,20 @@ static void btif_dm_remote_service_record_evt(uint16_t event, char* p_param) {
   }
 }
 
-static void report_inquiry_status_change(uint8_t status) {
-  if (status == BTM_INQUIRY_STARTED) {
+static void report_busy_level_change(uint8_t busy_level_flags) {
+  if (busy_level_flags == BTM_BL_INQUIRY_STARTED) {
     HAL_CBACK(bt_hal_cbacks, discovery_state_changed_cb, BT_DISCOVERY_STARTED);
     btif_dm_inquiry_in_progress = true;
-  } else if (status == BTM_INQUIRY_CANCELLED) {
+  } else if (busy_level_flags == BTM_BL_INQUIRY_CANCELLED) {
     HAL_CBACK(bt_hal_cbacks, discovery_state_changed_cb, BT_DISCOVERY_STOPPED);
     btif_dm_inquiry_in_progress = false;
-  } else if (status == BTM_INQUIRY_COMPLETE) {
+  } else if (busy_level_flags == BTM_BL_INQUIRY_COMPLETE) {
     btif_dm_inquiry_in_progress = false;
   }
 }
 
-void BTIF_dm_report_inquiry_status_change(uint8_t status) {
-  do_in_jni_thread(base::Bind(report_inquiry_status_change, status));
+void BTIF_dm_report_busy_level_change(uint8_t busy_level_flags) {
+  do_in_jni_thread(base::Bind(report_busy_level_change, busy_level_flags));
 }
 
 /*******************************************************************************
@@ -1864,13 +1864,9 @@ static void btif_dm_upstreams_evt(uint16_t event, char* p_param) {
           break;
       }
       break;
-    case BTA_DM_BLE_CONSENT_REQ_EVT:
-      BTIF_TRACE_DEBUG("BTA_DM_BLE_CONSENT_REQ_EVT. ");
-      btif_dm_ble_sec_req_evt(&p_data->ble_req, true);
-      break;
     case BTA_DM_BLE_SEC_REQ_EVT:
       BTIF_TRACE_DEBUG("BTA_DM_BLE_SEC_REQ_EVT. ");
-      btif_dm_ble_sec_req_evt(&p_data->ble_req, false);
+      btif_dm_ble_sec_req_evt(&p_data->ble_req);
       break;
     case BTA_DM_BLE_PASSKEY_NOTIF_EVT:
       BTIF_TRACE_DEBUG("BTA_DM_BLE_PASSKEY_NOTIF_EVT. ");
@@ -3107,14 +3103,14 @@ void btif_dm_remove_ble_bonding_keys(void) {
  * Returns          void
  *
  ******************************************************************************/
-void btif_dm_ble_sec_req_evt(tBTA_DM_BLE_SEC_REQ* p_ble_req, bool is_consent) {
+void btif_dm_ble_sec_req_evt(tBTA_DM_BLE_SEC_REQ* p_ble_req) {
   bt_bdname_t bd_name;
   uint32_t cod;
   int dev_type;
 
   BTIF_TRACE_DEBUG("%s", __func__);
 
-  if (!is_consent && pairing_cb.state == BT_BOND_STATE_BONDING) {
+  if (pairing_cb.state == BT_BOND_STATE_BONDING) {
     BTIF_TRACE_DEBUG("%s Discard security request", __func__);
     return;
   }
--- stack/acl/btm_acl.cc
+++ stack/acl/btm_acl.cc
@@ -56,6 +56,7 @@ tBTM_SEC_DEV_REC* btm_find_dev_by_handle(uint16_t handle);
 tBTM_SEC_DEV_REC* btm_find_or_alloc_dev(const RawAddress& bd_addr);
 tBTM_STATUS btm_sec_execute_procedure(tBTM_SEC_DEV_REC* p_dev_rec);
 tBTM_STATUS btm_set_packet_types(tACL_CONN* p, uint16_t pkt_types);
+void btm_acl_update_busy_level(tBTM_BLI_EVENT event);
 void btm_ble_refresh_local_resolvable_private_addr(
     const RawAddress& pseudo_addr, const RawAddress& local_rpa);
 void btm_establish_continue(tACL_CONN* p_acl_cb);
@@ -76,9 +77,7 @@ static void btm_read_tx_power_timeout(void* data);
 static void btm_process_remote_ext_features(tACL_CONN* p_acl_cb,
                                             uint8_t num_read_pages);
 
-void BTIF_dm_report_inquiry_status_change(uint8_t busy_level_flags);
-void BTA_dm_acl_up(const RawAddress bd_addr, tBT_TRANSPORT transport,
-                   uint16_t handle);
+void BTIF_dm_report_busy_level_change(uint8_t busy_level_flags);
 /* 3 seconds timeout waiting for responses */
 #define BTM_DEV_REPLY_TIMEOUT_MS (3 * 1000)
 
@@ -442,9 +441,34 @@ void btm_acl_device_down(void) {
 
 void btm_acl_set_paging(bool value) { btm_cb.is_paging = value; }
 
-void btm_acl_update_inquiry_status(uint8_t status) {
-  btm_cb.is_inquiry = status == BTM_INQUIRY_STARTED;
-  BTIF_dm_report_inquiry_status_change(status);
+/*******************************************************************************
+ *
+ * Function         btm_acl_update_busy_level
+ *
+ * Description      This function is called to update the busy level of the
+ *                  system.
+ *
+ * Returns          void
+ *
+ ******************************************************************************/
+void btm_acl_update_busy_level(tBTM_BLI_EVENT event) {
+  switch (event) {
+    case BTM_BLI_INQ_EVT:
+      BTM_TRACE_DEBUG("BTM_BLI_INQ_EVT");
+      btm_cb.is_inquiry = true;
+      BTIF_dm_report_busy_level_change(BTM_BL_INQUIRY_STARTED);
+      break;
+    case BTM_BLI_INQ_CANCEL_EVT:
+      BTM_TRACE_DEBUG("BTM_BLI_INQ_CANCEL_EVT");
+      btm_cb.is_inquiry = false;
+      BTIF_dm_report_busy_level_change(BTM_BL_INQUIRY_CANCELLED);
+      break;
+    case BTM_BLI_INQ_DONE_EVT:
+      BTM_TRACE_DEBUG("BTM_BLI_INQ_DONE_EVT");
+      btm_cb.is_inquiry = false;
+      BTIF_dm_report_busy_level_change(BTM_BL_INQUIRY_COMPLETE);
+      break;
+  }
 }
 
 /*******************************************************************************
@@ -1095,6 +1119,7 @@ void btm_read_remote_ext_features_failed(uint8_t status, uint16_t handle) {
  *
  ******************************************************************************/
 void btm_establish_continue(tACL_CONN* p_acl_cb) {
+  tBTM_BL_EVENT_DATA evt_data;
   BTM_TRACE_DEBUG("btm_establish_continue");
 #if (BTM_BYPASS_EXTRA_ACL_SETUP == FALSE)
   if (p_acl_cb->transport == BT_TRANSPORT_BR_EDR) {
@@ -1114,8 +1139,18 @@ void btm_establish_continue(tACL_CONN* p_acl_cb) {
   }
   p_acl_cb->link_up_issued = true;
 
-  BTA_dm_acl_up(p_acl_cb->remote_addr, p_acl_cb->transport,
-                p_acl_cb->hci_handle);
+  /* If anyone cares, tell them that the database changed */
+  if (btm_cb.acl_cb_.p_bl_changed_cb) {
+    evt_data.event = BTM_BL_CONN_EVT;
+    evt_data.conn.p_bda = &p_acl_cb->remote_addr;
+    evt_data.conn.p_bdn = p_acl_cb->remote_name;
+    evt_data.conn.p_dc = p_acl_cb->remote_dc;
+    evt_data.conn.p_features = p_acl_cb->peer_lmp_feature_pages[0];
+    evt_data.conn.handle = p_acl_cb->hci_handle;
+    evt_data.conn.transport = p_acl_cb->transport;
+
+    (*btm_cb.acl_cb_.p_bl_changed_cb)(&evt_data);
+  }
 }
 
 /*******************************************************************************
