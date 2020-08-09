1a583dc5aed909480bd9b4b4f48318deaa896c36
liujinye <liujinye@xiaomi.com>
2020-08-10 06:21:16 +0800

    Merge changes If1a1f94d,I1e2604fe,Ibcda9b0b,Idd9e543f am: fe9e05b4b9
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1394476
    
    Change-Id: I5398dc0b7468bf502e047c6ca02fdd56a046529d

--- bta/sys/bta_sys_main.cc
+++ bta/sys/bta_sys_main.cc
@@ -44,10 +44,6 @@
 #include "bta_ar_api.h"
 #endif
 
-void BTA_dm_on_hw_on();
-void BTA_dm_on_hw_error();
-void BTA_dm_on_hw_off();
-
 /* system manager control block definition */
 tBTA_SYS_CB bta_sys_cb;
 
@@ -55,6 +51,8 @@ tBTA_SYS_CB bta_sys_cb;
 /* TODO Hard-coded trace levels -  Needs to be configurable */
 uint8_t appl_trace_level = BT_TRACE_LEVEL_WARNING;  // APPL_INITIAL_TRACE_LEVEL;
 uint8_t btif_trace_level = BT_TRACE_LEVEL_WARNING;
+
+static const tBTA_SYS_REG bta_sys_hw_reg = {bta_sys_sm_execute, NULL};
 
 /*******************************************************************************
  *
@@ -70,6 +68,12 @@ void bta_sys_init(void) {
   memset(&bta_sys_cb, 0, sizeof(tBTA_SYS_CB));
 
   appl_trace_level = APPL_INITIAL_TRACE_LEVEL;
+
+  /* register BTA SYS message handler */
+  bta_sys_register(BTA_ID_SYS, &bta_sys_hw_reg);
+
+  /* register for BTM notifications */
+  BTM_RegisterForDeviceStatusNotif(&bta_sys_hw_btm_cback);
 
 #if (defined BTA_AR_INCLUDED) && (BTA_AR_INCLUDED == TRUE)
   bta_ar_init();
@@ -91,12 +95,13 @@ void bta_sys_set_state(tBTA_SYS_HW_STATE value) { bta_sys_cb.state = value; }
  * Returns          void
  *
  ******************************************************************************/
-static void bta_sys_sm_execute(tBTA_SYS_HW_EVT event) {
-  APPL_TRACE_EVENT("bta_sys_sm_execute state:%d, event:0x%x", bta_sys_cb.state);
+bool bta_sys_sm_execute(BT_HDR* p_msg) {
+  APPL_TRACE_EVENT("bta_sys_sm_execute state:%d, event:0x%x", bta_sys_cb.state,
+                   p_msg->event);
 
   switch (bta_sys_cb.state) {
     case BTA_SYS_HW_OFF:
-      switch (event) {
+      switch (p_msg->event) {
         case BTA_SYS_API_ENABLE_EVT:
           bta_sys_set_state(BTA_SYS_HW_STARTING);
           bta_sys_hw_api_enable();
@@ -105,20 +110,25 @@ static void bta_sys_sm_execute(tBTA_SYS_HW_EVT event) {
           bta_sys_set_state(BTA_SYS_HW_ON);
           break;
         case BTA_SYS_API_DISABLE_EVT:
-          BTA_dm_on_hw_off();
+          bta_sys_hw_evt_disabled();
           break;
         default:
           break;
       }
       break;
     case BTA_SYS_HW_STARTING:
-      switch (event) {
+      switch (p_msg->event) {
         case BTA_SYS_EVT_STACK_ENABLED_EVT:
           bta_sys_set_state(BTA_SYS_HW_ON);
-          BTA_dm_on_hw_on();
+          bta_sys_hw_evt_stack_enabled();
           break;
         case BTA_SYS_API_DISABLE_EVT:
           bta_sys_set_state(BTA_SYS_HW_STOPPING);
+          break;
+        case BTA_SYS_EVT_DISABLED_EVT:
+          bta_sys_set_state(BTA_SYS_HW_STARTING);
+          bta_sys_hw_evt_disabled();
+          bta_sys_hw_api_enable();
           break;
         case BTA_SYS_ERROR_EVT:
           bta_sys_set_state(BTA_SYS_HW_ON);
@@ -129,7 +139,7 @@ static void bta_sys_sm_execute(tBTA_SYS_HW_EVT event) {
       }
       break;
     case BTA_SYS_HW_ON:
-      switch (event) {
+      switch (p_msg->event) {
         case BTA_SYS_API_ENABLE_EVT:
           bta_sys_hw_api_enable();
           break;
@@ -137,6 +147,7 @@ static void bta_sys_sm_execute(tBTA_SYS_HW_EVT event) {
           bta_sys_hw_api_disable();
           break;
         case BTA_SYS_ERROR_EVT:
+        case BTA_SYS_EVT_DISABLED_EVT:
           bta_sys_hw_error();
           break;
         default:
@@ -144,13 +155,17 @@ static void bta_sys_sm_execute(tBTA_SYS_HW_EVT event) {
       }
       break;
     case BTA_SYS_HW_STOPPING:
-      switch (event) {
+      switch (p_msg->event) {
         case BTA_SYS_API_ENABLE_EVT:
           bta_sys_set_state(BTA_SYS_HW_STARTING);
           break;
         case BTA_SYS_EVT_STACK_ENABLED_EVT:
-          BTA_dm_on_hw_on();
+          bta_sys_hw_evt_stack_enabled();
           bta_sys_hw_api_disable();
+          break;
+        case BTA_SYS_EVT_DISABLED_EVT:
+          bta_sys_set_state(BTA_SYS_HW_OFF);
+          bta_sys_hw_evt_disabled();
           break;
         case BTA_SYS_ERROR_EVT:
           bta_sys_hw_api_disable();
@@ -162,10 +177,43 @@ static void bta_sys_sm_execute(tBTA_SYS_HW_EVT event) {
     default:
       break;
   }
+  return true;
 }
 
-void send_bta_sys_hw_event(tBTA_SYS_HW_EVT event) {
-  do_in_main_thread(FROM_HERE, base::Bind(bta_sys_sm_execute, event));
+void bta_sys_hw_register(tBTA_SYS_HW_CBACK* cback) {
+  bta_sys_cb.sys_hw_cback = cback;
+}
+
+void bta_sys_hw_unregister() { bta_sys_cb.sys_hw_cback = NULL; }
+
+/*******************************************************************************
+ *
+ * Function         bta_sys_hw_btm_cback
+ *
+ * Description     This function is registered by BTA SYS to BTM in order to get
+ *                 status notifications
+ *
+ *
+ * Returns
+ *
+ ******************************************************************************/
+void bta_sys_hw_btm_cback(tBTM_DEV_STATUS status) {
+  tBTA_SYS_HW_MSG* sys_event =
+      (tBTA_SYS_HW_MSG*)osi_malloc(sizeof(tBTA_SYS_HW_MSG));
+
+  APPL_TRACE_DEBUG("%s was called with parameter: %i", __func__, status);
+
+  /* send a message to BTA SYS */
+  if (status == BTM_DEV_STATUS_UP) {
+    sys_event->hdr.event = BTA_SYS_EVT_STACK_ENABLED_EVT;
+  } else if (status == BTM_DEV_STATUS_DOWN) {
+    sys_event->hdr.event = BTA_SYS_ERROR_EVT;
+  } else {
+    /* BTM_DEV_STATUS_CMD_TOUT is ignored for now. */
+    osi_free_and_reset((void**)&sys_event);
+  }
+
+  if (sys_event) bta_sys_sendmsg(sys_event);
 }
 
 /*******************************************************************************
@@ -181,8 +229,8 @@ void send_bta_sys_hw_event(tBTA_SYS_HW_EVT event) {
  ******************************************************************************/
 void bta_sys_hw_error() {
   APPL_TRACE_DEBUG("%s", __func__);
-  if (bta_sys_cb.bluetooth_active) {
-    BTA_dm_on_hw_error();
+  if (bta_sys_cb.bluetooth_active && bta_sys_cb.sys_hw_cback != NULL) {
+    bta_sys_cb.sys_hw_cback(BTA_SYS_HW_ERROR_EVT);
   }
 }
 
@@ -206,7 +254,10 @@ void bta_sys_hw_api_enable() {
     BTM_DeviceReset();
   } else {
     bta_sys_cb.bluetooth_active = true;
-    BTA_dm_on_hw_on();
+
+    /* HW already in use, so directly notify the caller */
+    if (bta_sys_cb.sys_hw_cback != NULL)
+      bta_sys_cb.sys_hw_cback(BTA_SYS_HW_ON_EVT);
   }
 }
 
@@ -228,8 +279,47 @@ void bta_sys_hw_api_disable() {
   /* register which module we turn off */
   bta_sys_cb.bluetooth_active = false;
 
-  bta_sys_set_state(BTA_SYS_HW_OFF);
-  BTA_dm_on_hw_off();
+  /* manually update the state of our system */
+  bta_sys_cb.state = BTA_SYS_HW_STOPPING;
+
+  tBTA_SYS_HW_MSG* p_msg =
+      (tBTA_SYS_HW_MSG*)osi_malloc(sizeof(tBTA_SYS_HW_MSG));
+  p_msg->hdr.event = BTA_SYS_EVT_DISABLED_EVT;
+
+  bta_sys_sendmsg(p_msg);
+}
+
+/*******************************************************************************
+ *
+ * Function         bta_sys_hw_event_disabled
+ *
+ * Description
+ *
+ *
+ * Returns          success or failure
+ *
+ ******************************************************************************/
+void bta_sys_hw_evt_disabled() {
+  if (bta_sys_cb.sys_hw_cback != NULL) {
+    bta_sys_cb.sys_hw_cback(BTA_SYS_HW_OFF_EVT);
+  }
+}
+
+/*******************************************************************************
+ *
+ * Function         bta_sys_hw_event_stack_enabled
+ *
+ * Description     we receive this event once the SW side is ready (stack, FW
+ *                 download,... ), i.e. we can really start using the device. So
+ *                 notify the app.
+ *
+ * Returns          success or failure
+ *
+ ******************************************************************************/
+void bta_sys_hw_evt_stack_enabled() {
+  if (bta_sys_cb.sys_hw_cback != NULL) {
+    bta_sys_cb.sys_hw_cback(BTA_SYS_HW_ON_EVT);
+  }
 }
 
 /*******************************************************************************
