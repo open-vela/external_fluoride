e02df68789c1625d8af17f896a43b47cfdf3b7dd
openvela-robot <openvela-robot@xiaomi.com>
2020-08-10 21:25:56 +0800

    Flatten BTM_DeviceReset (and address a todo from 2014)
    Bug: 159815595
    Tag: #refactor
    Test: compile & verify basic functions working
    Change-Id: Ibb0e21c0b005f823ffbb5fb044fe12fe614cecf1

--- bta/sys/bta_sys_main.cc
+++ bta/sys/bta_sys_main.cc
@@ -41,6 +41,7 @@
 #include "utl.h"
 
 
+void BTA_dm_on_hw_on();
 void BTA_dm_on_hw_error();
 void BTA_dm_on_hw_off();
 
@@ -87,12 +88,25 @@ static void bta_sys_sm_execute(tBTA_SYS_HW_EVT event) {
   switch (bta_sys_cb.state) {
     case BTA_SYS_HW_OFF:
       switch (event) {
+        case BTA_SYS_EVT_STACK_ENABLED_EVT:
+          bta_sys_set_state(BTA_SYS_HW_ON);
+          break;
+        case BTA_SYS_API_DISABLE_EVT:
+          BTA_dm_on_hw_off();
+          break;
         default:
           break;
       }
       break;
     case BTA_SYS_HW_STARTING:
       switch (event) {
+        case BTA_SYS_EVT_STACK_ENABLED_EVT:
+          bta_sys_set_state(BTA_SYS_HW_ON);
+          BTA_dm_on_hw_on();
+          break;
+        case BTA_SYS_API_DISABLE_EVT:
+          bta_sys_set_state(BTA_SYS_HW_STOPPING);
+          break;
         case BTA_SYS_ERROR_EVT:
           bta_sys_set_state(BTA_SYS_HW_ON);
           bta_sys_hw_error();
@@ -103,6 +117,9 @@ static void bta_sys_sm_execute(tBTA_SYS_HW_EVT event) {
       break;
     case BTA_SYS_HW_ON:
       switch (event) {
+        case BTA_SYS_API_DISABLE_EVT:
+          bta_sys_hw_api_disable();
+          break;
         case BTA_SYS_ERROR_EVT:
           bta_sys_hw_error();
           break;
@@ -112,6 +129,10 @@ static void bta_sys_sm_execute(tBTA_SYS_HW_EVT event) {
       break;
     case BTA_SYS_HW_STOPPING:
       switch (event) {
+        case BTA_SYS_EVT_STACK_ENABLED_EVT:
+          BTA_dm_on_hw_on();
+          bta_sys_hw_api_disable();
+          break;
         case BTA_SYS_ERROR_EVT:
           bta_sys_hw_api_disable();
           break;
