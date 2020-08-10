1ba64828d313ff02d0c66f9bdb32bf043b015e6c
liujinye <liujinye@xiaomi.com>
2020-08-10 21:25:56 +0800

    Flatten BTM_DeviceReset (and address a todo from 2014)
    Bug: 159815595
    Tag: #refactor
    Test: compile & verify basic functions working
    Change-Id: Ibb0e21c0b005f823ffbb5fb044fe12fe614cecf1

--- stack/include/btu.h
+++ stack/include/btu.h
@@ -65,6 +65,7 @@ bt_status_t do_in_main_thread_delayed(const base::Location& from_here,
                                       base::OnceClosure task,
                                       const base::TimeDelta& delay);
 
+void BTU_StartUp(void);
 void BTU_ShutDown(void);
 
 #endif
