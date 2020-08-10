7406891a48fed842c89343633e0bc5b39c13a24d
liujinye <liujinye@xiaomi.com>
2020-08-10 21:08:12 +0800

    flatten send_bta_sys_hw_event(BTA_SYS_API_ENABLE_EVT)
    Bug: 159815595
    Tag: #refactor
    Test: compile & verify basic functions working
    Change-Id: I74bd01384659826d8e30f0fef1d594538187ae6c

--- bta/dm/bta_dm_act.cc
+++ bta/dm/bta_dm_act.cc
@@ -248,6 +248,17 @@ static uint8_t btm_local_io_caps;
 
 /** Initialises the BT device manager */
 void bta_dm_enable(tBTA_DM_SEC_CBACK* p_sec_cback) {
+  /* if already in use, return an error */
+  if (bta_dm_cb.is_bta_dm_active) {
+    tBTA_DM_SEC enable_event;
+    APPL_TRACE_WARNING("%s Device already started by another application",
+                       __func__);
+    memset(&enable_event, 0, sizeof(tBTA_DM_SEC));
+    enable_event.enable.status = BTA_FAILURE;
+    if (p_sec_cback != NULL) p_sec_cback(BTA_DM_ENABLE_EVT, &enable_event);
+    return;
+  }
+
   /* make sure security callback is saved - if no callback, do not erase the
   previous one,
   it could be an error recovery mechanism */
@@ -2438,6 +2449,8 @@ static uint8_t bta_dm_sp_cback(tBTM_SP_EVT event, tBTM_SP_EVT_DATA* p_data) {
  ******************************************************************************/
 static void bta_dm_local_name_cback(UNUSED_ATTR void* p_name) {
   tBTA_DM_SEC sec_event;
+
+  sec_event.enable.status = BTA_SUCCESS;
 
   if (bta_dm_cb.p_sec_cback)
     bta_dm_cb.p_sec_cback(BTA_DM_ENABLE_EVT, &sec_event);
