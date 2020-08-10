4a3695965922db58ec48afaee3059d7e1b5b1fcb
openvela-robot <openvela-robot@xiaomi.com>
2020-08-10 21:28:22 +0800

    Remove module_start_up_callbacked_wrapper
    It's no longer needed
    
    Bug: 159815595
    Tag: #refactor
    Test: compile & verify basic functions working
    Change-Id: I0e74fc268a10302b120f1be53825643718af944b

--- btif/src/stack_manager.cc
+++ btif/src/stack_manager.cc
@@ -65,12 +65,7 @@
 #endif
 #include "bta/sys/bta_sys_int.h"
 #include "bta_dm_int.h"
-#include "btif/include/btif_pan.h"
-#include "btif/include/btif_sock.h"
 #include "main/shim/controller.h"
-
-void BTA_dm_on_hw_on();
-void BTA_dm_on_hw_off();
 
 using bluetooth::common::MessageLoopThread;
 
@@ -197,6 +192,8 @@ static void event_start_up_stack(UNUSED_ATTR void* context) {
     module_start_up(get_module(HCI_MODULE));
   }
 
+  BTU_StartUp();
+
   btm_init();
   l2c_init();
   sdp_init();
@@ -240,9 +237,6 @@ static void event_start_up_stack(UNUSED_ATTR void* context) {
   }
   BTM_reset_complete();
 
-  bta_sys_set_state(BTA_SYS_HW_ON);
-  BTA_dm_on_hw_on();
-
   if (future_await(local_hack_future) != FUTURE_SUCCESS) {
     LOG_ERROR("%s failed to start up the stack", __func__);
     stack_is_running = true;  // So stack shutdown actually happens
@@ -267,22 +261,7 @@ static void event_shut_down_stack(UNUSED_ATTR void* context) {
   hack_future = local_hack_future;
   stack_is_running = false;
 
-  do_in_main_thread(FROM_HERE, base::Bind(&btm_ble_multi_adv_cleanup));
-
-  btif_dm_on_disable();
-  btif_sock_cleanup();
-  btif_pan_cleanup();
-
-  do_in_main_thread(FROM_HERE, base::Bind(bta_dm_disable));
-
-  future_await(local_hack_future);
-  local_hack_future = future_new();
-  hack_future = local_hack_future;
-
-  bta_sys_disable();
-  bta_sys_set_state(BTA_SYS_HW_OFF);
-  BTA_dm_on_hw_off();
-
+  btif_disable_bluetooth();
   module_shut_down(get_module(BTIF_CONFIG_MODULE));
 
   future_await(local_hack_future);
