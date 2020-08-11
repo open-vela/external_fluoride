c443bb2c291caa67cb080f69bf568af799af4d4e
liujinye <liujinye@xiaomi.com>
2020-08-11 17:32:54 +0800

    Merge changes Id108e40c,I1ce27a47,Ia4fbf395,I9f3db595 am: bc4af7972a am: b0e1903d82 am: 0ecf81a3fc am: 03469a103e
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1396094
    
    Change-Id: I2818e76ff6038769297a12d707c99a944d2655e5

--- gd/hci/controller.h
+++ gd/hci/controller.h
@@ -156,7 +156,6 @@ class Controller : public Module {
   static const ModuleFactory Factory;
 
   static constexpr uint64_t kDefaultEventMask = 0x3dbfffffffffffff;
-  static constexpr uint64_t kDefaultLeEventMask = 0x0000000000021e7f;
 
  protected:
   void ListDependencies(ModuleList* list) override;
