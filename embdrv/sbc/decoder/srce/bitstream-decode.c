b8d838cf29264912766ee68c09e0a76061858ef8
liujinye <liujinye@xiaomi.com>
2020-08-11 09:03:58 +0800

    Merge "Simplify some L2cap security check"

--- embdrv/sbc/decoder/srce/bitstream-decode.c
+++ embdrv/sbc/decoder/srce/bitstream-decode.c
@@ -39,9 +39,10 @@ Functions for manipulating input bitstreams.
 #include "oi_stddefs.h"
 
 PRIVATE void OI_BITSTREAM_ReadInit(OI_BITSTREAM* bs, const OI_BYTE* buffer) {
-  bs->value = 0;
-  bs->ptr.r = buffer;
-  bs->bitPtr = 32;
+  bs->value =
+      ((int32_t)buffer[0] << 16) | ((int32_t)buffer[1] << 8) | (buffer[2]);
+  bs->ptr.r = buffer + 3;
+  bs->bitPtr = 8;
 }
 
 PRIVATE uint32_t OI_BITSTREAM_ReadUINT(OI_BITSTREAM* bs, OI_UINT bits) {
