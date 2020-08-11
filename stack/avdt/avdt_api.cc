9f45ba16c94b098097fb4b5130d357eacf97e7c0
openvela-robot <openvela-robot@xiaomi.com>
2020-08-12 01:01:52 +0800

    AVDT: Don't explicitly require no-security on MEDIA and REPORT am: 5fa7d6b516 am: 841447417a
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1397053
    
    Change-Id: I28f1cea2730012f87ecda64c760545907c5ccaf0

--- stack/avdt/avdt_api.cc
+++ stack/avdt/avdt_api.cc
@@ -95,9 +95,9 @@ void AVDT_Register(AvdtpRcb* p_reg, tAVDT_CTRL_CBACK* p_cback) {
 
   /* set security level */
   BTM_SetSecurityLevel(true, "", BTM_SEC_SERVICE_AVDTP, p_reg->sec_mask,
-                       AVDT_PSM, 0, 0);
+                       AVDT_PSM, BTM_SEC_PROTO_AVDT, AVDT_CHAN_SIG);
   BTM_SetSecurityLevel(false, "", BTM_SEC_SERVICE_AVDTP, p_reg->sec_mask,
-                       AVDT_PSM, 0, 0);
+                       AVDT_PSM, BTM_SEC_PROTO_AVDT, AVDT_CHAN_SIG);
 
   /* initialize AVDTP data structures */
   avdt_scb_init();
