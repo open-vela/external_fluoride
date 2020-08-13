7e521979c5e7d0d624f66c0a4e63f7999f1a22d5
openvela-robot <openvela-robot@xiaomi.com>
2020-08-13 16:58:11 +0800

    RootCanal: Remove dead code am: 748d65eb53 am: 45dd28af6f am: 7353d41518
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1398370
    
    Change-Id: Iae5b5ee3295cb9ff6a9591cddced27b803ec766b

--- stack/test/a2dp/misc_fake.cc
+++ stack/test/a2dp/misc_fake.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 The Android Open Source Project
+ * Copyright 2020 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,15 +14,15 @@
  * limitations under the License.
  */
 
-#include "os/parameter_provider.h"
+#include "service/common/bluetooth/a2dp_codec_config.h"
+#include "stack/include/a2dp_vendor_ldac.h"
 
-namespace bluetooth {
-namespace os {
-
-// On Android we always write a single default location
-std::string ParameterProvider::ConfigFilePath() {
-  return "/data/misc/bluedroid/bt_config.conf";
+bluetooth::A2dpCodecConfig* bta_av_get_a2dp_current_codec(void) {
+  return nullptr;
 }
 
-}  // namespace os
-}  // namespace bluetooth
+int A2DP_VendorGetTrackSampleRateLdac(const uint8_t* p_codec_info) { return 0; }
+int A2DP_VendorGetTrackBitsPerSampleLdac(const uint8_t* p_codec_info) {
+  return 0;
+}
+int A2DP_VendorGetChannelModeCodeLdac(const uint8_t* p_codec_info) { return 0; }
