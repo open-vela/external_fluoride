ff93a2e59087144ef87c621ecd2888cc5ad7540e
liujinye <liujinye@xiaomi.com>
2020-08-13 17:55:50 +0800

    Merge "Revert "HAL: Add ISO packet callbacks"" am: 03eaf15b7c am: 8b57869263
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1399629
    
    Change-Id: Ie4e9518013c63472480323303576f5e2e19514a2

--- btif/src/btif_dm.cc
+++ btif/src/btif_dm.cc
@@ -48,7 +48,6 @@
 #include "advertise_data_parser.h"
 #include "bt_common.h"
 #include "bta_gatt_api.h"
-#include "btif/include/stack_manager.h"
 #include "btif_api.h"
 #include "btif_av.h"
 #include "btif_bqr.h"
@@ -987,8 +986,8 @@ static void btif_dm_pin_req_evt(tBTA_DM_PIN_REQ* p_pin_req) {
  ******************************************************************************/
 static void btif_dm_ssp_cfm_req_evt(tBTA_DM_SP_CFM_REQ* p_ssp_cfm_req) {
   bt_bdname_t bd_name;
-  uint32_t cod;
   bool is_incoming = !(pairing_cb.state == BT_BOND_STATE_BONDING);
+  uint32_t cod;
   int dev_type;
 
   BTIF_TRACE_DEBUG("%s", __func__);
@@ -1740,7 +1739,7 @@ static void btif_dm_upstreams_evt(uint16_t event, char* p_param) {
         }
       }
       bluetooth::bqr::EnableBtQualityReport(false);
-      future_ready(stack_manager_get_hack_future(), FUTURE_SUCCESS);
+      btif_disable_bluetooth_evt();
       break;
 
     case BTA_DM_PIN_REQ_EVT:
@@ -1866,9 +1865,13 @@ static void btif_dm_upstreams_evt(uint16_t event, char* p_param) {
           break;
       }
       break;
+    case BTA_DM_BLE_CONSENT_REQ_EVT:
+      BTIF_TRACE_DEBUG("BTA_DM_BLE_CONSENT_REQ_EVT. ");
+      btif_dm_ble_sec_req_evt(&p_data->ble_req, true);
+      break;
     case BTA_DM_BLE_SEC_REQ_EVT:
       BTIF_TRACE_DEBUG("BTA_DM_BLE_SEC_REQ_EVT. ");
-      btif_dm_ble_sec_req_evt(&p_data->ble_req);
+      btif_dm_ble_sec_req_evt(&p_data->ble_req, false);
       break;
     case BTA_DM_BLE_PASSKEY_NOTIF_EVT:
       BTIF_TRACE_DEBUG("BTA_DM_BLE_PASSKEY_NOTIF_EVT. ");
@@ -3099,14 +3102,14 @@ void btif_dm_remove_ble_bonding_keys(void) {
  * Returns          void
  *
  ******************************************************************************/
-void btif_dm_ble_sec_req_evt(tBTA_DM_BLE_SEC_REQ* p_ble_req) {
+void btif_dm_ble_sec_req_evt(tBTA_DM_BLE_SEC_REQ* p_ble_req, bool is_consent) {
   bt_bdname_t bd_name;
   uint32_t cod;
   int dev_type;
 
   BTIF_TRACE_DEBUG("%s", __func__);
 
-  if (pairing_cb.state == BT_BOND_STATE_BONDING) {
+  if (!is_consent && pairing_cb.state == BT_BOND_STATE_BONDING) {
     BTIF_TRACE_DEBUG("%s Discard security request", __func__);
     return;
   }
