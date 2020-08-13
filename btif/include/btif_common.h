042a6fd3ce82ec7eec5ede62eb72724cf20436c1
openvela-robot <openvela-robot@xiaomi.com>
2020-08-13 23:05:23 +0800

    Merge "Define appl_trace_level needed for coverage instrumentation" am: c7482f4b86
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1399810
    
    Change-Id: I80ea7d8b238ca2e99e32d1ae8bf9a00e702dc61e

--- btif/include/btif_common.h
+++ btif/include/btif_common.h
@@ -210,7 +210,8 @@ void btif_remote_properties_evt(bt_status_t status, RawAddress* remote_addr,
                                 uint32_t num_props, bt_property_t* p_props);
 
 void bte_load_did_conf(const char* p_path);
-void bte_main_init(void);
+void bte_main_boot_entry(void);
+void bte_main_cleanup(void);
 
 bt_status_t btif_transfer_context(tBTIF_CBACK* p_cback, uint16_t event,
                                   char* p_params, int param_len,
