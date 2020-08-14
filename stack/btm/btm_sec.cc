a4c0c6e0de21ff4f73ab0f58d57b8f13aae496fb
openvela-robot <openvela-robot@xiaomi.com>
2020-08-14 18:03:04 +0800

    Merge "If a LINK_KEY_REQUEST comes in without us calling CreateBond then it isn't explicitly triggered by the local stack."

--- stack/btm/btm_sec.cc
+++ stack/btm/btm_sec.cc
@@ -2731,19 +2731,12 @@ void btm_sec_rmt_host_support_feat_evt(uint8_t* p) {
  *
  ******************************************************************************/
 void btm_io_capabilities_req(const RawAddress& p) {
-  tBTM_SEC_DEV_REC* p_dev_rec = btm_find_or_alloc_dev(p);
-
-  if ((btm_cb.security_mode == BTM_SEC_MODE_SC) &&
-      (p_dev_rec->num_read_pages == 0)) {
-    BTM_TRACE_EVENT("%s: Device security mode is SC only.",
-                    "To continue need to know remote features.", __func__);
-
-    // ACL calls back to btm_sec_set_peer_sec_caps after it gets data
-    p_dev_rec->remote_features_needed = true;
-    return;
-  }
-
   tBTM_SP_IO_REQ evt_data;
+  uint8_t err_code = 0;
+  tBTM_SEC_DEV_REC* p_dev_rec;
+  bool is_orig = true;
+  uint8_t callback_rc = BTM_SUCCESS;
+
   evt_data.bd_addr = p;
 
   /* setup the default response according to compile options */
@@ -2753,15 +2746,22 @@ void btm_io_capabilities_req(const RawAddress& p) {
   evt_data.oob_data = BTM_OOB_NONE;
   evt_data.auth_req = BTM_DEFAULT_AUTH_REQ;
 
-  uint8_t err_code = 0;
-  bool is_orig = true;
-  uint8_t callback_rc = BTM_SUCCESS;
-
   BTM_TRACE_EVENT("%s: State: %s", __func__,
                   btm_pair_state_descr(btm_cb.pairing_state));
 
+  p_dev_rec = btm_find_or_alloc_dev(evt_data.bd_addr);
+
   BTM_TRACE_DEBUG("%s:Security mode: %d, Num Read Remote Feat pages: %d",
                   __func__, btm_cb.security_mode, p_dev_rec->num_read_pages);
+
+  if ((btm_cb.security_mode == BTM_SEC_MODE_SC) &&
+      (p_dev_rec->num_read_pages == 0)) {
+    BTM_TRACE_EVENT("%s: Device security mode is SC only.",
+                    "To continue need to know remote features.", __func__);
+
+    p_dev_rec->remote_features_needed = true;
+    return;
+  }
 
   p_dev_rec->sm4 |= BTM_SM4_TRUE;
 
