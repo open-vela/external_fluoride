786dc92007a6218320051853a8fc2ab1bc3f8f92
liujinye <liujinye@xiaomi.com>
2020-08-14 16:15:52 +0800

    Internalize btm_handle_to_acl_index to stack/acl am: 54dff9477a
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1399950
    
    Change-Id: I1d4ba591f8626c6c5a3ba8a9dcdfd136fe473f18

--- main/shim/btm_api.h
+++ main/shim/btm_api.h
@@ -1880,7 +1880,8 @@ tBTM_STATUS BTM_SetPowerMode(uint8_t pm_id, const RawAddress& remote_bda,
  *                  BTM_UNKNOWN_ADDR if bd addr is not active or bad
  *
  ******************************************************************************/
-bool BTM_ReadPowerMode(const RawAddress& remote_bda, tBTM_PM_MODE* p_mode);
+tBTM_STATUS BTM_ReadPowerMode(const RawAddress& remote_bda,
+                              tBTM_PM_MODE* p_mode);
 
 /*******************************************************************************
  *
