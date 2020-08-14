f7f3b42a0123da9f34208f7ecefe54656a2b9787
liujinye <liujinye@xiaomi.com>
2020-08-14 23:51:08 +0800

    Merge "Consolidate evt data initialization" am: 57e431a60b am: 7d84bec169 am: bd2925c74f
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1396991
    
    Change-Id: Iea872a1f898cc7cb3c93893274a050959f0d4d9a

--- stack/btm/btm_ble.cc
+++ stack/btm/btm_ble.cc
@@ -663,10 +663,10 @@ bool BTM_UseLeLink(const RawAddress& bd_addr) {
   tBLE_ADDR_TYPE addr_type;
   bool use_le = false;
 
-  if (BTM_IsAclConnectionUp(bd_addr, BT_TRANSPORT_BR_EDR)) {
+  if (!BTM_IsAclConnectionUp(bd_addr, BT_TRANSPORT_BR_EDR)) {
     return use_le;
   } else {
-    if (BTM_IsAclConnectionUp(bd_addr, BT_TRANSPORT_LE)) {
+    if (!BTM_IsAclConnectionUp(bd_addr, BT_TRANSPORT_LE)) {
       use_le = true;
     } else {
       BTM_ReadDevInfo(bd_addr, &dev_type, &addr_type);
@@ -1825,6 +1825,7 @@ uint8_t btm_proc_smp_cback(tSMP_EVT event, const RawAddress& bd_addr,
         p_dev_rec->sec_flags |= BTM_SEC_LE_AUTHENTICATED;
         FALLTHROUGH_INTENDED; /* FALLTHROUGH */
 
+      case SMP_CONSENT_REQ_EVT:
       case SMP_SEC_REQUEST_EVT:
         if (event == SMP_SEC_REQUEST_EVT &&
             btm_cb.pairing_state != BTM_PAIR_STATE_IDLE) {
@@ -1832,7 +1833,9 @@ uint8_t btm_proc_smp_cback(tSMP_EVT event, const RawAddress& bd_addr,
           break;
         }
         btm_cb.pairing_bda = bd_addr;
-        p_dev_rec->sec_state = BTM_SEC_STATE_AUTHENTICATING;
+        if (event != SMP_CONSENT_REQ_EVT) {
+          p_dev_rec->sec_state = BTM_SEC_STATE_AUTHENTICATING;
+        }
         btm_cb.pairing_flags |= BTM_PAIR_FLAGS_LE_ACTIVE;
         FALLTHROUGH_INTENDED; /* FALLTHROUGH */
 
