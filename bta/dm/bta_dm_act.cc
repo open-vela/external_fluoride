3144bfc979bff8934f9973d43db2f6dc19ec371a
liujinye <liujinye@xiaomi.com>
2020-08-16 17:25:40 +0800

    Move bluetooth.cc call ins to main thread am: 42b442fcf0 am: f93a19d9d0 am: 8c5e1adc7f
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1401767
    
    Change-Id: I6b8c552f57fa00b9f0b711f2bb2aacd7b2b2e215

--- bta/dm/bta_dm_act.cc
+++ bta/dm/bta_dm_act.cc
@@ -63,9 +63,6 @@
 
 using bluetooth::Uuid;
 
-void BTIF_dm_enable();
-void BTIF_dm_disable();
-
 static void bta_dm_inq_results_cb(tBTM_INQ_RESULTS* p_inq, uint8_t* p_eir,
                                   uint16_t eir_len);
 static void bta_dm_inq_cmpl_cb(void* p_result);
@@ -311,7 +308,8 @@ void bta_dm_deinit_cb(void) {
 }
 
 void BTA_dm_on_hw_off() {
-  BTIF_dm_disable();
+  if (bta_dm_cb.p_sec_cback != NULL)
+    bta_dm_cb.p_sec_cback(BTA_DM_DISABLE_EVT, NULL);
 
   /* reinitialize the control block */
   bta_dm_deinit_cb();
@@ -481,7 +479,7 @@ static void bta_dm_disable_timer_cback(void* data) {
     bta_dm_cb.disabling = false;
 
     bta_sys_remove_uuid(UUID_SERVCLASS_PNP_INFORMATION);
-    BTIF_dm_disable();
+    bta_dm_cb.p_sec_cback(BTA_DM_DISABLE_EVT, NULL);
   }
 }
 
@@ -1564,6 +1562,21 @@ void bta_dm_search_cancel_cmpl(UNUSED_ATTR tBTA_DM_MSG* p_data) {
 
 /*******************************************************************************
  *
+ * Function         bta_dm_search_cancel_transac_cmpl
+ *
+ * Description      Current Service Discovery or remote name procedure is
+ *                  completed after search cancellation
+ *
+ * Returns          void
+ *
+ ******************************************************************************/
+void bta_dm_search_cancel_transac_cmpl(UNUSED_ATTR tBTA_DM_MSG* p_data) {
+  osi_free_and_reset((void**)&bta_dm_search_cb.p_sdp_db);
+  bta_dm_search_cancel_notify(NULL);
+}
+
+/*******************************************************************************
+ *
  * Function         bta_dm_search_cancel_notify
  *
  * Description      Notify application that search has been cancelled
@@ -2426,7 +2439,10 @@ static uint8_t bta_dm_sp_cback(tBTM_SP_EVT event, tBTM_SP_EVT_DATA* p_data) {
  *
  ******************************************************************************/
 static void bta_dm_local_name_cback(UNUSED_ATTR void* p_name) {
-  BTIF_dm_enable();
+  tBTA_DM_SEC sec_event;
+
+  if (bta_dm_cb.p_sec_cback)
+    bta_dm_cb.p_sec_cback(BTA_DM_ENABLE_EVT, &sec_event);
 }
 
 static void handle_role_change(const RawAddress& bd_addr, uint8_t new_role,
