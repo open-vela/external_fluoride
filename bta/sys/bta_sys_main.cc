27269204a2231a5db2cf74603c55040cf0a1c89e
openvela-robot <openvela-robot@xiaomi.com>
2020-08-16 17:36:58 +0800

    Move bluetooth.cc call ins to main thread am: 42b442fcf0 am: f93a19d9d0 am: 8c5e1adc7f am: d76ade8387
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1401767
    
    Change-Id: I9baa4619b581fade79e8b8d8d39f22f3e750154d

--- bta/sys/bta_sys_main.cc
+++ bta/sys/bta_sys_main.cc
@@ -220,3 +220,14 @@ void bta_sys_disable() {
     }
   }
 }
+
+/*******************************************************************************
+ *
+ * Function         bta_sys_set_trace_level
+ *
+ * Description      Set trace level for BTA
+ *
+ * Returns          void
+ *
+ ******************************************************************************/
+void bta_sys_set_trace_level(uint8_t level) { appl_trace_level = level; }
