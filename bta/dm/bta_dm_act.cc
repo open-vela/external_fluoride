2f76bea4f19781077b257c0d714b2544f3681e2f
liujinye <liujinye@xiaomi.com>
2020-08-16 10:28:24 +0800

    Remove unused arg from bta_dm_search_cancel_notify
    Bug: 159815595
    Tag: #refactor
    Test: compile & verify basic functions working
    Change-Id: I24e59faf767ca6371ddc65c69a73dfc00a5c1d3a

--- bta/dm/bta_dm_act.cc
+++ bta/dm/bta_dm_act.cc
@@ -859,7 +859,7 @@ void bta_dm_search_cancel(UNUSED_ATTR tBTA_DM_MSG* p_data) {
 
   if (BTM_IsInquiryActive()) {
     if (BTM_CancelInquiry() == BTM_SUCCESS) {
-      bta_dm_search_cancel_notify(NULL);
+      bta_dm_search_cancel_notify();
       p_msg = (tBTA_DM_MSG*)osi_malloc(sizeof(tBTA_DM_MSG));
       p_msg->hdr.event = BTA_DM_SEARCH_CMPL_EVT;
       p_msg->hdr.layer_specific = BTA_DM_API_DISCOVER_EVT;
@@ -1564,21 +1564,6 @@ void bta_dm_search_cancel_cmpl(UNUSED_ATTR tBTA_DM_MSG* p_data) {
 
 /*******************************************************************************
  *
- * Function         bta_dm_search_cancel_transac_cmpl
- *
- * Description      Current Service Discovery or remote name procedure is
- *                  completed after search cancellation
- *
- * Returns          void
- *
- ******************************************************************************/
-void bta_dm_search_cancel_transac_cmpl(UNUSED_ATTR tBTA_DM_MSG* p_data) {
-  osi_free_and_reset((void**)&bta_dm_search_cb.p_sdp_db);
-  bta_dm_search_cancel_notify(NULL);
-}
-
-/*******************************************************************************
- *
  * Function         bta_dm_search_cancel_notify
  *
  * Description      Notify application that search has been cancelled
@@ -1586,7 +1571,7 @@ void bta_dm_search_cancel_transac_cmpl(UNUSED_ATTR tBTA_DM_MSG* p_data) {
  * Returns          void
  *
  ******************************************************************************/
-void bta_dm_search_cancel_notify(UNUSED_ATTR tBTA_DM_MSG* p_data) {
+void bta_dm_search_cancel_notify() {
   if (bta_dm_search_cb.p_search_cback) {
     bta_dm_search_cb.p_search_cback(BTA_DM_SEARCH_CANCEL_CMPL_EVT, NULL);
   }
@@ -1963,7 +1948,7 @@ static void bta_dm_inq_cmpl_cb(void* p_result) {
     p_msg->inq_cmpl.num = ((tBTM_INQUIRY_CMPL*)p_result)->num_resp;
   } else {
     bta_dm_search_cb.cancel_pending = false;
-    bta_dm_search_cancel_notify(NULL);
+    bta_dm_search_cancel_notify();
     p_msg->hdr.event = BTA_DM_SEARCH_CMPL_EVT;
     p_msg->hdr.layer_specific = BTA_DM_API_DISCOVER_EVT;
   }
@@ -3518,16 +3503,6 @@ static uint8_t bta_dm_ble_smp_cback(tBTM_LE_EVT event, const RawAddress& bda,
                  &p_data->io_req.init_keys, &p_data->io_req.resp_keys);
       APPL_TRACE_EVENT("io mitm: %d oob_data:%d", p_data->io_req.auth_req,
                        p_data->io_req.oob_data);
-      break;
-
-    case BTM_LE_CONSENT_REQ_EVT:
-      sec_event.ble_req.bd_addr = bda;
-      p_name = BTM_SecReadDevName(bda);
-      if (p_name != NULL)
-        strlcpy((char*)sec_event.ble_req.bd_name, p_name, BD_NAME_LEN);
-      else
-        sec_event.ble_req.bd_name[0] = 0;
-      bta_dm_cb.p_sec_cback(BTA_DM_BLE_CONSENT_REQ_EVT, &sec_event);
       break;
 
     case BTM_LE_SEC_REQUEST_EVT:
