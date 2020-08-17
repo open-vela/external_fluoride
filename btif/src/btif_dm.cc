7aa888823f27629a1a746d8bb6496236a6672242
openvela-robot <openvela-robot@xiaomi.com>
2020-08-17 21:34:32 +0800

    Merge changes I6f7bb67a,Idb75cbfb,I29c6eeae,I8a23fbc8,I225d01c4, ...
    * changes:
      Fold BTA_DmDiscoverByTransport into BTA_DmDiscover
      The extended part of service mask is never used
      mask_ext.num_uuid is always 0
      Remove sdp_search from discovery, it's always true
      Remove hack from I2819702ac068be9515824234e85d81ac2ec7a748
      Minor improvement
      Fix error when converting state machine table

--- btif/src/btif_dm.cc
+++ btif/src/btif_dm.cc
@@ -271,7 +271,6 @@ static void btif_dm_send_bond_state_changed(RawAddress address, bt_bond_state_t 
   }
 
   invoke_bond_state_changed_cb(BT_STATUS_SUCCESS, address, bond_state);
-  btif_dm_get_remote_services_by_transport(&address, BT_TRANSPORT_UNKNOWN);
 }
 
 void btif_dm_init(uid_set_t* set) {
@@ -838,8 +837,8 @@ static void btif_dm_pin_req_evt(tBTA_DM_PIN_REQ* p_pin_req) {
  ******************************************************************************/
 static void btif_dm_ssp_cfm_req_evt(tBTA_DM_SP_CFM_REQ* p_ssp_cfm_req) {
   bt_bdname_t bd_name;
-  bool is_incoming = !(pairing_cb.state == BT_BOND_STATE_BONDING);
   uint32_t cod;
+  bool is_incoming = !(pairing_cb.state == BT_BOND_STATE_BONDING);
   int dev_type;
 
   BTIF_TRACE_DEBUG("%s", __func__);
@@ -1700,13 +1699,9 @@ static void btif_dm_upstreams_evt(uint16_t event, char* p_param) {
           break;
       }
       break;
-    case BTA_DM_BLE_CONSENT_REQ_EVT:
-      BTIF_TRACE_DEBUG("BTA_DM_BLE_CONSENT_REQ_EVT. ");
-      btif_dm_ble_sec_req_evt(&p_data->ble_req, true);
-      break;
     case BTA_DM_BLE_SEC_REQ_EVT:
       BTIF_TRACE_DEBUG("BTA_DM_BLE_SEC_REQ_EVT. ");
-      btif_dm_ble_sec_req_evt(&p_data->ble_req, false);
+      btif_dm_ble_sec_req_evt(&p_data->ble_req);
       break;
     case BTA_DM_BLE_PASSKEY_NOTIF_EVT:
       BTIF_TRACE_DEBUG("BTA_DM_BLE_PASSKEY_NOTIF_EVT. ");
@@ -2242,7 +2237,7 @@ void btif_dm_get_remote_services(const RawAddress remote_addr) {
   BTIF_TRACE_EVENT("%s: bd_addr=%s", __func__, remote_addr.ToString().c_str());
 
   BTA_DmDiscover(remote_addr, BTA_ALL_SERVICE_MASK, bte_dm_search_services_evt,
-                 true);
+                 BT_TRANSPORT_UNKNOWN);
 }
 
 /*******************************************************************************
@@ -2259,14 +2254,8 @@ bt_status_t btif_dm_get_remote_services_by_transport(RawAddress* remote_addr,
   BTIF_TRACE_EVENT("%s: transport=%d, remote_addr=%s", __func__, transport,
                    remote_addr->ToString().c_str());
 
-  /* Set the mask extension */
-  tBTA_SERVICE_MASK_EXT mask_ext;
-  mask_ext.num_uuid = 0;
-  mask_ext.p_uuid = NULL;
-  mask_ext.srvc_mask = BTA_ALL_SERVICE_MASK;
-
-  BTA_DmDiscoverByTransport(*remote_addr, &mask_ext, bte_dm_search_services_evt,
-                            true, transport);
+  BTA_DmDiscover(*remote_addr, BTA_ALL_SERVICE_MASK, bte_dm_search_services_evt,
+                 transport);
 
   return BT_STATUS_SUCCESS;
 }
@@ -2281,7 +2270,7 @@ bt_status_t btif_dm_get_remote_services_by_transport(RawAddress* remote_addr,
 void btif_dm_get_remote_service_record(const RawAddress remote_addr,
                                        const Uuid uuid) {
   BTIF_TRACE_EVENT("%s: bd_addr=%s", __func__, remote_addr.ToString().c_str());
-  BTA_DmDiscoverUUID(remote_addr, uuid, bte_dm_remote_service_record_evt, true);
+  BTA_DmDiscoverUUID(remote_addr, uuid, bte_dm_remote_service_record_evt);
 }
 
 void btif_dm_enable_service(tBTA_SERVICE_ID service_id, bool enable) {
@@ -2735,14 +2724,14 @@ void btif_dm_remove_ble_bonding_keys(void) {
  * Returns          void
  *
  ******************************************************************************/
-void btif_dm_ble_sec_req_evt(tBTA_DM_BLE_SEC_REQ* p_ble_req, bool is_consent) {
+void btif_dm_ble_sec_req_evt(tBTA_DM_BLE_SEC_REQ* p_ble_req) {
   bt_bdname_t bd_name;
   uint32_t cod;
   int dev_type;
 
   BTIF_TRACE_DEBUG("%s", __func__);
 
-  if (!is_consent && pairing_cb.state == BT_BOND_STATE_BONDING) {
+  if (pairing_cb.state == BT_BOND_STATE_BONDING) {
     BTIF_TRACE_DEBUG("%s Discard security request", __func__);
     return;
   }
