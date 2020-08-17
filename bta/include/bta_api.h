61633eea2704dcf9b841e92cc7f7597e5392da08
openvela-robot <openvela-robot@xiaomi.com>
2020-08-18 04:10:56 +0800

    Flatten btm_dm_start_disc_gatt_services am: b9bb0154cd am: 718b65e696 am: 0422c5a168
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1403067
    
    Change-Id: Ia3844335280d3ca7e6dd29b616d2c03abc8a066b

--- bta/include/bta_api.h
+++ bta/include/bta_api.h
@@ -583,6 +583,7 @@ typedef void(tBTA_DM_SEC_CBACK)(tBTA_DM_SEC_EVT event, tBTA_DM_SEC* p_data);
 #define BTA_DM_DISC_BLE_RES_EVT \
   3 /* Discovery result for BLE GATT based servoce on a peer device. */
 #define BTA_DM_DISC_CMPL_EVT 4          /* Discovery complete. */
+#define BTA_DM_DI_DISC_CMPL_EVT 5       /* Discovery complete. */
 #define BTA_DM_SEARCH_CANCEL_CMPL_EVT 6 /* Search cancelled */
 
 typedef uint8_t tBTA_DM_SEARCH_EVT;
@@ -619,6 +620,13 @@ typedef struct {
   uint8_t num_resps; /* Number of inquiry responses. */
 } tBTA_DM_INQ_CMPL;
 
+/* Structure associated with BTA_DM_DI_DISC_CMPL_EVT */
+typedef struct {
+  RawAddress bd_addr; /* BD address peer device. */
+  uint8_t num_record; /* Number of DI record */
+  tBTA_STATUS result;
+} tBTA_DM_DI_DISC_CMPL;
+
 /* Structure associated with BTA_DM_DISC_RES_EVT */
 typedef struct {
   RawAddress bd_addr;          /* BD address peer device. */
@@ -646,6 +654,8 @@ typedef union {
   tBTA_DM_DISC_RES disc_res; /* Discovery result for a peer device. */
   tBTA_DM_DISC_BLE_RES
       disc_ble_res;             /* discovery result for GATT based service */
+  tBTA_DM_DI_DISC_CMPL di_disc; /* DI discovery result for a peer device */
+
 } tBTA_DM_SEARCH;
 
 /* Search callback */
