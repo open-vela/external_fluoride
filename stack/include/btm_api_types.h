3a8d50317b1530abeef2517dece2cf833cc4ac9e
openvela-robot <openvela-robot@xiaomi.com>
2020-08-19 04:01:25 +0800

    Merge changes I228ee25f,I85914c62,I3a5c970d,I5f54b858,I6a885f21, ...
    * changes:
      report_dup is always true
      Remove useless states
      Flatten btm_set_inq_event_filter
      pending_filt_complete_event is always zero
      inqfilt_active is always false
      Setting inqfilt_active to true has no effect
      p_inq->state is always BTM_INQ_SET_FILT_STATE when calling
      inqfilt_active is always true inside btm_set_inq_event_filter
      Status is always success
      p_inqfilter_cmpl_cb is never set
      hci_status is always success
      We never set the inquiry filter
      btm_set_inq_event_filter always returns CMD_STARTED

--- stack/include/btm_api_types.h
+++ stack/include/btm_api_types.h
@@ -417,8 +417,6 @@ typedef struct /* contains the parameters passed to the inquiry functions */
   uint8_t mode;      /* general or limited */
   uint8_t duration;  /* duration of the inquiry (1.28 sec increments) */
   uint8_t max_resps; /* maximum number of responses to return */
-  bool report_dup; /* report duplicated inquiry response with higher RSSI value
-                      */
   tBTM_INQ_FILT_COND filter_cond; /* filter value based on filter cond type */
 } tBTM_INQ_PARMS;
 
@@ -1022,7 +1020,6 @@ typedef void(tBTM_BOND_CANCEL_CMPL_CALLBACK)(tBTM_STATUS result);
 #define BTM_LE_LAST_FROM_SMP BTM_LE_BR_KEYS_REQ_EVT
 /* KEY update event */
 #define BTM_LE_KEY_EVT (BTM_LE_LAST_FROM_SMP + 1)
-#define BTM_LE_CONSENT_REQ_EVT SMP_CONSENT_REQ_EVT
 typedef uint8_t tBTM_LE_EVT;
 
 #define BTM_LE_KEY_NONE 0
