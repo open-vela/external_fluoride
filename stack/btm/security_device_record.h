cfbda3b58460809452fcbebcc4bbc509349f4d33
liujinye <liujinye@xiaomi.com>
2020-08-18 17:08:31 +0800

    Flatten btm_set_inq_event_filter
    Bug: 159815595
    Tag: #refactor
    Test: compile & verify basic functions working
    Change-Id: I3a5c970d973b821f015a922b7152dcee29c5994c

--- stack/btm/security_device_record.h
+++ stack/btm/security_device_record.h
@@ -187,8 +187,6 @@ typedef struct {
 
   tBTM_CMPL_CB* p_inq_cmpl_cb;
   tBTM_INQ_RESULTS_CB* p_inq_results_cb;
-  tBTM_CMPL_CB* p_inqfilter_cmpl_cb; /* Called (if not NULL) after inquiry
-                                        filter completed */
   uint32_t inq_counter; /* Counter incremented each time an inquiry completes */
   /* Used for determining whether or not duplicate devices */
   /* have responded to the same inquiry */
@@ -202,10 +200,6 @@ typedef struct {
 
   uint16_t per_min_delay; /* Current periodic minimum delay */
   uint16_t per_max_delay; /* Current periodic maximum delay */
-  bool inqfilt_active;
-  uint8_t pending_filt_complete_event; /* to take care of
-                                          btm_event_filter_complete
-                                          corresponding to */
   /* inquiry that has been cancelled*/
   uint8_t inqfilt_type; /* Contains the inquiry filter type (BD ADDR, COD, or
                            Clear) */
