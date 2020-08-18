e4648d0d486e81614813d2fd6f12f2aed46c852f
liujinye <liujinye@xiaomi.com>
2020-08-19 05:43:40 +0800

    Simplify bta_dm_add_device am: 087cb81bfe am: de17270e90
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1404361
    
    Change-Id: I531b81a13e9783276f81509e1adaad2759fd56cf

--- bta/dm/bta_dm_act.cc
+++ bta/dm/bta_dm_act.cc
@@ -640,7 +640,7 @@ void bta_dm_add_device(std::unique_ptr<tBTA_DM_API_ADD_DEVICE> msg) {
   if (msg->link_key_known) p_lc = &msg->link_key;
 
   if (!BTM_SecAddDevice(msg->bd_addr, p_dc, msg->bd_name, msg->features,
-                        trusted_services_mask, p_lc, msg->key_type,
+                        trusted_services_mask, p_lc, msg->key_type, 0,
                         msg->pin_length)) {
     LOG(ERROR) << "BTA_DM: Error adding device " << msg->bd_addr;
   }
@@ -3184,6 +3184,16 @@ static uint8_t bta_dm_ble_smp_cback(tBTM_LE_EVT event, const RawAddress& bda,
                  &p_data->io_req.init_keys, &p_data->io_req.resp_keys);
       APPL_TRACE_EVENT("io mitm: %d oob_data:%d", p_data->io_req.auth_req,
                        p_data->io_req.oob_data);
+      break;
+
+    case BTM_LE_CONSENT_REQ_EVT:
+      sec_event.ble_req.bd_addr = bda;
+      p_name = BTM_SecReadDevName(bda);
+      if (p_name != NULL)
+        strlcpy((char*)sec_event.ble_req.bd_name, p_name, BD_NAME_LEN);
+      else
+        sec_event.ble_req.bd_name[0] = 0;
+      bta_dm_cb.p_sec_cback(BTA_DM_BLE_CONSENT_REQ_EVT, &sec_event);
       break;
 
     case BTM_LE_SEC_REQUEST_EVT:
