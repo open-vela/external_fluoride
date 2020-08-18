eeb1a8e1e33b30e4c128ca2a33662757ae6be1af
liujinye <liujinye@xiaomi.com>
2020-08-18 19:14:51 +0800

    Merge "hci: Support reading peer SCA" am: 8b10a82283 am: ec9665e869 am: d60bc3d27a
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1398107
    
    Change-Id: I2676538966e333281bb2787fe69dd872cccee0e6

--- gd/security/facade_configuration_api.cc
+++ gd/security/facade_configuration_api.cc
@@ -24,10 +24,6 @@
 namespace bluetooth {
 namespace security {
 
-void FacadeConfigurationApi::SetDisconnectCallback(internal::SecurityManagerImpl::FacadeDisconnectCallback callback) {
-  security_handler_->CallOn(security_manager_impl_, &internal::SecurityManagerImpl::SetDisconnectCallback, callback);
-}
-
 void FacadeConfigurationApi::SetIoCapability(hci::IoCapability io_capability) {
   security_handler_->CallOn(security_manager_impl_, &internal::SecurityManagerImpl::SetIoCapability, io_capability);
 }
