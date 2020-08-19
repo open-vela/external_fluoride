048b4ec1e61f5bddd6fba0387cdb5262bcb63b7b
liujinye <liujinye@xiaomi.com>
2020-08-19 21:24:35 +0800

    Merge changes I8fd02d8f,Ia21eeb73 am: c5fc120586 am: 2c90830659
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1405268
    
    Change-Id: I5a2fcb5bdba4dcb84601a75d2ce356c30c1b69c4

--- bta/dm/bta_dm_act.cc
+++ bta/dm/bta_dm_act.cc
@@ -410,8 +410,8 @@ void bta_dm_disable() {
   /* disable all active subsystems */
   bta_sys_disable();
 
-  BTM_SetDiscoverability(BTM_NON_DISCOVERABLE);
-  BTM_SetConnectability(BTM_NON_CONNECTABLE);
+  BTM_SetDiscoverability(BTM_NON_DISCOVERABLE, 0, 0);
+  BTM_SetConnectability(BTM_NON_CONNECTABLE, 0, 0);
 
   bta_dm_disable_pm();
   bta_dm_disable_search_and_disc();
@@ -513,8 +513,8 @@ bool BTA_DmSetVisibility(bt_scan_mode_t mode) {
       return false;
   }
 
-  BTM_SetDiscoverability(disc_mode_param);
-  BTM_SetConnectability(conn_mode_param);
+  BTM_SetDiscoverability(disc_mode_param, 0, 0);
+  BTM_SetConnectability(conn_mode_param, 0, 0);
   return true;
 }
 
@@ -3183,6 +3183,16 @@ static uint8_t bta_dm_ble_smp_cback(tBTM_LE_EVT event, const RawAddress& bda,
                  &p_data->io_req.init_keys, &p_data->io_req.resp_keys);
       APPL_TRACE_EVENT("io mitm: %d oob_data:%d", p_data->io_req.auth_req,
                        p_data->io_req.oob_data);
+      break;
+
+    case BTM_LE_CONSENT_REQ_EVT:
+      sec_event.ble_req.bd_addr = bda;
+      p_name = BTM_SecReadDevName(bda);
+      if (p_name != NULL)
+        strlcpy((char*)sec_event.ble_req.bd_name, p_name, BD_NAME_LEN);
+      else
+        sec_event.ble_req.bd_name[0] = 0;
+      bta_dm_cb.p_sec_cback(BTA_DM_BLE_CONSENT_REQ_EVT, &sec_event);
       break;
 
     case BTM_LE_SEC_REQUEST_EVT:
