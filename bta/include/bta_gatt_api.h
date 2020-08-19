6c1635303e45c7040c52669c1c12aa58b2de7947
openvela-robot <openvela-robot@xiaomi.com>
2020-08-19 11:43:39 +0800

    Simplify BTM_SetConnectability
    The windows are fixed defaults, and need no validation.
    
    Bug: 159815595
    Tag: #refactor
    Test: compile & verify basic functions working
    Change-Id: Ib3e5580af6d742333f2946d393d7bdcded7d9737

--- bta/include/bta_gatt_api.h
+++ bta/include/bta_gatt_api.h
@@ -387,7 +387,7 @@ using BtaAppRegisterCallback =
  * p_client_cb - pointer to the application callback function.
  **/
 extern void BTA_GATTC_AppRegister(tBTA_GATTC_CBACK* p_client_cb,
-                                  BtaAppRegisterCallback cb, bool eatt_support);
+                                  BtaAppRegisterCallback cb);
 
 /*******************************************************************************
  *
@@ -803,13 +803,12 @@ extern void BTA_GATTS_Disable(void);
  *
  * Parameters       p_app_uuid - applicaiton UUID
  *                  p_cback - pointer to the application callback function.
- *                  eatt_support: indicate eatt support.
  *
  * Returns          None
  *
  ******************************************************************************/
 extern void BTA_GATTS_AppRegister(const bluetooth::Uuid& app_uuid,
-                                  tBTA_GATTS_CBACK* p_cback, bool eatt_support);
+                                  tBTA_GATTS_CBACK* p_cback);
 
 /*******************************************************************************
  *
