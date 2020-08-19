044bfd44f84ae6caf3ef0c9226917abc41fd7d85
liujinye <liujinye@xiaomi.com>
2020-08-19 11:10:01 +0800

    These were never set
    Bug: 159815595
    Tag: #refactor
    Test: compile & verify basic functions working
    Change-Id: Ifaec802bff1fa8e1cc497e86a7d22d458f18fae8

--- bta/dm/bta_dm_act.cc
+++ bta/dm/bta_dm_act.cc
@@ -489,34 +489,33 @@ void bta_dm_set_dev_name(const std::vector<uint8_t>& name) {
 }
 
 /** Sets discoverability, connectability and pairability */
-void bta_dm_set_visibility(tBTA_DM_DISC disc_mode_param,
-                           tBTA_DM_CONN conn_mode_param) {
-  uint16_t window, interval;
-  uint16_t le_disc_mode = BTM_BleReadDiscoverability();
-  uint16_t le_conn_mode = BTM_BleReadConnectability();
-  uint16_t disc_mode = BTM_ReadDiscoverability(&window, &interval);
-  uint16_t conn_mode = BTM_ReadConnectability(&window, &interval);
+bool BTA_DmSetVisibility(bt_scan_mode_t mode) {
+  tBTA_DM_DISC disc_mode_param;
+  tBTA_DM_CONN conn_mode_param;
 
-  /* set modes for Discoverability and connectability if not ignore */
-  if (disc_mode_param != (BTA_DM_IGNORE | BTA_DM_LE_IGNORE)) {
-    if ((disc_mode_param & BTA_DM_LE_IGNORE) == BTA_DM_LE_IGNORE)
-      disc_mode_param = ((disc_mode_param & ~BTA_DM_LE_IGNORE) | le_disc_mode);
-    if ((disc_mode_param & BTA_DM_IGNORE) == BTA_DM_IGNORE)
-      disc_mode_param = ((disc_mode_param & ~BTA_DM_IGNORE) | disc_mode);
+  switch (mode) {
+    case BT_SCAN_MODE_NONE:
+      disc_mode_param = BTA_DM_NON_DISC;
+      conn_mode_param = BTA_DM_NON_CONN;
+      break;
 
-    BTM_SetDiscoverability(disc_mode_param, bta_dm_cb.inquiry_scan_window,
-                           bta_dm_cb.inquiry_scan_interval);
+    case BT_SCAN_MODE_CONNECTABLE:
+      disc_mode_param = BTA_DM_NON_DISC;
+      conn_mode_param = BTA_DM_CONN;
+      break;
+
+    case BT_SCAN_MODE_CONNECTABLE_DISCOVERABLE:
+      disc_mode_param = BTA_DM_GENERAL_DISC;
+      conn_mode_param = BTA_DM_CONN;
+      break;
+
+    default:
+      return false;
   }
 
-  if (conn_mode_param != (BTA_DM_IGNORE | BTA_DM_LE_IGNORE)) {
-    if ((conn_mode_param & BTA_DM_LE_IGNORE) == BTA_DM_LE_IGNORE)
-      conn_mode_param = ((conn_mode_param & ~BTA_DM_LE_IGNORE) | le_conn_mode);
-    if ((conn_mode_param & BTA_DM_IGNORE) == BTA_DM_IGNORE)
-      conn_mode_param = ((conn_mode_param & ~BTA_DM_IGNORE) | conn_mode);
-
-    BTM_SetConnectability(conn_mode_param, bta_dm_cb.page_scan_window,
-                          bta_dm_cb.page_scan_interval);
-  }
+  BTM_SetDiscoverability(disc_mode_param, 0, 0);
+  BTM_SetConnectability(conn_mode_param, 0, 0);
+  return true;
 }
 
 static void bta_dm_process_remove_device_no_callback(
@@ -3186,16 +3185,6 @@ static uint8_t bta_dm_ble_smp_cback(tBTM_LE_EVT event, const RawAddress& bda,
                        p_data->io_req.oob_data);
       break;
 
-    case BTM_LE_CONSENT_REQ_EVT:
-      sec_event.ble_req.bd_addr = bda;
-      p_name = BTM_SecReadDevName(bda);
-      if (p_name != NULL)
-        strlcpy((char*)sec_event.ble_req.bd_name, p_name, BD_NAME_LEN);
-      else
-        sec_event.ble_req.bd_name[0] = 0;
-      bta_dm_cb.p_sec_cback(BTA_DM_BLE_CONSENT_REQ_EVT, &sec_event);
-      break;
-
     case BTM_LE_SEC_REQUEST_EVT:
       sec_event.ble_req.bd_addr = bda;
       p_name = BTM_SecReadDevName(bda);
@@ -3510,7 +3499,7 @@ static void bta_dm_gattc_register(void) {
                             else
                               bta_dm_search_cb.client_if = BTA_GATTS_INVALID_IF;
 
-                          }), false);
+                          }));
   }
 }
 
