7041cfa9703612601d1eb481cd03ccf633b230ec
liujinye <liujinye@xiaomi.com>
2020-08-24 15:11:48 +0800

    Remove sys id param from bta_ar_reg_avdt
    Bug: 159815595
    Tag: #refactor
    Test: compile & verify basic functions working
    Change-Id: Ia0ead371d73a289f773d560a6b1125074f6e1343

--- bta/dm/bta_dm_act.cc
+++ bta/dm/bta_dm_act.cc
@@ -2393,7 +2393,7 @@ static void bta_dm_rm_cback(tBTA_SYS_CONN_STATUS status, uint8_t id,
     }
   }
 
-  if ((BTA_ID_AV == id) || (BTA_ID_AVK == id)) {
+  if (BTA_ID_AV == id) {
     if (status == BTA_SYS_CONN_BUSY) {
       if (p_dev) p_dev->info |= BTA_DM_DI_AV_ACTIVE;
       /* AV calls bta_sys_conn_open with the A2DP stream count as app_id */
@@ -3124,16 +3124,6 @@ static uint8_t bta_dm_ble_smp_cback(tBTM_LE_EVT event, const RawAddress& bda,
                  &p_data->io_req.init_keys, &p_data->io_req.resp_keys);
       APPL_TRACE_EVENT("io mitm: %d oob_data:%d", p_data->io_req.auth_req,
                        p_data->io_req.oob_data);
-      break;
-
-    case BTM_LE_CONSENT_REQ_EVT:
-      sec_event.ble_req.bd_addr = bda;
-      p_name = BTM_SecReadDevName(bda);
-      if (p_name != NULL)
-        strlcpy((char*)sec_event.ble_req.bd_name, p_name, BD_NAME_LEN);
-      else
-        sec_event.ble_req.bd_name[0] = 0;
-      bta_dm_cb.p_sec_cback(BTA_DM_BLE_CONSENT_REQ_EVT, &sec_event);
       break;
 
     case BTM_LE_SEC_REQUEST_EVT:
