8ff96b82a18cb59c5688b94171933d4c09037790
openvela-robot <openvela-robot@xiaomi.com>
2020-08-25 02:37:46 +0800

    Merge changes I3df02f6a,I97d16350,Ia6ffaa4f am: 81b7d5872d am: 9b97b3b322 am: a327e653b8
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1409089
    
    Change-Id: Iac7638831d20409c0f495d8980b4826cac7f1610

--- internal_include/bt_target.h
+++ internal_include/bt_target.h
@@ -71,6 +71,10 @@
 #define BTA_HH_LE_INCLUDED TRUE
 #endif
 
+#ifndef BTA_AR_INCLUDED
+#define BTA_AR_INCLUDED TRUE
+#endif
+
 #ifndef BTA_AV_SINK_INCLUDED
 #define BTA_AV_SINK_INCLUDED FALSE
 #endif
--- stack/avct/avct_api.cc
+++ stack/avct/avct_api.cc
@@ -29,7 +29,6 @@
 #include "bt_target.h"
 #include "bt_types.h"
 #include "bt_utils.h"
-#include "bta/include/bta_api.h"
 #include "btm_api.h"
 #include "l2c_api.h"
 #include "l2cdefs.h"
@@ -53,7 +52,8 @@ tAVCT_CB avct_cb;
  * Returns          void
  *
  ******************************************************************************/
-void AVCT_Register(uint16_t mtu, UNUSED_ATTR uint16_t mtu_br) {
+void AVCT_Register(uint16_t mtu, UNUSED_ATTR uint16_t mtu_br,
+                   uint8_t sec_mask) {
   AVCT_TRACE_API("AVCT_Register");
 
   /* initialize AVCTP data structures */
@@ -67,8 +67,7 @@ void AVCT_Register(uint16_t mtu, UNUSED_ATTR uint16_t mtu_br) {
   L2CA_Register(AVCT_PSM, (tL2CAP_APPL_INFO*)&avct_l2c_appl,
                 true /* enable_snoop */, nullptr, avct_cb.mtu);
 
-  BTM_SimpleSetSecurityLevel(BTM_SEC_SERVICE_AVCTP, BTA_SEC_AUTHENTICATE,
-                             AVCT_PSM);
+  BTM_SimpleSetSecurityLevel(BTM_SEC_SERVICE_AVCTP, sec_mask, AVCT_PSM);
 
   /* Include the browsing channel which uses eFCR */
   tL2CAP_ERTM_INFO ertm_info;
@@ -87,8 +86,7 @@ void AVCT_Register(uint16_t mtu, UNUSED_ATTR uint16_t mtu_br) {
 
   /* AVCTP browsing channel uses the same security service as AVCTP control
    * channel */
-  BTM_SimpleSetSecurityLevel(BTM_SEC_SERVICE_AVCTP, BTA_SEC_AUTHENTICATE,
-                             AVCT_BR_PSM);
+  BTM_SimpleSetSecurityLevel(BTM_SEC_SERVICE_AVCTP, sec_mask, AVCT_BR_PSM);
 
 #if defined(AVCT_INITIAL_TRACE_LEVEL)
   avct_cb.trace_level = AVCT_INITIAL_TRACE_LEVEL;
