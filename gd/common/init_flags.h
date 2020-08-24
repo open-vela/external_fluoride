b70fb2d8dbbae4b58c0de25f974ed4f685795235
liujinye <liujinye@xiaomi.com>
2020-08-25 06:42:41 +0800

    Re-include stack/l2cap/l2c_api am: 302c600873 am: 5ac234b24d am: 6d469d5c49 am: ac16cd0abb
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1409098
    
    Change-Id: I7d5b7195cb3362e507d0c84aa78705a63151943d

--- gd/common/init_flags.h
+++ gd/common/init_flags.h
@@ -35,10 +35,6 @@ class InitFlags final {
     return gd_core_enabled;
   }
 
-  static bool GattRobustCachingEnabled() {
-    return gatt_robust_caching_enabled;
-  }
-
   static void SetAllForTesting();
 
  private:
@@ -46,7 +42,6 @@ class InitFlags final {
   static bool gd_hci_enabled;
   static bool gd_controller_enabled;
   static bool gd_core_enabled;
-  static bool gatt_robust_caching_enabled;
 };
 
 }  // namespace common
