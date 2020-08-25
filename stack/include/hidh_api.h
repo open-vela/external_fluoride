edd9dc5512ef051abd807be9044fa90d21429a15
openvela-robot <openvela-robot@xiaomi.com>
2020-08-25 22:59:54 +0800

    Merge "Break out stack/include/hci_error_code" am: d508feee16 am: 2f710b09d8
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1410827
    
    Change-Id: I84ae6654275b01dc87a7083695368e3d45c1ecda

--- stack/include/hidh_api.h
+++ stack/include/hidh_api.h
@@ -196,6 +196,16 @@ extern tHID_STATUS HID_HostCloseDev(uint8_t dev_handle);
 extern void HID_HostInit(void);
 
 /*******************************************************************************
+ * Function        HID_HostSetSecurityLevel
+ *
+ * Description     This function sets the security level for the devices which
+ *                 are marked by application as requiring security
+ *
+ * Returns         tHID_STATUS
+ ******************************************************************************/
+extern tHID_STATUS HID_HostSetSecurityLevel();
+
+/*******************************************************************************
  *
  * Function         HID_HostSetTraceLevel
  *
