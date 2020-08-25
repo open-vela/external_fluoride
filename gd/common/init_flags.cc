974194d663a07d88ee6a883e6a8a69ca0fab9930
liujinye <liujinye@xiaomi.com>
2020-08-26 02:16:18 +0800

    Add L2CA_Register2, which does security for you am: 77bf41237d am: 925d0bdb65 am: f665d70525
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1410831
    
    Change-Id: Id4331a21f9347c4995a5da8b2053f0074f5fdc5f

--- gd/common/init_flags.cc
+++ gd/common/init_flags.cc
@@ -34,9 +34,6 @@ bool InitFlags::gd_controller_enabled = false;
 const std::string kGdCoreFlag = "INIT_gd_core";
 bool InitFlags::gd_core_enabled = false;
 
-const std::string kGattRobustCachingFlag = "INIT_gatt_robust_caching";
-bool InitFlags::gatt_robust_caching_enabled = false;
-
 void InitFlags::Load(const char** flags) {
   SetAll(false);
   while (flags != nullptr && *flags != nullptr) {
@@ -46,8 +43,6 @@ void InitFlags::Load(const char** flags) {
       gd_hci_enabled = true;
     } else if (kGdControllerFlag == *flags) {
       gd_controller_enabled = true;
-    } else if (kGattRobustCachingFlag == *flags) {
-      gatt_robust_caching_enabled = true;
     }
     flags++;
   }
@@ -70,7 +65,6 @@ void InitFlags::SetAll(bool value) {
   gd_core_enabled = value;
   gd_controller_enabled = value;
   gd_hci_enabled = value;
-  gatt_robust_caching_enabled = value;
 }
 
 void InitFlags::SetAllForTesting() {
