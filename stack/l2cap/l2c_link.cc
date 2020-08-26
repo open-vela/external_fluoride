b6a85578d5c30cbd40e254e0ede5bddb9166d48d
openvela-robot <openvela-robot@xiaomi.com>
2020-08-26 22:01:09 +0800

    Merge "GD-Cert: Add btconfig parameter to real device test" am: ef524cc9dc am: 1169a3a388 am: 87f1fb43e1 am: 32f6726493
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1412291
    
    Change-Id: I016ec00fc2c33c70a7a9a1f9902c42992a0f1d03

--- stack/l2cap/l2c_link.cc
+++ stack/l2cap/l2c_link.cc
@@ -125,6 +125,7 @@ void l2c_link_hci_conn_comp(uint8_t status, uint16_t handle,
   tL2C_CONN_INFO ci;
   tL2C_LCB* p_lcb;
   tL2C_CCB* p_ccb;
+  tBTM_SEC_DEV_REC* p_dev_info = NULL;
 
   /* Save the parameters */
   ci.status = status;
@@ -169,7 +170,13 @@ void l2c_link_hci_conn_comp(uint8_t status, uint16_t handle,
     l2cu_send_peer_info_req(p_lcb, L2CAP_EXTENDED_FEATURES_INFO_TYPE);
 
     /* Tell BTM Acl management about the link */
-    btm_acl_created(ci.bd_addr, handle, p_lcb->LinkRole(), BT_TRANSPORT_BR_EDR);
+    p_dev_info = btm_find_dev(p_bda);
+    if (p_dev_info != NULL)
+      btm_acl_created(ci.bd_addr, handle, p_lcb->LinkRole(),
+                      BT_TRANSPORT_BR_EDR);
+    else
+      btm_acl_created(ci.bd_addr, handle, p_lcb->LinkRole(),
+                      BT_TRANSPORT_BR_EDR);
 
     BTM_SetLinkSuperTout(ci.bd_addr, acl_get_link_supervision_timeout());
 
