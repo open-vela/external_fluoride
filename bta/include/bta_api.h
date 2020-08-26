b0ce11ff2843bf5270d4da7b958a40a690f656b4
liujinye <liujinye@xiaomi.com>
2020-08-26 17:19:40 +0800

    Merge changes I5b76ec20,I11c768ec,I36a436de,I7b30020b,Ie6b729f1, ... am: 11d2d12d4b am: 6a8bed3e1b am: 620c34749e am: ac9b2b4148
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1411267
    
    Change-Id: I41dd96180588c0880778b7cb4a5786209fd28c89

--- bta/include/bta_api.h
+++ bta/include/bta_api.h
@@ -289,6 +289,7 @@ typedef uint8_t tBTA_SIG_STRENGTH_MASK;
                                     */
 #define BTA_DM_ENER_INFO_READ 28 /* Energy info read */
 #define BTA_DM_BLE_SC_OOB_REQ_EVT 29 /* SMP SC OOB request event */
+#define BTA_DM_BLE_CONSENT_REQ_EVT 30 /* SMP consent request event */
 typedef uint8_t tBTA_DM_SEC_EVT;
 
 /* Structure associated with BTA_DM_PIN_REQ_EVT */
@@ -1039,10 +1040,10 @@ extern void BTA_GetEirService(uint8_t* p_eir, size_t eir_len,
  *
  * Description      Returns whether the remote device is currently connected.
  *
- * Returns          true if the device is NOT connected, false otherwise.
+ * Returns          0 if the device is NOT connected.
  *
  ******************************************************************************/
-extern bool BTA_DmGetConnectionState(const RawAddress& bd_addr);
+extern uint16_t BTA_DmGetConnectionState(const RawAddress& bd_addr);
 
 /*******************************************************************************
  *
