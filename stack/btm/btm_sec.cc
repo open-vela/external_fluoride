b743984ce8f365868dfe9acd4ea91b5841601ef1
openvela-robot <openvela-robot@xiaomi.com>
2020-08-28 07:31:41 +0800

    Hearing aid doesn't care about credit received event am: 181808d214 am: 1685429aa7
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1413763
    
    Change-Id: I2f76b28370f1f237b55d59543a4d5cceeee0963e

--- stack/btm/btm_sec.cc
+++ stack/btm/btm_sec.cc
@@ -52,6 +52,7 @@
 extern void btm_ble_advertiser_notify_terminated_legacy(
     uint8_t status, uint16_t connection_handle);
 extern void bta_dm_remove_device(const RawAddress& bd_addr);
+extern void bta_dm_process_remove_device(const RawAddress& bd_addr);
 
 /*******************************************************************************
  *             L O C A L    F U N C T I O N     P R O T O T Y P E S            *
@@ -2530,6 +2531,13 @@ void btm_sec_rmt_host_support_feat_evt(uint8_t* p) {
  *
  ******************************************************************************/
 void btm_io_capabilities_req(const RawAddress& p) {
+  if (btm_sec_is_a_bonded_dev(p)) {
+    BTM_TRACE_WARNING(
+        "%s: Incoming bond request, but %s is already bonded (removing)",
+        __func__, p.ToString().c_str());
+    bta_dm_process_remove_device(p);
+  }
+
   tBTM_SEC_DEV_REC* p_dev_rec = btm_find_or_alloc_dev(p);
 
   if ((btm_cb.security_mode == BTM_SEC_MODE_SC) &&
@@ -2558,7 +2566,7 @@ void btm_io_capabilities_req(const RawAddress& p) {
 
   BTM_TRACE_EVENT("%s: State: %s", __func__,
                   btm_pair_state_descr(btm_cb.pairing_state));
-
+ 
   BTM_TRACE_DEBUG("%s:Security mode: %d, Num Read Remote Feat pages: %d",
                   __func__, btm_cb.security_mode, p_dev_rec->num_read_pages);
 
@@ -3385,8 +3393,12 @@ void btm_sec_encrypt_change(uint16_t handle, uint8_t status,
         /* BR/EDR is encrypted with LK that can be used to derive LE LTK */
         p_dev_rec->new_encryption_key_is_p256 = false;
 
-        BTM_TRACE_DEBUG("%s start SM over BR/EDR", __func__);
-        SMP_BR_PairWith(p_dev_rec->bd_addr);
+        if (p_dev_rec->no_smp_on_br) {
+          BTM_TRACE_DEBUG("%s NO SM over BR/EDR", __func__);
+        } else {
+          BTM_TRACE_DEBUG("%s start SM over BR/EDR", __func__);
+          SMP_BR_PairWith(p_dev_rec->bd_addr);
+        }
       }
     } else {
       // BR/EDR is successfully encrypted. Correct LK type if needed
@@ -3894,7 +3906,8 @@ void btm_sec_disconnected(uint16_t handle, uint8_t reason) {
    */
   if (is_sample_ltk(p_dev_rec->ble.keys.pltk)) {
     android_errorWriteLog(0x534e4554, "128437297");
-    LOG(INFO) << __func__ << " removing bond to device that used sample LTK: " << p_dev_rec->bd_addr;
+    LOG(INFO) << __func__ << " removing bond to device that used sample LTK: "
+              << p_dev_rec->bd_addr;
 
     bta_dm_remove_device(p_dev_rec->bd_addr);
   }
