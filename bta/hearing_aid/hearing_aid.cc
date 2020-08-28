d303af4fef3ebb91deb54d82b6e8a2020ed9f037
liujinye <liujinye@xiaomi.com>
2020-08-28 21:34:08 +0800

    Merge "Send a response to an smp security request depending on the callback event" into qt-dev

--- bta/hearing_aid/hearing_aid.cc
+++ bta/hearing_aid/hearing_aid.cc
@@ -20,15 +20,12 @@
 
 #include "bta_gatt_api.h"
 #include "bta_gatt_queue.h"
-#include "btm_api.h"
+#include "btm_int.h"
 #include "device/include/controller.h"
 #include "embdrv/g722/g722_enc_dec.h"
 #include "gap_api.h"
 #include "gatt_api.h"
 #include "osi/include/properties.h"
-#include "stack/btm/btm_sec.h"
-#include "stack/include/acl_api.h"
-#include "types/bt_transport.h"
 
 #include <base/bind.h>
 #include <base/logging.h>
@@ -94,7 +91,8 @@ Uuid LE_PSM_UUID               = Uuid::FromString("2d410339-82b6-42aa-b34e-e2e01
 // clang-format on
 
 void hearingaid_gattc_callback(tBTA_GATTC_EVT event, tBTA_GATTC* p_data);
-void encryption_callback(const RawAddress*, tBT_TRANSPORT, void*, tBTM_STATUS);
+void encryption_callback(const RawAddress*, tGATT_TRANSPORT, void*,
+                         tBTM_STATUS);
 void read_rssi_cb(void* p_void);
 
 inline BT_HDR* malloc_l2cap_buf(uint16_t len) {
@@ -234,7 +232,7 @@ class HearingAidImpl : public HearingAid {
   uint16_t overwrite_min_ce_len;
 
  public:
-  ~HearingAidImpl() override = default;
+  virtual ~HearingAidImpl() = default;
 
   HearingAidImpl(bluetooth::hearing_aid::HearingAidCallbacks* callbacks,
                  Closure initCb)
@@ -276,7 +274,7 @@ class HearingAidImpl : public HearingAid {
               instance->gatt_if = client_id;
               initCb.Run();
             },
-            initCb), false);
+            initCb));
   }
 
   uint16_t UpdateBleConnParams(const RawAddress& address) {
@@ -314,13 +312,13 @@ class HearingAidImpl : public HearingAid {
   void Connect(const RawAddress& address) override {
     DVLOG(2) << __func__ << " " << address;
     hearingDevices.Add(HearingDevice(address, true));
-    BTA_GATTC_Open(gatt_if, address, true, BT_TRANSPORT_LE, false);
+    BTA_GATTC_Open(gatt_if, address, true, GATT_TRANSPORT_LE, false);
   }
 
   void AddToWhiteList(const RawAddress& address) override {
     VLOG(2) << __func__ << " address: " << address;
     hearingDevices.Add(HearingDevice(address, true));
-    BTA_GATTC_Open(gatt_if, address, false, BT_TRANSPORT_LE, false);
+    BTA_GATTC_Open(gatt_if, address, false, GATT_TRANSPORT_LE, false);
   }
 
   void AddFromStorage(const HearingDevice& dev_info, uint16_t is_white_listed) {
@@ -335,7 +333,8 @@ class HearingAidImpl : public HearingAid {
       // BTM_BleSetConnScanParams(2048, 1024);
 
       /* add device into BG connection to accept remote initiated connection */
-      BTA_GATTC_Open(gatt_if, dev_info.address, false, BT_TRANSPORT_LE, false);
+      BTA_GATTC_Open(gatt_if, dev_info.address, false, GATT_TRANSPORT_LE,
+                     false);
     }
 
     callbacks->OnDeviceAvailable(dev_info.capabilities, dev_info.hi_sync_id,
@@ -346,7 +345,7 @@ class HearingAidImpl : public HearingAid {
 
   void OnGattConnected(tGATT_STATUS status, uint16_t conn_id,
                        tGATT_IF client_if, RawAddress address,
-                       tBT_TRANSPORT transport, uint16_t mtu) {
+                       tBTA_TRANSPORT transport, uint16_t mtu) {
     VLOG(2) << __func__ << ": address=" << address << ", conn_id=" << conn_id;
 
     HearingDevice* hearingDevice = hearingDevices.FindByAddress(address);
@@ -391,32 +390,35 @@ class HearingAidImpl : public HearingAid {
       hearingDevice->connection_update_status = AWAITING;
     }
 
-    if (controller_get_interface()->supports_ble_2m_phy()) {
-      LOG(INFO) << address << " set preferred PHY to 2M";
-      BTM_BleSetPhy(address, PHY_LE_2M, PHY_LE_2M, 0);
-    }
-
     // Set data length
     // TODO(jpawlowski: for 16khz only 87 is required, optimize
     BTM_SetBleDataLength(address, 167);
 
-    if (BTM_SecIsSecurityPending(address)) {
-      /* if security collision happened, wait for encryption done
-       * (BTA_GATTC_ENC_CMPL_CB_EVT) */
-      return;
+    tBTM_SEC_DEV_REC* p_dev_rec = btm_find_dev(address);
+    if (p_dev_rec) {
+      if (p_dev_rec->sec_state == BTM_SEC_STATE_ENCRYPTING ||
+          p_dev_rec->sec_state == BTM_SEC_STATE_AUTHENTICATING) {
+        /* if security collision happened, wait for encryption done
+         * (BTA_GATTC_ENC_CMPL_CB_EVT) */
+        return;
+      }
     }
 
     /* verify bond */
-    if (BTM_IsEncrypted(address, BT_TRANSPORT_LE)) {
+    uint8_t sec_flag = 0;
+    BTM_GetSecurityFlagsByTransport(address, &sec_flag, BT_TRANSPORT_LE);
+
+    if (sec_flag & BTM_SEC_FLAG_ENCRYPTED) {
       /* if link has been encrypted */
       OnEncryptionComplete(address, true);
       return;
     }
 
-    if (BTM_IsLinkKeyKnown(address, BT_TRANSPORT_LE)) {
+    if (sec_flag & BTM_SEC_FLAG_LKEY_KNOWN) {
       /* if bonded and link not encrypted */
-      BTM_SetEncryption(address, BT_TRANSPORT_LE, encryption_callback, nullptr,
-                        BTM_BLE_SEC_ENCRYPT);
+      sec_flag = BTM_BLE_SEC_ENCRYPT;
+      BTM_SetEncryption(address, BTA_TRANSPORT_LE, encryption_callback, nullptr,
+                        sec_flag);
       return;
     }
 
@@ -912,6 +914,9 @@ class HearingAidImpl : public HearingAid {
     }
 
     if (hearingDevice->first_connection) {
+      /* add device into BG connection to accept remote initiated connection */
+      BTA_GATTC_Open(gatt_if, address, false, GATT_TRANSPORT_LE, false);
+
       btif_storage_add_hearing_aid(*hearingDevice);
 
       hearingDevice->first_connection = false;
@@ -1000,16 +1005,13 @@ class HearingAidImpl : public HearingAid {
     }
   }
 
-  void OnAudioSuspend(const std::function<void()>& stop_audio_ticks) {
-    CHECK(stop_audio_ticks) << "stop_audio_ticks is empty";
-
+  void OnAudioSuspend() {
     if (!audio_running) {
       LOG(WARNING) << __func__ << ": Unexpected audio suspend";
     } else {
       LOG(INFO) << __func__ << ": audio_running=" << audio_running;
     }
     audio_running = false;
-    stop_audio_ticks();
 
     std::vector<uint8_t> stop({CONTROL_POINT_OP_STOP});
     for (auto& device : hearingDevices.devices) {
@@ -1030,33 +1032,23 @@ class HearingAidImpl : public HearingAid {
     }
   }
 
-  void OnAudioResume(const std::function<void()>& start_audio_ticks) {
-    CHECK(start_audio_ticks) << "start_audio_ticks is empty";
-
+  void OnAudioResume() {
     if (audio_running) {
       LOG(ERROR) << __func__ << ": Unexpected Audio Resume";
     } else {
       LOG(INFO) << __func__ << ": audio_running=" << audio_running;
     }
-
-    for (auto& device : hearingDevices.devices) {
-      if (!device.accepting_audio) continue;
-      audio_running = true;
-      SendStart(&device);
-    }
-
-    if (!audio_running) {
-      LOG(INFO) << __func__ << ": No device (0/" << GetDeviceCount()
-                << ") ready to start";
-      return;
-    }
+    audio_running = true;
 
     // TODO: shall we also reset the encoder ?
     encoder_state_release();
     encoder_state_init();
     seq_counter = 0;
 
-    start_audio_ticks();
+    for (auto& device : hearingDevices.devices) {
+      if (!device.accepting_audio) continue;
+      SendStart(&device);
+    }
   }
 
   uint8_t GetOtherSideStreamStatus(HearingDevice* this_side_device) {
@@ -1148,9 +1140,10 @@ class HearingAidImpl : public HearingAid {
     }
 
     if (left == nullptr && right == nullptr) {
-      LOG(WARNING) << __func__ << ": No more (0/" << GetDeviceCount()
-                   << ") devices ready";
-      DoDisconnectAudioStop();
+      HearingAidAudioSource::Stop();
+      audio_running = false;
+      encoder_state_release();
+      current_volume = VOLUME_UNKNOWN;
       return;
     }
 
@@ -1357,6 +1350,14 @@ class HearingAidImpl : public HearingAid {
       case GAP_EVT_CONN_UNCONGESTED:
         DVLOG(2) << "GAP_EVT_CONN_UNCONGESTED";
         break;
+
+      case GAP_EVT_LE_COC_CREDITS: {
+        auto& tmp = data->coc_credits;
+        DVLOG(2) << "GAP_EVT_LE_COC_CREDITS, for device: "
+                 << hearingDevice->address << " added" << tmp.credits_received
+                 << " credit_count: " << tmp.credit_count;
+        break;
+      }
     }
   }
 
@@ -1455,17 +1456,8 @@ class HearingAidImpl : public HearingAid {
 
     hearingDevices.Remove(address);
 
-    if (!connected) {
-      return;
-    }
-
-    callbacks->OnConnectionState(ConnectionState::DISCONNECTED, address);
-    for (const auto& device : hearingDevices.devices) {
-      if (device.accepting_audio) return;
-    }
-    LOG(INFO) << __func__ << ": No more (0/" << GetDeviceCount()
-              << ") devices ready";
-    DoDisconnectAudioStop();
+    if (connected)
+      callbacks->OnConnectionState(ConnectionState::DISCONNECTED, address);
   }
 
   void OnGattDisconnected(tGATT_STATUS status, uint16_t conn_id,
@@ -1487,19 +1479,7 @@ class HearingAidImpl : public HearingAid {
 
     DoDisconnectCleanUp(hearingDevice);
 
-    // This is needed just for the first connection. After stack is restarted,
-    // code that loads device will add them to whitelist.
-    BTA_GATTC_Open(gatt_if, hearingDevice->address, false, BT_TRANSPORT_LE,
-                   false);
-
     callbacks->OnConnectionState(ConnectionState::DISCONNECTED, remote_bda);
-
-    for (const auto& device : hearingDevices.devices) {
-      if (device.accepting_audio) return;
-    }
-    LOG(INFO) << __func__ << ": No more (0/" << GetDeviceCount()
-              << ") devices ready";
-    DoDisconnectAudioStop();
   }
 
   void DoDisconnectCleanUp(HearingDevice* hearingDevice) {
@@ -1530,13 +1510,6 @@ class HearingAidImpl : public HearingAid {
               << ", playback_started=" << hearingDevice->playback_started;
     hearingDevice->playback_started = false;
     hearingDevice->command_acked = false;
-  }
-
-  void DoDisconnectAudioStop() {
-    HearingAidAudioSource::Stop();
-    audio_running = false;
-    encoder_state_release();
-    current_volume = VOLUME_UNKNOWN;
   }
 
   void SetVolume(int8_t volume) override {
@@ -1737,7 +1710,7 @@ void hearingaid_gattc_callback(tBTA_GATTC_EVT event, tBTA_GATTC* p_data) {
   }
 }
 
-void encryption_callback(const RawAddress* address, tBT_TRANSPORT, void*,
+void encryption_callback(const RawAddress* address, tGATT_TRANSPORT, void*,
                          tBTM_STATUS status) {
   if (instance) {
     instance->OnEncryptionComplete(*address,
@@ -1750,11 +1723,14 @@ class HearingAidAudioReceiverImpl : public HearingAidAudioReceiver {
   void OnAudioDataReady(const std::vector<uint8_t>& data) override {
     if (instance) instance->OnAudioDataReady(data);
   }
-  void OnAudioSuspend(const std::function<void()>& stop_audio_ticks) override {
-    if (instance) instance->OnAudioSuspend(stop_audio_ticks);
+  void OnAudioSuspend(std::promise<void> do_suspend_promise) override {
+    if (instance) instance->OnAudioSuspend();
+    do_suspend_promise.set_value();
   }
-  void OnAudioResume(const std::function<void()>& start_audio_ticks) override {
-    if (instance) instance->OnAudioResume(start_audio_ticks);
+
+  void OnAudioResume(std::promise<void> do_resume_promise) override {
+    if (instance) instance->OnAudioResume();
+    do_resume_promise.set_value();
   }
 };
 
