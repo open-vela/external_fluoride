3b18b650ed87109a826ba32f9444d8268a2d30a9
openvela-robot <openvela-robot@xiaomi.com>
2020-08-31 18:02:07 +0800

    [automerger skipped] Don't persist bonds using sample LTK am: 70411f0877 -s ours am: f79ba4c65d am: 1c00e23d9f
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/bt/+/12484285
    
    Change-Id: I2b6213a807eedaa840a15813efed81ed17d81e6d

--- gd/os/linux_generic/repeating_alarm.cc
+++ gd/os/linux_generic/repeating_alarm.cc
@@ -17,12 +17,9 @@
 #include "os/repeating_alarm.h"
 
 #include <sys/timerfd.h>
+#include <cstring>
 #include <unistd.h>
 
-#include <cstring>
-
-#include "common/bind.h"
-#include "os/linux_generic/linux.h"
 #include "os/log.h"
 #include "os/utils.h"
 
@@ -34,29 +31,31 @@
 
 namespace bluetooth {
 namespace os {
-using common::Closure;
 
-RepeatingAlarm::RepeatingAlarm(Handler* handler) : handler_(handler), fd_(TIMERFD_CREATE(ALARM_CLOCK, 0)) {
+RepeatingAlarm::RepeatingAlarm(Thread* thread)
+  : thread_(thread),
+    fd_(timerfd_create(ALARM_CLOCK, 0)) {
   ASSERT(fd_ != -1);
 
-  token_ = handler_->thread_->GetReactor()->Register(
-      fd_, common::Bind(&RepeatingAlarm::on_fire, common::Unretained(this)), common::Closure());
+  token_ = thread_->GetReactor()->Register(fd_, [this] { on_fire(); }, nullptr);
 }
 
 RepeatingAlarm::~RepeatingAlarm() {
-  handler_->thread_->GetReactor()->Unregister(token_);
+  thread_->GetReactor()->Unregister(token_);
 
   int close_status;
-  RUN_NO_INTR(close_status = TIMERFD_CLOSE(fd_));
+  RUN_NO_INTR(close_status = close(fd_));
   ASSERT(close_status != -1);
 }
 
 void RepeatingAlarm::Schedule(Closure task, std::chrono::milliseconds period) {
   std::lock_guard<std::mutex> lock(mutex_);
   long period_ms = period.count();
-  itimerspec timer_itimerspec{{period_ms / 1000, period_ms % 1000 * 1000000},
-                              {period_ms / 1000, period_ms % 1000 * 1000000}};
-  int result = TIMERFD_SETTIME(fd_, 0, &timer_itimerspec, nullptr);
+  itimerspec timer_itimerspec{
+    {period_ms / 1000, period_ms % 1000 * 1000000},
+    {period_ms / 1000, period_ms % 1000 * 1000000}
+  };
+  int result = timerfd_settime(fd_, 0, &timer_itimerspec, nullptr);
   ASSERT(result == 0);
 
   task_ = std::move(task);
@@ -65,7 +64,7 @@ void RepeatingAlarm::Schedule(Closure task, std::chrono::milliseconds period) {
 void RepeatingAlarm::Cancel() {
   std::lock_guard<std::mutex> lock(mutex_);
   itimerspec disarm_itimerspec{/* disarm timer */};
-  int result = TIMERFD_SETTIME(fd_, 0, &disarm_itimerspec, nullptr);
+  int result = timerfd_settime(fd_, 0, &disarm_itimerspec, nullptr);
   ASSERT(result == 0);
 }
 
@@ -75,7 +74,7 @@ void RepeatingAlarm::on_fire() {
   uint64_t times_invoked;
   auto bytes_read = read(fd_, &times_invoked, sizeof(uint64_t));
   lock.unlock();
-  task.Run();
+  task();
   ASSERT(bytes_read == static_cast<ssize_t>(sizeof(uint64_t)));
 }
 
--- stack/btu/btu_task.cc
+++ stack/btu/btu_task.cc
@@ -27,12 +27,11 @@
 #include "btcore/include/module.h"
 #include "bte.h"
 #include "btif/include/btif_common.h"
-#include "btm_iso_api.h"
 #include "common/message_loop_thread.h"
 #include "osi/include/osi.h"
 #include "stack/btm/btm_int.h"
 #include "stack/include/btu.h"
-#include "stack/include/l2cap_acl_interface.h"
+#include "stack/l2cap/l2c_int.h"
 
 #include <base/bind.h>
 #include <base/logging.h>
@@ -40,7 +39,6 @@
 #include <base/threading/thread.h>
 
 using bluetooth::common::MessageLoopThread;
-using bluetooth::hci::IsoManager;
 
 /* Define BTU storage area */
 uint8_t btu_trace_level = HCI_INITIAL_TRACE_LEVEL;
@@ -73,11 +71,6 @@ void btu_hci_msg_process(BT_HDR* p_msg) {
       btu_hcif_send_cmd((uint8_t)(p_msg->event & BT_SUB_EVT_MASK), p_msg);
       break;
 
-    case BT_EVT_TO_BTU_HCI_ISO:
-      IsoManager::GetInstance()->HandleIsoData(p_msg);
-      osi_free(p_msg);
-      break;
-
     default:
       osi_free(p_msg);
       break;
@@ -99,18 +92,26 @@ bt_status_t do_in_main_thread(const base::Location& from_here,
   return BT_STATUS_SUCCESS;
 }
 
-bt_status_t do_in_main_thread_delayed(const base::Location& from_here,
-                                      base::OnceClosure task,
-                                      const base::TimeDelta& delay) {
-  if (!get_main_message_loop()->task_runner()->PostDelayedTask(
-          from_here, std::move(task), delay)) {
-    LOG(ERROR) << __func__ << ": failed from " << from_here.ToString();
-    return BT_STATUS_FAIL;
-  }
-  return BT_STATUS_SUCCESS;
-}
+void btu_task_start_up(UNUSED_ATTR void* context) {
+  LOG(INFO) << "Bluetooth chip preload is complete";
 
-void main_thread_start_up() {
+  /* Initialize the mandatory core stack control blocks
+     (BTU, BTM, L2CAP, and SDP)
+   */
+  btu_init_core();
+
+  /* Initialize any optional stack components */
+  BTE_InitStack();
+
+  bta_sys_init();
+
+  /* Initialise platform trace levels at this point as BTE_InitStack() and
+   * bta_sys_init()
+   * reset the control blocks and preset the trace level with
+   * XXX_INITIAL_TRACE_LEVEL
+   */
+  module_init(get_module(BTE_LOGMSG_MODULE));
+
   main_thread.StartUp();
   if (!main_thread.IsRunning()) {
     LOG(FATAL) << __func__ << ": unable to start btu message loop thread.";
@@ -118,6 +119,18 @@ void main_thread_start_up() {
   if (!main_thread.EnableRealTimeScheduling()) {
     LOG(FATAL) << __func__ << ": unable to enable real time scheduling";
   }
+  if (do_in_jni_thread(FROM_HERE, base::Bind(btif_init_ok, 0, nullptr)) !=
+      BT_STATUS_SUCCESS) {
+    LOG(FATAL) << __func__ << ": unable to continue starting Bluetooth";
+  }
 }
 
-void main_thread_shut_down() { main_thread.ShutDown(); }
+void btu_task_shut_down(UNUSED_ATTR void* context) {
+  // Shutdown message loop on task completed
+  main_thread.ShutDown();
+
+  module_clean_up(get_module(BTE_LOGMSG_MODULE));
+
+  bta_sys_free();
+  btu_free_core();
+}
