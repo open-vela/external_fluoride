ed092f53f9f66e636439f89965417a8e594c3944
liujinye <liujinye@xiaomi.com>
2020-09-02 15:22:06 +0800

    Merge "Fix potential overflow for gatt process write"

--- gd/hci/le_advertising_manager.h
+++ gd/hci/le_advertising_manager.h
@@ -65,7 +65,6 @@ using AdvertiserId = int32_t;
 class LeAdvertisingManager : public bluetooth::Module {
  public:
   static constexpr AdvertiserId kInvalidId = -1;
-  static constexpr uint8_t kInvalidHandle = 0xFF;
   LeAdvertisingManager();
 
   size_t GetNumberOfAdvertisingInstances() const;
