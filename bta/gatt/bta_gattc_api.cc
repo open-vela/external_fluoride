82191c5bd38ca677df007e1c08fa409cf8150635
openvela-robot <openvela-robot@xiaomi.com>
2020-09-04 09:03:19 +0800

    Merge changes from topic "bt-char-ext-prop" am: d5421f74f4
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/710986
    
    Change-Id: Ia66f3381ee94b45b29c7b84bd0e1f5b1e43738c2

--- bta/gatt/bta_gattc_api.cc
+++ bta/gatt/bta_gattc_api.cc
@@ -599,21 +599,21 @@ void BTA_GATTC_ExecuteWrite(uint16_t conn_id, bool is_execute) {
  * Description      This function is called to send handle value confirmation.
  *
  * Parameters       conn_id - connection ID.
- *                  cid
+ *                    p_char_id - characteristic ID to confirm.
  *
  * Returns          None
  *
  ******************************************************************************/
-void BTA_GATTC_SendIndConfirm(uint16_t conn_id, uint16_t cid) {
+void BTA_GATTC_SendIndConfirm(uint16_t conn_id, uint16_t handle) {
   tBTA_GATTC_API_CONFIRM* p_buf =
       (tBTA_GATTC_API_CONFIRM*)osi_calloc(sizeof(tBTA_GATTC_API_CONFIRM));
 
-  VLOG(1) << __func__ << ": conn_id=" << +conn_id << " cid=0x" << std::hex
-          << +cid;
+  VLOG(1) << __func__ << ": conn_id=" << +conn_id << " handle=0x" << std::hex
+          << +handle;
 
   p_buf->hdr.event = BTA_GATTC_API_CONFIRM_EVT;
   p_buf->hdr.layer_specific = conn_id;
-  p_buf->cid = cid;
+  p_buf->handle = handle;
 
   bta_sys_sendmsg(p_buf);
 }
