8b3a5dddcbd5ed61097c340e7b5e4874d6b51b14
liujinye <liujinye@xiaomi.com>
2020-09-09 01:00:17 +0800

    BTIF DM Shim: Remove obsolete components am: 8db8f6a4ed am: 5a7b22514a
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1414122
    
    Change-Id: I798f0d4847bf814fd7e7b4633904d43a4c7eb6dd

--- main/shim/btm_api.cc
+++ main/shim/btm_api.cc
@@ -20,7 +20,6 @@
 
 #include <mutex>
 
-#include "common/metric_id_allocator.h"
 #include "common/time_util.h"
 #include "device/include/controller.h"
 #include "gd/common/callback.h"
@@ -36,8 +35,6 @@
 #include "main/shim/stack.h"
 #include "stack/btm/btm_int_types.h"
 #include "types/raw_address.h"
-
-using bluetooth::common::MetricIdAllocator;
 
 #define BTIF_DM_DEFAULT_INQ_MAX_RESULTS 0
 #define BTIF_DM_DEFAULT_INQ_MAX_DURATION 10
@@ -432,21 +429,12 @@ class ShimBondListener : public bluetooth::security::ISecurityManagerListener {
             bluetooth::ToRawAddress(device.GetAddress()), 0, name, BTM_SUCCESS);
       }
     }
-    MetricIdAllocator::GetInstance().AllocateId(
-        bluetooth::ToRawAddress(device.GetAddress()));
-    if (!MetricIdAllocator::GetInstance().SaveDevice(
-            bluetooth::ToRawAddress(device.GetAddress()))) {
-      LOG(FATAL) << __func__ << ": Fail to save metric id for device "
-                 << bluetooth::ToRawAddress(device.GetAddress());
-    }
   }
 
   void OnDeviceUnbonded(bluetooth::hci::AddressWithType device) override {
     if (bta_callbacks_->p_bond_cancel_cmpl_callback) {
       (*bta_callbacks_->p_bond_cancel_cmpl_callback)(BTM_SUCCESS);
     }
-    MetricIdAllocator::GetInstance().ForgetDevice(
-        bluetooth::ToRawAddress(device.GetAddress()));
   }
 
   void OnDeviceBondFailed(bluetooth::hci::AddressWithType device) override {
