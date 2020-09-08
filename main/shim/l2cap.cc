9f1dc8dec2b7f4a65a4a7e788837ec2d3a735cd5
openvela-robot <openvela-robot@xiaomi.com>
2020-09-09 00:14:51 +0800

    Update btif/src/::ble_addr_type am: 0aed830450 am: e3daa9dad7 am: d6adf87ee1 am: 1ef02f0682
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1418795
    
    Change-Id: I56b6557ae547117d299223e033f4dd6890140bc6

--- main/shim/l2cap.cc
+++ main/shim/l2cap.cc
@@ -43,14 +43,17 @@ bool bluetooth::shim::legacy::PsmManager::IsPsmRegistered(uint16_t psm) const {
 }
 
 bool bluetooth::shim::legacy::PsmManager::HasClient(uint16_t psm) const {
-  return IsPsmRegistered(psm) &&
-         psm_to_callback_map_.find(psm) != psm_to_callback_map_.end();
+  return IsPsmRegistered(psm) && psm_to_callback_map_.at(psm) != nullptr;
 }
 
 void bluetooth::shim::legacy::PsmManager::RegisterPsm(
-    uint16_t psm, const tL2CAP_APPL_INFO& callbacks) {
+    uint16_t psm, const tL2CAP_APPL_INFO* callbacks) {
   CHECK(!HasClient(psm));
-  psm_to_callback_map_.try_emplace(psm, tL2CAP_APPL_INFO(callbacks));
+  psm_to_callback_map_[psm] = callbacks;
+}
+
+void bluetooth::shim::legacy::PsmManager::RegisterPsm(uint16_t psm) {
+  RegisterPsm(psm, nullptr);
 }
 
 void bluetooth::shim::legacy::PsmManager::UnregisterPsm(uint16_t psm) {
@@ -58,7 +61,7 @@ void bluetooth::shim::legacy::PsmManager::UnregisterPsm(uint16_t psm) {
   psm_to_callback_map_.erase(psm);
 }
 
-const tL2CAP_APPL_INFO bluetooth::shim::legacy::PsmManager::Callbacks(
+const tL2CAP_APPL_INFO* bluetooth::shim::legacy::PsmManager::Callbacks(
     uint16_t psm) {
   CHECK(HasClient(psm));
   return psm_to_callback_map_[psm];
@@ -161,7 +164,7 @@ uint16_t bluetooth::shim::legacy::L2cap::GetNextDynamicClassicPsm() {
 }
 
 uint16_t bluetooth::shim::legacy::L2cap::RegisterService(
-    uint16_t psm, const tL2CAP_APPL_INFO& callbacks, bool enable_snoop,
+    uint16_t psm, const tL2CAP_APPL_INFO* callbacks, bool enable_snoop,
     tL2CAP_ERTM_INFO* p_ertm_info, uint16_t required_mtu) {
   if (Classic().IsPsmRegistered(psm)) {
     LOG_WARN("Service is already registered psm:%hd", psm);
@@ -266,7 +269,7 @@ void bluetooth::shim::legacy::L2cap::OnLocalInitiatedConnectionCreated(
     }
     do_in_main_thread(
         FROM_HERE,
-        base::Bind(classic_.Callbacks(psm).pL2CA_ConnectCfm_Cb, cid,
+        base::Bind(classic_.Callbacks(psm)->pL2CA_ConnectCfm_Cb, cid,
                    connected ? (kConnectionSuccess) : (kConnectionFail)));
 
   } else {
@@ -297,7 +300,7 @@ void bluetooth::shim::legacy::L2cap::OnRemoteInitiatedConnectionCreated(
   SetDownstreamCallbacks(cid);
   do_in_main_thread(
       FROM_HERE,
-      base::Bind(classic_.Callbacks(CidToPsm(cid)).pL2CA_ConnectInd_Cb,
+      base::Bind(classic_.Callbacks(CidToPsm(cid))->pL2CA_ConnectInd_Cb,
                  raw_address, cid, psm, kUnusedId));
 }
 
@@ -323,7 +326,7 @@ void bluetooth::shim::legacy::L2cap::SetDownstreamCallbacks(uint16_t cid) {
         bt_hdr->len = data.size();
         do_in_main_thread(
             FROM_HERE,
-            base::Bind(classic_.Callbacks(CidToPsm(cid)).pL2CA_DataInd_Cb, cid,
+            base::Bind(classic_.Callbacks(CidToPsm(cid))->pL2CA_DataInd_Cb, cid,
                        base::Unretained(bt_hdr)));
       });
 
@@ -339,15 +342,15 @@ void bluetooth::shim::legacy::L2cap::SetDownstreamCallbacks(uint16_t cid) {
           do_in_main_thread(
               FROM_HERE,
               base::Bind(
-                  classic_.Callbacks(CidToPsm(cid)).pL2CA_DisconnectCfm_Cb, cid,
-                  kUnusedResult));
+                  classic_.Callbacks(CidToPsm(cid))->pL2CA_DisconnectCfm_Cb,
+                  cid, kUnusedResult));
 
         } else {
           do_in_main_thread(
               FROM_HERE,
               base::Bind(
-                  classic_.Callbacks(CidToPsm(cid)).pL2CA_DisconnectInd_Cb, cid,
-                  kDisconnectResponseRequired));
+                  classic_.Callbacks(CidToPsm(cid))->pL2CA_DisconnectInd_Cb,
+                  cid, kDisconnectResponseRequired));
         }
         cid_to_psm_map_.erase(cid);
         cid_to_remote_cid_map_.erase(cid);
@@ -385,11 +388,11 @@ bool bluetooth::shim::legacy::L2cap::ConfigRequest(
     LOG(INFO) << __func__ << "Rcvd config request";
     do_in_main_thread(
         FROM_HERE,
-        base::Bind(classic_.Callbacks(CidToPsm(cid)).pL2CA_ConfigInd_Cb, cid,
+        base::Bind(classic_.Callbacks(CidToPsm(cid))->pL2CA_ConfigInd_Cb, cid,
                    base::Unretained(&cfg_info)));
     do_in_main_thread(
         FROM_HERE,
-        base::Bind(classic_.Callbacks(CidToPsm(cid)).pL2CA_ConfigCfm_Cb, cid,
+        base::Bind(classic_.Callbacks(CidToPsm(cid))->pL2CA_ConfigCfm_Cb, cid,
                    base::Unretained(&cfg_info)));
   });
   return true;
