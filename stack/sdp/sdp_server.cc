90b4b56cb189bd9f0eaff9d0620d83b206fd4573
openvela-robot <openvela-robot@xiaomi.com>
2020-09-14 20:52:50 +0800

    add neighbor_inquiry API am: 999dcb7d7d am: 99706dcaf9
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1421138
    
    Change-Id: Ief0346d125ed41d95632a7ed4bbd652273477231

--- stack/sdp/sdp_server.cc
+++ stack/sdp/sdp_server.cc
@@ -825,4 +825,5 @@ static void process_service_search_attr_req(tCONN_CB* p_ccb, uint16_t trans_num,
   /* Send the buffer through L2CAP */
   L2CA_DataWrite(p_ccb->connection_id, p_buf);
 }
+
 #endif /* SDP_SERVER_ENABLED == TRUE */
