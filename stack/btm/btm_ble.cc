fe6110fed5b0349382f9a1adeb0d2526779ee95e
liujinye <liujinye@xiaomi.com>
2020-09-19 08:18:54 +0800

    Remove unnecessary #def BTA_LE_KEY_ am: 30de55bca1 am: 882ae8330a am: 2120656b6a
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1431908
    
    Change-Id: I1ed9938ec8b4a9afca015726dcd78d7f82528644

--- stack/btm/btm_ble.cc
+++ stack/btm/btm_ble.cc
@@ -1727,15 +1727,23 @@ void btm_ble_connected(const RawAddress& bda, uint16_t handle, uint8_t enc_mode,
                        bool addr_matched) {
   tBTM_SEC_DEV_REC* p_dev_rec = btm_find_dev(bda);
   if (!p_dev_rec) {
-    LOG_DEBUG("Creating new device record for new ble connection");
+    VLOG(1) << __func__ << " Security Manager: handle:" << handle
+            << " enc_mode:" << loghex(enc_mode) << "  bda: " << bda
+            << " p_dev_rec:" << p_dev_rec;
+    /* There is no device record for new connection.  Allocate one */
     p_dev_rec = btm_sec_alloc_dev(bda);
     if (p_dev_rec == nullptr) {
-      LOG_WARN("Unable to create device record for new ble connection");
+      LOG_WARN("%s Unable to create ble connection", __func__);
       return;
     }
-  } else {
-    LOG_DEBUG("Updating device record timestamp for existing ble connection");
-    // TODO() Why is timestamp a counter ?
+  } else /* Update the timestamp for this device */
+  {
+    VLOG(1) << __func__ << " Security Manager: handle:" << handle
+            << " enc_mode:" << loghex(enc_mode) << "  bda: " << bda
+            << " RName: " << p_dev_rec->sec_bd_name
+            << " p_dev_rec:" << p_dev_rec;
+
+    BTM_TRACE_DEBUG("btm_ble_connected sec_flags=0x%x", p_dev_rec->sec_flags);
     p_dev_rec->timestamp = btm_cb.dev_rec_count++;
   }
 
