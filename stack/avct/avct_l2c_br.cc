3b9634d6c67afa6283ff107d0af31f32faee1f30
openvela-robot <openvela-robot@xiaomi.com>
2020-09-23 20:54:42 +0800

    SDP_BROWSE_PLUS is never true am: 6065a41765 am: f1f3bc62d5 am: d6d6cc45fa am: 0bef5ab112
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1434998
    
    Change-Id: I12d2499a7f4523401694a0bf6aaf62d0bd38049b

--- stack/avct/avct_l2c_br.cc
+++ stack/avct/avct_l2c_br.cc
@@ -86,7 +86,7 @@ const tL2CAP_APPL_INFO avct_l2c_br_appl = {avct_l2c_br_connect_ind_cback,
                                            avct_l2c_br_config_ind_cback,
                                            avct_l2c_br_config_cfm_cback,
                                            avct_l2c_br_disconnect_ind_cback,
-                                           NULL,
+                                           avct_l2c_br_disconnect_cfm_cback,
                                            avct_l2c_br_data_ind_cback,
                                            avct_l2c_br_congestion_ind_cback,
                                            NULL,
@@ -335,6 +335,11 @@ void avct_l2c_br_disconnect_ind_cback(uint16_t lcid, bool ack_needed) {
   /* look up lcb for this channel */
   p_lcb = avct_bcb_by_lcid(lcid);
   if (p_lcb == NULL) return;
+
+  if (ack_needed) {
+    /* send L2CAP disconnect response */
+    L2CA_DisconnectRsp(lcid);
+  }
 
   tAVCT_LCB_EVT avct_lcb_evt;
   avct_lcb_evt.result = result;
