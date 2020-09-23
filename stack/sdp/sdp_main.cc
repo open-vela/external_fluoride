6b129940e0338c5827d293732e88b91ad95b4187
openvela-robot <openvela-robot@xiaomi.com>
2020-09-24 04:03:37 +0800

    RFCOMM code minor clean up am: 4d9eeb522b am: 09bbc3fa56 am: b29843f36d
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1435838
    
    Change-Id: Ibd970d73219f6ffc45d37c0161bdd515c2f4a647

--- stack/sdp/sdp_main.cc
+++ stack/sdp/sdp_main.cc
@@ -72,9 +72,11 @@ void sdp_init(void) {
     sdp_cb.ccb[i].sdp_conn_timer = alarm_new("sdp.sdp_conn_timer");
   }
 
-  /* Initialize the L2CAP configuration. We only care about MTU */
+  /* Initialize the L2CAP configuration. We only care about MTU and flush */
   sdp_cb.l2cap_my_cfg.mtu_present = true;
   sdp_cb.l2cap_my_cfg.mtu = SDP_MTU_SIZE;
+  sdp_cb.l2cap_my_cfg.flush_to_present = true;
+  sdp_cb.l2cap_my_cfg.flush_to = SDP_FLUSH_TO;
 
   sdp_cb.max_attr_list_size = SDP_MTU_SIZE - 16;
   sdp_cb.max_recs_per_search = SDP_MAX_DISC_SERVER_RECS;
@@ -300,6 +302,16 @@ static void sdp_config_cfm(uint16_t l2cap_cid, tL2CAP_CFG_INFO* p_cfg) {
       }
     }
   } else {
+    /* If peer has rejected FCR and suggested basic then try basic */
+    if (p_cfg->fcr_present) {
+      tL2CAP_CFG_INFO cfg = sdp_cb.l2cap_my_cfg;
+      cfg.fcr_present = false;
+      L2CA_ConfigReq(l2cap_cid, &cfg);
+
+      /* Remain in configure state */
+      return;
+    }
+
     sdp_disconnect(p_ccb, SDP_CFG_FAILED);
   }
 }
