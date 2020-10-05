cfcc62db95e80b3cd33eea3bb722a89bab4862b6
liujinye <liujinye@xiaomi.com>
2020-10-05 08:31:59 +0800

    Properly add typedef for gatt status am: 0fb7cc4885
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1448155
    
    Change-Id: Iaebf9ecfbcd5eb72c58ff5059b503ee32174ac5f

--- stack/gatt/gatt_attr.cc
+++ stack/gatt/gatt_attr.cc
@@ -500,8 +500,7 @@ static bool gatt_svc_read_cl_supp_feat_req(uint16_t conn_id,
 
   tGATT_STATUS status = GATTC_Read(conn_id, GATT_READ_BY_TYPE, &param);
   if (status != GATT_SUCCESS) {
-    LOG(ERROR) << __func__ << " Read failed. Status: "
-               << loghex(static_cast<uint8_t>(status));
+    LOG(ERROR) << __func__ << " Read failed. Status: " << loghex(status);
     return false;
   }
 
@@ -521,8 +520,7 @@ static bool gatt_att_write_cl_supp_feat(uint16_t conn_id, uint16_t handle) {
 
   tGATT_STATUS status = GATTC_Write(conn_id, GATT_WRITE, &attr);
   if (status != GATT_SUCCESS) {
-    LOG(ERROR) << __func__ << " Write failed. Status: "
-               << loghex(static_cast<uint8_t>(status));
+    LOG(ERROR) << __func__ << " Write failed. Status: " << loghex(status);
     return false;
   }
 
@@ -543,8 +541,8 @@ static void gatt_cl_op_cmpl_cback(uint16_t conn_id, tGATTC_OPTYPE op,
                                   tGATT_CL_COMPLETE* p_data) {
   auto iter = OngoingOps.find(conn_id);
 
-  VLOG(1) << __func__ << " opcode: " << loghex(op) << " status: " << status
-          << " conn id: " << loghex(static_cast<uint8_t>(conn_id));
+  VLOG(1) << __func__ << " opcode: " << loghex(op)
+          << " status: " << loghex(status) << " conn id: " << loghex(conn_id);
 
   if (op != GATTC_OPTYPE_READ) return;
 
