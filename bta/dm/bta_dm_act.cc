b1c7a35b4c0d102eeaaa60cdfec560b8b13c6891
liujinye <liujinye@xiaomi.com>
2020-10-13 20:50:28 +0800

    Remove unused function stack/smp/smp_utils::smp_reverse_array am: a487dd5790
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1458582
    
    Change-Id: I8e5b035cfe7954e488063591b263656785da1246

--- bta/dm/bta_dm_act.cc
+++ bta/dm/bta_dm_act.cc
@@ -2254,7 +2254,8 @@ static tBTA_DM_PEER_DEVICE* allocate_device_for(const RawAddress& bd_addr,
 static void bta_dm_acl_up(const RawAddress& bd_addr, tBT_TRANSPORT transport) {
   auto device = allocate_device_for(bd_addr, transport);
   if (device == nullptr) {
-    LOG_WARN("Unable to allocate device resources for new connection");
+    APPL_TRACE_ERROR("%s max active connection reached, no resources",
+                     __func__);
     return;
   }
   device->conn_state = BTA_DM_CONNECTED;
@@ -2273,7 +2274,6 @@ static void bta_dm_acl_up(const RawAddress& bd_addr, tBT_TRANSPORT transport) {
     conn.link_up.bd_addr = bd_addr;
 
     bta_dm_cb.p_sec_cback(BTA_DM_LINK_UP_EVT, &conn);
-    LOG_DEBUG("Executed security callback for new connection available");
   }
   bta_dm_adjust_roles(true);
 }
@@ -3269,6 +3269,16 @@ static uint8_t bta_dm_ble_smp_cback(tBTM_LE_EVT event, const RawAddress& bda,
                  &p_data->io_req.init_keys, &p_data->io_req.resp_keys);
       APPL_TRACE_EVENT("io mitm: %d oob_data:%d", p_data->io_req.auth_req,
                        p_data->io_req.oob_data);
+      break;
+
+    case BTM_LE_CONSENT_REQ_EVT:
+      sec_event.ble_req.bd_addr = bda;
+      p_name = BTM_SecReadDevName(bda);
+      if (p_name != NULL)
+        strlcpy((char*)sec_event.ble_req.bd_name, p_name, BD_NAME_LEN);
+      else
+        sec_event.ble_req.bd_name[0] = 0;
+      bta_dm_cb.p_sec_cback(BTA_DM_BLE_CONSENT_REQ_EVT, &sec_event);
       break;
 
     case BTM_LE_SEC_REQUEST_EVT:
