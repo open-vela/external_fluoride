10234bee120c091b768d01033ba2674ca7507694
Chris Manton <cmanton@google.com>
2020-10-27 23:53:20 +0000

    Use proper gatt_notify_conn_update::status type at corresponding level am: b6882a6504
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1477052
    
    Change-Id: Ib22fc5dd35634f4e03ad3c32e5db89c9d3b1c68b

--- stack/btu/btu_hcif.cc
+++ stack/btu/btu_hcif.cc
@@ -37,7 +37,6 @@
 #include "osi/include/log.h"
 #include "stack/btm/btm_int.h"
 #include "stack/include/acl_hci_link_interface.h"
-#include "stack/include/ble_acl_interface.h"
 #include "stack/include/ble_hci_link_interface.h"
 #include "stack/include/btm_iso_api.h"
 #include "stack/include/btu.h"
@@ -1723,8 +1722,10 @@ static void btu_ble_ll_conn_param_upd_evt(uint8_t* p, uint16_t evt_len) {
   STREAM_TO_UINT16(latency, p);
   STREAM_TO_UINT16(timeout, p);
 
-  acl_ble_update_event_received(static_cast<tHCI_STATUS>(status), handle,
-                                interval, latency, timeout);
+  l2cble_process_conn_update_evt(handle, status, interval, latency, timeout);
+
+  gatt_notify_conn_update(handle & 0x0FFF, interval, latency, timeout,
+                          static_cast<tHCI_STATUS>(status));
 }
 
 static void btu_ble_proc_ltk_req(uint8_t* p) {
