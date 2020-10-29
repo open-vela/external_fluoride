3a02e28bc2195ef4ea611a7e46363e6e16325856
openvela-robot <openvela-robot@xiaomi.com>
2020-10-30 07:49:33 +0800

    Re-log stack/gatt/gatt_api::GATT_Connect am: c0d111af49
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1480215
    
    Change-Id: I05dbb5178e1bed12c0abffc83959cb0bb0cf1fff

--- btif/src/btif_config.cc
+++ btif/src/btif_config.cc
@@ -150,8 +150,7 @@ bool btif_get_address_type(const RawAddress& bda, tBLE_ADDR_TYPE* p_addr_type) {
   if (!btif_config_get_int(bd_addr_str, "AddrType", &val)) return false;
   *p_addr_type = static_cast<tBLE_ADDR_TYPE>(val);
 
-  LOG_DEBUG("Device [%s] address type %s", bd_addr_str,
-            AddressTypeText(*p_addr_type).c_str());
+  LOG_INFO("Device [%s] address type %d", bd_addr_str, *p_addr_type);
   return true;
 }
 
--- gd/hci/le_advertising_manager.h
+++ gd/hci/le_advertising_manager.h
@@ -17,7 +17,6 @@
 
 #include <memory>
 
-#include "common/callback.h"
 #include "hci/address_with_type.h"
 #include "hci/hci_packets.h"
 #include "module.h"
@@ -61,15 +60,6 @@ class ExtendedAdvertisingConfig : public AdvertisingConfig {
   ExtendedAdvertisingConfig(const AdvertisingConfig& config);
 };
 
-class PeriodicAdvertisingParameters {
- public:
-  uint16_t min_interval;
-  uint16_t max_interval;
-  uint16_t properties;
-
-  enum AdvertisingProperty { INCLUDE_TX_POWER = 0x06 };
-};
-
 using AdvertiserId = uint8_t;
 
 class AdvertisingCallback {
@@ -99,7 +89,6 @@ class LeAdvertisingManager : public bluetooth::Module {
  public:
   static constexpr AdvertiserId kInvalidId = 0xFF;
   static constexpr uint8_t kInvalidHandle = 0xFF;
-  static constexpr uint8_t kAdvertisingSetIdMask = 0x0F;
   LeAdvertisingManager();
 
   size_t GetNumberOfAdvertisingInstances() const;
@@ -116,19 +105,7 @@ class LeAdvertisingManager : public bluetooth::Module {
       const common::Callback<void(ErrorCode, uint8_t, uint8_t)>& set_terminated_callback,
       os::Handler* handler);
 
-  void SetParameters(AdvertiserId advertiser_id, ExtendedAdvertisingConfig config);
-
-  void SetData(AdvertiserId advertiser_id, bool set_scan_rsp, std::vector<GapData> data);
-
-  void EnableAdvertiser(AdvertiserId advertiser_id, bool enable, uint16_t duration, uint8_t maxExtAdvEvents);
-
-  void SetPeriodicParameters(AdvertiserId advertiser_id, PeriodicAdvertisingParameters periodic_advertising_parameters);
-
-  void SetPeriodicData(AdvertiserId advertiser_id, std::vector<GapData> data);
-
-  void EnablePeriodicAdvertising(AdvertiserId advertiser_id, bool enable);
-
-  void RemoveAdvertiser(AdvertiserId advertiser_id);
+  void RemoveAdvertiser(AdvertiserId id);
 
   void RegisterAdvertisingCallback(AdvertisingCallback* advertising_callback);
 
