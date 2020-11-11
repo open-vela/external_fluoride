245bb29859623162a1ab338e5d3ef11f211e2726
Michael Sun <michaelfsun@google.com>
2020-11-11 00:00:11 +0000

    Merge changes from topics "btaa-jni-1", "btaa-jni-2"
    * changes:
      btaa: introduce btif interface for btaa
      btaa: new include files in bt stack for BTAA interface
    
    Change-Id: I9484deab4ca56ea6fc8f4a811744b8b681b67b66

--- btif/src/btif_activity_attribution.cc
+++ btif/src/btif_activity_attribution.cc
@@ -20,7 +20,6 @@
 
 #include <hardware/bt_activity_attribution.h>
 
-#include "btaa/include/activity_attribution.h"
 #include "btif/include/btif_common.h"
 #include "stack/include/btu.h"
 
@@ -38,7 +37,6 @@ class ActivityAttributionInterfaceImpl : public ActivityAttributionCallbacks,
 
   void Init(ActivityAttributionCallbacks* callbacks) override {
     this->callbacks = callbacks;
-    ActivityAttribution::Initialize(this);
   }
 
   void OnWakeup(Activity activity, const RawAddress& address) override {
@@ -48,9 +46,7 @@ class ActivityAttributionInterfaceImpl : public ActivityAttributionCallbacks,
                                      Unretained(callbacks), activity, address));
   }
 
-  void Cleanup(void) override {
-    do_in_main_thread(FROM_HERE, Bind(&ActivityAttribution::CleanUp));
-  }
+  void Cleanup(void) override {}
 
  private:
   ActivityAttributionCallbacks* callbacks;
