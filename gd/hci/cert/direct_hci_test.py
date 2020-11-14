e2d01bf34a9770c0e5906155dff0dbec31257fce
openvela-robot <openvela-robot@xiaomi.com>
2020-11-14 12:48:57 +0800

    Add HAL reset helper
    Bug: 171749953
    Tag: #gd-refactor
    Test: gd/cert/run --host
    Change-Id: Ia2df0a7e05e8d8fccc8af5005c3fb2e4a5d6a6fe

--- gd/hci/cert/direct_hci_test.py
+++ gd/hci/cert/direct_hci_test.py
@@ -215,52 +215,10 @@ class DirectHciTest(GdBaseTestClass):
                                               OwnAddressType.RANDOM_DEVICE_ADDRESS, AddressType.RANDOM_DEVICE_ADDRESS,
                                               '0D:05:04:03:02:01', 1, [phy_scan_params]))
 
-        # DUT Advertises
-        advertising_handle = 0
-        self.dut_hci.send_command_with_complete(
-            LeSetExtendedAdvertisingLegacyParametersBuilder(
-                advertising_handle,
-                LegacyAdvertisingProperties.ADV_IND,
-                400,
-                450,
-                7,
-                OwnAddressType.RANDOM_DEVICE_ADDRESS,
-                PeerAddressType.PUBLIC_DEVICE_OR_IDENTITY_ADDRESS,
-                '00:00:00:00:00:00',
-                AdvertisingFilterPolicy.ALL_DEVICES,
-                0xF8,
-                1,  #SID
-                Enable.DISABLED  # Scan request notification
-            ))
-
-        self.dut_hci.send_command_with_complete(
-            LeSetExtendedAdvertisingRandomAddressBuilder(advertising_handle, '0D:05:04:03:02:01'))
-
-        gap_name = GapData()
-        gap_name.data_type = GapDataType.COMPLETE_LOCAL_NAME
-        gap_name.data = list(bytes(b'Im_The_DUT'))
-
-        self.dut_hci.send_command_with_complete(
-            LeSetExtendedAdvertisingDataBuilder(advertising_handle, Operation.COMPLETE_ADVERTISEMENT,
-                                                FragmentPreference.CONTROLLER_SHOULD_NOT, [gap_name]))
-
-        gap_short_name = GapData()
-        gap_short_name.data_type = GapDataType.SHORTENED_LOCAL_NAME
-        gap_short_name.data = list(bytes(b'Im_The_D'))
-
-        self.dut_hci.send_command_with_complete(
-            LeSetExtendedAdvertisingScanResponseBuilder(advertising_handle, Operation.COMPLETE_ADVERTISEMENT,
-                                                        FragmentPreference.CONTROLLER_SHOULD_NOT, [gap_short_name]))
-
-        enabled_set = EnabledSet()
-        enabled_set.advertising_handle = advertising_handle
-        enabled_set.duration = 0
-        enabled_set.max_extended_advertising_events = 0
-        self.dut_hci.send_command_with_complete(LeSetExtendedAdvertisingEnableBuilder(Enable.ENABLED, [enabled_set]))
-
-        # Check for success of Enable
-        assertThat(self.dut_hci.get_event_stream()).emits(
-            HciMatchers.CommandComplete(OpCode.LE_SET_EXTENDED_ADVERTISING_ENABLE))
+        advertisement = self.dut_hci.create_advertisement(0, '0D:05:04:03:02:01')
+        advertisement.set_data(b'Im_The_DUT')
+        advertisement.set_scan_response(b'Im_The_D')
+        advertisement.start()
 
         (dut_handle, cert_handle) = self._verify_le_connection_complete()
 
@@ -286,90 +244,33 @@ class DirectHciTest(GdBaseTestClass):
                                               OwnAddressType.RANDOM_DEVICE_ADDRESS, AddressType.RANDOM_DEVICE_ADDRESS,
                                               'BA:D5:A4:A3:A2:A1', 1, [phy_scan_params]))
 
-        # CERT Advertises
-        advertising_handle = 1
-        self.cert_hal.send_hci_command(
-            LeSetExtendedAdvertisingLegacyParametersBuilder(
-                advertising_handle,
-                LegacyAdvertisingProperties.ADV_IND,
-                512,
-                768,
-                7,
-                OwnAddressType.RANDOM_DEVICE_ADDRESS,
-                PeerAddressType.PUBLIC_DEVICE_OR_IDENTITY_ADDRESS,
-                'A6:A5:A4:A3:A2:A1',
-                AdvertisingFilterPolicy.ALL_DEVICES,
-                0x7F,
-                0,  # SID
-                Enable.DISABLED  # Scan request notification
-            ))
-
-        self.cert_hal.send_hci_command(
-            LeSetExtendedAdvertisingRandomAddressBuilder(advertising_handle, '0C:05:04:03:02:01'))
-
-        gap_name = GapData()
-        gap_name.data_type = GapDataType.COMPLETE_LOCAL_NAME
-        gap_name.data = list(bytes(b'Im_A_Cert'))
-
-        self.cert_hal.send_hci_command(
-            LeSetExtendedAdvertisingDataBuilder(advertising_handle, Operation.COMPLETE_ADVERTISEMENT,
-                                                FragmentPreference.CONTROLLER_SHOULD_NOT, [gap_name]))
-        enabled_set = EnabledSet()
-        enabled_set.advertising_handle = 1
-        enabled_set.duration = 0
-        enabled_set.max_extended_advertising_events = 0
-        self.cert_hal.send_hci_command(LeSetExtendedAdvertisingEnableBuilder(Enable.ENABLED, [enabled_set]))
+        advertisement = self.cert_hal.create_advertisement(
+            1,
+            '0C:05:04:03:02:01',
+            min_interval=512,
+            max_interval=768,
+            peer_address='A6:A5:A4:A3:A2:A1',
+            tx_power=0x7f,
+            sid=0)
+        advertisement.set_data(b'Im_A_Cert')
+        advertisement.start()
 
         # LeConnectionComplete
         self._verify_le_connection_complete()
 
-    def _verify_connection_complete(self):
-        cert_connection_complete_capture = HalCaptures.ConnectionCompleteCapture()
-        assertThat(self.cert_hal.get_hci_event_stream()).emits(cert_connection_complete_capture)
-        cert_handle = cert_connection_complete_capture.get().GetConnectionHandle()
-
-        dut_connection_complete_capture = HciCaptures.ConnectionCompleteCapture()
-        assertThat(self.dut_hci.get_event_stream()).emits(dut_connection_complete_capture)
-        dut_handle = dut_connection_complete_capture.get().GetConnectionHandle()
-
-        return (dut_handle, cert_handle)
-
     def test_connection_dut_connects(self):
         self.dut_hci.send_command_with_complete(WritePageTimeoutBuilder(0x4000))
 
-        # CERT Enables scans and gets its address
-        self.cert_hal.send_hci_command(ReadBdAddrBuilder())
+        self.cert_hal.enable_inquiry_and_page_scan()
+        address = self.cert_hal.read_own_address()
 
-        cert_read_bd_addr_capture = HalCaptures.ReadBdAddrCompleteCapture()
-        assertThat(self.cert_hal.get_hci_event_stream()).emits(cert_read_bd_addr_capture)
-        address = cert_read_bd_addr_capture.get().GetBdAddr()
-
-        self.cert_hal.send_hci_command(WriteScanEnableBuilder(ScanEnable.INQUIRY_AND_PAGE_SCAN))
-
-        # DUT Connects
-        self.dut_hci.send_command_with_status(
-            CreateConnectionBuilder(
-                address,
-                0xcc18,  # Packet Type
-                PageScanRepetitionMode.R0,
-                0,
-                ClockOffsetValid.INVALID,
-                CreateConnectionRoleSwitch.ALLOW_ROLE_SWITCH))
-
-        # Cert Accepts
-        connect_request_capture = HalCaptures.ConnectionRequestCapture()
-        assertThat(self.cert_hal.get_hci_event_stream()).emits(connect_request_capture, timeout=timedelta(seconds=20))
-        connection_request = connect_request_capture.get()
-        self.cert_hal.send_hci_command(
-            AcceptConnectionRequestBuilder(connection_request.GetBdAddr(),
-                                           AcceptConnectionRequestRole.REMAIN_PERIPHERAL))
-
-        (dut_handle, cert_handle) = self._verify_connection_complete()
+        self.dut_hci.initiate_connection(address)
+        cert_acl = self.cert_hal.accept_connection()
+        dut_acl = self.dut_hci.complete_connection()
 
         # Send ACL Data
-        self.enqueue_acl_data(dut_handle, PacketBoundaryFlag.FIRST_NON_AUTOMATICALLY_FLUSHABLE,
-                              BroadcastFlag.POINT_TO_POINT, bytes(b'Just SomeAclData'))
-        self.cert_hal.send_acl_first(cert_handle, bytes(b'Just SomeMoreAclData'))
+        dut_acl.send_first(b'Just SomeAclData')
+        cert_acl.send_first(b'Just SomeMoreAclData')
 
         assertThat(self.cert_hal.get_acl_stream()).emits(lambda packet: b'SomeAclData' in packet.payload)
         assertThat(self.dut_hci.get_raw_acl_stream()).emits(lambda packet: b'SomeMoreAclData' in packet.data)
@@ -377,38 +278,16 @@ class DirectHciTest(GdBaseTestClass):
     def test_connection_cert_connects(self):
         self.cert_hal.send_hci_command(WritePageTimeoutBuilder(0x4000))
 
-        # DUT Enables scans and gets its address
-        self.dut_hci.send_command_with_complete(WriteScanEnableBuilder(ScanEnable.INQUIRY_AND_PAGE_SCAN))
-        self.dut_hci.send_command_with_complete(ReadBdAddrBuilder())
+        self.dut_hci.enable_inquiry_and_page_scan()
+        address = self.dut_hci.read_own_address()
 
-        read_bd_addr_capture = HciCaptures.ReadBdAddrCompleteCapture()
-        assertThat(self.dut_hci.get_event_stream()).emits(read_bd_addr_capture)
-        address = read_bd_addr_capture.get().GetBdAddr()
-
-        # Cert Connects
-        self.cert_hal.send_hci_command(
-            CreateConnectionBuilder(
-                address,
-                0xcc18,  # Packet Type
-                PageScanRepetitionMode.R0,
-                0,
-                ClockOffsetValid.INVALID,
-                CreateConnectionRoleSwitch.ALLOW_ROLE_SWITCH))
-
-        # DUT Accepts
-        connection_request_capture = HciCaptures.ConnectionRequestCapture()
-        assertThat(self.dut_hci.get_event_stream()).emits(connection_request_capture, timeout=timedelta(seconds=20))
-        connection_request = connection_request_capture.get()
-        self.dut_hci.send_command_with_status(
-            AcceptConnectionRequestBuilder(connection_request.GetBdAddr(),
-                                           AcceptConnectionRequestRole.REMAIN_PERIPHERAL))
-
-        (dut_handle, cert_handle) = self._verify_connection_complete()
+        self.cert_hal.initiate_connection(address)
+        dut_acl = self.dut_hci.accept_connection()
+        cert_acl = self.cert_hal.complete_connection()
 
         # Send ACL Data
-        self.enqueue_acl_data(dut_handle, PacketBoundaryFlag.FIRST_NON_AUTOMATICALLY_FLUSHABLE,
-                              BroadcastFlag.POINT_TO_POINT, bytes(b'This is just SomeAclData'))
-        self.cert_hal.send_acl_first(cert_handle, bytes(b'This is just SomeMoreAclData'))
+        dut_acl.send_first(b'This is just SomeAclData')
+        cert_acl.send_first(b'This is just SomeMoreAclData')
 
         assertThat(self.cert_hal.get_acl_stream()).emits(lambda packet: b'SomeAclData' in packet.payload)
         assertThat(self.dut_hci.get_raw_acl_stream()).emits(lambda packet: b'SomeMoreAclData' in packet.data)
