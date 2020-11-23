015f1faded04de29fd7df3bb69cef1bb97ac5986
openvela-robot <openvela-robot@xiaomi.com>
2020-11-24 03:44:12 +0800

    L2cap shim: Introduce Classic Dynamic Channel Helper am: d67452503c
    Original change: https://android-review.googlesource.com/c/platform/system/bt/+/1506628
    
    Change-Id: Iebe4631b71642369a6aab9e2a74ed3b276b5d4b3

--- gd/hci/le_advertising_manager.h
+++ gd/hci/le_advertising_manager.h
@@ -97,7 +97,6 @@ class LeAdvertisingManager : public bluetooth::Module {
   static constexpr AdvertiserId kInvalidId = 0xFF;
   static constexpr uint8_t kInvalidHandle = 0xFF;
   static constexpr uint8_t kAdvertisingSetIdMask = 0x0F;
-  static constexpr uint16_t kLeMaximumFragmentLength = 251;
   static constexpr FragmentPreference kFragment_preference = FragmentPreference::CONTROLLER_SHOULD_NOT;
   LeAdvertisingManager();
 
