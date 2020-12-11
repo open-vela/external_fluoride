e5d885ad272ddf6c44ea4dfc421046713cff5551
liujinye <liujinye@xiaomi.com>
2020-12-11 20:50:56 +0800

    Merge "Cert: Run script additions"

--- gd/security/internal/security_manager_impl.h
+++ gd/security/internal/security_manager_impl.h
@@ -188,7 +188,6 @@ class SecurityManagerImpl : public channel::ISecurityManagerChannelListener, pub
   void SetDisconnectCallback(FacadeDisconnectCallback callback);
   void SetIoCapability(hci::IoCapability io_capability);
   void SetAuthenticationRequirements(hci::AuthenticationRequirements authentication_requirements);
-  void GetOutOfBandData(channel::SecurityCommandStatusCallback callback);
   void SetLeIoCapability(security::IoCapability io_capability);
   void SetLeAuthRequirements(uint8_t auth_req);
   void SetLeMaximumEncryptionKeySize(uint8_t maximum_encryption_key_size);
