66c1753993a91cd9ac2299d28c8e6dc2259008d7
openvela-robot <openvela-robot@xiaomi.com>
2020-12-12 22:45:46 +0800

    rusty-gd: implement le meta events
    and oh hey, pattern matching is pretty freaking great
    
    Bug: 171749953
    Tag: #gd-refactor
    Test: gd/cert/run --rhost SimpleHalTest
    Change-Id: I4ce65e275ffe0e16161260e854d02987d166e4ca

--- gd/rust/hci/src/lib.rs
+++ gd/rust/hci/src/lib.rs
@@ -7,8 +7,10 @@ pub mod error;
 pub mod facade;
 
 use bt_hal::HalExports;
-use bt_packets::hci;
-use bt_packets::hci::EventChild::{CommandStatus,CommandComplete};
+use bt_packets::hci::EventChild::{CommandComplete, CommandStatus, LeMetaEvent};
+use bt_packets::hci::{
+    AclPacket, CommandPacket, EventCode, EventPacket, LeMetaEventPacket, OpCode, SubeventCode,
+};
 use error::Result;
 use gddi::{module, provides, Stoppable};
 use std::collections::HashMap;
@@ -32,9 +34,11 @@ module! {
 async fn provide_hci(hal_exports: HalExports, rt: Arc<Runtime>) -> HciExports {
     let (cmd_tx, cmd_rx) = channel::<Command>(10);
     let evt_handlers = Arc::new(Mutex::new(HashMap::new()));
+    let le_evt_handlers = Arc::new(Mutex::new(HashMap::new()));
 
     rt.spawn(dispatch(
         evt_handlers.clone(),
+        le_evt_handlers.clone(),
         hal_exports.evt_rx,
         hal_exports.cmd_tx,
         cmd_rx,
@@ -43,6 +47,7 @@ async fn provide_hci(hal_exports: HalExports, rt: Arc<Runtime>) -> HciExports {
     HciExports {
         cmd_tx,
         evt_handlers,
+        le_evt_handlers,
         acl_tx: hal_exports.acl_tx,
         acl_rx: hal_exports.acl_rx,
     }
@@ -53,30 +58,31 @@ async fn provide_hci(hal_exports: HalExports, rt: Arc<Runtime>) -> HciExports {
 /// to the command is received
 #[derive(Debug)]
 struct Command {
-    cmd: hci::CommandPacket,
-    fut: oneshot::Sender<hci::EventPacket>,
+    cmd: CommandPacket,
+    fut: oneshot::Sender<EventPacket>,
 }
 
 #[derive(Debug)]
 struct PendingCommand {
-    opcode: hci::OpCode,
-    fut: oneshot::Sender<hci::EventPacket>,
+    opcode: OpCode,
+    fut: oneshot::Sender<EventPacket>,
 }
 
 /// HCI interface
 #[derive(Clone, Stoppable)]
 pub struct HciExports {
     cmd_tx: Sender<Command>,
-    evt_handlers: Arc<Mutex<HashMap<hci::EventCode, Sender<hci::EventPacket>>>>,
+    evt_handlers: Arc<Mutex<HashMap<EventCode, Sender<EventPacket>>>>,
+    le_evt_handlers: Arc<Mutex<HashMap<SubeventCode, Sender<LeMetaEventPacket>>>>,
     /// Transmit end of a channel used to send ACL data
-    pub acl_tx: Sender<hci::AclPacket>,
+    pub acl_tx: Sender<AclPacket>,
     /// Receive end of a channel used to receive ACL data
-    pub acl_rx: Arc<Mutex<Receiver<hci::AclPacket>>>,
+    pub acl_rx: Arc<Mutex<Receiver<AclPacket>>>,
 }
 
 impl HciExports {
-    async fn send(&mut self, cmd: hci::CommandPacket) -> Result<hci::EventPacket> {
-        let (tx, rx) = oneshot::channel::<hci::EventPacket>();
+    async fn send(&mut self, cmd: CommandPacket) -> Result<EventPacket> {
+        let (tx, rx) = oneshot::channel::<EventPacket>();
         self.cmd_tx.send(Command { cmd, fut: tx }).await?;
         let event = rx.await?;
         Ok(event)
@@ -84,54 +90,81 @@ impl HciExports {
 
     /// Enqueue an HCI command expecting a command complete
     /// response from the controller
-    pub async fn enqueue_command_with_complete(&mut self, cmd: hci::CommandPacket) -> hci::EventPacket {
+    pub async fn enqueue_command_with_complete(&mut self, cmd: CommandPacket) -> EventPacket {
         self.send(cmd).await.unwrap()
     }
 
     /// Enqueue an HCI command expecting a status response
     /// from the controller
-    pub async fn enqueue_command_with_status(&mut self, cmd: hci::CommandPacket) -> hci::EventPacket {
+    pub async fn enqueue_command_with_status(&mut self, cmd: CommandPacket) -> EventPacket {
         self.send(cmd).await.unwrap()
     }
 
     /// Indicate interest in specific HCI events
-    pub async fn register_event_handler(&mut self, evt_code: hci::EventCode, sender: Sender<hci::EventPacket>) {
+    pub async fn register_event_handler(
+        &mut self,
+        evt_code: EventCode,
+        sender: Sender<EventPacket>,
+    ) {
         self.evt_handlers.lock().await.insert(evt_code, sender);
+    }
+
+    /// Indicate interest in specific LE events
+    pub async fn register_le_event_handler(
+        &mut self,
+        evt_code: SubeventCode,
+        sender: Sender<LeMetaEventPacket>,
+    ) {
+        self.le_evt_handlers.lock().await.insert(evt_code, sender);
     }
 }
 
 async fn dispatch(
-    evt_handlers: Arc<Mutex<HashMap<hci::EventCode, Sender<hci::EventPacket>>>>,
-    evt_rx: Arc<Mutex<Receiver<hci::EventPacket>>>,
-    cmd_tx: Sender<hci::CommandPacket>,
+    evt_handlers: Arc<Mutex<HashMap<EventCode, Sender<EventPacket>>>>,
+    le_evt_handlers: Arc<Mutex<HashMap<SubeventCode, Sender<LeMetaEventPacket>>>>,
+    evt_rx: Arc<Mutex<Receiver<EventPacket>>>,
+    cmd_tx: Sender<CommandPacket>,
     mut cmd_rx: Receiver<Command>,
 ) {
-    let mut pending_cmds: Vec<PendingCommand> = Vec::new();
+    let mut pending_cmd: Option<PendingCommand> = None;
     loop {
         select! {
             Some(evt) = consume(&evt_rx) => {
                 match evt.specialize() {
                     CommandStatus(evt) => {
-                        let opcode = *evt.get_command_op_code();
-                        if let Some(pending_cmd) = remove_first(&mut pending_cmds, |entry| entry.opcode == opcode) {
-                            pending_cmd.fut.send(evt.into()).unwrap();
+                        let this_opcode = *evt.get_command_op_code();
+                        match pending_cmd.take() {
+                            Some(PendingCommand{opcode, fut}) if opcode == this_opcode  => fut.send(evt.into()).unwrap(),
+                            Some(PendingCommand{opcode, ..}) => panic!("Waiting for {:?}, got {:?}", opcode, this_opcode),
+                            None => panic!("Unexpected status event with opcode {:?}", this_opcode),
                         }
                     },
                     CommandComplete(evt) => {
-                        let opcode = *evt.get_command_op_code();
-                        if let Some(pending_cmd) = remove_first(&mut pending_cmds, |entry| entry.opcode == opcode) {
-                            pending_cmd.fut.send(evt.into()).unwrap();
+                        let this_opcode = *evt.get_command_op_code();
+                        match pending_cmd.take() {
+                            Some(PendingCommand{opcode, fut}) if opcode == this_opcode  => fut.send(evt.into()).unwrap(),
+                            Some(PendingCommand{opcode, ..}) => panic!("Waiting for {:?}, got {:?}", opcode, this_opcode),
+                            None => panic!("Unexpected complete event with opcode {:?}", this_opcode),
+                        }
+                    },
+                    LeMetaEvent(evt) => {
+                        let code = evt.get_subevent_code();
+                        match le_evt_handlers.lock().await.get(code) {
+                            Some(sender) => sender.send(evt).await.unwrap(),
+                            None => panic!("Unhandled le subevent {:?}", code),
                         }
                     },
                     _ => {
-                        if let Some(sender) = evt_handlers.lock().await.get(evt.get_event_code()) {
-                            sender.send(evt).await.unwrap();
+                        let code = evt.get_event_code();
+                        match evt_handlers.lock().await.get(code) {
+                            Some(sender) => sender.send(evt).await.unwrap(),
+                            None => panic!("Unhandled le subevent {:?}", code),
                         }
                     },
                 }
             },
-            Some(cmd) = cmd_rx.recv() => {
-                pending_cmds.push(PendingCommand {
+            Some(cmd) = cmd_rx.recv(), if pending_cmd.is_none() => {
+                pending_cmd = Some(PendingCommand {
                     opcode: *cmd.cmd.get_op_code(),
                     fut: cmd.fut,
                 });
@@ -142,17 +175,6 @@ async fn dispatch(
     }
 }
 
-async fn consume(evt_rx: &Arc<Mutex<Receiver<hci::EventPacket>>>) -> Option<hci::EventPacket> {
+async fn consume(evt_rx: &Arc<Mutex<Receiver<EventPacket>>>) -> Option<EventPacket> {
     evt_rx.lock().await.recv().await
-}
-
-fn remove_first<T, P>(vec: &mut Vec<T>, predicate: P) -> Option<T>
-where
-    P: FnMut(&T) -> bool,
-{
-    if let Some(i) = vec.iter().position(predicate) {
-        Some(vec.remove(i))
-    } else {
-        None
-    }
 }
