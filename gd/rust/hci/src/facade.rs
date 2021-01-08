96129da5514bc73f15e3830ae5c2d49ee042a4e4
liujinye <liujinye@xiaomi.com>
2021-01-08 21:42:32 +0800

    Merge changes from topic "dip1.3"
    * changes:
      Use Bluetooth UUID definitions of header file
      DIP: Implement API to get device identification information

--- gd/rust/hci/src/facade.rs
+++ gd/rust/hci/src/facade.rs
@@ -6,17 +6,15 @@ use bt_facade_proto::common::Data;
 use bt_facade_proto::empty::Empty;
 use bt_facade_proto::hci_facade::EventRequest;
 use bt_facade_proto::hci_facade_grpc::{create_hci_layer_facade, HciLayerFacade};
-use bt_packets::hci::{
-    AclPacket, CommandPacket, EventCode, EventPacket, LeMetaEventPacket, SubeventCode,
-};
+use bt_packets::hci;
 use futures::sink::SinkExt;
 use gddi::{module, provides, Stoppable};
 use grpcio::*;
-use num_traits::FromPrimitive;
 use std::sync::Arc;
 use tokio::runtime::Runtime;
 use tokio::sync::mpsc::{channel, Receiver, Sender};
 use tokio::sync::Mutex;
+use num_traits::FromPrimitive;
 
 module! {
     facade_module,
@@ -27,15 +25,12 @@ module! {
 
 #[provides]
 async fn provide_facade(hci_exports: HciExports, rt: Arc<Runtime>) -> HciLayerFacadeService {
-    let (from_hci_evt_tx, to_grpc_evt_rx) = channel::<EventPacket>(10);
-    let (from_hci_le_evt_tx, to_grpc_le_evt_rx) = channel::<LeMetaEventPacket>(10);
+    let (from_hci_evt_tx, to_grpc_evt_rx) = channel::<hci::EventPacket>(10);
     HciLayerFacadeService {
         hci_exports,
         rt,
         from_hci_evt_tx,
         to_grpc_evt_rx: Arc::new(Mutex::new(to_grpc_evt_rx)),
-        from_hci_le_evt_tx,
-        to_grpc_le_evt_rx: Arc::new(Mutex::new(to_grpc_le_evt_rx)),
     }
 }
 
@@ -44,10 +39,8 @@ async fn provide_facade(hci_exports: HciExports, rt: Arc<Runtime>) -> HciLayerFa
 pub struct HciLayerFacadeService {
     hci_exports: HciExports,
     rt: Arc<Runtime>,
-    from_hci_evt_tx: Sender<EventPacket>,
-    to_grpc_evt_rx: Arc<Mutex<Receiver<EventPacket>>>,
-    from_hci_le_evt_tx: Sender<LeMetaEventPacket>,
-    to_grpc_le_evt_rx: Arc<Mutex<Receiver<LeMetaEventPacket>>>,
+    from_hci_evt_tx: Sender<hci::EventPacket>,
+    to_grpc_evt_rx: Arc<Mutex<Receiver<hci::EventPacket>>>,
 }
 
 impl GrpcFacade for HciLayerFacadeService {
@@ -63,11 +56,10 @@ impl HciLayerFacade for HciLayerFacadeService {
         mut data: Data,
         sink: UnarySink<Empty>,
     ) {
-        self.rt
-            .block_on(
-                self.hci_exports.send_raw(CommandPacket::parse(&data.take_payload()).unwrap()),
-            )
-            .unwrap();
+        self.rt.block_on(
+            self.hci_exports
+                .send_raw(hci::CommandPacket::parse(&data.take_payload()).unwrap()),
+        ).unwrap();
         sink.success(Empty::default());
     }
 
@@ -77,39 +69,34 @@ impl HciLayerFacade for HciLayerFacadeService {
         mut data: Data,
         sink: UnarySink<Empty>,
     ) {
-        self.rt
-            .block_on(
-                self.hci_exports.send_raw(CommandPacket::parse(&data.take_payload()).unwrap()),
-            )
-            .unwrap();
+        self.rt.block_on(
+            self.hci_exports
+                .send_raw(hci::CommandPacket::parse(&data.take_payload()).unwrap()),
+        ).unwrap();
         sink.success(Empty::default());
     }
 
-    fn request_event(&mut self, _ctx: RpcContext<'_>, req: EventRequest, sink: UnarySink<Empty>) {
-        self.rt.block_on(self.hci_exports.register_event_handler(
-            EventCode::from_u32(req.get_code()).unwrap(),
-            self.from_hci_evt_tx.clone(),
-        ));
+    fn request_event(&mut self, _ctx: RpcContext<'_>, code: EventRequest, sink: UnarySink<Empty>) {
+        self.rt.block_on(
+            self.hci_exports
+                .register_event_handler(hci::EventCode::from_u32(code.get_code()).unwrap(), self.from_hci_evt_tx.clone()),
+        );
         sink.success(Empty::default());
     }
 
     fn request_le_subevent(
         &mut self,
         _ctx: RpcContext<'_>,
-        req: EventRequest,
-        sink: UnarySink<Empty>,
+        _code: EventRequest,
+        _sink: UnarySink<Empty>,
     ) {
-        self.rt.block_on(self.hci_exports.register_le_event_handler(
-            SubeventCode::from_u32(req.get_code()).unwrap(),
-            self.from_hci_le_evt_tx.clone(),
-        ));
-        sink.success(Empty::default());
+        unimplemented!()
     }
 
     fn send_acl(&mut self, _ctx: RpcContext<'_>, mut packet: Data, sink: UnarySink<Empty>) {
         let acl_tx = self.hci_exports.acl_tx.clone();
         self.rt.block_on(async move {
-            acl_tx.send(AclPacket::parse(&packet.take_payload()).unwrap()).await.unwrap();
+            acl_tx.send(hci::AclPacket::parse(&packet.take_payload()).unwrap()).await.unwrap();
         });
         sink.success(Empty::default());
     }
@@ -135,17 +122,9 @@ impl HciLayerFacade for HciLayerFacadeService {
         &mut self,
         _ctx: RpcContext<'_>,
         _req: Empty,
-        mut resp: ServerStreamingSink<Data>,
+        mut _resp: ServerStreamingSink<Data>,
     ) {
-        let evt_rx = self.to_grpc_le_evt_rx.clone();
-
-        self.rt.spawn(async move {
-            while let Some(event) = evt_rx.lock().await.recv().await {
-                let mut evt = Data::default();
-                evt.set_payload(event.to_vec());
-                resp.send((evt, WriteFlags::default())).await.unwrap();
-            }
-        });
+        unimplemented!()
     }
 
     fn stream_acl(
