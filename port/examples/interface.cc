193d9487ee00bb0180cb5ca0626057e77a475241
chao.an <anchao@xiaomi.com>
2021-03-15 22:26:10 +0800

    fluoride/example: remove unused connect/disconnect
    Change-Id: I27b297284c3643fbe946b1d40889572688a9d84a

--- port/examples/interface.cc
+++ port/examples/interface.cc
@@ -138,16 +138,9 @@ static void bond_state_changed(bt_status_t status, RawAddress *remote_bd_addr, b
 {
   struct fluoride_s *flrd = fluoride_interface_get();
   bt_property_t *property;
-  RawAddress bd_addr;
 
   LOG_SAMPLES("%s: state: %d\n", __func__, state);
   if (state == BT_BOND_STATE_BONDED) {
-    bd_addr.FromOctets(reinterpret_cast<const uint8_t*>(remote_bd_addr));
-    if (flrd->sink)
-      flrd->sink->connect(bd_addr);
-    if (flrd->avrcs)
-      flrd->avrcs->ConnectDevice(bd_addr);
-
     property = property_new_scan_mode(BT_SCAN_MODE_NONE);
     flrd->interface->set_adapter_property(property);
     property_free(property);
